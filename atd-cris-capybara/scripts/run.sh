4f70599037c44dfbc059741539aab06a49f33426
Sergio Garcia <sergiogcx@gmail.com>
2019-07-27 13:46:18 -0500

    Updated run scripts

--- atd-cris-capybara/scripts/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm \
---env ATD_CRIS_USERNAME=$ATD_CRIS_USERNAME \
---env ATD_CRIS_PASSWORD=$ATD_CRIS_PASSWORD \
---env ATD_CRIS_REPORT_DATE_START="01/01/2019" \
---env ATD_CRIS_REPORT_DATE_END=`date +%m/%d/%Y` \
-atddocker/atd-cris-capybara \
-sh -c "xvfb-run --server-args=\"-screen 0 1024x768x24\" ruby main.rb"
