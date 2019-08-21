655a778c573a125e78160cc1a162594a7d97e33f
Sergio Garcia <sergiogcx@gmail.com>
2019-08-20 23:38:18 -0500

    Prettyfied Crash change log

--- atd-vze/src/views/Crashes/CrashChangeLog.js
+++ atd-vze/src/views/Crashes/CrashChangeLog.js
@@ -1,216 +1,262 @@
 import React, { Component } from "react";
 
 import {
-    Badge,
-    Button,
-    Card,
-    CardBody,
-    CardHeader,
-    Col,
-    Table,
-    Row,
-    Modal, ModalHeader, ModalBody, ModalFooter
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Table,
+  Row,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
 } from "reactstrap";
 
-
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class CrashChangeLog extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.closeModal = this.closeModal.bind(this);
-        this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.showModal = this.showModal.bind(this);
 
-        this.compare = this.compare.bind(this);
+    this.compare = this.compare.bind(this);
 
-        this.state = {
-            modal: false,
-            modalBody: null,
-            dataFrom: null,
-            dataTo: null,
-            data: this.props.data
-        };
-    }
+    this.state = {
+      modal: false,
+      modalBody: null,
+      dataFrom: null,
+      dataTo: null,
+      data: this.props.data
+    };
+  }
 
-    /**
-     * Converts a PostgreSQL Timestamp string into a human readable date-time
-     * @param {String} the timestamp as provided by postgres
-     */
-    timeConverter(UNIX_timestamp){
-        let a = new Date(`${UNIX_timestamp}`.replace(' ', 'T'));
-        let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        return a.getDate() + ' ' + months[a.getMonth()] + ' ' + a.getFullYear() + ' ' + a.getHours() + ':' + a.getMinutes();
-    }
+  /**
+   * Converts a PostgreSQL Timestamp string into a human readable date-time
+   * @param {String} the timestamp as provided by postgres
+   */
+  timeConverter(UNIX_timestamp) {
+    let a = new Date(`${UNIX_timestamp}`.replace(" ", "T"));
+    let months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+    return (
+      a.getDate() +
+      " " +
+      months[a.getMonth()] +
+      " " +
+      a.getFullYear() +
+      " " +
+      a.getHours() +
+      ":" +
+      a.getMinutes()
+    );
+  }
 
-    /**
-     * Shows the modal box by changing the state, does not alter the body.
-     */
-    showModal() {
-        this.setState({
-            modal: true
-        });
-    }
+  /**
+   * Shows the modal box by changing the state, does not alter the body.
+   */
+  showModal() {
+    this.setState({
+      modal: true
+    });
+  }
 
-    /**
-     * Closes the modal box by changing the state and emptying the modal body.
-     */
-    closeModal() {
-        this.setState({
-            modal: false,
-            modalBody: null
-        });
-    }
+  /**
+   * Closes the modal box by changing the state and emptying the modal body.
+   */
+  closeModal() {
+    this.setState({
+      modal: false,
+      modalBody: null
+    });
+  }
 
-    /**
-     * Compares the archived json object against the current data object as populated via props.
-     * @param {Object} record's json
-     */
-    compare(record) {
-        // Holds only the keys
-        let diff = [];
+  /**
+   * Compares the archived json object against the current data object as populated via props.
+   * @param {Object} record's json
+   */
+  compare(record) {
+    // Holds only the keys
+    let diff = [];
 
-        // Holds the html for our modal box
-        let modalBody = null;
+    // Holds the html for our modal box
+    let modalBody = null;
 
-        // Iterate through our record's key & values
-        for (let [key, value] of Object.entries(this.props.data.atd_txdot_crashes[0])) {
-            // Let's get rid of typename
-            if(key === "__typename") continue;
+    // Iterate through our record's key & values
+    for (let [key, value] of Object.entries(
+      this.props.data.atd_txdot_crashes[0]
+    )) {
+      // Let's get rid of typename
+      if (key === "__typename") continue;
 
-            try {
-                // Gather the archived value for the current field
-                let archivedRecordValue = record.record_json[key];
+      try {
+        // Gather the archived value for the current field
+        let archivedRecordValue = record.record_json[key];
 
-                // If the value is different from the current value, then put it in the diff array.
-                if(archivedRecordValue !== value) {
-                    diff.push({
-                        'original_record_key': key,
-                        'original_record_value': value,
-                        'archived_record_value': archivedRecordValue
-                    });
-                }
-            } catch (error) {
-                console.log(error);
-            }
+        // If the value is different from the current value, then put it in the diff array.
+        if (archivedRecordValue !== value) {
+          diff.push({
+            original_record_key: key,
+            original_record_value: value,
+            archived_record_value: archivedRecordValue
+          });
         }
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-        // For each entry created in the diff array, generate an HTML table row.
-        let modalItems = diff.map(item => (
-            <tr key={record.id}>
-                <td>
-                    {item.original_record_key}
-                </td>
-                <td>
-                    <Badge color="primary">{String(item.original_record_value)}</Badge>
-                </td>
-                <td>
-                    <Badge color="danger">{String(item.archived_record_value)}</Badge>
-                </td>
+    // For each entry created in the diff array, generate an HTML table row.
+    let modalItems = diff.map(item => (
+      <tr key={record.id}>
+        <td>{item.original_record_key}</td>
+        <td>
+          <Badge color="primary">{String(item.original_record_value)}</Badge>
+        </td>
+        <td>
+          <Badge color="danger">{String(item.archived_record_value)}</Badge>
+        </td>
+      </tr>
+    ));
+
+    // Generate the body of the modal box
+    modalBody = (
+      <section>
+        <h6>
+          Crash ID:{" "}
+          <Badge color={"secondary"} className={"float-right"}>
+            {record.record_crash_id}
+          </Badge>
+        </h6>
+        <h6>
+          Archive Date:{" "}
+          <Badge color={"secondary"} className={"float-right"}>
+            {record.update_timestamp}
+          </Badge>
+        </h6>
+        <h6>
+          Created by:{" "}
+          <Badge color={"secondary"} className={"float-right"}>
+            {record.record_json.updated_by}
+          </Badge>
+        </h6>
+        &nbsp;
+        <Table responsive>
+          <thead>
+            <tr>
+              <td>Field</td>
+              <td>Current Value</td>
+              <td>Archived Value</td>
+              <td></td>
             </tr>
-        ));
+          </thead>
+          <tbody>{modalItems}</tbody>
+        </Table>
+      </section>
+    );
 
-        // Generate the body of the modal box
-        modalBody = <section>
-            <h6>Crash ID: <Badge color={"secondary"} className={"float-right"}>{record.record_crash_id}</Badge></h6>
-            <h6>Archive Date: <Badge color={"secondary"} className={"float-right"}>{record.update_timestamp}</Badge></h6>
-            <h6>Created by: <Badge color={"secondary"} className={"float-right"}>{record.record_json.updated_by}</Badge></h6>
-            &nbsp;
-            <Table responsive>
-                <thead>
-                <tr>
-                    <td>Field</td>
-                    <td>Current Value</td>
-                    <td>Archived Value</td>
-                    <td></td>
+    // Set the state of the modal box to contain the body
+    this.setState({ modalBody: modalBody });
+    // Show the modal
+    this.showModal();
+  }
+
+  render() {
+    let modal = null,
+      content = null;
+    // If there are no records, let's not render anything...
+    if (this.props.data.atd_txdot_change_log.length === 0) {
+      modal = null;
+      content = <h4>No changes found for this record.</h4>;
+    } else {
+      modal = (
+        <Modal isOpen={this.state.modal} className={this.props.className}>
+          <ModalHeader>Record Differences</ModalHeader>
+          <ModalBody>{this.state.modalBody}</ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={this.closeModal}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      );
+      content = (
+        <>
+          <h4>Record History</h4>
+
+          <Table responsive>
+            <thead>
+              <tr>
+                <td>ChangeLog ID</td>
+                <td>Date Archived</td>
+                <td>Updated by</td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.data.atd_txdot_change_log.map(record => (
+                <tr key={record.id}>
+                  <td>
+                    <Link to={`crashes/${record.id}`}>{record.id}</Link>
+                  </td>
+                  <td>
+                    <Badge color="warning">
+                      {this.timeConverter(record.update_timestamp)}
+                    </Badge>
+                  </td>
+                  <td>
+                    <Badge color="danger">{String(record.updated_by)} </Badge>
+                  </td>
+                  <td>
+                    <Button
+                      color="primary"
+                      size="sm"
+                      onClick={() => this.compare(record)}
+                    >
+                      Compare
+                    </Button>
+                  </td>
                 </tr>
-                </thead>
-                <tbody>
-                    {modalItems}
-                </tbody>
-            </Table>
-        </section>;
-
-        // Set the state of the modal box to contain the body
-        this.setState({ modalBody: modalBody });
-        // Show the modal
-        this.showModal();
+              ))}
+            </tbody>
+          </Table>
+        </>
+      );
     }
 
-    render() {
-        let modal = null, content = null;
-        // If there are no records, let's not render anything...
-        if(this.props.data.atd_txdot_change_log.length == 0) {
-            modal = null;
-            content = <h4>No changes found for this record.</h4>;
-        } else {
-            modal = <Modal isOpen={this.state.modal} className={this.props.className}>
-                <ModalHeader>Record Differences</ModalHeader>
-                <ModalBody>
-                    {this.state.modalBody}
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="secondary" onClick={this.closeModal}>Close</Button>
-                </ModalFooter>
-            </Modal>;
-            content = <>
-                <h4>Record History</h4>
-
-                <Table responsive>
-                    <thead>
-                    <tr>
-                        <td>ChangeLog ID</td>
-                        <td>Date Archived</td>
-                        <td>Updated by</td>
-                        <td></td>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {this.props.data.atd_txdot_change_log.map(record => (
-                        <tr key={record.id}>
-                            <td>
-                                <Link to={`crashes/${record.id}`}>
-                                    {record.id}
-                                </Link>
-                            </td>
-                            <td>
-                                <Badge color="warning">{this.timeConverter(record.update_timestamp)}</Badge>
-                            </td>
-                            <td>
-                                <Badge color="danger">{String(record.updated_by)} </Badge>
-                            </td>
-                            <td>
-                                <Button color="primary" size="sm"
-                                        onClick={() => this.compare(record)}>Compare</Button>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
-                </Table>
-            </>;
-        }
-
-        // Render Box
-        return (
-            <div className="animated fadeIn">
-                {modal}
-                <Row>
-                    <Col>
-                        <Card>
-                            <CardHeader>
-                                <i className="fa fa-align-justify"/> Change Log
-                            </CardHeader>
-                            <CardBody>
-                                {content}
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
-        );
-
-    }
+    // Render Box
+    return (
+      <div className="animated fadeIn">
+        {modal}
+        <Row>
+          <Col>
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify" /> Change Log
+              </CardHeader>
+              <CardBody>{content}</CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
-
 
 export default CrashChangeLog;
