79b4c1dd0a3495688fffea4e4cc1968bc26ef4e9
Sergio Garcia <sergiogcx@gmail.com>
2019-08-21 18:46:55 -0500

    Prettified Crashes QA data

--- atd-vze/src/views/Crashes/CrashesQA.js
+++ atd-vze/src/views/Crashes/CrashesQA.js
@@ -1,8 +1,17 @@
-import React, {Component} from "react";
-import { Button, Badge, Card, CardBody, CardHeader, Col, Row, Table } from "reactstrap";
+import React, { Component } from "react";
+import {
+  Button,
+  Badge,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Row,
+  Table
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
-import CrashesQAData from  "./CrashesQAData"
+import CrashesQAData from "./CrashesQAData";
 
 class CrashesQA extends Component {
   constructor(props) {
@@ -11,45 +20,43 @@ class CrashesQA extends Component {
       page: 1,
       limit: 25,
       offset: 0
-    }
+    };
   }
 
   moveNext = () => {
-    const page = this.state.page;
-    const oldOffset = this.state.offset;
-    const newOffset = ((page + 1) * this.state.limit) - this.state.limit;
-    this.setState({
-      page: page + 1,
-      offset: newOffset
-    });
-    console.log("oldOffset: " + oldOffset);
-    console.log("newOffset: " + newOffset);
-  }
+    this.setPage(Number(this.state.page) + 1);
+  };
 
   moveBack = () => {
-    // Only if we have space to move back...
-    if(this.state.page == 1) {
-      this.setState({
-        page: 1,
-        offset: 0
-      });
-      return;
-    }
+    this.setPage(Number(this.state.page) - 1);
+  };
 
+  setPage = pagenum => {
     // If we do, then go ahead
     const oldOffset = this.state.offset;
-    const newOffset = (this.state.page -1) * this.state.limit;
+    const newOffset = pagenum * this.state.limit - this.state.limit;
+    console.log("PageNum: " + pagenum);
     this.setState({
-      page: (this.state.page - 1),
+      page: pagenum,
       offset: newOffset
     });
+
     console.log("oldOffset: " + oldOffset);
     console.log("newOffset: " + newOffset);
-  }
+  };
+
+  changePage = event => {
+    this.setPage(event.target.value);
+  };
 
   render() {
     return (
-        <CrashesQAData state={this.state} moveNext={this.moveNext} moveBack={this.moveBack} />
+      <CrashesQAData
+        state={this.state}
+        moveNext={this.moveNext}
+        moveBack={this.moveBack}
+        changePage={this.changePage}
+      />
     );
   }
 }
