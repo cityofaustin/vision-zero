5150c389cf2cd876adeaab8f48d2ea0a299f7d1a
Sergio Garcia <sergiogcx@gmail.com>
2019-08-21 16:12:23 -0500

    Added route for QA Page

--- atd-vze/src/routes.js
+++ atd-vze/src/routes.js
@@ -41,6 +41,7 @@ const Users = React.lazy(() => import("./views/Users/Users"));
 const User = React.lazy(() => import("./views/Users/User"));
 const Crashes = React.lazy(() => import("./views/Crashes/Crashes"));
 const Crash = React.lazy(() => import("./views/Crashes/Crash"));
+const CrashesQA = React.lazy(() => import("./views/Crashes/CrashesQA"));
 const Profile = React.lazy(() => import("./views/Profile/Profile"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
@@ -109,6 +110,7 @@ const routes = [
   { path: "/users", exact: true, name: "Users", component: Users },
   { path: "/users/:id", exact: true, name: "User Details", component: User },
   { path: "/crashes", exact: true, name: "Crashes", component: Crashes },
+  { path: "/crashes/qa", exact: true, name: "CrashesQA", component: CrashesQA },
   {
     path: "/crashes/:id",
     exact: true,
