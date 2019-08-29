391f82d206d74207c9be608e2a90b3238fad163a
Sergio Garcia <sergiogcx@gmail.com>
2019-08-29 17:12:27 -0500

    Modified locations query

--- atd-vze/src/queries/Locations.js
+++ atd-vze/src/queries/Locations.js
@@ -3,8 +3,43 @@ import { gql } from "apollo-boost";
 export const GET_LOCATIONS = gql`
     query GetLocations($recordLimit: Int, $recordOffset: Int) {
         atd_txdot_locations(limit: $recordLimit, offset: $recordOffset) {
-            unique_id
-            description
+            unique_id,
+            description,
+            last_update,
+        },
+        atd_txdot_locations_aggregate {
+            aggregate {
+                count
+            }
         }
     }
 `;
+
+
+export const GET_LOCATION = gql`
+    query GetLocation($id: String) {
+        atd_txdot_locations(where: {unique_id: {_eq: $id}}) {
+            unique_id,
+            address,
+            description,
+            geometry,
+            metadata,
+            last_update,
+            is_retired
+        }   
+    }
+
+`;
+
+/*
+{
+  atd_txdot_crash_locations(where: {location_id: {_eq: "5554229"}}) {
+    location_crashes {
+      crash_id,
+      crash_date
+      death_cnt
+      sus_serious_injry_cnt
+    }
+  }
+}
+*/
