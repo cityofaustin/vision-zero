94373ca3e0ceb522c0d0ca21ceb63011ba1265ab
Sergio Garcia <sergiogcx@gmail.com>
2019-08-29 19:35:11 -0500

    Fixed Routes

--- atd-vze/src/routes.js
+++ atd-vze/src/routes.js
@@ -45,6 +45,7 @@ const CrashesQA = React.lazy(() => import("./views/Crashes/CrashesQA"));
 const Profile = React.lazy(() => import("./views/Profile/Profile"));
 const Locations = React.lazy(() => import("./views/Locations/Locations"));
 const Location = React.lazy(() => import("./views/Locations/Location"));
+const LocationsQA = React.lazy(() => import("./views/Locations/LocationsQA"));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -111,10 +112,13 @@ const routes = [
   { path: "/profile", name: "Profile", component: Profile },
   { path: "/users", exact: true, name: "Users", component: Users },
   { path: "/users/:id", exact: true, name: "User Details", component: User },
+
   { path: "/locations", exact: true, name: "Locations", component: Locations },
+  { path: "/locations/qa", exact: true, name: "Locations Q/A", component: LocationsQA },
   { path: "/locations/:id", exact: true, name: "Location Details", component: Location},
+
   { path: "/crashes", exact: true, name: "Crashes", component: Crashes },
-  { path: "/crashes/qa", exact: true, name: "CrashesQA", component: CrashesQA },
+  { path: "/crashes/qa", exact: true, name: "Crashes Q/A", component: CrashesQA },
   {
     path: "/crashes/:id",
     exact: true,
