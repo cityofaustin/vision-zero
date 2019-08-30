13ebbcaf09863ebe0d8c610b5771aeb3e085326f
Sergio Garcia <sergiogcx@gmail.com>
2019-08-29 19:35:50 -0500

    Prettified locations data

--- atd-vze/src/views/Locations/LocationsData.js
+++ atd-vze/src/views/Locations/LocationsData.js
@@ -1,48 +1,32 @@
 import React from "react";
-import {
-  Button,
-  Badge,
-  Card,
-  CardBody,
-  CardHeader,
-  Col,
-  Row,
-  Table,
-} from "reactstrap";
+import { Table } from "reactstrap";
 import { Link } from "react-router-dom";
 
-import { useQuery } from "@apollo/react-hooks";
-import { withApollo } from "react-apollo";
-
-import { GET_CRASHES_QA } from "../../queries/CrashesQA";
-
-const columns = [
-  "Location ID",
-  "Description"
-];
+const columns = ["#", "Location ID", "Description"];
 function LocationsData(props) {
   return (
-      <Table responsive>
-                <thead>
-                  <tr>
-                    {columns.map((col, i) => (
-                      <th key={`th-${i}`}>{col}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {props.data.atd_txdot_locations.map(location => (
-                    <tr key={location.unique_id}>
-                      <td>
-                        <Link to={`/locations/${location.unique_id}`}>
-                          {location.unique_id}
-                        </Link>
-                      </td>
-                      <td>{location.description}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
+    <Table responsive>
+      <thead>
+        <tr>
+          {columns.map((col, i) => (
+            <th key={`th-${i}`}>{col}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.atd_txdot_locations.map((location, i) => (
+          <tr key={location.unique_id}>
+            <td>{props.offset + i + 1}</td>
+            <td>
+              <Link to={`/locations/${location.unique_id}`}>
+                {location.unique_id}
+              </Link>
+            </td>
+            <td>{location.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
