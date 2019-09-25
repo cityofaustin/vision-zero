128ed541995660702c1172ecac3b947c0fe0a36f
Sergio Garcia <sergiogcx@gmail.com>
2019-09-25 12:11:44 -0500

    Removed unused components, prettified.

--- atd-vze/src/Components/GridTableSearch.js
+++ atd-vze/src/Components/GridTableSearch.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { withApollo } from "react-apollo";
 import {
-  Row,
   Col,
   Button,
   Form,
