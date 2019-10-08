81f8539a058101c790d039be88fbdf47481e5894
Sergio Garcia <sergiogcx@gmail.com>
2019-10-08 14:51:49 -0500

    Removed unnecessary files

--- atd-vze/src/views/Locations/LocationsQA.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { withApollo } from "react-apollo";
-
-function LocationsQA(props) {
-  return <div className="animated fadeIn">Not Yet Implemented</div>;
-}
-
-export default withApollo(LocationsQA);
