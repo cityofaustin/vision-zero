9508871a28ffddc1a8f6a41b3ae2d9b301d04be6
Mike Dilley <mike.dilley@gmail.com>
2019-10-09 16:29:32 -0500

    Add toggleModal handler and isModalOpen state to open and close modal, wrap Save button with CSVLink component

--- atd-vze/src/Components/GridExportData.js
+++ atd-vze/src/Components/GridExportData.js
@@ -1,17 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { withApollo } from "react-apollo";
 import { CSVLink } from "react-csv";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 const GridExportData = ({ exportData, query }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => setIsModalOpen(!isModalOpen);
+
   return (
     <>
-      <CSVLink
-        className=""
-        data={exportData[query.table]}
-        filename={query.table + Date.now()}
-      >
-        <i className="fa fa-save fa-2x ml-2 mt-1" />
-      </CSVLink>
+      <i className="fa fa-save fa-2x ml-2 mt-1" onClick={toggleModal} />
+      <Modal isOpen={isModalOpen} toggle={toggleModal} className={"modal-sm "}>
+        <ModalHeader toggle={toggleModal}>Export to .csv</ModalHeader>
+        <ModalBody>Put save options here</ModalBody>
+        <ModalFooter>
+          <CSVLink
+            className=""
+            data={exportData[query.table]}
+            filename={query.table + Date.now()}
+          >
+            <Button color="primary" onClick={toggleModal}>
+              Save
+            </Button>
+          </CSVLink>{" "}
+          <Button color="secondary" onClick={toggleModal}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
     </>
   );
 };
