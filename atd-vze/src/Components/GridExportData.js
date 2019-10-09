cad34041312bcebd9cc327113a9baf8ade62121b
Mike Dilley <mike.dilley@gmail.com>
2019-10-09 16:37:44 -0500

    Add styling to GridExportData component disk icon

--- atd-vze/src/Components/GridExportData.js
+++ atd-vze/src/Components/GridExportData.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { withApollo } from "react-apollo";
 import { CSVLink } from "react-csv";
+import styled from "styled-components";
+import { colors } from "../styles/colors";
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+
+const StyledSaveLink = styled.i`
+  color: ${colors.info};
+  cursor: pointer;
+  :hover {
+    color: ${colors.primary};
+  }
+`;
 
 const GridExportData = ({ exportData, query }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,7 +20,10 @@ const GridExportData = ({ exportData, query }) => {
 
   return (
     <>
-      <i className="fa fa-save fa-2x ml-2 mt-1" onClick={toggleModal} />
+      <StyledSaveLink>
+        <i className="fa fa-save fa-2x ml-2 mt-1" onClick={toggleModal} />
+      </StyledSaveLink>
+
       <Modal isOpen={isModalOpen} toggle={toggleModal} className={"modal-sm "}>
         <ModalHeader toggle={toggleModal}>Export to .csv</ModalHeader>
         <ModalBody>Put save options here</ModalBody>
