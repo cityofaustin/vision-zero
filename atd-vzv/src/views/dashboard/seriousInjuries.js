cf69b93fb5b9a56f54a0c5231702ffce34f9d3ae
Mike Dilley <mike.dilley@gmail.com>
2019-10-21 16:53:11 -0500

    Add Serious Injuries and Fatal Crashes query

--- atd-vzv/src/views/dashboard/seriousInjuries.js
+++ atd-vzv/src/views/dashboard/seriousInjuries.js
@@ -10,8 +10,8 @@ const SeriousInjuries = props => {
     .subtract(1, "year")
     .format("YYYY");
 
-  const yearToDateUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=sus_serious_injry_cnt > 0 and crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
-  const previousYearUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=sus_serious_injry_cnt > 0 and crash_date between '${lastYear}-01-01T00:00:00' and '${lastYear}${todayMonthYear}T23:59:59'`;
+  const yearToDateUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=sus_serious_injry_cnt > 0 AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
+  const previousYearUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=sus_serious_injry_cnt > 0 AND crash_date between '${lastYear}-01-01T00:00:00' and '${lastYear}${todayMonthYear}T23:59:59'`;
 
   const [yearToDateInjuryTotal, setYearToDateInjuryTotal] = useState(0);
   const [lastYearToDateInjuryTotal, setLastYearToDateInjuryTotal] = useState(0);
--- atd-vzv/src/views/dashboard/seriousInjuryAndFatalCrashesByMonth.js
+++ atd-vzv/src/views/dashboard/seriousInjuryAndFatalCrashesByMonth.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import moment from "moment";
+import line from "react-chartjs-2";
 
 // Endpoint: https://data.austintexas.gov/resource/y2wy-tgr5.json
 // Need to display line graph (x-axis is Jan-Dec, y-axis is count) of:
 // 1. Year-to-date
 // 2. Previous year
 
-const SeriousInjuries = props => {
+const SeriousInjuryAndFatalCrashesByMonth = props => {
+  const today = moment().format("YYYY-MM-DD");
+  const todayMonthYear = moment().format("-MM-DD");
+  const thisYear = moment().format("YYYY");
+  const lastYear = moment()
+    .subtract(1, "year")
+    .format("YYYY");
+
+  const yearToDateUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=(sus_serious_injry_cnt > 0 OR death_cnt > 0) AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
+  const previousYearUri = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=(sus_serious_injry_cnt > 0 OR death_cnt > 0) AND crash_date between '${lastYear}-01-01T00:00:00' and '${lastYear}${todayMonthYear}T23:59:59'`;
+
+  const [yearToDateInjuryTotal, setYearToDateInjuryTotal] = useState(0);
+  const [lastYearToDateInjuryTotal, setLastYearToDateInjuryTotal] = useState(0);
+
+  const calculateTotalInjuries = data => {
+    let total = 0;
+    data.data.forEach(record => {
+      total += parseInt(record.sus_serious_injry_cnt);
+      total += parseInt(record.death_cnt);
+    });
+    return total;
+  };
+
+  useEffect(() => {
+    // Fetch year-to-date records
+    axios.get(yearToDateUri).then(res => {
+      setYearToDateInjuryTotal(calculateTotalInjuries(res));
+    });
+
+    // Fetch last year-to-date records
+    axios.get(previousYearUri).then(res => {
+      setLastYearToDateInjuryTotal(calculateTotalInjuries(res));
+    });
+  }, []);
+
   return (
-    <div>TODO Year-to-Date Serious Injuries - display sums as integers</div>
+    <div>
+      <h3>Year-to-Date Serious Injuries</h3>
+      <p>
+        {thisYear}: {yearToDateInjuryTotal}
+      </p>
+      <p>
+        {lastYear}: {lastYearToDateInjuryTotal}
+      </p>
+    </div>
   );
 };
 
-export default SeriousInjuries;
+export default SeriousInjuryAndFatalCrashesByMonth;
