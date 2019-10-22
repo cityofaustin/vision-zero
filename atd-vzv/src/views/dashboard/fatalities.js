d84ce022ef2efe57720950a659b044348624cdf3
Mike Dilley <mike.dilley@gmail.com>
2019-10-22 12:03:34 -0500

    Add FontAwesome and add pictorial chart to Fatalities component

--- atd-vzv/src/views/dashboard/fatalities.js
+++ atd-vzv/src/views/dashboard/fatalities.js
@@ -2,10 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import moment from "moment";
 
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMale } from "@fortawesome/free-solid-svg-icons";
+
 // Endpoint: https://data.austintexas.gov/resource/y2wy-tgr5.json
 // Need to display pictorial chart of:
 // 1. Year-to-date fatalities for current
 // 2. Fatalities from previous year
+
+const StyledIcon = styled.i`
+  margin: 0px 2px 0px 2px;
+`;
 
 const Fatalities = props => {
   const today = moment().format("YYYY-MM-DD");
@@ -18,38 +26,50 @@ const Fatalities = props => {
   const yearToDateUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=death_cnt > 0 AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
   const previousYearUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=death_cnt > 0 AND crash_date between '${lastYear}-01-01T00:00:00' and '${lastYear}${todayMonthYear}T23:59:59'`;
 
-  const [yearToDateInjuryTotal, setYearToDateInjuryTotal] = useState(0);
-  const [lastYearToDateInjuryTotal, setLastYearToDateInjuryTotal] = useState(0);
+  const [yearToDateFatalityTotal, setYearToDateFatalityTotal] = useState(0);
+  const [
+    lastYearToDateFatalityTotal,
+    setLastYearToDateFatalityTotal
+  ] = useState(0);
 
-  const calculateTotalInjuries = data => {
+  const calculateTotalFatalities = data => {
     let total = 0;
-    data.data.forEach(
-      record => (total += parseInt(record.sus_serious_injry_cnt))
-    );
+    data.data.forEach(record => (total += parseInt(record.death_cnt)));
     return total;
   };
+
+  const renderFatalityIcons = fatalityTotal =>
+    [...Array(fatalityTotal)].map((e, i) => (
+      <StyledIcon>
+        <FontAwesomeIcon icon={faMale} key={i} size="lg" />
+      </StyledIcon>
+    ));
 
   useEffect(() => {
     // Fetch year-to-date records
     axios.get(yearToDateUrl).then(res => {
-      setYearToDateInjuryTotal(calculateTotalInjuries(res));
+      setYearToDateFatalityTotal(calculateTotalFatalities(res));
     });
 
     // Fetch last year-to-date records
     axios.get(previousYearUrl).then(res => {
-      setLastYearToDateInjuryTotal(calculateTotalInjuries(res));
+      setLastYearToDateFatalityTotal(calculateTotalFatalities(res));
     });
   }, []);
 
   return (
     <div>
-      <h3>Year-to-Date Serious Injuries</h3>
+      <h3>Year-to-Date Fatalities</h3>
       <p>
-        {thisYear}: {yearToDateInjuryTotal}
+        {thisYear}: {yearToDateFatalityTotal}
       </p>
       <p>
-        {lastYear}: {lastYearToDateInjuryTotal}
+        {lastYear}: {lastYearToDateFatalityTotal}
       </p>
+      <p>{thisYear}</p>
+      <p>{renderFatalityIcons(yearToDateFatalityTotal)}</p>
+      <p>{lastYear}</p>
+      <p>{renderFatalityIcons(lastYearToDateFatalityTotal)}</p>
     </div>
   );
 };
