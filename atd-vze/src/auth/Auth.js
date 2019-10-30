d905fe29540b9f834ce63e58f05bad0e11f87111
Mateo Clarke <code@mateoclarke.com>
2019-10-30 17:54:52 -0500

    bases callback origin on current URL instead of ENV_VAR

--- atd-vze/src/auth/Auth.js
+++ atd-vze/src/auth/Auth.js
@@ -7,7 +7,7 @@ export default class Auth {
     this.auth0 = new auth0.WebAuth({
       domain: process.env.REACT_APP_AUTH0_DOMAIN,
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
+      redirectUri: `${window.location.origin}/callback`,
       responseType: "token id_token",
       scope: "openid profile email",
     });
