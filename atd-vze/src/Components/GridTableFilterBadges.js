7f8293b3abef8b5696f9ecb18d8a8297b044a66c
Mike Dilley <mike.dilley@gmail.com>
2019-10-30 13:36:52 -0500

    Add Date Range filter badge

--- atd-vze/src/Components/GridTableFilterBadges.js
+++ atd-vze/src/Components/GridTableFilterBadges.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 
 import { Col, Row, Badge } from "reactstrap";
 
@@ -8,14 +9,26 @@ const GridTableFilterBadges = ({
   dateRangeParams,
 }) => {
   const [searchBadgeText, setSearchBadgeText] = useState(null);
+  const [dateRangeBadgeText, setDateRangeBadgeText] = useState(null);
 
   useEffect(() => {
-    console.log(searchParams);
     const searchText = searchParams.value
       ? `Search: ${searchParams.value}`
       : null;
     setSearchBadgeText(searchText);
   }, [searchParams]);
+
+  useEffect(() => {
+    console.log(dateRangeParams);
+    const startDateRangeText = moment(dateRangeParams.startDate).format(
+      "MM/DD/YYYY"
+    );
+    const endDateRangeText = moment(dateRangeParams.endDate).format(
+      "MM/DD/YYYY"
+    );
+    const formattedDateRangeText = `Date: ${startDateRangeText} to ${endDateRangeText}`;
+    setDateRangeBadgeText(formattedDateRangeText);
+  }, [dateRangeParams]);
 
   return (
     <Row className="mb-2">
@@ -24,6 +37,11 @@ const GridTableFilterBadges = ({
         {searchBadgeText && (
           <Badge className="mr-1" color="primary">
             {searchBadgeText}
+          </Badge>
+        )}
+        {dateRangeBadgeText && (
+          <Badge className="mr-1" color="primary">
+            {dateRangeBadgeText}
           </Badge>
         )}
       </Col>
