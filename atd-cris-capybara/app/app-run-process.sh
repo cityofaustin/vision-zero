0283c9d3c03d5aaa6e66a391dc64941153ac4b6c
Sergio Garcia <sergiogcx@gmail.com>
2019-11-05 14:45:21 -0600

    Added sigterm trap capabilities to process script

--- atd-cris-capybara/app/app-run-process.sh
+++ atd-cris-capybara/app/app-run-process.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 PROCESS_TYPE=$1
+SKIP_ROWS=$2
+QUIT=0
 
-for CURRENT_FILE in $(find ./data -name "extract_*$PROCESS_TYPE*.csv"); do
-    python3 process.py $PROCESS_TYPE $CURRENT_FILE;
+function PreTrap {
+    QUIT=1;
+}
+
+trap PreTrap SIGINT SIGTERM SIGTSTP
+
+for CURRENT_FILE in $(find /data -name "extract_*$PROCESS_TYPE*.csv"); do
+    if [[ "${QUIT}" = "1" ]]; then
+        exit;
+    fi;
+
+    python3 process.py $PROCESS_TYPE $CURRENT_FILE $SKIP_ROWS;
 done;
