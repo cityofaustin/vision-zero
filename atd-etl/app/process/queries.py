1c9a32a9b1cd10348b10fef6bae5b7bfbbc0a034
Sergio Garcia <sergiogcx@gmail.com>
2019-11-07 10:42:00 -0600

    Added new queries file

---
+++ atd-etl/app/process/queries.py
@@ -0,0 +1,77 @@
+#
+# All queries necessary to run this thing...
+#
+
+
+def search_crash_query(crash_id):
+    """
+    Generates a graphql query to search for a crash
+    :param crash_id: stirng - The Crash ID to search for.
+    :return: string
+    """
+    return """
+        query search_crash_query {
+          atd_txdot_crashes(limit: 1, where: {crash_id: {_eq: %CRASH_ID%}}){
+            crash_id
+          }
+        }
+    """.replace("%CRASH_ID%", crash_id)
+
+
+def search_person(line):
+    """
+    Generates a graphql query to search for a specific person.
+    :param line: string - The raw CSV line
+    :return: string
+    """
+    # (p.crash_id, p.unit_nbr, p.prsn_nbr, p.prsn_type_id, p.prsn_occpnt_pos_id)
+    return line
+
+
+def search_primaryperson(line):
+    """
+    Generates a graphql query to search for a specific primary person.
+    :param line: string - The raw CSV line
+    :return: string
+    """
+    # (p.crash_id, p.unit_nbr, p.prsn_nbr, p.prsn_type_id, p.prsn_occpnt_pos_id)
+    return line
+
+
+def search_charges(line):
+    """
+    Generates a graphql query to search for a specific charge.
+    :param line: string - The raw CSV line
+    :return: string
+    """
+    # Search by everything
+    charge = line.split(",")
+
+
+    return """
+        query search_charges {
+            atd_txdot_charges(
+                where: {
+                    crash_id: {_eq: %s},
+                    unit_nbr: {_eq: %s}
+                    prsn_nbr: {_eq: %s},
+                    charge_cat_id: {_eq: %s},
+                    charge: {_eq: ""},
+                    citation_nbr: {_eq: ""},
+                }
+            ) {
+            charge_id
+          }
+        }
+    """ % (charge[0], charge[1], charge[2], charge[3], charge[4], charge[5])
+    return line
+
+
+def search_units(line):
+    """
+    Generates a graphql query to search for a specific unit.
+    :param line: string - The raw CSV line
+    :return: string
+    """
+    #(p.crash_id, p.unit_nbr)
+    return line
