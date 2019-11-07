5c6198d84fd438bd29eb5019facc108c8ec4605d
Sergio Garcia <sergiogcx@gmail.com>
2019-11-07 10:09:36 -0600

    Added new Shell files for new etl

---
+++ atd-etl/app/app-run-geocode.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo -e "\n\n"
+echo -e "Starting GeoCode process..."
+echo -e "------------------------------------------------"
+python3 process_hasura_geocode_records.py
+echo -e "Done.\n\n"
---
+++ atd-etl/app/app-run-process.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PROCESS_TYPE=$1
+SKIP_ROWS=$2
+QUIT=0
+
+function PreTrap {
+    QUIT=1;
+}
+
+trap PreTrap SIGINT SIGTERM SIGTSTP
+
+for CURRENT_FILE in $(find /data -name "extract_*$PROCESS_TYPE*.csv"); do
+    if [[ "${QUIT}" = "1" ]]; then
+        exit;
+    fi;
+
+    python3 process_hasura_import.py $PROCESS_TYPE $CURRENT_FILE $SKIP_ROWS;
+done;
