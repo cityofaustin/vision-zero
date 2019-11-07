73fc43bd4fb9676ca225696f4ce498958704cb9d
Sergio Garcia <sergiogcx@gmail.com>
2019-11-07 10:39:00 -0600

    Refactored CR3 initializer

--- atd-etl/app/app-run-process-cr3.sh
+++ atd-etl/app/app-run-process-cr3.sh
@@ -3,5 +3,5 @@
 echo -e "\n\n"
 echo -e "Starting CRIS Search CR3 request process..."
 echo -e "------------------------------------------------"
-xvfb-run --server-args="-screen 0 1024x768x24" ruby process_cr3.rb
+python process_hasura_cr3.py
 echo -e "Done.\n"
