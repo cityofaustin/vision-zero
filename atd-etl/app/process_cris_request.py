3a0f76fba85b4b4646f50d3e6ccb0ce4e061c0b6
Sergio Garcia <sergiogcx@gmail.com>
2019-11-11 21:55:43 -0600

    Added request file as placeholder

---
+++ atd-etl/app/process_cris_request.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+CRIS - Request Script
+Author: Austin Transportation Department, Data and Technology Office
+
+Description: This script allows the user to request new CSV data extracts.
+It will sign-in to the CRIS website, and given date parameters, it will
+fill out the form and request the extract step-by-step.
+
+The application requires the splinter library:
+    https://splinter.readthedocs.io/en/latest/
+"""
+print("CRIS - Request Script: Not yet implemented.")
