c899385f36ae552c70b834cbd8eec0016894ca91
Sergio Garcia <sergiogcx@gmail.com>
2019-11-11 21:54:08 -0600

    Expanded configuration settings

--- atd-etl/app/process/config.py
+++ atd-etl/app/process/config.py
@@ -1,26 +1,41 @@
-#
-# Configuration for ETL processes
-#
+"""
+ETL Configuration File
+Author: Austin Transportation Department, Data and Technology Office
+
+Description: This file contains an dictionary that contains environment
+variables for all scripts, including CRIS, Hasura, Socrata, ETC. These
+environment variables are fed through Docker. Please refer to the project
+readme for further details on how to import environment variables.
+"""
+
 import os
 
 # AWS Bucket
 ATD_ETL_CONFIG = {
-    "AWS_DOWNLOAD_PATH": "/app/tmp/",
-    "AWS_BUCKET": os.getenv("AWS_BUCKET_NAME"),
-    "AWS_BUCKET_PATH": os.getenv("AWS_BUCKET_PATH", "production/cris-cr3-files-unassigned"),
+    # AWS
+    "AWS_DEFALUT_REGION": os.getenv("AWS_DEFALUT_REGION", ""),
+    "AWS_ACCESS_KEY_ID": os.getenv("AWS_ACCESS_KEY_ID", ""),
+    "AWS_SECRET_ACCESS_KEY": os.getenv("AWS_SECRET_ACCESS_KEY", ""),
 
     # HASURA
-    "HASURA_ENDPOINT": os.getenv("HASURA_ENDPOINT"),
-    "HASURA_ADMIN_KEY": os.getenv("HASURA_ADMIN_KEY"),
+    "HASURA_ENDPOINT": os.getenv("HASURA_ENDPOINT", ""),
+    "HASURA_ADMIN_KEY": os.getenv("HASURA_ADMIN_KEY", ""),
 
     # CRIS
-    "ATD_CRIS_USERNAME_CR3": os.getenv("ATD_CRIS_USERNAME"),
-    "ATD_CRIS_PASSWORD_CR3": os.getenv("ATD_CRIS_PASSWORD"),
+    "ATD_CRIS_USERNAME_CR3": os.getenv("ATD_CRIS_USERNAME", ""),
+    "ATD_CRIS_PASSWORD_CR3": os.getenv("ATD_CRIS_PASSWORD", ""),
     "ATD_CRIS_CR3_DOWNLOADS_PER_RUN": os.getenv("ATD_CRIS_DOWNLOADS_PER_RUN", "25"),
+
+    # SOCRATA
+    "SOCRATA_KEY_ID": os.getenv("SOCRATA_KEY_ID", ""),
+    "SOCRATA_KEY_SECRET": os.getenv("SOCRATA_KEY_SECRET", ""),
 
     # CR3
     "ATD_CRIS_WEBSITE": "https://cris.dot.state.tx.us/",
     "ATD_CRIS_CR3_URL": "https://cris.dot.state.tx.us/secure/ImageServices/DisplayImageServlet?target=",
+    "AWS_CRIS_CR3_DOWNLOAD_PATH": os.getenv("AWS_CRIS_CR3_DOWNLOAD_PATH", "/app/tmp"),
+    "AWS_CRIS_CR3_BUCKET_NAME": os.getenv("AWS_CRIS_CR3_BUCKET_NAME", ""),
+    "AWS_CRIS_CR3_BUCKET_PATH": os.getenv("AWS_CRIS_CR3_BUCKET_PATH", "production/cris-cr3-files-unassigned"),
 
     # REQUEST
     "ATD_CRIS_WEBSITE_HOME": "https://cris.dot.state.tx.us/secure/Share/app/home/welcome",
@@ -30,5 +45,5 @@ ATD_ETL_CONFIG = {
         "location": "https://cris.dot.state.tx.us/secure/Share/app/home/request/location",
         "date":  "https://cris.dot.state.tx.us/secure/Share/app/home/request/date",
         "summary": "https://cris.dot.state.tx.us/secure/Share/app/home/request/summary"
-    }
+    },
 }
