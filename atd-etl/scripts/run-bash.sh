0021832a69aa2f3bd7bf7a50f9380c582237fd97
Sergio Garcia <sergiogcx@gmail.com>
2019-11-11 21:36:08 -0600

    Removed more unnecessary scripts

--- atd-etl/scripts/run-bash.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-
-source ./scripts/run-config.sh;
-
-docker run -it --rm -v $(pwd)/app:/app -v $(pwd)/data:/data \
-    --env-file $ATD_CRIS_CONFIG $ATD_DOCKER_IMAGE bash;
--- atd-etl/scripts/run-geocode.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm \
-    --env-file $ATD_CRIS_CONFIG \
-    atddocker/atd-cris-capybara \
-    "sh -c /app/app-run-geocode.sh"
