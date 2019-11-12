1aa6b8cfdc7e5ef0c7a52bf4cd09808ffdf742df
Sergio Garcia <sergiogcx@gmail.com>
2019-11-12 13:27:03 -0600

    Updated CircleCI configuration

--- .circleci/docker-helper.sh
+++ .circleci/docker-helper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export ATD_IMAGE="atddocker/atd-cris-capybara";
+export ATD_IMAGE="atddocker/atd-vz-etl";
 
 #
 # We need to assign the name of the branch as the tag to be deployed
@@ -12,7 +12,7 @@ function build_containers {
     docker login -u $ATD_DOCKER_USER -p $ATD_DOCKER_PASS
 
     echo "docker build --no-cache -f Dockerfile -t $ATD_IMAGE:$ATD_TAG .";
-    docker build -f atd-cris-capybara/Dockerfile -t $ATD_IMAGE:$ATD_TAG ./atd-cris-capybara
+    docker build -f --no-cache atd-etl/Dockerfile -t $ATD_IMAGE:$ATD_TAG ./atd-etl
 
     echo "docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;";
     docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;
