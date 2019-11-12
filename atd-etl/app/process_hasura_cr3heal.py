85bb5a914cb766dd204499ab5574464b0a27901c
Sergio Garcia <sergiogcx@gmail.com>
2019-11-11 21:56:09 -0600

    Added cr3heal file as placeholder

---
+++ atd-etl/app/process_hasura_cr3heal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Hasura - Healer
+Author: Austin Transportation Department, Data and Technology Office
+
+Description: The purpose of this script is to search for records that
+seem to be associated to a CR3, but the file is not present in S3.
+
+The application requires the boto3 library:
+    https://pypi.org/project/boto3/
+"""
+print("Hasura - Healer: Request Download: Not yet implemented.")
