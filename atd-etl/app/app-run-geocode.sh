22253c05269951b77aeff32f944fff157aa18655
Sergio Garcia <sergiogcx@gmail.com>
2019-11-11 21:35:43 -0600

    Removed unecessary script files

--- atd-etl/app/app-run-geocode.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n\n"
-echo -e "Starting GeoCode process..."
-echo -e "------------------------------------------------"
-python3 process_hasura_geocode_records.py
-echo -e "Done.\n\n"
--- atd-etl/app/app-run-process-cr3.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "\n\n"
-echo -e "Starting CRIS Search CR3 request process..."
-echo -e "------------------------------------------------"
-python process_hasura_cr3.py
-echo -e "Done.\n"
