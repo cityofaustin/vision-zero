1421b27b64fde698b008c455792e7a352117fd90
Mike Dilley <mike.dilley@gmail.com>
2019-11-14 16:07:26 -0600

    Add method to flatten Hasura response

--- atd-etl/app/process_socrata_export.py
+++ atd-etl/app/process_socrata_export.py
@@ -13,6 +13,7 @@ The application requires the requests and sodapy libraries:
 
 from sodapy import Socrata
 from string import Template
+from copy import deepcopy
 import requests
 import json
 import os
@@ -76,32 +77,81 @@ crashes_query_template = Template(
             unkn_injry_cnt
             tot_injry_cnt
             death_cnt
+            units {
+                contrib_factr_p1_id
+                contrib_factr_p2_id
+                body_style {
+                    veh_body_styl_desc
+                }
+                unit_description {
+                    veh_unit_desc_desc
+                }
+            }
         }
     }
 """
 )
+
+
+def flatten_hasura_response(records):
+    formatted_records = []
+    for record in records:
+        # Create copy of record to mutate
+        formatted_record = deepcopy(record)
+        # Look through key values for data lists
+        for k, v in record.items():
+            # If list is found, iterate to bring key values to top-level
+            if type(v) == list:
+                for item in v:
+                    for key, value in item.items():
+                        # Handle nested values
+                        if type(value) == dict:
+                            # Handles concat of values here
+                            print(f"Nested: {value}")
+                            for nested_key, nested_value in value.items():
+                                if nested_key in formatted_record.keys():
+                                    next_record = f"|{nested_value}"
+                                    # Need to join records here
+                                    formatted_record[nested_key] = formatted_record[nested_key] += next_record
+                                else:
+                                    formatted_record[nested_key] = nested_value
+                        # Copy non-nested key-values to top-level
+                        else:
+                            formatted_record[key] = value
+                # del formatted_record[k]
+        formatted_records.append(formatted_record)
+    return formatted_records
+
 
 # Start timer
 start = time.time()
 
 # while loop to request records from Hasura and post to Socrata
 records = None
-offset = 0
-limit = 4000
+offset = 3
+limit = 1
 total_records_published = 0
 
 # Get records from Hasura until res is []
-while records != []:
-    crashes_query = crashes_query_template.substitute(
-        limit=limit, offset=offset)
-    offset += limit
-    crashes = run_hasura_query(crashes_query)
-    records = crashes['data']['atd_txdot_crashes']
+# while records != []:
+#     crashes_query = crashes_query_template.substitute(
+#         limit=limit, offset=offset)
+#     offset += limit
+#     crashes = run_hasura_query(crashes_query)
+#     records = crashes['data']['atd_txdot_crashes']
 
-    # Upsert records to Socrata
-    client.upsert("rrxh-grh6", records)
-    total_records_published += len(records)
-    print(f"{total_records_published} records published")
+#     # Upsert records to Socrata
+#     client.upsert("rrxh-grh6", records)
+#     total_records_published += len(records)
+#     print(f"{total_records_published} records published")
+
+crashes_query = crashes_query_template.substitute(
+    limit=limit, offset=offset)
+crashes = run_hasura_query(crashes_query)
+records = crashes['data']['atd_txdot_crashes']
+formatted_records = flatten_hasura_response(records)
+print(formatted_records[0])
+
 
 # Terminate Socrata connection
 client.close()
