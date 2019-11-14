f0f4c24cf6995fa7a18dfb97f68c5433b54fe94e
Sergio Garcia <sergiogcx@gmail.com>
2019-11-14 11:41:05 -0600

    Initial Refactor

--- atd-etl/app/process_hasura_import.py
+++ atd-etl/app/process_hasura_import.py
@@ -61,105 +61,26 @@ signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 # This string must match with the export_*.csv file name.
 FILE_TYPE = str(sys.argv[1]).lower()
 
-# We also need to know the exact location of the file we are currently processing.
-FILE_PATH = sys.argv[2]
-
+# Secondly, we need to know how many rows to skip
 try:
     # The user should also be able to specify how many rows to skip on the 3rd argument
-    FILE_SKIP_ROWS = sys.argv[3]
+    FILE_SKIP_ROWS = sys.argv[2]
 except:
     # Otherwise, skip none.
     FILE_SKIP_ROWS = 0
 
-
-# Print what we are currently doing, and where we are going to insert data.
-print("Processing file '%s' of type '%s', skipping: '%s'" % (FILE_PATH, FILE_TYPE, FILE_SKIP_ROWS))
-print("Endpoint: %s" % ATD_ETL_CONFIG["HASURA_ENDPOINT"])
-
-# We need counts of the
-records_inserted = 0
-existing_records = 0
-insert_errors = 0
-current_line = 0
-
-# This array stores the header names taken from the CSV file,
-# this will later be used to generate our graphql queries.
-fieldnames = []
-
-# This will hold the official number of rows being skipped, just to be safe.
-# If FILE_SKIP_ROWS contains no value, assumes 0 rows to be skipped.
-skip_rows_parsed = int(FILE_SKIP_ROWS) if FILE_SKIP_ROWS != "" else 0
-print("We are skipping: %d" % skip_rows_parsed)
-
-# Open FILE_PATH as a file pointer:
-with open(FILE_PATH) as fp:
-    # Then read the first line
-    line = fp.readline()
-
-    # While we haven't reached the EOF
-    while line:
-        # Check if there is a keyboard interrupt, exit if so.
-        if STOP_EXEC:
-            exit(1)
-        else:
-            # If this is the first line
-            if current_line == 0:
-                # Then split each word and use as field names for our GraphQL query
-                fieldnames = line.strip().split(",")
-
-            # This is another
-            else:
-                #
-                # Skipping `skip_rows_parsed` number of lines
-                #
-                if skip_rows_parsed != 0 and skip_rows_parsed > current_line:
-                    current_line += 1
-                    line = fp.readline() # Move pointer to next line
-                    continue
-
-                #
-                # Not skipping
-                #
-
-                # Read the crash_id from the current line
-                # Applies to: crashes, unit, person, primary person, charges
-                crash_id = line.strip().split(",")[0]
-
-                # First we need to check if the current record exists, skip if so.
-                if record_exists_hook(line=line, type=FILE_TYPE):
-                    print("[%s] Exists: %s (%s)" % (str(current_line), str(crash_id), FILE_TYPE))
-                    existing_records += 1
-
-                # The record does not exist, insert.
-                else:
-                    print("[%s] Inserting: %s" % (str(current_line), str(crash_id)))
-                    gql = generate_gql(line=line, fieldnames=fieldnames, type=FILE_TYPE)
-                    print("GQL: " + gql)
-                    #response = run_query(gql)
-
-                    response = {
-                        "errors": "Made up!"
-                    }
-
-                    print(json.dumps(response))
-
-                    # If there is an error, run the hook.
-                    if "errors" in response:
-                        insert_errors += 1
-                        # Gather from this function if we need to stop the execution.
-                        stop_execution = handle_record_error_hook(line=line, gql=gql, type=FILE_TYPE)
-                        if stop_execution:
-                            STOP_EXEC = True
-                            exit(1)
+if FILE_TYPE == "":
+    print("No File Type provided")
+    exit(1)
 
 
-                    records_inserted += 1
+for
+FILE_LIST = glob.glob("/data/extract_*%s*.csv" % FILE_TYPE)
 
-        current_line += 1
-        line = fp.readline()  # Move pointer to next line
+for CURRENT_FILE in FILE_LIST:
+    print("Processing file: '%s'" % CURRENT_FILE)
+
+    print("Skipping Lines: %s" % FILE_SKIP_ROWS)
 
 
-print("Total Existing Records: %s" % (existing_records))
-print("Total Records Inserted: %s" % (records_inserted))
-print("Total Errors: %s" % (insert_errors))
-print("")
+
