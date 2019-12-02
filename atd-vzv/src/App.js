1aba617ba6baa0dcbebc305b2534f1ca5055c231
Mike Dilley <mike.dilley@gmail.com>
2019-12-02 16:03:10 -0600

    Install hookrouter, create routes, and add mock map

--- atd-vzv/src/App.js
+++ atd-vzv/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./views/dashboard/dashboard";
+import { useRoutes } from "hookrouter";
+
+const routes = {
+  "/dashboard": <Dashboard />,
+  "/map": <Map />
+};
 
 function App() {
   return (
---
+++ atd-vzv/src/views/map/map.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+const Map = () => {
+  return (
+    <Container>
+      <h3>Map Test</h3>
+    </Container>
+  );
+};
+
+export default Map;
