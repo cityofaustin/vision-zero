fc73a059f8f77295f6e5e993bc6455e8a7a386f1
Mike Dilley <mike.dilley@gmail.com>
2019-12-02 16:13:47 -0600

    Move WIP msg to <App/>, render routeResult

--- atd-vzv/src/App.js
+++ atd-vzv/src/App.js
@@ -1,22 +1,38 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./views/dashboard/dashboard";
+import Map from "./views/map/map";
+import { Row, Col, Alert } from "reactstrap";
 import { useRoutes } from "hookrouter";
 
 const routes = {
-  "/dashboard": <Dashboard />,
-  "/map": <Map />
+  "/dashboard": () => <Dashboard />,
+  "/map": () => <Map />
 };
 
-function App() {
+const App = () => {
+  const routeResult = useRoutes(routes);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Vision Zero Viewer</h1>
       </header>
-      <Dashboard />
+      <Row>
+        <Col md="12">
+          <Alert color="primary">
+            <h4 className="alert-heading">This site is a work in progress.</h4>
+            <p>
+              The information displayed below may be outdated or incorrent.
+              <br></br>
+              Check back later for live Vision Zero data.
+            </p>
+          </Alert>
+        </Col>
+      </Row>
+      {routeResult}
     </div>
   );
-}
+};
 
 export default App;
--- atd-vzv/src/views/dashboard/dashboard.js
+++ atd-vzv/src/views/dashboard/dashboard.js
@@ -11,18 +11,6 @@ const Dashboard = () => {
   return (
     <Container fluid>
       <Row>
-        <Col md="12">
-          <Alert color="primary">
-            <h4 className="alert-heading">This site is a work in progress.</h4>
-            <p>
-              The information displayed below may be outdated or incorrent.
-              <br></br>
-              Check back later for live Vision Zero data.
-            </p>
-          </Alert>
-        </Col>
-      </Row>
-      <Row>
         <Col md="6">
           <Fatalities />
         </Col>
