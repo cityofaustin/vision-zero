9e38bbca3f1d5087496f24d385bd52d1eb19d372
Mike Dilley <mike.dilley@gmail.com>
2019-12-03 14:06:01 -0600

    Create styled components and move styles from App.css

--- atd-vzv/src/views/content/content.js
+++ atd-vzv/src/views/content/content.js
@@ -1,34 +1,47 @@
 import React from "react";
 import classNames from "classnames";
 import { Container, Row, Col, Alert } from "reactstrap";
+import styled from "styled-components";
 import { useRoutes } from "hookrouter";
 import { routes } from "../../routes/routes";
 import Header from "../nav/header";
 import NotFound from "../NotFound/NotFound";
 
+const StyledContent = styled.div`
+  .content {
+    padding: 20px;
+    margin-left: 0;
+    height: 100vh;
+  }
+`;
+
 const Content = ({ toggle, isOpen }) => {
   const routeResult = useRoutes(routes);
 
   return (
-    <Container
-      fluid
-      className={classNames("content", "bg-light", { "is-open": isOpen })}
-    >
-      <Header toggleSidebar={toggle} />
-      <Row>
-        <Col md="12">
-          <Alert color="primary">
-            <h4 className="alert-heading">This site is a work in progress.</h4>
-            <p>
-              The information displayed below may be outdated or incorrent.
-              <br></br>
-              Check back later for live Vision Zero data.
-            </p>
-          </Alert>
-        </Col>
-      </Row>
-      {routeResult || <NotFound />}
-    </Container>
+    <StyledContent>
+      <Container
+        fluid
+        className={classNames("content", "bg-light", { "is-open": isOpen })}
+      >
+        <Header toggleSidebar={toggle} />
+        <Row>
+          <Col md="12">
+            <Alert color="primary">
+              <h4 className="alert-heading">
+                This site is a work in progress.
+              </h4>
+              <p>
+                The information displayed below may be outdated or incorrent.
+                <br></br>
+                Check back later for live Vision Zero data.
+              </p>
+            </Alert>
+          </Col>
+        </Row>
+        {routeResult || <NotFound />}
+      </Container>
+    </StyledContent>
   );
 };
 
--- atd-vzv/src/views/nav/sidebar.js
+++ atd-vzv/src/views/nav/sidebar.js
@@ -2,34 +2,118 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faMap } from "@fortawesome/free-solid-svg-icons";
 import { NavItem, NavLink, Nav } from "reactstrap";
+import styled from "styled-components";
 import classNames from "classnames";
 import { A } from "hookrouter";
 
+const StyledSidebar = styled.div`
+  .sidebar {
+    min-width: 250px;
+    max-width: 250px;
+    background: #2f353a;
+    color: #fff;
+    margin-left: -250px;
+    transition: all 0.5s;
+  }
+
+  .sidebar.is-open {
+    margin-left: 0;
+    transition: 0.5s;
+  }
+
+  .sidebar-header {
+    background: #c8ced3;
+  }
+
+  .sidebar-header h3 {
+    color: #2f353a;
+    padding: 1em;
+  }
+
+  .sidebar ul p {
+    color: #fff;
+    padding: 10px;
+  }
+
+  .menu-open {
+    background: #6d7fcc;
+  }
+
+  .nav-link {
+    color: #63c2de;
+  }
+
+  .nav-item:hover {
+    color: #7386d5;
+    background: #fff;
+  }
+
+  .items-menu {
+    color: #fff;
+    background: #6d7fcc;
+  }
+
+  li a.dropdown-toggle::after {
+    display: inline-flex;
+    position: relative;
+    left: 60%;
+    top: 10%;
+  }
+
+  .sidebar-header > span {
+    position: relative;
+    float: right;
+    margin: 0.5em;
+    font-size: 2rem;
+    cursor: pointer;
+    display: none;
+  }
+  .side-menu {
+    height: calc(100vh - 130px);
+    overflow-y: scroll;
+  }
+
+  .side-menu::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  .side-menu::-webkit-scrollbar-thumb {
+    background: #5466b3;
+    border-radius: 3px;
+  }
+
+  .side-menu::-webkit-scrollbar-thumb:hover {
+    background: #3a4c99;
+  }
+`;
+
 const SideBar = ({ toggle, isOpen }) => (
-  <div className={classNames("sidebar", { "is-open": isOpen })}>
-    <div className="sidebar-header">
-      <span color="info" onClick={toggle} style={{ color: "#fff" }}>
-        &times;
-      </span>
-      <h3>Vision Zero Viewer</h3>
+  <StyledSidebar>
+    <div className={classNames("sidebar", { "is-open": isOpen })}>
+      <div className="sidebar-header">
+        <span color="info" onClick={toggle} style={{ color: "#fff" }}>
+          &times;
+        </span>
+        <h3>Vision Zero Viewer</h3>
+      </div>
+      <div className="side-menu">
+        <Nav vertical className="list-unstyled pb-3">
+          <NavItem>
+            <NavLink tag={A} href="/">
+              <FontAwesomeIcon icon={faHome} className="mr-2" />
+              Dashboard
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink tag={A} href="/map">
+              <FontAwesomeIcon icon={faMap} className="mr-2" />
+              Map
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
     </div>
-    <div className="side-menu">
-      <Nav vertical className="list-unstyled pb-3">
-        <NavItem>
-          <NavLink tag={A} href="/">
-            <FontAwesomeIcon icon={faHome} className="mr-2" />
-            Dashboard
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink tag={A} href="/map">
-            <FontAwesomeIcon icon={faMap} className="mr-2" />
-            Map
-          </NavLink>
-        </NavItem>
-      </Nav>
-    </div>
-  </div>
+  </StyledSidebar>
 );
 
 export default SideBar;
