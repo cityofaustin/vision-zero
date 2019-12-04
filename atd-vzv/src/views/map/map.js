fbec44d5904c5218a41bcaafaed37a977cd48611
Mike Dilley <mike.dilley@gmail.com>
2019-12-04 11:42:04 -0600

    Add reactmap-gl and render map

--- atd-vzv/src/views/map/map.js
+++ atd-vzv/src/views/map/map.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 import { Container } from "reactstrap";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const MAPBOX_TOKEN = `pk.eyJ1Ijoiam9obmNsYXJ5IiwiYSI6ImNrM29wNnB3dDAwcXEzY29zMTU5bWkzOWgifQ.KKvoz6s4NKNHkFVSnGZonw`;
 
 const Map = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
+
   return (
     <Container>
       <h3>Map Test</h3>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={viewport => setViewport(viewport)}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      />
     </Container>
   );
 };
