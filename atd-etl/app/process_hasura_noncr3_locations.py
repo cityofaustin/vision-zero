6c512de7f215be88e929fc913eaf8d7855644756
Mateo Clarke <code@mateoclarke.com>
2019-12-13 01:14:52 -0600

    remove unneeded code

--- atd-etl/app/process_hasura_noncr3_locations.py
+++ atd-etl/app/process_hasura_noncr3_locations.py
@@ -2,11 +2,10 @@
 """
 Hasura - Non-CR3 Collision to Location Association
 Author: Austin Transportation Department, Data and Technology Services
-Description: This script searches through the crashes table (through
+Description: This script searches through the non-CR3 collisions table (through
 Hasura) in Postgres, for any crashes that do not have a location
 assigned. If the crash cannot be associated to a location, then it
-should skip it. Crashes with fatalities and serious injuries should
-take priority.
+should skip it. 
 Note: This script should run always in the background at a
 proper interval.
 The application requires the requests library:
@@ -21,36 +20,12 @@ import web_pdb
 from process.request import run_query
 from string import Template
 
-from process.config import ATD_ETL_CONFIG
 
 from datetime import datetime
 
 
 start_time = datetime.now()
 
-config = {
-    "service_name": "intersection_polygons_test",
-    "layer_id": "0"
-}
-
-geojson = {
-    "type": "FeatureCollection",
-    "features": []
-}
-
-templateFeature = {
-    "type": "Feature",
-    "location": "",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [0, 0]
-    },
-    "properties": {
-        "icon": "car",
-        "marker-color": "#f00",
-        "stroke": "#eb4034"
-    }
-}
 
 query = """
     mutation MakeMutation($locationId:String, $crashId:Int){
@@ -64,7 +39,7 @@ query = """
 
 noncr3_query = """
     query getNonCR3Collision {
-        atd_apd_blueform(limit: 100)  {
+        atd_apd_blueform(limit: 200, offset: 200)  {
             address
             hour
             latitude
@@ -75,15 +50,6 @@ noncr3_query = """
         }
     }
 """
-
-headers = {
-    "content-type": "application/json",
-    "x-hasura-admin-secret": "aeMW9z75MtdK49tvFii56U9M78AcRdfK42aFpRHxZza4eAgcs53tSNg9K42rfUW2"
-}
-
-url = "https://vzd-staging.austinmobility.io/v1/graphql"
-
-print("Endpoint: %s" % ATD_ETL_CONFIG["HASURA_ENDPOINT"])
 
 find_location_query = Template("""
 query findLocation {
@@ -103,150 +69,30 @@ update_record_noncr3 = Template("""
 """)
 
 # web_pdb.set_trace()
-# foo = update_record_noncr3.substitute(id=1, location_id=999)
-# print(foo)
 
+# Query for a list of Non-CR3 Collisions
 result = run_query(noncr3_query)
 non_CR3_collisions_array = result['data']['atd_apd_blueform']
+
+# Loop over each record
 for collision in non_CR3_collisions_array:
     print("Processing NON CR3 RECORD: '%s' " % collision["form_id"])
     collision_query = find_location_query.substitute(id=collision["form_id"])
+
+    # Query to see if the record has a matching Location
+    # using findLocation SQL function
     location_result = run_query(collision_query)
-    record = location_result['data']['find_location_for_noncr3_collision']
-    if record:
-        location_id = record[0]['unique_id']
-        print(type(location_id))
-        location_desc = record[0]['description']
+    location = location_result['data']['find_location_for_noncr3_collision']
+
+    # If there is a matching location...
+    if location:
+        location_id = location[0]['unique_id']
+        location_desc = location[0]['description']
         non_cr3_mutation = update_record_noncr3.substitute(
             id=collision["form_id"], location_id=location_id)
+        # ...run a mutation on the collision and update its location_id
         mutation_result = run_query(non_cr3_mutation)
         print(mutation_result)
-
-# def request_noncr3_locations(query, url, headers):
-#     """
-#     Request non-CR3 crashes from VZD.
-#     """
-#     request = requests.get(
-#         url, json={'query': query}, headers=headers)
-#     print(request)
-#     web_pdb.set_trace()
-#     if request.status_code == 200:
-#         return request.json()
-#     else:
-#         print(request)
-#         web_pdb.set_trace()
-#         raise Exception("non-CR3 request query failed to run by returning code of {}. {}".format(
-#             request.status_code, query))
-
-# request_noncr3_locations(noncr3_query, url, headers)
-
-
-def make_query(query, variables, url, headers):
-    """
-    Make query response
-    """
-    request = requests.post(
-        url, json={'query': query, 'variables': variables}, headers=headers)
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(
-            request.status_code, query))
-
-
-def find_location(longitude, latitude):
-    params = {
-        "f": "json",
-        "outFields": "*",
-        "geometry": "%s,%s" % (longitude, latitude),
-        "geomtryType": "esriGeometryPoint",
-        "returnGeometry": False,
-        "spatialRel": "esriSpatialRelIntersects",
-        "inSR": 4326,
-        "geometryType": "esriGeometryPoint",
-        "distance": None,
-        "units": None,
-    }
-
-    try:
-        intersectionData = agolutil.point_in_poly(
-            config['service_name'], config['layer_id'], params)
-        intersectionId = intersectionData['features'][0]['attributes']['INTERSECTION_ID']
-        return intersectionId
-    except Exception as e:
-        print("Error: %s" % str(e))
-        return None
-
-
-def build_feature(long, lat):
-    """
-    Copies and builds a GeoJson entry from template (above).
-    :param long:
-    :param lat:
-    :return:
-    """
-    output = copy.deepcopy(templateFeature)
-    output['geometry']['coordinates'] = [long, lat]
-    return output
-
-
-#
-# This
-#
-def add_feature(feature):
-    """
-    This function will add a feature to the final GeoJson object.
-    :param feature:
-    :return:
-    """
-    geojson['features'].append(feature)
-
-
-#
-# This function is passed to the thread pool executor and
-# will process a single crash item object.
-#
-def process_crash(item):
-    """
-    The crash item that contains the longitude and latitude
-    :param item:
-    :return:
-    """
-
-    crash = build_feature(item['longitude'], item['latitude'])
-    location_id = find_location(item['longitude'], item['latitude'])
-
-    variables = {
-        "locationId": str(location_id),
-        "crashId": item['crash_id']
-    }
-
-    req = make_query(url=url, headers=headers,
-                     query=query, variables=variables)
-
-    crash['mutation'] = req
-
-    if location_id is None:
-        crash['location'] = str(location_id)
-        crash['properties']['stroke'] = '#0b6fd4'
-    else:
-        crash['location'] = "None"
-        crash['properties']['stroke'] = '#eb4034'
-
-    add_feature(crash)
-
-
-# call a method to request Hasura data
-
-
-#
-# Setting up a concurrency of 10 max_threads, that should be enough.
-# We will need to test how much more ESRI will allow us to run reliably.
-#
-# with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-#     executor.map(process_crash, crash_data['data']['atd_txdot_crashes'])
-
-print(json.dumps(geojson))
 
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
