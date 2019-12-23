2809a86b75019cbe50bda395409d425144515447
Sergio Garcia <sergiogcx@gmail.com>
2019-12-23 13:28:14 -0600

    Added Crash Changes component

---
+++ atd-vze/src/views/Crashes/CrashesChanges.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { withApollo } from "react-apollo";
+
+import GridTable from "../../Components/GridTable";
+import gqlAbstract from "../../queries/gqlAbstract";
+
+import {
+    crashChangesGridTableColumns
+} from "./crashChangesGridTableParameters";
+
+// Our initial query configuration
+let queryConf = {
+    table: "atd_txdot_changes",
+    single_item: "crashes",
+    columns: crashChangesGridTableColumns,
+    order_by: {},
+    where: {
+        city_id: "_eq: 22",
+    },
+    limit: 25,
+    offset: 0,
+};
+
+let crashesQuery = new gqlAbstract(queryConf);
+
+const CrashesChanges = () => (
+    <GridTable
+    query={crashesQuery}
+    title={"Crash Record Changes"}
+/>
+);
+
+export default withApollo(Crashes);
