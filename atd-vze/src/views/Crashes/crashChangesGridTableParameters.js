7fc2b5feb45a8b766ff6bb9a5c33de0bee9280d1
Sergio Garcia <sergiogcx@gmail.com>
2019-12-23 14:54:27 -0600

    Changed order, removed type

--- atd-vze/src/views/Crashes/crashChangesGridTableParameters.js
+++ atd-vze/src/views/Crashes/crashChangesGridTableParameters.js
@@ -1,22 +1,16 @@
 export const crashChangesGridTableColumns = {
-    change_id: {
-        searchable: false,
-        sortable: false,
-        label_table: "Change ID",
-        type: "Int",
-    },
-    record_type: {
-        searchable: false,
-        sortable: true,
-        label_table: "Record Type",
-        type: "String",
-    },
     record_id: {
         primary_key: true,
         searchable: true,
         sortable: true,
         label_search: "Search by Record ID",
         label_table: "Record ID",
+        type: "Int",
+    },
+    change_id: {
+        searchable: false,
+        sortable: false,
+        label_table: "Change ID",
         type: "Int",
     },
     created_timestamp: {
