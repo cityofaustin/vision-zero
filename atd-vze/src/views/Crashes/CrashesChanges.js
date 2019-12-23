8eaf29eea1436ab6b3a92244df3e22a9a8a8e7fe
Sergio Garcia <sergiogcx@gmail.com>
2019-12-23 13:28:54 -0600

    Corrected syntax

--- atd-vze/src/views/Crashes/CrashesChanges.js
+++ atd-vze/src/views/Crashes/CrashesChanges.js
@@ -4,30 +4,25 @@ import { withApollo } from "react-apollo";
 import GridTable from "../../Components/GridTable";
 import gqlAbstract from "../../queries/gqlAbstract";
 
-import {
-    crashChangesGridTableColumns
-} from "./crashChangesGridTableParameters";
+import { crashChangesGridTableColumns } from "./crashChangesGridTableParameters";
 
 // Our initial query configuration
 let queryConf = {
-    table: "atd_txdot_changes",
-    single_item: "crashes",
-    columns: crashChangesGridTableColumns,
-    order_by: {},
-    where: {
-        city_id: "_eq: 22",
-    },
-    limit: 25,
-    offset: 0,
+  table: "atd_txdot_changes",
+  single_item: "crashes",
+  columns: crashChangesGridTableColumns,
+  order_by: {},
+  where: {
+    city_id: "_eq: 22",
+  },
+  limit: 25,
+  offset: 0,
 };
 
 let crashesQuery = new gqlAbstract(queryConf);
 
 const CrashesChanges = () => (
-    <GridTable
-    query={crashesQuery}
-    title={"Crash Record Changes"}
-/>
+  <GridTable query={crashesQuery} title={"Crash Record Changes"} />
 );
 
-export default withApollo(Crashes);
+export default withApollo(CrashesChanges);
