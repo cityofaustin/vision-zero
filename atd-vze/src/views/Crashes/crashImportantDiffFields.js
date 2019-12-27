f9d762adec8f01f77bf65a9094b22f17ea88f397
Sergio Garcia <sergiogcx@gmail.com>
2019-12-27 17:39:13 -0600

    Re-factored important fields

---
+++ atd-vze/src/views/Crashes/crashImportantDiffFields.js
@@ -0,0 +1,136 @@
+/**
+ * The Crash important difference fields is an object that contains the fields (as a key)
+ * and its attributes (as a value).
+ * @type {object}
+ */
+
+export const crashImportantDiffFields = {
+    "case_id": {
+        label: "Case ID",
+        type: "integer"
+    },
+    "crash_date": {
+        label: "Crash Date",
+        type: "date"
+    },
+    "crash_time": {
+        label: "Crash Time",
+        type: "time"
+    },
+    "crash_fatal_fl": {
+        label: "Crash Fatal Flag",
+        type: "flag"
+    },
+    "private_dr_fl": {
+        label: "Private Drive Flag",
+        type: "flag"
+    },
+    "rpt_outside_city_limit_fl": {
+        label: "Outside City Limit Flag",
+        type: "flag"
+    },
+    "rpt_latitude": {
+        label: "LABELHERE",
+        type: "double"
+    },
+    "rpt_longitude": {
+        label: "LABELHERE",
+        type: "double"
+    },
+    "rpt_hwy_num": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "rpt_block_num": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "rpt_street_name": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "rpt_sec_hwy_num": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "rpt_sec_block_num": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "rpt_sec_street_name": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "hwy_nbr": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "hwy_sys": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "hwy_sys_2": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "hwy_nbr_2": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "city_id": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "latitude": {
+        label: "LABELHERE",
+        type: "double"
+    },
+    "longitude": {
+        label: "LABELHERE",
+        type: "double"
+    },
+    "street_name": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "street_nbr": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "street_name_2": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "street_nbr_2": {
+        label: "LABELHERE",
+        type: "string"
+    },
+    "sus_serious_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "nonincap_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "poss_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "non_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "unkn_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "tot_injry_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+    "death_cnt": {
+        label: "LABELHERE",
+        type: "integer"
+    },
+};
