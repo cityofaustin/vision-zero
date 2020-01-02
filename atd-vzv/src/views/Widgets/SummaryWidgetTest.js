442b4d5bb288e337ae3ec76f9cedba5ff129ee3a
Mike Dilley <mike.dilley@gmail.com>
2020-01-02 10:08:26 -0600

    Style and size custom widget blockIcon

--- atd-vzv/src/views/Widgets/SummaryWidgetTest.js
+++ atd-vzv/src/views/Widgets/SummaryWidgetTest.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 // Create widget out of reactstrap Card component and styled with styled components
 // Props: FA icon, blockIcon bg color, header, mainText
 
-const SummaryWidgetTest = () => {
-  const blockIcon = () => <div class="block-icon"></div>;
+const SummaryWidgetTest = (icon, backgroundColor) => {
+  const StyledWidget = styled.div`
+    block-icon {
+      background-color: ${backgroundColor};
+    }
+  `;
+
+  const blockIcon = () => (
+    <div class="block-icon mr-3 float-left">
+      <FontAwesomeIcon icon={icon} size="2x" />
+    </div>
+  );
 
   return (
-    <Card>
-      <CardBody>
-        <CardTitle>Card title</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-      </CardBody>
-    </Card>
+    <StyledWidget>
+      <Card>
+        <CardBody>
+          {blockIcon()}
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+        </CardBody>
+      </Card>
+    </StyledWidget>
   );
 };
