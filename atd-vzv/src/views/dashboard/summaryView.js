39548a5583159a57b1cf6730640c14929e06fea1
Mike Dilley <mike.dilley@gmail.com>
2020-01-02 13:47:00 -0600

    Add years of life lost data

--- atd-vzv/src/views/dashboard/summaryView.js
+++ atd-vzv/src/views/dashboard/summaryView.js
@@ -4,6 +4,7 @@ import { colors } from "../../constants/colors";
 import moment from "moment";
 import axios from "axios";
 import {
+  faCar,
   faHourglass,
   faHeartbeat,
   faMedkit
@@ -16,10 +17,12 @@ const SummaryView = () => {
   const fatalitiesYearToDateUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=death_cnt > 0 AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
   const seriousInjuriesYearToDateUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where=sus_serious_injry_cnt > 0 AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
   const totalCrashesUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.json?$where= crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
+  const yearsOfLifeLostUrl = `https://data.austintexas.gov/resource/xecs-rpy9.json?$where=prsn_injry_sev_id = '4' AND crash_date between '${thisYear}-01-01T00:00:00' and '${today}T23:59:59'`;
 
   const [fatalities, setFatalities] = useState("-");
   const [seriousInjuries, setSeriousInjuries] = useState("-");
   const [totalCrashes, setTotalCrashes] = useState("-");
+  const [yearsOfLifeLost, setYearsOfLifeLost] = useState("-");
 
   const calculateTotalFatalities = data => {
     let total = 0;
@@ -41,6 +44,23 @@ const SummaryView = () => {
     return total;
   };
 
+  const getYearsOfLifeLost = fatalityData => {
+    // Assume 75 year life expectancy,
+    // Find the difference between person.prsn_age & 75
+    // Sum over the list of ppl with .reduce
+    return fatalityData.reduce((accumulator, fatalityRecord) => {
+      let years = 0;
+      if (fatalityRecord.prsn_age !== undefined) {
+        let yearsLifeLost = 75 - Number(fatalityRecord.prsn_age);
+        // What if the person is older than 75?
+        // For now, so we don't have negative numbers,
+        // Assume years of life lost is 0
+        years = yearsLifeLost < 0 ? 0 : yearsLifeLost;
+      }
+      return accumulator + years;
+    }, 0); // start with a count at 0 years
+  };
+
   useEffect(() => {
     // Fetch year-to-date records
     axios.get(fatalitiesYearToDateUrl).then(res => {
@@ -54,7 +74,16 @@ const SummaryView = () => {
     axios.get(totalCrashesUrl).then(res => {
       setTotalCrashes(calculateTotalCrashes(res));
     });
-  }, [fatalitiesYearToDateUrl, seriousInjuriesYearToDateUrl, totalCrashesUrl]);
+
+    axios.get(yearsOfLifeLostUrl).then(res => {
+      setYearsOfLifeLost(getYearsOfLifeLost(res.data));
+    });
+  }, [
+    fatalitiesYearToDateUrl,
+    seriousInjuriesYearToDateUrl,
+    totalCrashesUrl,
+    yearsOfLifeLostUrl
+  ]);
 
   return (
     <Row>
@@ -78,14 +107,14 @@ const SummaryView = () => {
         <SummaryWidgetTest
           header={`Total Crashes in ${thisYear}`}
           mainText={totalCrashes.toString()}
-          icon={faHourglass}
+          icon={faCar}
           backgroundColor={colors.info}
         />
       </Col>
       <Col sm="auto">
         <SummaryWidgetTest
-          header={`Test 4`}
-          mainText={"Years of Life Lost"}
+          header={`Years of Life Lost in ${thisYear}`}
+          mainText={yearsOfLifeLost.toString()}
           icon={faHourglass}
           backgroundColor={colors.info}
         />
