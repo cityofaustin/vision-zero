fd44439b558a30303bd64c035d4fc8b6185aa0ef
Mike Dilley <mike.dilley@gmail.com>
2020-01-05 23:11:03 -0600

    Pull branch 545 changes, refactor Navbar in Header

--- atd-vzv/src/views/nav/header.js
+++ atd-vzv/src/views/nav/header.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { A } from "hookrouter";
 
-import { Container, Navbar, Button, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Container,
+  Navbar,
+  Button,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler
+} from "reactstrap";
 import styled from "styled-components";
 import { responsive } from "../../constants/responsive";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -13,54 +22,61 @@ const StyledNavbar = styled.div`
       display: none;
     }
   }
+
+  .nav-button {
+    /* Set width to keep buttons equal width */
+    width: 140px;
+  }
 `;
 
-const Header = ({ toggleSidebar }) => (
-  <Container fluid>
-    <StyledNavbar>
-      <Navbar
-        color="light"
-        light
-        className="navbar shadow-sm p-3 mb-4 bg-white rounded"
-        expand="xs"
-      >
-        <Button
-          className="mr-2 collapse-toggle"
-          color="info"
-          onClick={toggleSidebar}
+const navConfig = [
+  {
+    title: "Summary",
+    url: "/summary"
+  },
+  { title: "Map", url: "/map" },
+  { title: "Engineering", url: "/engineering" },
+  { title: "Enforcement", url: "/enforcement" },
+  { title: "Education", url: "/education" }
+];
+
+const Header = ({ toggleSidebar }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Container fluid>
+      <StyledNavbar>
+        <Navbar
+          color="light"
+          light
+          className="navbar shadow-sm p-3 mb-4 bg-white rounded"
+          expand="md"
         >
-          <FontAwesomeIcon icon={faBars} />
-        </Button>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink tag={A} href="/summary">
-              <Button>Summary</Button>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={A} href="/map">
-              <Button>Map</Button>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={A} href="/engineering">
-              <Button>Engineering</Button>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={A} href="/enforcement">
-              <Button>Enforcement</Button>
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={A} href="/education">
-              <Button>Education</Button>
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-    </StyledNavbar>
-  </Container>
-);
+          <Button
+            className="mr-2 collapse-toggle"
+            color="info"
+            onClick={toggleSidebar}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </Button>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              {navConfig.map((config, i) => (
+                <NavItem key={i}>
+                  <NavLink tag={A} href={config.url}>
+                    <Button className="nav-button">{config.title}</Button>
+                  </NavLink>
+                </NavItem>
+              ))}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </StyledNavbar>
+    </Container>
+  );
+};
 
 export default Header;
