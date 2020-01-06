632210b6465dbc4ebfce16f68c2b53353b8be636
Mike Dilley <mike.dilley@gmail.com>
2020-01-06 14:06:25 -0600

    Restore files

---
+++ atd-vzv/src/views/nav/SideDrawer.js
@@ -0,0 +1,107 @@
+import React from "react";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import styled from "styled-components";
+import { drawer } from "../../constants/drawer";
+import { colors } from "../../constants/colors";
+
+const drawerWidth = drawer.width;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("md")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background: colors.dark,
+    color: colors.light,
+    border: 0
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}));
+
+const StyledDrawerHeader = styled.div`
+  background-color: ${colors.light};
+  color: ${colors.dark};
+  padding: 20px;
+  height: ${drawer.height}px;
+
+  .logo {
+    position: absolute;
+    width: ${drawer.width}px;
+    left: 0;
+    top: 35px;
+  }
+`;
+
+const SideDrawer = ({ toggle, isOpen }) => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const drawerContent = (
+    <div className="side-menu">
+      <StyledDrawerHeader>
+        <img
+          className="logo"
+          src="vz_logo.png"
+          alt="Vision Zero Austin Logo"
+        ></img>
+      </StyledDrawerHeader>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden mdUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={isOpen}
+            onClose={toggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawerContent}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawerContent}
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  );
+};
+
+export default SideDrawer;
