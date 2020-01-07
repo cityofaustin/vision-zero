23676e0097ab255d4a2e38764b0bf54b9716f1f2
Mike Dilley <mike.dilley@gmail.com>
2020-01-07 13:56:42 -0600

    Add mode filters to map

--- atd-vzv/src/views/map/Map.js
+++ atd-vzv/src/views/map/Map.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { StoreContext } from "../../utils/store";
 import ReactMapGL, { Source, Layer } from "react-map-gl";
 import { dataLayer } from "./map-style";
 import axios from "axios";
@@ -30,6 +31,10 @@ const Map = () => {
   const [mapData, setMapData] = useState("");
   const [hoveredFeature, setHoveredFeature] = useState(null);
 
+  const {
+    mapFilters: [filters]
+  } = React.useContext(StoreContext);
+
   // Fetch crash data
   useEffect(() => {
     const apiUrl =
@@ -38,6 +43,24 @@ const Map = () => {
       setMapData(res.data);
     });
   }, []);
+
+  // Set map filters and fetch data
+  useEffect(() => {
+    const createMapDataUrl = filters => {
+      let whereFilterString = "";
+      filters.forEach(filter => {
+        if (filter.type === "where") {
+          whereFilterString += ` ${filter.operator} ${filter.syntax}`;
+        }
+      });
+      const apiUrl = `https://data.austintexas.gov/resource/y2wy-tgr5.geojson?$limit=1000&$where=crash_date between '2019-01-01T00:00:00' and '2019-12-07T23:59:59' ${whereFilterString}`;
+      axios.get(apiUrl).then(res => {
+        setMapData(res.data);
+      });
+    };
+
+    createMapDataUrl(filters);
+  }, [filters, setMapData]);
 
   const _onViewportChange = viewport => setViewport(viewport);
 
