56ac61eebbafa5f38a4f29700bd8a7acb5352146
Mike Dilley <mike.dilley@gmail.com>
2020-01-13 13:09:20 -0600

    Add date picker and style it

--- atd-vzv/src/views/nav/SideMapControl.js
+++ atd-vzv/src/views/nav/SideMapControl.js
@@ -4,6 +4,7 @@ import { StoreContext } from "../../utils/store";
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
 import { ButtonGroup, Button, Card, Label } from "reactstrap";
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faWalking,
@@ -26,7 +27,50 @@ const StyledCard = styled.div`
   }
 
   .card-body {
-    background-color: ${colors.white};
+    background: ${colors.white};
+  }
+
+  /* Make the parent container flex to keep calendar within Filter card */
+  .react-daterange-picker {
+    display: flex;
+  }
+
+  /* Set flex and give width to contain picker in card */
+  .react-daterange-picker__wrapper {
+    flex: 1 0 auto;
+    width: 200px;
+    /* Bootstrapify */
+    border: 1px solid ${colors.info};
+    border-radius: 5px;
+    color: ${colors.info};
+    height: 32px;
+    text-align: center;
+
+    /* Set flex and give width to contain picker in card and center dates */
+    .react-daterange-picker__inputGroup {
+      flex: 1 0 auto;
+      justify-content: center;
+      min-width: 50px;
+    }
+
+    /* Color x and calendar icons */
+    svg {
+      stroke: ${colors.info};
+    }
+  }
+
+  /* Bootstrapify and align colors */
+  .react-calendar {
+    font-size: 1em;
+    background: ${colors.white};
+    border: 1px solid ${colors.info};
+    border-radius: 5px;
+    z-index: 1;
+    width: 350px;
+
+    .react-calendar__tile--active {
+      background: ${colors.info};
+    }
   }
 `;
 
@@ -162,6 +206,7 @@ const SideMapControl = () => {
             ))}
           </ButtonGroup>
         ))}
+        <DateRangePicker className="date-picker" />
       </Card>
     </StyledCard>
   );
