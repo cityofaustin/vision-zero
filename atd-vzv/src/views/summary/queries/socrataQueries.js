15ace8ba40ec6b94f1e0c8b761d54b5ab5b77527
Mike Dilley <mike.dilley@gmail.com>
2020-03-02 14:08:47 -0600

    Add env check to set data endpoints

--- atd-vzv/src/views/summary/queries/socrataQueries.js
+++ atd-vzv/src/views/summary/queries/socrataQueries.js
@@ -1,3 +1,8 @@
-export const crashEndpointUrl = `https://data.austintexas.gov/resource/3aut-fhzp.json`;
-export const crashGeoJSONEndpointUrl = `https://data.austintexas.gov/resource/3aut-fhzp.geojson`;
-export const demographicsEndpointUrl = `https://data.austintexas.gov/resource/xecs-rpy9.json`;
+import { isDevelopment } from "../../../constants/nav";
+
+const crashDatasetID = isDevelopment ? "3aut-fhzp" : "y2wy-tgr5";
+const demographicsDatasetID = isDevelopment ? "v3x4-fjgm" : "xecs-rpy9";
+
+export const crashEndpointUrl = `https://data.austintexas.gov/resource/${crashDatasetID}.json`;
+export const crashGeoJSONEndpointUrl = `https://data.austintexas.gov/resource/${crashDatasetID}.geojson`;
+export const demographicsEndpointUrl = `https://data.austintexas.gov/resource/${demographicsDatasetID}.json`;
