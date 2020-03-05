ab179156f34cc20fa2839f08620fc539008b43ce
Sergio Garcia <sergiogcx@gmail.com>
2020-03-05 16:11:16 -0600

    Added AWS CircleCI Helper

---
+++ .circleci/aws-helper.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Determine work branch
+case "${CIRCLE_BRANCH}" in
+  "production")
+    export WORKING_STAGE="production";
+    ;;
+
+  "master")
+    export WORKING_STAGE="staging";
+    ;;
+  *)
+    unset WORKING_STAGE;
+    echo "We can only deploy master or production.";
+    exit 1;
+    ;;
+esac
+
+# Deploys API to AWS
+function update_cr3_api {
+    if [[ "${WORKING_STAGE}" == "" ]]; then
+        echo "No working stage could be determined."
+        exit 1;
+    fi;
+
+    cd "atd-cr3-api";
+    pip install -r requirements.txt;
+    echo $ZAPPA_SETTINGS > zappa_settings.json;
+    zappa update $WORKING_STAGE;
+}
