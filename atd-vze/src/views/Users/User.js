461d472808f4809ecef273d4a16d3762a163c3b0
Mike Dilley <mike.dilley@gmail.com>
2020-03-06 13:00:54 -0600

    Fetch user data and display primitive data types

--- atd-vze/src/views/Users/User.js
+++ atd-vze/src/views/Users/User.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import {
   Button,
   Card,
@@ -10,70 +11,69 @@ import {
   Table,
 } from "reactstrap";
 
-import usersData from "./UsersData";
+const User = () => {
+  const token = window.localStorage.getItem("id_token");
+  const { id } = useParams();
 
-class User extends Component {
-  render() {
-    const user = usersData.find(
-      user => user.id.toString() === this.props.match.params.id
-    );
+  const [user, setUser] = useState(null);
 
-    const userDetails = user
-      ? Object.entries(user).filter(entry => entry !== "id")
-      : [
-          [
-            "id",
-            <span>
-              <i className="text-muted icon-ban"></i> Not found
-            </span>,
-          ],
-        ];
+  useEffect(() => {
+    const endpoint = `${process.env.REACT_APP_CR3_API_DOMAIN}/user/get_user/${id}`;
+    axios
+      .get(endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(res => {
+        setUser(res.data);
+      });
+  }, [token]);
 
-    return (
-      <div className="animated fadeIn">
-        <Row>
-          <Col xl={6}>
-            <Card>
-              <CardHeader>
-                <strong>
-                  <i className="icon-info pr-1"></i>User ID:{" "}
-                  {this.props.match.params.id}
-                </strong>
-              </CardHeader>
-              <CardBody>
-                <Row className="align-items-center mb-3">
-                  <Col col="6" sm="4" md="2" xl className="mb-xl-0">
-                    <Link
-                      to={`/users/edit/${this.props.match.params.id}`}
-                      className="link"
-                    >
-                      <Button color="primary">
-                        <i className="fa fa-user-plus"></i> Edit User
-                      </Button>
-                    </Link>
-                  </Col>
-                </Row>
-                <Table responsive striped hover>
-                  <tbody>
-                    {userDetails.map(([key, value]) => {
-                      return (
-                        <tr key={key}>
-                          <td>{`${key}:`}</td>
-                          <td>
-                            <strong>{value}</strong>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="animated fadeIn">
+      <Row>
+        <Col xl={6}>
+          <Card>
+            <CardHeader>
+              <strong>
+                <i className="icon-info pr-1"></i>User ID: {id}
+              </strong>
+            </CardHeader>
+            <CardBody>
+              <Row className="align-items-center mb-3">
+                <Col col="6" sm="4" md="2" xl className="mb-xl-0">
+                  <Link to={`/users/edit/${id}`} className="link">
+                    <Button color="primary">
+                      <i className="fa fa-user-plus"></i> Edit User
+                    </Button>
+                  </Link>
+                </Col>
+              </Row>
+              <Table responsive striped hover>
+                <tbody>
+                  {!!user &&
+                    Object.entries(user).map(([key, value]) => (
+                      <tr key={key}>
+                        <td>{`${key}:`}</td>
+                        <td>
+                          <strong>
+                            {key !== "identities" &&
+                              key !== "app_metadata" &&
+                              key !== "user_metadata" &&
+                              value}
+                          </strong>
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </Table>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default User;
