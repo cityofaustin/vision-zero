5a6eb54fa7657dad587c5427427b0eea76134765
Sergio Garcia <sergiogcx@gmail.com>
2020-03-31 10:29:55 -0500

    Added Changes table to production

---
+++ atd-vzd/schema/atd_txdot_changes.sql
@@ -0,0 +1,20 @@
+-- Changes Table
+
+create table atd_txdot_changes
+(
+	change_id serial not null
+		constraint atd_txdot_changes_pk
+			primary key,
+	record_id integer not null,
+	record_type varchar(32) not null,
+	record_json json not null,
+	update_timestamp timestamp default now(),
+	created_timestamp timestamp default now(),
+	updated_by varchar(128) default 'System'::character varying,
+	status_id integer default 0 not null,
+	affected_columns text,
+	constraint atd_txdot_changes_unique
+		unique (record_id, record_type, status_id)
+);
+
+alter table atd_txdot_changes owner to atd_vz_data;
