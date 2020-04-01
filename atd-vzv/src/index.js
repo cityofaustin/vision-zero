6e9089ffc1bb3fe61c59acd62cb372cab6f1dbb7
Mike Dilley <mike.dilley@gmail.com>
2020-04-01 17:31:44 -0500

    Get styles working and add TODO

--- atd-vzv/src/index.js
+++ atd-vzv/src/index.js
@@ -7,6 +7,64 @@ import App from "./App";
 import StoreProvider from "./utils/store";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import ThemedStyleSheet from "react-with-styles/lib/ThemedStyleSheet";
+import aphroditeInterface from "react-with-styles-interface-aphrodite";
+import DefaultTheme from "react-dates/lib/theme/DefaultTheme";
+import { colors } from "./constants/colors";
+
+// TODO: Move to SideMapControlDateRange component
+export const vzTheme = {
+  reactDates: {
+    ...DefaultTheme.reactDates,
+    zIndex: 1301,
+    border: {
+      ...DefaultTheme.reactDates.border,
+      input: {
+        ...DefaultTheme.reactDates.border.input,
+        borderBottomFocused: `2px solid ${colors.infoDark}`
+      }
+    },
+    color: {
+      ...DefaultTheme.reactDates.color,
+      selected: {
+        backgroundColor: `${colors.infoDark}`,
+        backgroundColor_active: `${colors.infoDark}`,
+        backgroundColor_hover: `${colors.infoDark}`,
+        borderColor: `${colors.light}`,
+        borderColor_active: `${colors.light}`,
+        borderColor_hover: `${colors.light}`,
+        color: `${colors.light}`,
+        color_active: `${colors.light}`,
+        color_hover: `${colors.light}`
+      },
+      selectedSpan: {
+        backgroundColor: `${colors.info}`,
+        backgroundColor_active: `${colors.info}`,
+        backgroundColor_hover: `${colors.infoDark}`,
+        borderColor: `${colors.light}`,
+        borderColor_active: `${colors.light}`,
+        borderColor_hover: `${colors.light}`,
+        color: `${colors.light}`,
+        color_active: `${colors.light}`,
+        color_hover: `${colors.light}`
+      },
+      hoveredSpan: {
+        backgroundColor: `${colors.secondary}`,
+        backgroundColor_active: `${colors.infoDark}`,
+        backgroundColor_hover: `${colors.infoDark}`,
+        borderColor: `${colors.light}`,
+        borderColor_active: `${colors.light}`,
+        borderColor_hover: `${colors.light}`,
+        color: `${colors.dark}`,
+        color_active: `${colors.light}`,
+        color_hover: `${colors.light}`
+      }
+    }
+  }
+};
+
+ThemedStyleSheet.registerTheme(vzTheme);
+ThemedStyleSheet.registerInterface(aphroditeInterface);
 
 // Account for /viewer/ basepath in all routing
 setBasepath(basepath);
--- atd-vzv/src/views/nav/SideMapControlDateRange.js
+++ atd-vzv/src/views/nav/SideMapControlDateRange.js
@@ -1,46 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { StoreContext } from "../../utils/store";
 import { DateRangePicker } from "react-dates";
-import {
-  dataStartDate,
-  dataEndDate,
-  isValidSocrataDate
-} from "../../constants/time";
-
-import "react-dates/initialize";
-import "react-dates/lib/css/_datepicker.css";
-import styled from "styled-components";
-import { colors } from "../../constants/colors";
+import { dataStartDate, dataEndDate } from "../../constants/time";
 
 const SideMapControlDateRange = () => {
-  const StyledDatePicker = styled.div`
-    .CalendarDay__default {
-      background: ${colors.light};
-      border: ${colors.light};
-    }
-
-    .CalendarDay__selected_start {
-      background: ${colors.infoDark};
-      border: ${colors.infoDark};
-    }
-
-    .CalendarDay__selected_span {
-      background: ${colors.info};
-      border: ${colors.info};
-    }
-
-    .CalendarDay__selected_end {
-      background: ${colors.infoDark};
-      border: ${colors.infoDark};
-    }
-
-    .DateRangePicker_picker {
-      position: absolute !important;
-      overflow-x: visible !important;
-      z-index: 4;
-    }
-  `;
-
   const [focused, setFocused] = useState(null);
   const [start, setStart] = useState(dataStartDate);
   const [end, setEnd] = useState(dataEndDate);
@@ -65,10 +28,7 @@ const SideMapControlDateRange = () => {
 
     endDate =
       // If endDate is not null and after n year window, set to dataEndDate
-      (!!endDate &&
-        isValidSocrataDate(startDate) &&
-        endDate.isAfter(dataEndDate, "day") &&
-        dataEndDate) ||
+      (!!endDate && endDate.isAfter(dataEndDate, "day") && dataEndDate) ||
       endDate;
 
     setStart(startDate);
@@ -80,23 +40,22 @@ const SideMapControlDateRange = () => {
     date.isBefore(dataStartDate, "day") || date.isAfter(dataEndDate, "day");
 
   return (
-    <StyledDatePicker>
-      <DateRangePicker
-        startDateId="start_date" // PropTypes.string.isRequired,
-        endDateId="end_date" // PropTypes.string.isRequired,
-        startDate={start} // momentPropTypes.momentObj or null,
-        endDate={end} // momentPropTypes.momentObj or null,
-        onDatesChange={handleDateChange} // PropTypes.func.isRequired,
-        focusedInput={focused} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-        onFocusChange={focusedInput => setFocused(focusedInput)} // PropTypes.func.isRequired,
-        minDate={dataStartDate}
-        maxDate={dataEndDate}
-        small
-        orientation="vertical"
-        isOutsideRange={() => false} // Enable past dates
-        isDayBlocked={isOutsideDateLimits} // Grey out dates
-      />
-    </StyledDatePicker>
+    <DateRangePicker
+      startDateId="start_date" // PropTypes.string.isRequired,
+      endDateId="end_date" // PropTypes.string.isRequired,
+      startDate={start} // momentPropTypes.momentObj or null,
+      endDate={end} // momentPropTypes.momentObj or null,
+      onDatesChange={handleDateChange} // PropTypes.func.isRequired,
+      focusedInput={focused} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+      onFocusChange={focusedInput => setFocused(focusedInput)} // PropTypes.func.isRequired,
+      minDate={dataStartDate}
+      maxDate={dataEndDate}
+      appendToBody
+      small
+      orientation="vertical"
+      isOutsideRange={() => false} // Enable past dates
+      isDayBlocked={isOutsideDateLimits} // Grey out dates
+    />
   );
 };
 
