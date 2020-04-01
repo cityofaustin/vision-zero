74cb8bac7c1c86617515d3c4a75e20041c8c8211
Mike Dilley <mike.dilley@gmail.com>
2020-04-01 13:53:07 -0500

    Auto-set date entries that fall outside range to limits

--- atd-vzv/src/views/nav/SideMapControlDateRange.js
+++ atd-vzv/src/views/nav/SideMapControlDateRange.js
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { StoreContext } from "../../utils/store";
 import { DateRangePicker } from "react-dates";
-import { dataStartDate, dataEndDate } from "../../constants/time";
+import {
+  dataStartDate,
+  dataEndDate,
+  isValidSocrataDate
+} from "../../constants/time";
 
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
@@ -29,13 +33,25 @@ const SideMapControlDateRange = () => {
       background: ${colors.infoDark};
       border: ${colors.infoDark};
     }
+
+    .DateRangePicker_picker {
+      position: absolute !important;
+      overflow-x: visible !important;
+      z-index: 4;
+    }
   `;
 
   const [focused, setFocused] = useState(null);
+  const [start, setStart] = useState(dataStartDate);
+  const [end, setEnd] = useState(dataEndDate);
 
   const {
     mapDateRange: [mapDate, setMapDate]
   } = React.useContext(StoreContext);
+
+  useEffect(() => {
+    setMapDate({ start, end });
+  }, [start, end, setMapDate]);
 
   const handleDateChange = dates => {
     let { startDate, endDate } = dates;
@@ -49,10 +65,14 @@ const SideMapControlDateRange = () => {
 
     endDate =
       // If endDate is not null and after n year window, set to dataEndDate
-      (!!endDate && endDate.isAfter(dataEndDate, "day") && dataEndDate) ||
+      (!!endDate &&
+        isValidSocrataDate(startDate) &&
+        endDate.isAfter(dataEndDate, "day") &&
+        dataEndDate) ||
       endDate;
-    // Issue happens when startDate does not update by itself
-    setMapDate({ start: startDate, end: endDate });
+
+    setStart(startDate);
+    setEnd(endDate);
   };
 
   // Check if date is outside n year rolling window
@@ -64,8 +84,8 @@ const SideMapControlDateRange = () => {
       <DateRangePicker
         startDateId="start_date" // PropTypes.string.isRequired,
         endDateId="end_date" // PropTypes.string.isRequired,
-        startDate={mapDate.start} // momentPropTypes.momentObj or null,
-        endDate={mapDate.end} // momentPropTypes.momentObj or null,
+        startDate={start} // momentPropTypes.momentObj or null,
+        endDate={end} // momentPropTypes.momentObj or null,
         onDatesChange={handleDateChange} // PropTypes.func.isRequired,
         focusedInput={focused} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
         onFocusChange={focusedInput => setFocused(focusedInput)} // PropTypes.func.isRequired,
@@ -73,7 +93,7 @@ const SideMapControlDateRange = () => {
         maxDate={dataEndDate}
         small
         orientation="vertical"
-        isOutsideRange={isOutsideDateLimits} // Enable past dates
+        isOutsideRange={() => false} // Enable past dates
         isDayBlocked={isOutsideDateLimits} // Grey out dates
       />
     </StyledDatePicker>
