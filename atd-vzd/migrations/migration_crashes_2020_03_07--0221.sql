f1c412fbd67c0448fc82b605bad2aa5211426e50
Sergio Garcia <sergiogcx@gmail.com>
2020-04-07 14:24:20 -0500

    Updated DB Migration

---
+++ atd-vzd/migrations/migration_crashes_2020_03_07--0221.sql
@@ -0,0 +1,7 @@
+-- We need to drop this index Hasura will not see
+drop index idx_atd_txdot_crashes_crash_id;
+
+-- And add a primary key constraint that will be detected by Hasura
+alter table atd_txdot_crashes
+	add constraint atd_txdot_crashes_pkey
+		primary key (crash_id);
