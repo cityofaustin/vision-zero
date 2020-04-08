bef59a34868a80ba278e443b31c13194e8791251
Mike Dilley <mike.dilley@gmail.com>
2020-04-08 17:07:12 -0500

    Reorganize map styles and summary styles in Content component

--- atd-vzv/src/constants/responsive.js
+++ atd-vzv/src/constants/responsive.js
@@ -1,5 +1,6 @@
 // Use to unify Bootstrap breakpoints with media queries
 export const responsive = {
+  bootstrapLarge: 992,
   bootstrapMedium: 768,
   // Use for media queries with min-width defined to avoid style conflicts
   bootstrapMediumMin: 769,
@@ -7,3 +8,6 @@ export const responsive = {
 };
 
 export const isMobile = () => window.innerWidth < responsive.bootstrapMedium;
+
+export const shouldContentContainerBeFluid = () =>
+  window.innerWidth < responsive.bootstrapLarge;
--- atd-vzv/src/views/content/Content.js
+++ atd-vzv/src/views/content/Content.js
@@ -7,7 +7,10 @@ import NotFound from "../NotFound/NotFound";
 import { Container } from "reactstrap";
 import styled from "styled-components";
 import { drawer } from "../../constants/drawer";
-import { responsive } from "../../constants/responsive";
+import {
+  responsive,
+  shouldContentContainerBeFluid
+} from "../../constants/responsive";
 
 const Content = () => {
   const routeResult = useRoutes(routes);
@@ -20,35 +23,39 @@ const Content = () => {
 
   // Map view needs to consider header height and have no overflow scroll to fill view
   // Non-map views need to scroll to show all content
-  const mapStyleExceptions = `
-    height: ${
-      currentPath === "/map"
-        ? `calc(100vh - ${drawer.headerHeight}px)`
-        : `100vh`
-    };
-    ${currentPath !== "/map" && `overflow-y: scroll;`}
+  const mapStyles = `
+    height: calc(100vh - ${drawer.headerHeight}px);
+    width: calc(100vw - ${drawer.width}px);
+    padding: 0px;
+  `;
+
+  const summaryStyles = `
+    width: 100vw;
+    height: 100vh;
+    overflow-y: scroll; 
   `;
 
   const StyledContent = styled.div`
     .content {
-      padding: 0px;
       top: ${drawer.headerHeight}px;
-      width: 100vw;
-      ${mapStyleExceptions}
+      ${currentPath === "/" && summaryStyles}
+      ${currentPath === "/map" && mapStyles}
     }
 
     /* Fill space left behind by SideDrawer on mobile */
     @media only screen and (max-width: ${responsive.bootstrapMedium}px) {
       .content {
         width: 100vw;
-        ${mapStyleExceptions}
       }
     }
   `;
 
   return (
-    <StyledContent>
-      <Container fluid={currentPath !== "/"} className="content">
+    <StyledContent className={!shouldContentContainerBeFluid() && `mx-auto`}>
+      <Container
+        fluid={shouldContentContainerBeFluid() || currentPath !== "/"}
+        className="content"
+      >
         <Header />
         {routeResult || <NotFound />}
       </Container>
