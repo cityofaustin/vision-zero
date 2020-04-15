ebcbd005e956ba0258d5a88c26c30dbb2ae993d9
Sergio Garcia <sergiogcx@gmail.com>
2020-04-15 13:55:41 -0500

    Added view for Inconsistent Crashes

---
+++ atd-vze/src/views/Reports/CrashesInconsistent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { withApollo } from "react-apollo";
+
+import GridTable from "../../Components/GridTable";
+import gqlAbstract from "../../queries/gqlAbstract";
+
+import {
+  crashGridTableColumns,
+} from "./crashesInconsistentTableParameters";
+
+// Our initial query configuration
+let queryConf = {
+  table: "view_crashes_inconsistent_numbers",
+  single_item: "crashes",
+  showDateRange: false,
+  columns: crashGridTableColumns,
+  where: {},
+  limit: 25,
+  offset: 0,
+};
+
+let exportFields = `
+ atc_crash_id
+ atc_death_cnt
+ atu_death_cnt
+ atpp_death_cnt
+ atp_death_cnt
+ sus_serious_injry_cnt
+ atu_sus_serious_injry_cnt
+ atpp_sus_serious_injry_cnt
+ atp_sus_serious_injry_cnt
+`
+
+let crashesQuery = new gqlAbstract(queryConf);
+
+const Crashes = () => (
+  <GridTable
+    query={crashesQuery}
+    title={"Inconsistent Crash Numbers"}
+    filters={null}
+    columnsToExport={exportFields}
+  />
+);
+
+export default withApollo(Crashes);
