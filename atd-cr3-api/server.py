255dc628fb1b72359b17245e50d5af0fefb3fb43
Mateo Clarke <code@mateoclarke.com>
2020-05-06 17:34:19 -0500

    Update webhook for location association

--- atd-cr3-api/server.py
+++ atd-cr3-api/server.py
@@ -13,6 +13,7 @@ from dotenv import load_dotenv, find_dotenv
 from os import environ as env
 from functools import wraps
 from six.moves.urllib.request import urlopen
+from string import Template
 
 from flask import Flask, request, redirect, jsonify, _request_ctx_stack, abort
 from flask_cors import cross_origin
@@ -39,6 +40,10 @@ AWS_S3_KEY = os.getenv("AWS_S3_KEY", "")
 AWS_S3_SECRET = os.getenv("AWS_S3_SECRET", "")
 AWS_S3_CR3_LOCATION = os.getenv("AWS_S3_CR3_LOCATION", "")
 AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET", "")
+
+# Hasura Config
+HASURA_ADMIN_SECRET = os.getenv("HASURA_ADMIN_SECRET", "")
+HASURA_ENDPOINT = os.getenv("HASURA_ENDPOINT", "")
 
 
 def get_api_token():
@@ -414,7 +419,64 @@ def user_delete_user(id):
 
 @APP.route("/crashes/associate_location/", methods=["PUT", "POST"])
 def associate_location():
-    return jsonify({"hello": "world"})
+    # Get data/crash_id from Hasura Event request
+    data = request.get_json(force=True)
+    crash_id = data['event']['data']['old']['crash_id']
+    old_location_id = data['event']['data']['old']['location_id']
+
+    # Prep Hasura query
+    HEADERS = {
+        'Content-Type': 'application/json',
+        'X-Hasura-Admin-Secret': HASURA_ADMIN_SECRET,
+    }
+
+    find_location_query = Template("""
+        query getLocationAssociation {
+            find_location_for_cr3_collision(args: {id: $crash_id}){
+                location_id
+            }
+        }
+    """).substitute(crash_id=crash_id)
+
+    json_body = {'query': find_location_query}
+
+    # Make request to Hasura expecting a Location Record in the response
+    response = requests.post(
+        HASURA_ENDPOINT, data=json.dumps(json_body), headers=HEADERS)
+
+    print(response.json())
+    new_location_id = response.json(
+    )['data']['find_location_for_cr3_collision'][0]['location_id']
+
+    if new_location_id == old_location_id:
+        return {
+            "statusCode": 200,
+            "body": json.dumps({'message': 'Success. No Location ID update required'})
+        }
+    else:
+        # Prep the mutation
+        update_location_mutation = """
+            mutation updateCrashLocationID($crashId: Int!, $locationId: String!) {
+                update_atd_txdot_crashes(where: {crash_id: {_eq: $crashId}}, _set: {location_id: $locationId}) {
+                    affected_rows
+                }
+            }
+        """
+
+        query_variables = {'crashId': crash_id, 'locationId': new_location_id}
+        mutation_json_body = {
+            'query': update_location_mutation, 'variables': query_variables}
+
+        # Execute the update
+        mutation_response = requests.post(
+            HASURA_ENDPOINT, data=json.dumps(mutation_json_body), headers=HEADERS)
+
+        print(mutation_response.json())
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({'message': 'Success. Location ID updated'})
+        }
 
 
 if __name__ == "__main__":
