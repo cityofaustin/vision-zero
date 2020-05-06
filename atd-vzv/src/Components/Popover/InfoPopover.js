134fa5dd6ab7da614638d4e69cb3ccf351b6c6d0
Mike Dilley <mike.dilley@gmail.com>
2020-05-06 17:24:58 -0500

    Add dynamic DOM selector for mobile and desktop tooltip

--- atd-vzv/src/Components/Popover/InfoPopover.js
+++ atd-vzv/src/Components/Popover/InfoPopover.js
@@ -6,55 +6,68 @@ import styled from "styled-components";
 const InfoPopover = ({ popoverTarget }) => {
   // TODO: Create custom hook to inject popover into DOM
   // TODO: Args - id of target, obj with text/icons/etc, action (hover, click, etc.)
+  const {
+    sidebarToggle: [isOpen],
+  } = React.useContext(StoreContext);
+
   const StyledPopover = styled.div`
     /* Set z-index higher than MUI Drawer default */
     z-index: 1400;
   `;
 
-  // TODO: Need to add popover to correct drawer
-  // isDrawerElement?
-  // Start at drawer level and render everything within permanent or temp drawer
-  // Line 147 and 157 in SideDrawer
+  const drawerId = isOpen ? "temporary-drawer" : "permanent-drawer";
+
+  const modifiers = {
+    preventOverflow: {
+      enabled: false,
+    },
+  };
+
+  const content = (
+    <>
+      <div className="mb-2">
+        Crash data is obtained from the Texas Department of Transportation
+        (TXDOT) Crash Record Information System (CRIS) database, which is
+        populated by Austin Police Department (APD) data and maintained by
+        TXDOT.
+      </div>
+      <div className="mb-2">
+        The geographic boundaries used to collect and display crash data in the
+        Vision Zero Viewer is the City of Austin city limit boundaries.
+        **explanation of why these numbers may be different from APD's
+        numbers.**
+      </div>
+      <div className="mb-2">
+        Crash data in Vision Zero Viewer is updated at the end of each month.{" "}
+      </div>
+      <div className="mb-2">
+        Crashes are mapped in Vision Zero Viewer using the lat/longs that have
+        been reported in the CRIS database. In some cases, the City of Austin
+        has manually updated locations based on.{" "}
+      </div>
+      <div className="mb-2">
+        Please note that the data and information on this website is for
+        informational purposes only. While we seek to provide accurate
+        information, please note that errors may be present and information
+        presented may not be complete.
+      </div>
+    </>
+  );
 
   return (
     <StyledPopover id={`${popoverTarget}-parent`}>
       <UncontrolledPopover
         placement="bottom"
-        target={popoverTarget}
+        target={() => document.querySelector(`#${drawerId} #${popoverTarget}`)}
         trigger="click" // Action here
         // Set popover to render within styled component so we can select/style it
-        container={() => document.getElementById(`${popoverTarget}-parent`)}
+        container={() =>
+          document.querySelector(`#${drawerId} #${popoverTarget}-parent`)
+        }
+        modifiers={modifiers}
       >
         <PopoverHeader>Traffic Crashes</PopoverHeader>
-        <PopoverBody>
-          <div className="mb-2">
-            Crash data is obtained from the Texas Department of Transportation
-            (TXDOT) Crash Record Information System (CRIS) database, which is
-            populated by Austin Police Department (APD) data and maintained by
-            TXDOT.
-          </div>
-          <div className="mb-2">
-            The geographic boundaries used to collect and display crash data in
-            the Vision Zero Viewer is the City of Austin city limit boundaries.
-            **explanation of why these numbers may be different from APD's
-            numbers.**
-          </div>
-          <div className="mb-2">
-            Crash data in Vision Zero Viewer is updated at the end of each
-            month.{" "}
-          </div>
-          <div className="mb-2">
-            Crashes are mapped in Vision Zero Viewer using the lat/longs that
-            have been reported in the CRIS database. In some cases, the City of
-            Austin has manually updated locations based on.{" "}
-          </div>
-          <div className="mb-2">
-            Please note that the data and information on this website is for
-            informational purposes only. While we seek to provide accurate
-            information, please note that errors may be present and information
-            presented may not be complete.
-          </div>
-        </PopoverBody>
+        <PopoverBody>{content}</PopoverBody>
       </UncontrolledPopover>
     </StyledPopover>
   );
