d77617f0ecb2a4ee428fa81791408fb652e7f273
Mateo Clarke <code@mateoclarke.com>
2020-05-18 01:19:23 -0500

    add data quality modal

---
+++ atd-vzv/src/views/summary/DataModal.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+const DataModal = () => {
+  const [modal, setModal] = useState(true);
+  const toggle = () => setModal(!modal);
+
+  return (
+    <Modal isOpen={modal} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Vision Zero Viewer</ModalHeader>
+      <ModalBody>
+        <h4>Data Quality Disclaimer</h4>
+        Please note that the information currently being displayed may be
+        outdated or inaccurate. Crash data obtained from the Texas Department of
+        Transportation (TxDOT) Crash Record Information System (CRIS) database
+        is currently being reprocessed. This process could affect fatality and
+        serious injury numbers and locations. Please refrain from performing any
+        critical data analysis at this time.
+      </ModalBody>
+      <ModalFooter>
+        <Button color="dark" onClick={toggle}>
+          Thanks
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default DataModal;
