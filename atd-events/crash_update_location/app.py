68e31de4027eeab1d74fc8387a3bba9a431ad0fc
Sergio Garcia <sergiogcx@gmail.com>
2020-05-27 11:30:42 -0500

    Adds hasura handler

--- atd-events/crash_update_location/app.py
+++ atd-events/crash_update_location/app.py
@@ -5,6 +5,102 @@
 import json
 import requests
 import time
+import os
+from string import Template
+
+HASURA_ADMIN_SECRET = os.getenv("HASURA_ADMIN_SECRET", "")
+HASURA_ENDPOINT = os.getenv("HASURA_ENDPOINT", "")
+HASURA_EVENT_API = os.getenv("HASURA_EVENT_API", "")
+
+
+def hasura_request(data):
+    """
+    Processes a location update event.
+    :param dict data: The json payload from Hasura
+    """
+    # Get data/crash_id from Hasura Event request
+    try:
+        crash_id = data["event"]["data"]["old"]["crash_id"]
+        old_location_id = data["event"]["data"]["old"]["location_id"]
+    except:
+        print(
+            json.dumps(
+                {
+                    "message": "Unable to parse request body to identify a Location Record"
+                }
+            )
+        )
+        exit(1)
+
+    # Prep Hasura query
+    HEADERS = {
+        "Content-Type": "application/json",
+        "X-Hasura-Admin-Secret": HASURA_ADMIN_SECRET,
+    }
+
+    find_location_query = Template(
+        """
+            query getLocationAssociation {
+                find_location_for_cr3_collision(args: {id: $crash_id}){
+                    location_id
+                }
+            }
+        """
+    ).substitute(crash_id=crash_id)
+
+    json_body = {"query": find_location_query}
+
+    # Make request to Hasura expecting a Location Record in the response
+    try:
+        response = requests.post(
+            HASURA_ENDPOINT, data=json.dumps(json_body), headers=HEADERS
+        )
+    except:
+        print(
+            json.dumps(
+                {
+                    "message": "Unable to parse request body to identify a Location Record"
+                }
+            )
+        )
+
+    new_location_id = response.json()["data"]["find_location_for_cr3_collision"][0][
+        "location_id"
+    ]
+
+    if new_location_id == old_location_id:
+        print(json.dumps({"message": "Success. No Location ID update required"}))
+    else:
+        # Prep the mutation
+        update_location_mutation = """
+                mutation updateCrashLocationID($crashId: Int!, $locationId: String!) {
+                    update_atd_txdot_crashes(where: {crash_id: {_eq: $crashId}}, _set: {location_id: $locationId}) {
+                        affected_rows
+                    }
+                }
+            """
+
+        query_variables = {"crashId": crash_id, "locationId": new_location_id}
+        mutation_json_body = {
+            "query": update_location_mutation,
+            "variables": query_variables,
+        }
+
+        # Execute the mutation
+        try:
+            mutation_response = requests.post(
+                HASURA_ENDPOINT, data=json.dumps(mutation_json_body), headers=HEADERS
+            )
+        except:
+            print(
+                json.dumps(
+                    {
+                        "message": "Unable to parse request body for location_id update"
+                    }
+                )
+            )
+
+        print(mutation_response.json())
 
 
 def handler(event, context):
@@ -13,9 +109,11 @@ def handler(event, context):
     :param dict event: One or many SQS messages
     :param dict context: Event context
     """
-    for record in event['Records']:
+    for record in event["Records"]:
         timeStr = time.ctime()
-        print('Current Timestamp : ', timeStr)
+        print("Current Timestamp : ", timeStr)
         print(json.dumps(record))
+        # Whenever we are ready to test:
+        # hasura_request(record)
         timeStr = time.ctime()
         print("Done executing: ", timeStr)
