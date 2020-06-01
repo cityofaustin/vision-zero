5768cff0d961be5add07bf7afec5003b747277a1
Mike Dilley <mike.dilley@gmail.com>
2020-06-01 11:51:44 -0500

    Refactor links and clean up CSS

--- atd-vzv/src/views/nav/Footer.js
+++ atd-vzv/src/views/nav/Footer.js
@@ -3,30 +3,23 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import styled from "styled-components";
 import { colors } from "../../constants/colors";
+import { responsive } from "../../constants/responsive";
 import logo from "./coa_seal_transparent_white.png";
 
 let pckg = require("../../../package.json");
 
 const Footer = () => {
   const StyledFooter = styled.div`
-    height: 280px;
-    background: ${colors.dark};
     color: ${colors.light};
     font-weight: bold;
 
-    .coa-logo {
-      position: relative;
-      left: 100px;
-      top: 45px;
-    }
-
     .link-table {
+      background: ${colors.dark};
       position: absolute;
       left: 0px;
       padding: 45px 40px 45px 25%;
       color: ${colors.light};
       font-size: 16px;
-      width: 100%;
     }
 
     .link-title {
@@ -38,10 +31,16 @@ const Footer = () => {
       padding: 10px 15px 10px 15px;
     }
 
+    .coa-seal {
+      z-index: 2;
+      position: relative;
+      left: 100px;
+      top: 45px;
+    }
+
     .version {
-      position: absolute;
-      right: 0px;
-      bottom: 0px;
+      font-weight: normal;
+      font-size: 14px;
     }
 
     a,
@@ -49,18 +48,17 @@ const Footer = () => {
       color: ${colors.light};
     }
 
-    @media only screen and (max-width: 990px) {
-      height: 334px;
-
-      .coa-logo {
+    /* Prevent links from overlapping CoA seal */
+    @media only screen and (max-width: ${responsive.bootstrapLarge}px) {
+      .coa-seal {
         position: relative;
         left: 40px;
         top: 45px;
       }
     }
 
-    @media only screen and (max-width: 768px) {
-      height: 450px;
+    /* Center CoA seal and links on mobile */
+    @media only screen and (max-width: ${responsive.bootstrapMedium}px) {
       text-align: center;
 
       .link-table {
@@ -68,7 +66,7 @@ const Footer = () => {
         margin: 0px auto;
       }
 
-      .coa-logo {
+      .coa-seal {
         position: relative;
         background: ${colors.dark};
         top: -20px;
@@ -80,47 +78,43 @@ const Footer = () => {
     }
   `;
 
-  // TODO: Breakpoint @ 768px where logo centers w/ circular border overlap, table centers
-  // TODO: Breakpoint for footer height increase below 1250px
+  const footerLinks = [
+    {
+      text: "Data",
+      url:
+        "https://data.austintexas.gov/Transportation-and-Mobility/-UNDER-CONSTRUCTION-Crash-Report-Data/y2wy-tgr5/data",
+    },
+    {
+      text: "Code",
+      url: "https://github.com/cityofaustin/atd-vz-data/tree/master/atd-vzv",
+    },
+    {
+      text: "Disclaimer",
+      url: "https://austintexas.gov/page/city-austin-open-data-terms-use",
+    },
+    { text: "Privacy", url: "https://www.austintexas.gov/page/privacy-policy" },
+    {
+      text: "Give feedback on Vision Zero Viewer",
+      url: "mailto:transportation.data@austintexas.gov",
+    },
+    { text: "Powered by Data & Technology Services" },
+    { text: <div className="version">v{pckg.version}</div> },
+  ];
 
   return (
     <StyledFooter>
       <Container fluid className="mt-5">
-        <img className="coa-logo float-left" height="100px" src={logo} />
+        <img className="coa-seal float-left" height="100px" src={logo} />
         <Row className="link-table">
           <Col xs="12" className="link-title">
             City of Austin Transportation Department
           </Col>
-          <Col xs="12" md="6" className="link">
-            <a href="https://data.austintexas.gov/Transportation-and-Mobility/-UNDER-CONSTRUCTION-Crash-Report-Data/y2wy-tgr5/data">
-              Data
-            </a>
-          </Col>
-          <Col xs="12" md="6" className="link">
-            <a href="https://github.com/cityofaustin/atd-vz-data/tree/master/atd-vzv">
-              Code
-            </a>
-          </Col>
-          <Col xs="12" md="6" className="link">
-            <a href="https://austintexas.gov/page/city-austin-open-data-terms-use">
-              Disclaimer
-            </a>
-          </Col>
-          <Col xs="12" md="6" className="link">
-            <a href="https://www.austintexas.gov/page/privacy-policy">
-              Privacy
-            </a>
-          </Col>
-          <Col xs="12" md="6" className="link">
-            <a href="mailto:transportation.data@austintexas.gov">
-              Give feedback on Vision Zero Viewer
-            </a>
-          </Col>
-          <Col xs="12" md="6" className="link">
-            Powered by Data & Technology Services
-          </Col>
+          {footerLinks.map((link) => (
+            <Col xs="12" md="6" className="link">
+              {link.url ? <a href={link.url}>{link.text}</a> : link.text}
+            </Col>
+          ))}
         </Row>
-        <div className="version">v{pckg.version}</div>
       </Container>
     </StyledFooter>
   );
