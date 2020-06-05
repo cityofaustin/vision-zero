6496803f3b98620f17fe7577809279bc62bd22ff
Mike Dilley <mike.dilley@gmail.com>
2020-06-05 11:52:02 -0500

    Create MapPolygonInfoBox component to display crash counts

---
+++ atd-vzv/src/views/map/MapPolygonInfoBox.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Popup } from "react-map-gl";
+import styled from "styled-components";
+import InfoCard from "./InfoCard";
+import { responsive } from "../../constants/responsive";
+
+const StyledDesktopInfo = styled.div`
+  position: absolute;
+  margin: 8px;
+  padding: 2px;
+  max-width: ${responsive.drawerWidth - 20}px;
+  z-index: 9 !important;
+  pointer-events: none;
+`;
+
+const StyledMobileInfo = styled.div`
+  .card {
+    background: none;
+    border: none;
+    max-width: ${responsive.drawerWidth - 20}px;
+  }
+`;
+
+const MapPolygonInfoBox = ({ crashCounts }) => {
+  // TODO: Create obj with title and content keys for crash counts
+  const content = null;
+
+  const infoCard = <InfoCard content={content} />;
+
+  return (
+    popupInfo &&
+    (isMobile ? (
+      <Popup
+        tipSize={10}
+        anchor="top"
+        longitude={parseFloat(popupInfo.longitude)}
+        latitude={parseFloat(popupInfo.latitude)}
+        onClose={() => setSelectedFeature(null)}
+      >
+        <StyledMobileInfo>{infoCard}</StyledMobileInfo>
+      </Popup>
+    ) : (
+      <StyledDesktopInfo>{infoCard}</StyledDesktopInfo>
+    ))
+  );
+};
+
+export default MapPolygonInfoBox;
