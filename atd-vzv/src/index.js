46db5057055fea2ad3a577047799f8459e10b26c
tcwcodes <35410637+tcwcodes@users.noreply.github.com>
2020-06-10 15:57:32 -0500

    working on ie11 after adding Event polyfill

--- atd-vzv/src/index.js
+++ atd-vzv/src/index.js
@@ -1,6 +1,6 @@
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
-import "es6-symbol/implement";
+import 'events-polyfill';
 
 import React from "react";
 import ReactDOM from "react-dom";
