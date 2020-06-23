febcffe8c30a65286aea99c4426335409be8f612
Sergio Garcia <sergiogcx@gmail.com>
2020-06-23 15:58:10 -0500

    Removes enable trigger line

--- atd-vzd/migrations/migration_crashes_2020_06_23--1243.sql
+++ atd-vzd/migrations/migration_crashes_2020_06_23--1243.sql
@@ -3,5 +3,4 @@ ALTER TABLE atd_txdot_crashes ADD original_city_id int;
 
 CREATE INDEX atd_txdot_crashes_original_city_id_index
     ON atd_txdot_crashes (original_city_id);
-
-ALTER TABLE atd_txdot_crashes ENABLE TRIGGER ALL;
+ 
