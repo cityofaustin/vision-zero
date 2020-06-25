5990270a6141d6937eeee8b8425431448a32a87d
Sergio Garcia <sergiogcx@gmail.com>
2020-06-25 15:53:40 -0500

    Prevents docker from building PRs

--- .circleci/docker-helper.sh
+++ .circleci/docker-helper.sh
@@ -1,32 +1,54 @@
 #!/usr/bin/env bash
 
+# Determine work branch
+case "${CIRCLE_BRANCH}" in
+  "production")
+    export WORKING_STAGE="production";
+  ;;
+
+  "master")
+    export WORKING_STAGE="staging";
+  ;;
+
+  *)
+    export WORKING_STAGE="pr";
+  ;;
+esac
+
 export ATD_IMAGE="atddocker/atd-vz-etl";
 export ATD_IMAGE_AGOL="atddocker/atd-vz-etl-agol"
 #
 # We need to assign the name of the branch as the tag to be deployed
 #
-export ATD_TAG="${CIRCLE_BRANCH}";
+export ATD_TAG="${WORKING_STAGE}";
+
 
 function build_containers {
-    echo "Logging in to Docker hub"
-    docker login -u $ATD_DOCKER_USER -p $ATD_DOCKER_PASS
 
-    # First build, tag and push the regular ETL image
-    echo "docker build -f Dockerfile -t $ATD_IMAGE:$ATD_TAG .";
-    docker build -f atd-etl/Dockerfile -t $ATD_IMAGE:$ATD_TAG ./atd-etl
-
-    echo "docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;";
-    docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;
-
-    echo "docker push $ATD_IMAGE:$ATD_TAG";
-    docker push $ATD_IMAGE:$ATD_TAG;
-
-    # Then build, tag and push the Agol-containing Image
-    echo "docker build -f Dockerfile.agol -t $ATD_IMAGE_AGOL:$ATD_TAG .";
-    docker build -f atd-etl/Dockerfile.agol -t $ATD_IMAGE_AGOL:$ATD_TAG ./atd-etl
-
-    echo "docker tag $ATD_IMAGE_AGOL:$ATD_TAG $ATD_IMAGE_AGOL:$ATD_TAG;";
-    docker tag $ATD_IMAGE_AGOL:$ATD_TAG $ATD_IMAGE_AGOL:$ATD_TAG;
+    if [[ "${WORKING_STAGE}" == "pr" ]]; then
+        echo "PRs are not currently deployed to the docker-hub.";
+        exit 0;
+    fi;
+    echo "It should not reach this point";
+#    echo "Logging in to Docker hub"
+#    docker login -u $ATD_DOCKER_USER -p $ATD_DOCKER_PASS
+#
+#    # First build, tag and push the regular ETL image
+#    echo "docker build -f Dockerfile -t $ATD_IMAGE:$ATD_TAG .";
+#    docker build -f atd-etl/Dockerfile -t $ATD_IMAGE:$ATD_TAG ./atd-etl
+#
+#    echo "docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;";
+#    docker tag $ATD_IMAGE:$ATD_TAG $ATD_IMAGE:$ATD_TAG;
+#
+#    echo "docker push $ATD_IMAGE:$ATD_TAG";
+#    docker push $ATD_IMAGE:$ATD_TAG;
+#
+#    # Then build, tag and push the Agol-containing Image
+#    echo "docker build -f Dockerfile.agol -t $ATD_IMAGE_AGOL:$ATD_TAG .";
+#    docker build -f atd-etl/Dockerfile.agol -t $ATD_IMAGE_AGOL:$ATD_TAG ./atd-etl
+#
+#    echo "docker tag $ATD_IMAGE_AGOL:$ATD_TAG $ATD_IMAGE_AGOL:$ATD_TAG;";
+#    docker tag $ATD_IMAGE_AGOL:$ATD_TAG $ATD_IMAGE_AGOL:$ATD_TAG;
 
     echo "docker push $ATD_IMAGE_AGOL:$ATD_TAG";
     docker push $ATD_IMAGE_AGOL:$ATD_TAG;
