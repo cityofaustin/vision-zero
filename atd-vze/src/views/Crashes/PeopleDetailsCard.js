44575bf3dffb5c063a27ca2518f6ff9b6674dc79
Mateo Clarke <code@mateoclarke.com>
2020-07-07 03:45:41 -0500

    Make primary person injury severity editable

--- atd-vze/src/views/Crashes/PeopleDetailsCard.js
+++ atd-vze/src/views/Crashes/PeopleDetailsCard.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { useAuth0, isReadOnly } from "../../auth/authContext";
 
 import {
   Card,
@@ -8,7 +10,11 @@ import {
   Badge,
   Button,
   Collapse,
+  Input,
 } from "reactstrap";
+
+import { GET_PERSON_LOOKUPS } from "../../queries/lookups";
+import { GET_PEOPLE, UPDATE_PRIMARYPERSON } from "../../queries/people";
 
 const getInjurySeverityColor = desc => {
   switch (desc) {
@@ -29,12 +35,72 @@ const getInjurySeverityColor = desc => {
   }
 };
 
-const PeopleDetailsCard = ({
-  primaryPersonData,
-  personData,
-  isExpanded,
-  toggleAccordion,
-}) => {
+const PeopleDetailsCard = ({ isExpanded, toggleAccordion, ...props }) => {
+  const { getRoles } = useAuth0();
+  const roles = getRoles();
+
+  const crashId = props.match.params.id;
+
+  const [editField, setEditField] = useState("");
+  const [editPerson, setEditPerson] = useState("");
+  const [formData, setFormData] = useState({});
+
+  const { data: lookupSelectOptions } = useQuery(GET_PERSON_LOOKUPS);
+  const { loading, error, data, refetch } = useQuery(GET_PEOPLE, {
+    variables: { crashId },
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    props.client
+      .mutate({
+        mutation: UPDATE_PRIMARYPERSON,
+        variables: {
+          crashId: crashId,
+          personId: editPerson,
+          changes: { ...formData },
+        },
+      })
+      .then(res => {
+        if (!res.errors) {
+          refetch();
+        } else {
+          console.log(res.errors);
+        }
+      });
+
+    // RESET state after submit
+    setEditField("");
+    setEditPerson("");
+    setFormData({});
+  };
+
+  const handleInputChange = e => {
+    const manualFieldKey = "prsn_injry_sev_id";
+
+    const newFormState = Object.assign(formData, {
+      [manualFieldKey]: e.target.value,
+      updated_by: localStorage.getItem("hasura_user_email"),
+    });
+    console.log(newFormState);
+    setFormData(newFormState);
+  };
+
+  const handleEditClick = (field, person) => {
+    setEditField(field);
+    setEditPerson(person);
+  };
+
+  const handleCancelClick = e => {
+    e.preventDefault();
+
+    // RESET state on cancel
+    setEditField("");
+    setEditPerson("");
+    setFormData({});
+  };
+
   return (
     <Card className="mb-0">
       <CardHeader id="headingOne">
@@ -49,7 +115,10 @@ const PeopleDetailsCard = ({
           <h5 className="m-0 p-0">
             <i className="fa fa-group" /> People{" "}
             <Badge color="secondary float-right">
-              {primaryPersonData.concat(personData).length}
+              {
+                data.atd_txdot_primaryperson.concat(data.atd_txdot_person)
+                  .length
+              }
             </Badge>
           </h5>
         </Button>
@@ -67,64 +136,144 @@ const PeopleDetailsCard = ({
             <thead>
               <tr>
                 <th>Unit</th>
-                <th>City</th>
-                <th>ZIP</th>
-                <th>Age</th>
                 <th>Injury Severity</th>
                 <th>Type</th>
+                <th>Age</th>
+                <th>City</th>
+                <th>ZIP</th>
               </tr>
             </thead>
             <tbody>
-              {primaryPersonData.map((person, i) => (
-                <tr key={`person-${i}`}>
-                  <td>{person.unit_nbr}</td>
-                  <td>{person.drvr_city_name}</td>
-                  <td>{person.drvr_zip}</td>
-                  <td>{person.prsn_age}</td>
-                  <td>
-                    <Badge
-                      color={getInjurySeverityColor(
-                        person.injury_severity.injry_sev_desc
-                      )}
-                    >
-                      {person.injury_severity.injry_sev_desc}
-                    </Badge>
-                  </td>
-                  <td>{person.person_type.prsn_type_desc}</td>
-                </tr>
-              ))}
+              {data.atd_txdot_primaryperson
+                .sort((a, b) => (a.unit_nbr > b.unit_nbr ? 1 : -1))
+                .map((person, i) => {
+                  debugger;
+                  const isEditing =
+                    editField === "injry_sev_desc" &&
+                    person.primaryperson_id === editPerson;
+
+                  const injurySeverityLookup =
+                    lookupSelectOptions.atd_txdot__injry_sev_lkp;
+
+                  // const defaultValue = injurySeverityLookup.find(
+                  //   opt => opt[`$injry_sev_desc`] === value
+                  // )[`injry_sev_id`];
+
+                  return (
+                    <tr key={`person-${i}`}>
+                      <td>{person.unit_nbr}</td>
+                      <td>
+                        {isEditing ? (
+                          <form onSubmit={e => handleSubmit(e)}>
+                            <Input
+                              name={"injry_sev_desc"}
+                              id={"injry_sev_desc"}
+                              onChange={e => handleInputChange(e)}
+                              defaultValue={person.injury_severity.injry_sev_id}
+                              type="select"
+                            >
+                              {injurySeverityLookup.map(option => (
+                                <option
+                                  value={option["injry_sev_id"]}
+                                  key={option["injry_sev_id"]}
+                                >
+                                  {option["injry_sev_desc"]}
+                                </option>
+                              ))}
+                            </Input>
+                            <Button
+                              type="submit"
+                              block
+                              color="primary"
+                              size="sm"
+                              className="btn-pill mt-2"
+                            >
+                              <i className="fa fa-check edit-toggle" />
+                            </Button>
+                            <Button
+                              type="cancel"
+                              block
+                              color="danger"
+                              size="sm"
+                              className="btn-pill mt-2"
+                              onClick={e => handleCancelClick(e)}
+                            >
+                              <i className="fa fa-times edit-toggle"></i>
+                            </Button>
+                          </form>
+                        ) : (
+                          <>
+                            <Badge
+                              color={getInjurySeverityColor(
+                                person.injury_severity.injry_sev_desc
+                              )}
+                            >
+                              {person.injury_severity.injry_sev_desc}
+                            </Badge>
+                            {/* {fieldConfig.editable &&
+                                  !isReadOnly(roles) &&
+                                  !isEditing && ( */}
+                            {
+                              <Button
+                                block
+                                color="secondary"
+                                size="sm"
+                                className="btn-pill mt-2"
+                                style={{ width: "50px" }}
+                                onClick={e =>
+                                  handleEditClick(
+                                    "injry_sev_desc",
+                                    person.primaryperson_id
+                                  )
+                                }
+                              >
+                                <i className="fa fa-pencil edit-toggle" />
+                              </Button>
+                            }
+                          </>
+                        )}
+                      </td>
+                      <td>{person.person_type.prsn_type_desc}</td>
+                      <td>{person.prsn_age}</td>
+                      <td>{person.drvr_city_name}</td>
+                      <td>{person.drvr_zip}</td>
+                    </tr>
+                  );
+                })}
             </tbody>
           </Table>
 
-          {personData.length > 0 && (
+          {data.atd_txdot_person.length > 0 && (
             <>
               <h5>Other People</h5>
               <Table responsive>
                 <thead>
                   <tr>
                     <th>Unit</th>
-                    <th>Age</th>
                     <th>Injury Severity</th>
                     <th>Type</th>
+                    <th>Age</th>
                   </tr>
                 </thead>
                 <tbody>
-                  {personData.map(person => (
-                    <tr>
-                      <td>{person.unit_nbr}</td>
-                      <td>{person.prsn_age}</td>
-                      <td>
-                        <Badge
-                          color={getInjurySeverityColor(
-                            person.injury_severity.injry_sev_desc
-                          )}
-                        >
-                          {person.injury_severity.injry_sev_desc}
-                        </Badge>
-                      </td>
-                      <td>{person.person_type.prsn_type_desc}</td>
-                    </tr>
-                  ))}
+                  {data.atd_txdot_person
+                    .sort((a, b) => (a.unit_nbr > b.unit_nbr ? 1 : -1))
+                    .map(person => (
+                      <tr>
+                        <td>{person.unit_nbr}</td>
+                        <td>
+                          <Badge
+                            color={getInjurySeverityColor(
+                              person.injury_severity.injry_sev_desc
+                            )}
+                          >
+                            {person.injury_severity.injry_sev_desc}
+                          </Badge>
+                        </td>
+                        <td>{person.person_type.prsn_type_desc}</td>
+                        <td>{person.prsn_age}</td>
+                      </tr>
+                    ))}
                 </tbody>
               </Table>
             </>
