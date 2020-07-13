6558ad806f57ce5a1e5816fe6a597c51f2b16d25
Mateo Clarke <code@mateoclarke.com>
2020-07-13 16:07:27 -0500

    remove warning banner

--- atd-vzv/src/views/summary/Summary.js
+++ atd-vzv/src/views/summary/Summary.js
@@ -49,19 +49,6 @@ const Summary = () => {
 
   return (
     <Container fluid>
-      <Row className="px-xs-0 mx-xs-0 px-lg-3 mx-lg-4 mt-4 mb-0">
-        <Col>
-          <Alert color="danger">
-            <FontAwesomeIcon icon={faExclamationTriangle} />
-            <span className="ml-2">
-              This is a beta version of the Vision Zero Viewer, published to
-              gather user feedback. Crash data displayed may be outdated or
-              inaccurate, and will be updated for the first public release
-              version.
-            </span>
-          </Alert>
-        </Col>
-      </Row>
       {/* Create whitespace on sides of view until mobile */}
       <Row className="px-xs-0 mx-xs-0 px-lg-3 mx-lg-4 mt-xs-3 mt-lg-4">
         <Col className="px-xs-0">
