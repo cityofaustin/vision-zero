46b29ee74910ea332f7df34505a1b3950ef22e3e
Sergio Garcia <sergiogcx@gmail.com>
2020-07-17 15:44:25 -0500

    Implements LocationDownloadGlobal component

---
+++ atd-vze/src/views/Locations/LocationDownloadGlobal.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+import GridExportData from "../../Components/GridExportData";
+import gqlAbstract from "../../queries/gqlAbstract";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const LocationDownloadGlobal = (props) => {
+
+  const columnsToExport = `
+      crash_id
+      location_id
+      case_id
+      crash_date
+      crash_time
+      day_of_week
+      crash_sev_id
+      longitude_primary
+      latitude_primary
+      address_confirmed_primary
+      address_confirmed_secondary
+      non_injry_cnt
+      nonincap_injry_cnt
+      poss_injry_cnt
+      sus_serious_injry_cnt
+      tot_injry_cnt
+      death_cnt
+      unkn_injry_cnt
+      est_comp_cost
+      collsn_desc
+      travel_direction
+      movement_desc
+      veh_body_styl_desc
+      veh_unit_desc_desc
+    `;
+
+  const crashesQuery = new gqlAbstract({
+    table: "view_location_crashes_global",
+    single_item: "crashes",
+    showDateRange: false,
+    columns: null,
+    order_by: {},
+    where: {
+      location_id: `_eq: "${props.locationId}"`,
+    },
+    limit: 25,
+    offset: 0,
+  });
+
+  const getGlobalCount = gql`
+      query getGlobalCount($locationId: String) {
+          view_location_crashes_global_aggregate (
+              where: {location_id: {_eq: $locationId}},
+              order_by: {}
+          ) {
+              aggregate {
+                  count
+              }
+          }
+      }
+  `;
+
+  const { data } = useQuery(getGlobalCount, {
+    variables: { locationId: props.locationId },
+  });
+
+
+  return <>
+    {Object.keys(data).includes("view_location_crashes_global_aggregate") && <GridExportData
+      query={crashesQuery}
+      columnsToExport={columnsToExport}
+      totalRecords={data.view_location_crashes_global_aggregate.aggregate.count}
+    />}
+
+  </>;
+}
+
+export default LocationDownloadGlobal;
