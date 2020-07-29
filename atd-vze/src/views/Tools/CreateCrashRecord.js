0c5eb19d1a0588ae0231032a7552e102b0b5c5b3
Mateo Clarke <code@mateoclarke.com>
2020-07-29 11:35:30 -0500

    wip adding GridTable

--- atd-vze/src/views/Tools/CreateCrashRecord.js
+++ atd-vze/src/views/Tools/CreateCrashRecord.js
@@ -19,6 +19,8 @@ import moment from "moment";
 import { withApollo } from "react-apollo";
 import { gql } from "apollo-boost";
 import "./CreateCrashRecord.css";
+import GridTable from "../../Components/GridTable";
+import gqlAbstract from "../../queries/gqlAbstract";
 
 const CreateCrashRecord = ({ client }) => {
   const [tempId, setTempId] = useState(1000);
@@ -161,123 +163,142 @@ const CreateCrashRecord = ({ client }) => {
       });
   };
 
-  return (
-    <Card>
-      <Form
-        onSubmit={e => handleFormSubmit(e)}
-        onReset={e => resetForm(e)}
-        className="form-horizontal"
-      >
-        <CardHeader>Create New Crash Record Set</CardHeader>
-        <CardBody>
-          <Alert
-            color="success"
-            isOpen={!!successfulNewRecordId}
-            toggle={e => setSuccessfulNewRecordId(false)}
-          >
-            {/*eslint-disable-next-line*/}
-            Crash record creation successful.{" "}
-            <Link
-              to={`/crashes/${successfulNewRecordId}`}
-              className="alert-link"
-            >
-              Open new Crash ID #{successfulNewRecordId} details page
-            </Link>
-            .
-          </Alert>
-          <Alert
-            color="danger"
-            isOpen={!!feedback}
-            toggle={e => setFeedback(false)}
-          >
-            {feedback.title}: {feedback.message}
-          </Alert>
+  let queryConf = {
+    table: "view_crashes_inconsistent_numbers",
+    single_item: "crashes",
+    showDateRange: false,
+    // columns: crashGridTableColumns,
+    where: {},
+    limit: 25,
+    offset: 0,
+  };
 
-          <FormGroup row>
-            <Col md="3">
-              <Label htmlFor="hf-email">Case ID</Label>
-            </Col>
-            <Col xs="12" md="9">
-              <Input
-                type="number"
-                id="hf-case-id"
-                name="hf-case-id"
-                placeholder="Enter Case ID..."
-                value={caseId}
-                onChange={e => setCaseId(e.target.value)}
-              />
-              <FormText className="help-block">
-                Please enter the Case ID
-              </FormText>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Col md="3">
-              <Label htmlFor="date-input">Crash Date</Label>
-            </Col>
-            <Col xs="12" md="9">
-              <Input
-                type="date"
-                id="date-input"
-                name="date-input"
-                placeholder="date"
-                value={crashDate}
-                onChange={e => setCrashDate(e.target.value)}
-              />
-              <FormText className="help-block">
-                Please enter the Crash Date
-              </FormText>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Col md="3">
-              <Label htmlFor="hf-fatality-count">Fatality Count</Label>
-            </Col>
-            <Col xs="12" md="9">
-              <Input
-                type="number"
-                id="hf-fatality-count"
-                name="hf-fatality-count"
-                placeholder="Enter Fatality Count..."
-                value={fatalityCount}
-                onChange={e => setFatalityCount(e.target.value)}
-              />
-              <FormText className="help-block">
-                Please enter the Fatality Count
-              </FormText>
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Col md="3">
-              <Label htmlFor="hf-fatality-count">
-                Suspected Serious Injury Count
-              </Label>
-            </Col>
-            <Col xs="12" md="9">
-              <Input
-                type="number"
-                id="hf-fatality-count"
-                name="hf-fatality-count"
-                placeholder="Enter Suspected Serious Injury Count..."
-                value={susSeriousInjuryCount}
-                onChange={e => setSusSeriousInjuryCount(e.target.value)}
-              />
-              <FormText className="help-block">
-                Please enter the Suspected Serious Injury Count
-              </FormText>
-            </Col>
-          </FormGroup>
-        </CardBody>
-        <CardFooter>
-          <Button type="submit" size="sm" color="primary" className="mr-2">
-            <i className="fa fa-dot-circle-o"></i> Submit
-          </Button>
-          <Button type="reset" size="sm" color="danger">
-            <i className="fa fa-ban"></i> Reset
-          </Button>
-        </CardFooter>
-      </Form>
-    </Card>
+  const tempRecordsQuery = new gqlAbstract(queryConf);
+
+  return (
+    <>
+      <Card>
+        <Form
+          onSubmit={e => handleFormSubmit(e)}
+          onReset={e => resetForm(e)}
+          className="form-horizontal"
+        >
+          <CardHeader>Create New Crash Record Set</CardHeader>
+          <CardBody>
+            <Alert
+              color="success"
+              isOpen={!!successfulNewRecordId}
+              toggle={e => setSuccessfulNewRecordId(false)}
+            >
+              {/*eslint-disable-next-line*/}
+              Crash record creation successful.{" "}
+              <Link
+                to={`/crashes/${successfulNewRecordId}`}
+                className="alert-link"
+              >
+                Open new Crash ID #{successfulNewRecordId} details page
+              </Link>
+              .
+            </Alert>
+            <Alert
+              color="danger"
+              isOpen={!!feedback}
+              toggle={e => setFeedback(false)}
+            >
+              {feedback.title}: {feedback.message}
+            </Alert>
+
+            <FormGroup row>
+              <Col md="3">
+                <Label htmlFor="hf-email">Case ID</Label>
+              </Col>
+              <Col xs="12" md="9">
+                <Input
+                  type="number"
+                  id="hf-case-id"
+                  name="hf-case-id"
+                  placeholder="Enter Case ID..."
+                  value={caseId}
+                  onChange={e => setCaseId(e.target.value)}
+                />
+                <FormText className="help-block">
+                  Please enter the Case ID
+                </FormText>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col md="3">
+                <Label htmlFor="date-input">Crash Date</Label>
+              </Col>
+              <Col xs="12" md="9">
+                <Input
+                  type="date"
+                  id="date-input"
+                  name="date-input"
+                  placeholder="date"
+                  value={crashDate}
+                  onChange={e => setCrashDate(e.target.value)}
+                />
+                <FormText className="help-block">
+                  Please enter the Crash Date
+                </FormText>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col md="3">
+                <Label htmlFor="hf-fatality-count">Fatality Count</Label>
+              </Col>
+              <Col xs="12" md="9">
+                <Input
+                  type="number"
+                  id="hf-fatality-count"
+                  name="hf-fatality-count"
+                  placeholder="Enter Fatality Count..."
+                  value={fatalityCount}
+                  onChange={e => setFatalityCount(e.target.value)}
+                />
+                <FormText className="help-block">
+                  Please enter the Fatality Count
+                </FormText>
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col md="3">
+                <Label htmlFor="hf-fatality-count">
+                  Suspected Serious Injury Count
+                </Label>
+              </Col>
+              <Col xs="12" md="9">
+                <Input
+                  type="number"
+                  id="hf-fatality-count"
+                  name="hf-fatality-count"
+                  placeholder="Enter Suspected Serious Injury Count..."
+                  value={susSeriousInjuryCount}
+                  onChange={e => setSusSeriousInjuryCount(e.target.value)}
+                />
+                <FormText className="help-block">
+                  Please enter the Suspected Serious Injury Count
+                </FormText>
+              </Col>
+            </FormGroup>
+          </CardBody>
+          <CardFooter>
+            <Button type="submit" size="sm" color="primary" className="mr-2">
+              <i className="fa fa-dot-circle-o"></i> Submit
+            </Button>
+            <Button type="reset" size="sm" color="danger">
+              <i className="fa fa-ban"></i> Reset
+            </Button>
+          </CardFooter>
+        </Form>
+      </Card>
+      <GridTable
+        query={tempRecordsQuery}
+        title={"Exisiting Temporary Crash Records"}
+        filters={null}
+      />
+    </>
   );
 };
 
