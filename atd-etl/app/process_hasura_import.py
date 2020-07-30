9bc8255b6e4962058c1ae3f9d101019484c6fceb
Sergio Garcia <sergiogcx@gmail.com>
2020-07-30 16:47:39 -0500

    Refactors temp record identification

--- atd-etl/app/process_hasura_import.py
+++ atd-etl/app/process_hasura_import.py
@@ -19,6 +19,7 @@ import time
 import signal
 import logging
 import concurrent.futures
+import web_pdb
 
 # We need to import our configuration, helpers and request methods
 from process.config import ATD_ETL_CONFIG
@@ -98,13 +99,13 @@ def process_line(
                 # There is, we need to delete it
                 print(
                     "Detected temporary record for crash_id/case_id '%s/%s'... deleting!"
-                    % (crash_id, case_id)
+                    % (crash_id, temporary_records[case_id])
                 )
-                delete_temp_record(crash_id)
-
+                delete_temp_record(temporary_records[case_id])
             else:
                 print("No temporary record detected. Ignoring check.")
 
+            web_pdb.set_trace()
             # Does the record exist already?
             record_exists = get_crash_record(crash_id)
             if record_exists:
