652bc8bad94a65e9c590fdcc5dae9723311b875d
Sergio Garcia <sergiogcx@gmail.com>
2020-08-25 22:13:09 -0500

    2nd attempt and exporting the zappa config

--- .github/workflows/aws-lambda-helper.sh
+++ .github/workflows/aws-lambda-helper.sh
@@ -70,8 +70,7 @@ function generate_env_vars {
       echo "LOCAL_STAGE: ${LOCAL_STAGE}";
       STAGE_ENV_VARS=$(cat zappa_settings.json | jq -r ".${LOCAL_STAGE}.aws_environment_variables");
       echo $STAGE_ENV_VARS | cut -c1-35;
-      echo -e "{\"Description\": \"ATD VisionZero Events Handler\", \"Environment\": { \"Variables\": ${STAGE_ENV_VARS}}}" > handler_config_long.json;
-      jq -c handler_config_long.json > handler_config.json;
+      echo -e "{\"Description\": \"ATD VisionZero Events Handler\", \"Environment\": { \"Variables\": ${STAGE_ENV_VARS}}}" | jq -r -c > handler_config.json;
       echo "Done";
 }
 
