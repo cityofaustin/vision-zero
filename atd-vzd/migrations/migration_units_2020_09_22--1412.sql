1495dcd5d8bd9d4ed2a434a7a4606193ea83c246
Sergio Garcia <sergiogcx@gmail.com>
2020-09-22 14:21:22 -0500

    Corrects table names

--- atd-vzd/migrations/migration_units_2020_09_22--1412.sql
+++ atd-vzd/migrations/migration_units_2020_09_22--1412.sql
@@ -1,8 +1,8 @@
 -- Adds new CRIS columns for the units table
 
 alter table atd_txdot_units add veh_damage_description1_id int default NULL;
-alter table atd_txdot_units add veh_damage_description1_id int default NULL;
-alter table atd_txdot_units add veh_damage_description1_id int default NULL;
-alter table atd_txdot_units add veh_damage_description1_id int default NULL;
-alter table atd_txdot_units add veh_damage_description1_id int default NULL;
-alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_severity1_id int default NULL;
+alter table atd_txdot_units add veh_damage_direction_of_force1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description2_id int default NULL;
+alter table atd_txdot_units add veh_damage_severity2_id int default NULL;
+alter table atd_txdot_units add veh_damage_direction_of_force2_id int default NULL;
