f6ac4b5492d7856bd1e65898c1b2469aac667a99
Sergio Garcia <sergiogcx@gmail.com>
2020-09-22 14:18:53 -0500

    Documents changes to the database

---
+++ atd-vzd/migrations/migration_units_2020_09_22--1412.sql
@@ -0,0 +1,8 @@
+-- Adds new CRIS columns for the units table
+
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
+alter table atd_txdot_units add veh_damage_description1_id int default NULL;
