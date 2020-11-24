4b7b37370122aa72c86aae208fd4913569409a36
Sergio Garcia <sergiogcx@gmail.com>
2020-11-24 10:00:47 -0700

    Document changes to the database

--- atd-vzd/triggers/find_location_for_cr3_collision.sql
+++ atd-vzd/triggers/find_location_for_cr3_collision.sql
@@ -1,17 +1,21 @@
-CREATE OR REPLACE FUNCTION public.find_location_for_cr3_collision
-(id int4)
- RETURNS SETOF atd_txdot_locations
- LANGUAGE sql
- STABLE
-AS $function$
-SELECT
-	*
-FROM
-	atd_txdot_locations AS LOCATION
-WHERE
-	ST_WITHIN((
-		SELECT
-			crash.position FROM atd_txdot_crashes AS crash
-		WHERE
-			crash.crash_id = id), location.shape)
-$function$
+CREATE OR REPLACE FUNCTION public.find_location_for_cr3_collision(
+    id integer)
+    RETURNS SETOF atd_txdot_locations
+    LANGUAGE 'sql'
+
+    COST 100
+    STABLE
+    ROWS 1000
+AS $BODY$SELECT atl.* FROM atd_txdot_crashes AS atc
+                               INNER JOIN atd_txdot_locations AS atl
+                                          ON ( 1=1
+                                              AND atl.location_group = 1
+                                              AND (atl.shape && st_setsrid(ST_POINT(atc.longitude_primary, atc.latitude_primary ), 4326))
+                                              AND ST_Contains(atl.shape, st_setsrid(ST_POINT(atc.longitude_primary, atc.latitude_primary ), 4326))
+                                              )
+         WHERE 1=1
+           AND atc.crash_id = id
+$BODY$;
+
+
+alter function find_location_for_cr3_collision(integer) owner to atd_vz_data;
--- atd-vzd/triggers/find_location_for_noncr3_collision.sql
+++ atd-vzd/triggers/find_location_for_noncr3_collision.sql
@@ -1,17 +1,21 @@
-create or replace function find_location_for_noncr3_collision(id integer) returns SETOF atd_txdot_locations
-    stable
-    language sql
-as
-$$
-SELECT atl.* FROM atd_apd_blueform AS aab
-                      INNER JOIN atd_txdot_locations AS atl
-                                 ON ( 1=1
-                                     AND (atl.shape && st_setsrid(ST_POINT(aab.longitude, aab.latitude ), 4326))
-                                     AND ST_Contains(atl.shape, st_setsrid(ST_POINT(aab.longitude, aab.latitude ), 4326))
-                                     )
-WHERE 1=1
-  AND aab.case_id = id
-$$;
+CREATE OR REPLACE FUNCTION public.find_location_for_noncr3_collision(
+    id integer)
+    RETURNS SETOF atd_txdot_locations
+    LANGUAGE 'sql'
+
+    COST 100
+    STABLE
+    ROWS 1000
+AS $BODY$SELECT atl.* FROM atd_apd_blueform AS aab
+                               INNER JOIN atd_txdot_locations AS atl
+                                          ON ( 1=1
+                                              AND atl.location_group = 1
+                                              AND (atl.shape && st_setsrid(ST_POINT(aab.longitude, aab.latitude ), 4326))
+                                              AND ST_Contains(atl.shape, st_setsrid(ST_POINT(aab.longitude, aab.latitude ), 4326))
+                                              )
+         WHERE 1=1
+           AND aab.case_id = id
+$BODY$;
 
 alter function find_location_for_noncr3_collision(integer) owner to atd_vz_data;
 
