27549a6c3420380f7799c8b44aa64a854be7d2d2
Mateo Clarke <code@mateoclarke.com>
2021-01-06 17:54:55 -0600

    Updates units table with new field, instead of crashes

---
+++ atd-vzd/migrations/migration_units_2021-01-06--1752.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "pedestrian_action_id" int4;
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "pedalcyclist_action_id" int4;
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "pbcat_pedestrian_id" int4;
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "pbcat_pedalcyclist_id" int4;
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "e_scooter_id" int4;
+ALTER TABLE "public"."atd_txdot_units" ADD COLUMN "autonomous_unit_id" int4;
