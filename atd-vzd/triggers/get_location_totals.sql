2dff381ae47febcd8aa3d5b1929efc16be165307
Frank Hereford <frank@frankhereford.com>
2021-07-01 15:26:21 -0500

    Re-write get_location_totals()
        My earlier commit today introduced a behavior where the use of the
        query as submitted was very expensive for the DB. It is not suitable for
        production.
    
        I would like to propose this re-write of the function. This is,
        essentially, two very simply queries which can each use the indexes in
        place for atd_apd_blueform and atd_txdot_crashes. Both queries only can
        return a single row, as all of the selected items are aggregate function
        results. These two queries are defined as CTE tables containing one row
        each, so the primary query can simply JOIN the two "tables", which
        concatenates the fields into one row. Here, you can add the columns you
        need to add and simply pass through the others.
    
        One thing to note, this version does allow the invocation of different
        locations for CR3 crashes vs. non-CR3 crashes, where the previous query
        would return nothing in that case. Since the arguments are both
        specified, I think this is the intended behavior. Also, like the
        previous query, a location_id is returned in the results, and this query
        returns the CR3 location_id and not the non-cr3 one.

--- atd-vzd/triggers/get_location_totals.sql
+++ atd-vzd/triggers/get_location_totals.sql
@@ -1,55 +1,30 @@
-CREATE
-OR REPLACE FUNCTION public.get_location_totals(
-  cr3_crash_date date,
-  noncr3_crash_date date,
-  cr3_location character varying,
-  noncr3_location character varying,
-  cost_per_crash numeric
-) RETURNS SETOF atd_location_crash_and_cost_totals AS $$
-SELECT
-  cr3.location_id,
-  (cr3.total_crashes + noncr3.total_crashes) AS total_crashes,
-  (cr3.est_comp_cost + noncr3.est_comp_cost) AS total_est_comp_cost,
-  cr3.total_crashes AS cr3_total_crashes,
-  cr3.est_comp_cost AS cr3_est_comp_cost,
-  noncr3.total_crashes AS noncr3_total_crashes,
-  noncr3.est_comp_cost AS noncr3_est_comp_cost
-FROM
-  (
-    SELECT
-      atdl.location_id AS location_id,
-      count(atdc) AS total_crashes,
-      coalesce(sum(atdc.est_comp_cost), 0) AS est_comp_cost
-    FROM
-      atd_txdot_locations AS atdl
-      LEFT JOIN atd_txdot_crashes AS atdc ON (
-        atdl.location_id = atdc.location_id
-        AND atdc.city_id = 22
-        AND atdc.crash_date >= cr3_crash_date :: date
-      )
-    WHERE
-      atdl.location_id = cr3_location :: text
-      AND atdl.location_id IS NOT NULL
-      AND atdl.location_id :: text <> 'None' :: text
-    GROUP BY
-      atdl.location_id
-  ) cr3
-  JOIN (
-    SELECT
-      atdl.location_id AS location_id,
-      count(atdbf) AS total_crashes,
-      coalesce((count(atdbf) * cost_per_crash), 0) AS est_comp_cost
-    FROM
-      atd_txdot_locations AS atdl
-      LEFT JOIN atd_apd_blueform AS atdbf ON (
-        atdl.location_id = atdbf.location_id
-        AND atdbf.date >= noncr3_crash_date :: date
-      )
-    WHERE
-      atdl.location_id IS NOT NULL
-      AND atdl.location_id :: text <> 'None' :: text
-      AND atdl.location_id = noncr3_location :: text
-    GROUP BY
-      atdl.location_id
-  ) noncr3 on cr3.location_id = noncr3.location_id
-  $$ LANGUAGE sql STABLE;
+CREATE OR REPLACE FUNCTION public.get_location_totals(cr3_crash_date date, noncr3_crash_date date, cr3_location character varying, noncr3_location character varying, cost_per_crash numeric)
+ RETURNS SETOF atd_location_crash_and_cost_totals
+ LANGUAGE sql
+ STABLE
+AS $function$
+with
+  noncr3 as (
+    select count(aab.case_id) as total_crashes,
+    count(aab.case_id) * cost_per_crash as est_comp_cost
+    from atd_apd_blueform aab
+    where aab.location_id = noncr3_location
+    and aab.date >= noncr3_crash_date
+  ),
+  cr3 as (
+    select count(atc.crash_id) as total_crashes,
+    sum(est_crash_based_comp_cost) as est_comp_cost
+    from atd_txdot_crashes atc
+    where atc.location_id = cr3_location
+    and atc.crash_date >= cr3_crash_date
+  )
+select cr3_location as location_id,
+       cr3.total_crashes + noncr3.total_crashes as total_crashes,
+       cr3.est_comp_cost + noncr3.est_comp_cost as total_est_comp_cost,
+       cr3.total_crashes as cr3_total_crashes,
+       cr3.est_comp_cost as cr3_est_comp_cost,
+       noncr3.total_crashes as noncr3_total_crashes,
+       noncr3.est_comp_cost as noncr3_est_comp_cost
+from noncr3, cr3
+  $function$
+;
