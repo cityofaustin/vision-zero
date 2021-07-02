089e9791cacbab60bd682ad59d8bb4c8d1404264
Frank Hereford <frank@frankhereford.com>
2021-07-02 13:38:34 -0500

    get_location_totals(): improve formatting and documentation
    Improved the SQL formatting and add some explanatory comments inline
    in the function. Thanks to Sergio for his feedback and suggestions!

--- atd-vzd/triggers/get_location_totals.sql
+++ atd-vzd/triggers/get_location_totals.sql
@@ -1,30 +1,61 @@
-CREATE OR REPLACE FUNCTION public.get_location_totals(cr3_crash_date date, noncr3_crash_date date, cr3_location character varying, noncr3_location character varying, cost_per_crash numeric)
- RETURNS SETOF atd_location_crash_and_cost_totals
- LANGUAGE sql
- STABLE
-AS $function$
-with
-  noncr3 as (
-    select count(aab.case_id) as total_crashes,
-    count(aab.case_id) * cost_per_crash as est_comp_cost
-    from atd_apd_blueform aab
-    where aab.location_id = noncr3_location
-    and aab.date >= noncr3_crash_date
+CREATE OR REPLACE FUNCTION public.get_location_totals(
+  cr3_crash_date    date, 
+  noncr3_crash_date date, 
+  cr3_location      character varying, 
+  noncr3_location   character varying, 
+  cost_per_crash    numeric
+  ) RETURNS SETOF atd_location_crash_and_cost_totals
+ LANGUAGE sql STABLE
+  AS $function$
+WITH
+  -- This CTE+join mechanism is a way to pack two, simple
+  -- queries together into one larger query and compute 
+  -- derived results from their results. 
+  --
+  -- In our first CTE, we'll count up the number of 
+  -- non-CR3 crashes and their sum of their comprehensive 
+  -- cost which are associated to a given location occuring 
+  -- after a given date.
+  -- 
+  -- All non-CR3 crashes are given a standard
+  -- comprehensive cost, and this value is provided as
+  -- an argument to this query.
+  -- 
+  -- An important thing to note is that this CTE query will
+  -- only return a single row under any circumstances.
+  noncr3 AS (
+    SELECT COUNT(aab.case_id) AS total_crashes,
+      COUNT(aab.case_id) * cost_per_crash AS est_comp_cost
+    FROM atd_apd_blueform aab
+    WHERE aab.location_id = noncr3_location
+      AND aab.date >= noncr3_crash_date
   ),
-  cr3 as (
-    select count(atc.crash_id) as total_crashes,
-    sum(est_comp_cost) as est_comp_cost
-    from atd_txdot_crashes atc
-    where atc.location_id = cr3_location
-    and atc.crash_date >= cr3_crash_date
+  -- A very similar query, again returning a single row,
+  -- to compute the count and aggregate comprehensive cost
+  -- for CR3 crashes. 
+  cr3 AS (
+    SELECT COUNT(atc.crash_id) AS total_crashes,
+      SUM(est_comp_cost) AS est_comp_cost
+    FROM atd_txdot_crashes atc
+    WHERE atc.location_id = cr3_location
+      AND atc.crash_date >= cr3_crash_date
   )
-select cr3_location as location_id,
-       cr3.total_crashes + noncr3.total_crashes as total_crashes,
-       cr3.est_comp_cost + noncr3.est_comp_cost as total_est_comp_cost,
-       cr3.total_crashes as cr3_total_crashes,
-       cr3.est_comp_cost as cr3_est_comp_cost,
-       noncr3.total_crashes as noncr3_total_crashes,
-       noncr3.est_comp_cost as noncr3_est_comp_cost
-from noncr3, cr3
-  $function$
-;
+-- Add the two crash types respective values together to 
+-- get values for all crashes for the location. Also, 
+-- pass through the individual crash type values in the final
+-- result.
+SELECT cr3_location AS location_id,
+       cr3.total_crashes + noncr3.total_crashes AS total_crashes,
+       cr3.est_comp_cost + noncr3.est_comp_cost AS total_est_comp_cost,
+       cr3.total_crashes AS cr3_total_crashes,
+       cr3.est_comp_cost AS cr3_est_comp_cost,
+       noncr3.total_crashes AS noncr3_total_crashes,
+       noncr3.est_comp_cost AS noncr3_est_comp_cost
+-- This is an implicit join of the two CTE tables. Because each
+-- table is known to have only a single row, the result will also
+-- be a single row, 1 * 1 = 1. This is why we have no need for a WHERE
+-- clause, as we narroed down to the actual data we need in the CTEs.
+-- Joining a table of a single row to another talbe of a single row
+-- essentaily performs a concatenation of the two rows.
+FROM noncr3, cr3
+  $function$;
