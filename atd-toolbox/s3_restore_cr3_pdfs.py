4aff88e3700351b69e844fe501aa0a472c4c73e7
Frank Hereford <frank@frankhereford.com>
2021-08-06 18:53:38 -0500

    Add argument to specify JSON file of crashes and parse it

--- atd-toolbox/s3_restore_cr3_pdfs.py
+++ atd-toolbox/s3_restore_cr3_pdfs.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import json
 import boto3
 import argparse
 from botocore.config import Config
@@ -13,11 +14,10 @@ SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 try:
     argparse = argparse.ArgumentParser(description = 'Utility to restore last valid PDF in S3 for ATD VZ')
     argparse.add_argument("-p", "--production", help = 'Specify the use of production environment', action = 'store_true')
+    argparse.add_argument("-c", "--crashes", help = 'Specify JSON file containing crashes to operate on. Format: { "crashes": [ crash_id_0, crash_id_1, .. ] }', required=True, metavar = 'crashes.json')
     args = argparse.parse_args()
 except:
-    print("Failure to setup and parse arguments or -h specified")
     sys.exit(1)
-
 
 # verify that environment variables were available and have populated variables used to auth to AWS
 try:
@@ -39,7 +39,27 @@ try:
             aws_secret_access_key = SECRET_KEY, 
             config = aws_config)
     prefix = 'production' if args.production else 'staging' + '/cris-cr3-files'
-    s3.list_objects(Bucket= 'atd-vision-zero-editor', Prefix = prefix)['Contents']
+    s3.list_objects(Bucket= 'atd-vision-zero-editor', Prefix = prefix)
 except:
     print("Unable to complete call to S3; check AWS credentials")
     sys.exit(1)
+
+
+# check to see if file is available on disk
+try:
+    assert(os.path.isfile(args.crashes))
+except:
+    print("Crashes file is not available on disk")
+    sys.exit(1)
+
+
+#parse JSON file containing crashes
+with open(args.crashes) as input_file:
+    try:
+        crashes = json.load(input_file)['crashes']
+    except:
+        print("Crashes file is invalid JSON")
+        sys.exit(1)
+
+for crash in crashes:
+    print(crash)
