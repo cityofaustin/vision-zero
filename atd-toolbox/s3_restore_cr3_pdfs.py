9a68ddd82ba01627b7f8249353b452907151232a
Frank Hereford <frank@frankhereford.com>
2021-08-06 18:10:36 -0500

    Add argument parsing and flag for staging/production

--- atd-toolbox/s3_restore_cr3_pdfs.py
+++ atd-toolbox/s3_restore_cr3_pdfs.py
@@ -3,13 +3,21 @@
 import os
 import sys
 import boto3
+import argparse
 from botocore.config import Config
-
-#FIXME
-# add command arg to specify production; default to staging
 
 ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
 SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+# setup and parse arguments
+try:
+    argparse = argparse.ArgumentParser(description = 'Utility to restore last valid PDF in S3 for ATD VZ')
+    argparse.add_argument("-p", "--production", help = 'Specify the use of production environment', action = 'store_true')
+    args = argparse.parse_args()
+except:
+    print("Failure to setup and parse arguments or -h specified")
+    sys.exit(1)
+
 
 # verify that environment variables were available and have populated variables used to auth to AWS
 try:
@@ -30,7 +38,8 @@ try:
             aws_access_key_id = ACCESS_KEY, 
             aws_secret_access_key = SECRET_KEY, 
             config = aws_config)
-    s3.list_objects(Bucket= 'atd-vision-zero-editor', Prefix = 'staging/cris-cr3-files')['Contents']
+    prefix = 'production' if args.production else 'staging' + '/cris-cr3-files'
+    s3.list_objects(Bucket= 'atd-vision-zero-editor', Prefix = prefix)['Contents']
 except:
     print("Unable to list directory; check AWS credentials")
     sys.exit(1)
