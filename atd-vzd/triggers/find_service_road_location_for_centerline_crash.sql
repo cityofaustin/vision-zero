88722629dbbf67dc5dffe8f22eace478e84f7571
Frank Hereford <frank@frankhereford.com>
2021-08-11 13:08:00 -0500

    Improve comments in SQL query and format for readability

--- atd-vzd/triggers/find_service_road_location_for_centerline_crash.sql
+++ atd-vzd/triggers/find_service_road_location_for_centerline_crash.sql
@@ -3,18 +3,23 @@ returns setof atd_txdot_locations
 language 'sql'
 stable
 as $QUERY$
+-- this CTE will setup a in-memory table containing one crash_id, its position, 
+-- and the seek direction if and only if the crash supplied lies within a mainlane strip
 with crash as (
-  -- this query will return a crash_id and seek direction iff the crash supplied lies within a mainlane strip
   WITH cr3_mainlanes AS (
     SELECT st_transform(st_buffer(st_transform(st_union(cr3_mainlanes.geometry), 2277), 1), 4326) AS geometry
       FROM cr3_mainlanes
       )
   select c.crash_id, position,
     CASE
-      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'w' OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'w' THEN 0
-      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'n' OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'n' THEN 90
-      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'e' OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'e' THEN 180
-      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 's' OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 's' THEN 270
+      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'w' 
+        OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'w' THEN 0
+      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'n' 
+        OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'n' THEN 90
+      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'e' 
+        OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 'e' THEN 180
+      WHEN "substring"(lower(c.rpt_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 's' 
+        OR "substring"(lower(c.rpt_sec_street_name), '\s?([nsew])[arthous]*\s??b(oun)?d?') ~* 's' THEN 270
       ELSE NULL
     END AS seek_direction
   from atd_txdot_crashes c
@@ -26,6 +31,8 @@ with crash as (
   and c.crash_id = input_crash_id
   limit 1
 )
+-- this query joins the crash found in the CTE with the polygon layer to find the closest 
+-- polygon centroid in the cone eminating from the crash location in the seek_direction defined in the CTE
 select l.*
 from atd_txdot_locations l
 join crash c on (1 = 1)
@@ -49,7 +56,7 @@ ELSE (st_azimuth(c.position, st_centroid(l.shape)) * 180 / pi()) >=
     OR (st_azimuth(c.position, st_centroid(l.shape)) * 180 / pi()) <= ((c.seek_direction + 65) % 360)
 end
 AND st_intersects(st_transform(st_buffer(st_transform(c.position, 2277), 750), 4326), l.shape) 
-AND l.description ~~* '%SVRD%'::text
+AND l.description ~~* '%SVRD%'
 ORDER BY (st_distance(st_centroid(l.shape), c.position))
 limit 1
 $QUERY$;
