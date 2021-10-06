a3808795590df758ab811849f149dc51b9ee0b1b
tillyw <tilly.whitson@austintexas.gov>
2021-10-06 10:34:45 -0500

    displaying measure cards

--- atd-vzv/src/routes/routes.js
+++ atd-vzv/src/routes/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Summary from "../views/summary/Summary";
+import SummaryView from "../views/summary/SummaryView";
 import Map from "../views/map/Map";
 
 // Set basepath for VZV url
@@ -7,5 +8,6 @@ export const basepath = "/viewer";
 
 export const routes = {
   "/": () => <Summary />,
+  "/measures": () => <SummaryView />,
   "/map": () => <Map />,
 };
