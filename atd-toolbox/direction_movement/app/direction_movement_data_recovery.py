adc55d9a9e2af32d78b82a61ea66331538526f7f
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-16 15:12:23 -0500

    detect changes since change_log entry

--- atd-toolbox/direction_movement/app/direction_movement_data_recovery.py
+++ atd-toolbox/direction_movement/app/direction_movement_data_recovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import json
+#import json
 
 import psycopg2
 from psycopg2 import extras # this feels like i should be able to just use the line above somehow
@@ -34,7 +34,20 @@ def get_change_events_from_past():
     changes = cursor.fetchall()
     return changes
 
+def check_current_state(id, previous_record):
+    sql = f"""
+    select * from atd_txdot_units where unit_id = {id}
+    """
+    cursor = now.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(sql)
+    current_value = cursor.fetchone()
+    for key in previous_record.keys():
+        if previous_record[key] != current_value[key]:
+            print('')
+            print(key)
+            print(previous_record[key])
+            print(current_value[key])
+
 changes = get_change_events_from_past()
-#print(changes)
 for change in changes:
-    print(change['record_json'])
+    check_current_state(change['record_id'], change['record_json'])
