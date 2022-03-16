87693e7347c9b01042c5645beea5a878ab787b70
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-16 11:41:31 -0500

    query in the set of changes we're interested in

--- atd-toolbox/direction_movement/app/direction_movement_data_recovery.py
+++ atd-toolbox/direction_movement/app/direction_movement_data_recovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+
 import psycopg2
 
 past = psycopg2.connect(
@@ -13,3 +15,24 @@ now = psycopg2.connect(
     database="current_vz",
     user="moped",
     password="")
+
+
+def get_change_events_from_past():
+    sql = """
+    select *,
+           (((extract(hour from update_timestamp) * 60) + extract(minute from update_timestamp))/30)::integer as time_bin
+    from atd_txdot_change_log
+    where 1 = 1
+    and record_type = 'units'
+    and (((extract(hour from update_timestamp) * 60) + extract(minute from update_timestamp))/30)::integer not in (16,17,18,19,20)
+    order by update_timestamp desc;
+    """
+    cursor = past.cursor()
+    cursor.execute(sql)
+    changes = cursor.fetchall()
+    return changes
+
+
+
+changes = get_change_events_from_past()
+print(changes)
