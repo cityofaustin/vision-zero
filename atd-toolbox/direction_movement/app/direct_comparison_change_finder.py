54f1d27e6b2c1af6a16c8893c4123c5d7d61f3b4
Frank Hereford <frankhereford@users.noreply.github.com>
2022-03-31 15:38:59 -0500

    boilerplate for new approach

---
+++ atd-toolbox/direction_movement/app/direct_comparison_change_finder.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import pprint
+from re import I
+import psycopg2
+from psycopg2 import (
+    extras,
+)
+
+# setup global objects
+pp = pprint.PrettyPrinter(indent=2)
+
+# setup both DB connections
+past = psycopg2.connect(host="localhost", database="past_vz", user="moped", password="")
+
+now = psycopg2.connect(
+    host="localhost", database="current_vz", user="moped", password=""
+)
