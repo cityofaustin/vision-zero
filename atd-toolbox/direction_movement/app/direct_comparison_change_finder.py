1bd6a338902d818eab94693193396aa7cdcc9eba
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-04 13:53:54 -0500

    Framework for direct comparison

--- atd-toolbox/direction_movement/app/direct_comparison_change_finder.py
+++ atd-toolbox/direction_movement/app/direct_comparison_change_finder.py
@@ -18,11 +18,55 @@ now = psycopg2.connect(
 )
 
 
-def main():
+def get_current_units():
     sql = """
-    select crash_id, unit_id, 
-    from atd_txdot_units
+    select crash_id, unit_id, movement_id, travel_direction, veh_trvl_dir_id 
+    from atd_txdot_units units
+    order by crash_id desc;
     """
+    cursor = now.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(sql)
+    current_units = cursor.fetchall()
+    return current_units
+
+
+def get_diff_from_past(current_unit):
+    sql = """
+    select units.crash_id, crashes.crash_date, units.unit_id, units.movement_id, units.travel_direction, units.veh_trvl_dir_id 
+    from atd_txdot_units units
+    left join atd_txdot_crashes crashes on (units.crash_id = crashes.crash_id)
+    where 1 = 1
+    and crashes.crash_date <= %s
+    and units.unit_id = %s 
+    and crashes.crash_id = %s 
+    order by crash_id desc;
+    """
+    cursor = past.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(
+        sql, ("2022-01-08", current_unit["unit_id"], current_unit["crash_id"])
+    )
+    past_unit = cursor.fetchone()
+
+    # we didn't find a crash in the past, this one is new.
+    if not past_unit:
+        return None
+
+    print(str(past_unit["crash_id"]) + ": " + str(past_unit["crash_date"]))
+
+    fields_to_check = {"movement_id", "travel_direction", "veh_trvl_dir_id"}
+    for field in fields_to_check:
+        print(field)
+    pp.pprint(past_unit)
+
+
+def main():
+    units = get_current_units()
+    for unit in units:
+        # pp.pprint(unit)
+        # print("")
+        # print(unit["crash_id"])
+        diff = get_diff_from_past(unit)
+        # pp.pprint(diff)
 
 
 if __name__ == "__main__":
