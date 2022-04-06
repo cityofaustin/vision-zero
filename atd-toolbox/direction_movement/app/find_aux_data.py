d8a837ac7925b868f1b41a2480820e8c94c58f53
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-06 09:45:42 -0500

    main defined

--- atd-toolbox/direction_movement/app/find_aux_data.py
+++ atd-toolbox/direction_movement/app/find_aux_data.py
@@ -10,11 +10,27 @@ from psycopg2 import (
 # setup global objects
 pp = pprint.PrettyPrinter(indent=2)
 
-now = psycopg2.connect(
+db = psycopg2.connect(
     host="localhost", database="current_vz", user="moped", password=""
 )
 
+
+def get_people(crash):
+    print(crash)
+
+
 def main():
+    sql = """
+    select crash_id
+    from movement_direction_corrections
+    """
+    cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(sql)
+    crashes = cursor.fetchall()
+    cursor.close()
+
+    for crash in crashes:
+        people = get_people(crash)
 
 
 if __name__ == "__main__":
