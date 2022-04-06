328422d65b82fc0323c066a0576d5e28866d3e04
Frank Hereford <frankhereford@users.noreply.github.com>
2022-04-06 10:18:02 -0500

    query for injuries

--- atd-toolbox/direction_movement/app/find_aux_data.py
+++ atd-toolbox/direction_movement/app/find_aux_data.py
@@ -16,7 +16,26 @@ db = psycopg2.connect(
 
 
 def get_people(crash):
-    print(crash)
+    sql = """
+        with persons as (
+        select crash_id, unit_nbr, death_cnt as k, sus_serious_injry_cnt as a, nonincap_injry_cnt as b
+        from atd_txdot_primaryperson
+        UNION ALL
+        select crash_id, unit_nbr, death_cnt as k, sus_serious_injry_cnt as a, nonincap_injry_cnt as b
+        from atd_txdot_person)
+    select crashes.crash_id, coalesce(sum(persons.k),0) as k, coalesce(sum(persons.a),0) as a, coalesce(sum(persons.b),0) as b
+    from atd_txdot_crashes crashes
+    left join atd_txdot_units units on (crashes.crash_id = units.crash_id)
+    left join persons on (crashes.crash_id = persons.crash_id and units.unit_nbr = persons.unit_nbr)
+    where crashes.crash_id >= 9999
+    and crashes.crash_id = %s
+    group by crashes.crash_id
+    """
+    cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute(sql, (crash,))
+    injuries = cursor.fetchone()
+    cursor.close()
+    return injuries
 
 
 def main():
@@ -30,7 +49,8 @@ def main():
     cursor.close()
 
     for crash in crashes:
-        people = get_people(crash)
+        injuries = get_people(crash[0])
+        print(injuries)
 
 
 if __name__ == "__main__":
