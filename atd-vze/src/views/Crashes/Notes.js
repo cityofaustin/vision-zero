949c3b898cbc05b29db459d54cc7e26fcc65a2a0
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-07-07 16:57:37 -0500

    refetch data upon mutation and refresh note input box

--- atd-vze/src/views/Crashes/Notes.js
+++ atd-vze/src/views/Crashes/Notes.js
@@ -1,26 +1,16 @@
 import React, { useState } from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
-import { Card, CardHeader, CardBody, CardFooter, Table, Input, Button } from "reactstrap";
+import { Card, CardHeader, CardBody, CardFooter, Table, Input, Button, Col } from "reactstrap";
 import moment from "moment";
 import { notesDataMap } from "./notesDataMap";
 import { GET_NOTES, INSERT_NOTE } from "../../queries/notes";
 import { useAuth0, isReadOnly } from "../../auth/authContext";
-
-import DatePicker from "react-datepicker";
-// import required css from library
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-import styled from "styled-components";
-import { colors } from "../../styles/colors";
-import Handsontable from "handsontable";
 
 // declare a notes component
 const Notes = ({ crashId }) => {
 
   // add a state variable to manage value when new note in entered
   const [newNote, setNewNote] = useState("");
-  // add a state variable to manage the value when date is selected
-  const [date, setDate] = useState(new Date());
-  const [notesData, setNotesData] = useState([]);
 
   // disable edit features if only role is "readonly"
   const { getRoles } = useAuth0();
@@ -31,6 +21,7 @@ const Notes = ({ crashId }) => {
     variables: { crashId },
   });
 
+  // declare insert mutation function
   const [addNote] = useMutation(INSERT_NOTE);
 
   if (loading) return "Loading...";
@@ -40,34 +31,19 @@ const Notes = ({ crashId }) => {
   const keyField = "id";
   const fieldConfig = notesDataMap[0];
 
-  const StyledDatePicker = styled.div`
-  /* Add Bootstrap styles to picker inputs */
-  .react-datepicker__input-container > input {
-    height: calc(1.5em + 0.75rem + 2px);
-    padding: 0.375rem 0.75rem;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    color: ${colors.grey700};
-    background-color: ${colors.white};
-    background-clip: padding-box;
-    border: 1px solid ${colors.grey200};
-    border-radius: 0.25rem;
-  }
-`;
-
-  // function to handle add button
+  // function to handle add button click
   const handleAddNoteClick = () => {
-    const userEmail = localStorage.getItem("hasura_user_email"); //FIXME: is this the best place to declare this variable?
+    const userEmail = localStorage.getItem("hasura_user_email");
     addNote({
       variables: {
         note: newNote,
         crashId: crashId,
-        date: date,
         userEmail: userEmail
       }
+    }).then(response => {
+      setNewNote("");
+      refetch();
     });
-    refetch();
   };
 
   // render notes card and table
@@ -76,34 +52,29 @@ const Notes = ({ crashId }) => {
       <CardHeader>{fieldConfig.title}</CardHeader>
       <CardBody>
         <Table>
-          <thead>
-            <tr>
-              {/* display label for each field in table header */}
-              {Object.keys(fieldConfig.fields).map(field => (
-                <th key={`th_${fieldConfig.fields[field].label}`}>
-                  {fieldConfig.fields[field].label}
-                </th>
-              ))}
-            </tr>
-          </thead>
+          <tr>
+            {/* display label for each field in table header */}
+            {Object.keys(fieldConfig.fields).map(field => (
+              <th key={`th_${fieldConfig.fields[field].label}`}>
+                {fieldConfig.fields[field].label}
+              </th>
+            ))}
+            <th>
+            </th>
+          </tr>
           <tbody>
-            {/* display user input row if user is not read only */}
+            {/* display user input row for users with edit permissions*/}
             {!isReadOnly(roles) &&
               <tr>
                 <td>
-                  {/* render date input with calendar drop down */}
-                  <StyledDatePicker>
-                    <DatePicker selected={date} onChange={(date) =>
-                      setDate(date)} />
-                  </StyledDatePicker>
                 </td>
                 <td>
                 </td>
-                {/* render text box for user to add note */}
                 <td>
                   <Input
                     type="textarea"
                     placeholder="Enter new note here..."
+                    value={newNote}
                     onChange={e => setNewNote(e.target.value)}
                   />
                 </td>
@@ -134,13 +105,16 @@ const Notes = ({ crashId }) => {
                       </td>
                     );
                   })}
+                  <td>
+                  </td>
                 </tr>
               );
             })}
           </tbody>
         </Table>
       </CardBody>
-      <CardFooter></CardFooter>
+      <CardFooter>
+      </CardFooter>
     </Card>
   );
 };
