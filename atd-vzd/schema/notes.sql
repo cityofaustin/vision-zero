d444840bffed43d46351122f7da92af74abc864e
Frank Hereford <frankhereford@users.noreply.github.com>
2022-07-28 13:33:27 -0500

    Add migrations for newly created tables in VZ

---
+++ atd-vzd/schema/notes.sql
@@ -0,0 +1,25 @@
+-- Name: notes; Type: TABLE; Schema: public; Owner: atd_vz_data
+--
+
+CREATE TABLE public.notes (
+    id integer NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    date timestamp with time zone DEFAULT now() NOT NULL,
+    text text NOT NULL,
+    crash_id integer NOT NULL,
+    user_email text
+);
+
+
+--
+-- Name: notes_id_seq; Type: SEQUENCE; Schema: public; Owner: atd_vz_data
+--
+
+CREATE SEQUENCE public.notes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
