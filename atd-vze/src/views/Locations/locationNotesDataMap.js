43a36aa5e42bb2c6d9778144ddb3ff27fb8daf8f
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-08-02 16:50:20 -0500

    create location notes datamap

---
+++ atd-vze/src/views/Locations/locationNotesDataMap.js
@@ -0,0 +1,22 @@
+export const locationNotesDataMap = [
+    {
+      title: "Notes",
+  
+      fields: {
+        date: {
+          label: "Date",
+          editable: false,
+          format: "datetime",
+        },
+        user_email: {
+          label: "Updated By",
+          editable: false,
+        },
+        text: {
+          label: "Note",
+          editable: true,
+        },
+      },
+    },
+  ];
+  
