529a9dddd1d4695bd51f9d53195b213f1d6cd41b
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-08-04 10:24:38 -0500

    add mutation functions

--- atd-vze/src/queries/locationNotes.js
+++ atd-vze/src/queries/locationNotes.js
@@ -13,3 +13,47 @@ export const GET_LOCATION_NOTES = gql`
     }
   }
 `;
+
+export const INSERT_LOCATION_NOTE = gql`
+  mutation InsertLocationNote($note: String!, $locationId: String!, $userEmail: String){
+    insert_location_notes(objects: {
+      text: $note
+      location_id: $locationId
+      user_email: $userEmail
+    }) {
+      returning {
+        location_id
+        text
+        date
+        user_email
+      }
+    }
+  }
+`;
+
+export const UPDATE_LOCATION_NOTE = gql`
+  mutation UpdateNote($note: String!, $id: Int!) {
+    update_location_notes_by_pk(
+      pk_columns: {id: $id}
+      _set: {text: $note}
+    ) {
+        location_id
+        text
+        date
+        user_email
+      }
+  }
+`;
+
+export const DELETE_LOCATION_NOTE = gql`
+  mutation DeleteNote($id: Int!) {
+    delete_location_notes_by_pk(
+      id: $id
+    ) {
+      location_id
+      text
+      date
+      user_email
+    }
+  }
+`;
