2c9f1a268692062e0c1f03711bbedab1e23e9a69
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-08-11 09:26:28 -0500

    oops accidentally deleted these

---
+++ atd-vze/src/views/Crashes/recommendationsDataMap.js
@@ -0,0 +1,20 @@
+export const recommendationsDataMap = {
+  title: "Fatality Review Board Recommendations",
+
+  fields: {
+    coordination_partner_id: {
+      label: "Coordination Partner: ",
+      lookupOptions: "atd__coordination_partners_lkp",
+      key: "description",
+    },
+    recommendation_status_id: {
+      label: "Status: ",
+      lookupOptions: "atd__recommendation_status_lkp",
+      key: "description",
+    },
+    text: {
+      label: "Note",
+      key: "text",
+    },
+  },
+};
