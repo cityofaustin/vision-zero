4aece6c363cd101bda7ade23858cfe3a28b8e9d0
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-08-11 09:15:44 -0500

    prettier formatting

--- atd-vze/src/queries/recommendations.js
+++ atd-vze/src/queries/recommendations.js
@@ -2,7 +2,7 @@ import { gql } from "apollo-boost";
 
 export const GET_RECOMMENDATIONS = gql`
   query FindRecommendations($crashId: Int) {
-    recommendations(where: {crash_id: {_eq: $crashId}}) {
+    recommendations(where: { crash_id: { _eq: $crashId } }) {
       id
       created_at
       text
