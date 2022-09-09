f84a6a4ae035965f4baab08ddb98109bed3fc950
John Clary <john.clary@austintexas.gov>
2022-09-09 14:08:08 -0400

    linting

--- atd-vze/src/helpers/map.js
+++ atd-vze/src/helpers/map.js
@@ -2,92 +2,89 @@
 const NEARMAP_KEY = process.env.REACT_APP_NEARMAP_KEY;
 
 export const LOCATION_MAP_CONFIG = {
-    mapStyle: "mapbox://styles/mapbox/satellite-streets-v11",
-    sources: {
-        aerials: 
-        {
-            type: "raster",
-            tiles: [
-                `https://api.nearmap.com/tiles/v3/Vert/{z}/{x}/{y}.jpg?apikey=${NEARMAP_KEY}`,
-            ],
-            tileSize: 256,
-        }
+  mapStyle: "mapbox://styles/mapbox/satellite-streets-v11",
+  sources: {
+    aerials: {
+      type: "raster",
+      tiles: [
+        `https://api.nearmap.com/tiles/v3/Vert/{z}/{x}/{y}.jpg?apikey=${NEARMAP_KEY}`,
+      ],
+      tileSize: 256,
     },
-    layers: { 
-        aerials:
-            {
-                id: "simple-tiles",
-                type: "raster",
-                source: "raster-tiles",
-                minzoom: 0,
-                maxzoom: 22,
-          }, 
-          streetLabels:
-            {                 
-            // borrowed from mapbox mapbox streets v11 style
-            type: "symbol",
-            metadata: {
-              "mapbox:featureComponent": "road-network",
-              "mapbox:group": "Road network, road-labels",
-            },
-            source: "composite",
-            "source-layer": "road",
-            minzoom: 12,
-            filter: [
-              "all",
-              ["has", "name"],
-              [
-                "match",
-                ["get", "class"],
-                [
-                  "motorway",
-                  "trunk",
-                  "primary",
-                  "secondary",
-                  "tertiary",
-                  "street",
-                  "street_limited",
-                ],
-                true,
-                false,
-              ],
-            ],
-            layout: {
-              "text-size": [
-                "interpolate",
-                ["linear"],
-                ["zoom"],
-                10,
-                [
-                  "match",
-                  ["get", "class"],
-                  ["motorway", "trunk", "primary", "secondary", "tertiary"],
-                  10,
-                  9,
-                ],
-                18,
-                [
-                  "match",
-                  ["get", "class"],
-                  ["motorway", "trunk", "primary", "secondary", "tertiary"],
-                  16,
-                  14,
-                ],
-              ],
-              "text-max-angle": 30,
-              "text-font": ["DIN Pro Regular", "Arial Unicode MS Regular"],
-              "symbol-placement": "line",
-              "text-padding": 1,
-              "text-rotation-alignment": "map",
-              "text-pitch-alignment": "viewport",
-              "text-field": ["coalesce", ["get", "name_en"], ["get", "name"]],
-              "text-letter-spacing": 0.01,
-            },
-            paint: {
-              "text-color": "#fff",
-              "text-halo-color": "#000",
-              "text-halo-width": 1,
-            },
-        }
-    }
+  },
+  layers: {
+    aerials: {
+      id: "simple-tiles",
+      type: "raster",
+      source: "raster-tiles",
+      minzoom: 0,
+      maxzoom: 22,
+    },
+    streetLabels: {
+      // borrowed from mapbox mapbox streets v11 style
+      type: "symbol",
+      metadata: {
+        "mapbox:featureComponent": "road-network",
+        "mapbox:group": "Road network, road-labels",
+      },
+      source: "composite",
+      "source-layer": "road",
+      minzoom: 12,
+      filter: [
+        "all",
+        ["has", "name"],
+        [
+          "match",
+          ["get", "class"],
+          [
+            "motorway",
+            "trunk",
+            "primary",
+            "secondary",
+            "tertiary",
+            "street",
+            "street_limited",
+          ],
+          true,
+          false,
+        ],
+      ],
+      layout: {
+        "text-size": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          10,
+          [
+            "match",
+            ["get", "class"],
+            ["motorway", "trunk", "primary", "secondary", "tertiary"],
+            10,
+            9,
+          ],
+          18,
+          [
+            "match",
+            ["get", "class"],
+            ["motorway", "trunk", "primary", "secondary", "tertiary"],
+            16,
+            14,
+          ],
+        ],
+        "text-max-angle": 30,
+        "text-font": ["DIN Pro Regular", "Arial Unicode MS Regular"],
+        "symbol-placement": "line",
+        "text-padding": 1,
+        "text-rotation-alignment": "map",
+        "text-pitch-alignment": "viewport",
+        "text-field": ["coalesce", ["get", "name_en"], ["get", "name"]],
+        "text-letter-spacing": 0.01,
+      },
+      paint: {
+        "text-color": "#fff",
+        "text-halo-color": "#000",
+        "text-halo-width": 1,
+      },
+    },
+  },
 };
