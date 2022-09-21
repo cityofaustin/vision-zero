8c12d8322f3c22b792f94b0b4a303e5db3f342a9
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-09-21 14:31:30 -0500

    move to a notes component folder

---
+++ atd-vze/src/Components/Notes/notesDataMap.js
@@ -0,0 +1,21 @@
+export const notesDataMap = [
+  {
+    title: "Notes",
+
+    fields: {
+      date: {
+        label: "Date",
+        editable: false,
+        format: "datetime",
+      },
+      user_email: {
+        label: "Updated By",
+        editable: false,
+      },
+      text: {
+        label: "Note",
+        editable: true,
+      },
+    },
+  },
+];
