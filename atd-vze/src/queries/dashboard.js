3897e7d647e3b8bdefab436dfab6f6086c5c0f81
Mike <mike.dilley@gmail.com>
2022-09-22 18:07:10 -0500

    Sketch out years_of_life_lost generated field

--- atd-vze/src/queries/dashboard.js
+++ atd-vze/src/queries/dashboard.js
@@ -60,3 +60,17 @@ export const GET_CRASHES_YTD = gql`
     }
   }
 `;
+
+// SELECT
+//   75 - prsn_age as years_of_life_lost
+// FROM
+//   atd_txdot_primaryperson as pp
+// WHERE
+//   1 = 1
+//   AND pp.prsn_injry_sev_id = 4
+//   AND pp.prsn_age < 75
+//   AND pp.prsn_age IS NOT NULL
+
+// ALTER TABLE atd_txdot_person
+// ADD COLUMN years_of_life_lost integer
+// GENERATED ALWAYS AS (GREATEST(75 - prsn_age, 0)) STORED;
