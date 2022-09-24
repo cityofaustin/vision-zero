3943c40023c92eea7352fb1904c456afab661280
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-24 15:19:42 -0500

    don't use numpy

--- atd-etl/prefect/afd_ems_incidents/afd_incident_upload.py
+++ atd-etl/prefect/afd_ems_incidents/afd_incident_upload.py
@@ -29,7 +29,6 @@ from datetime import datetime, timedelta
 import psycopg2
 import psycopg2.extras
 from psycopg2 import Error
-import numpy as np
 from prefect.storage import GitHub
 from prefect.run_configs import UniversalRun
 from prefect.backend import get_key_value
