ef868571375b43ea1f77ed56888f468a825dbb52
Frank Hereford <frankhereford@users.noreply.github.com>
2022-09-26 11:39:57 -0500

    make this more developer friendly

--- atd-etl/prefect/afd_ems_incidents/afd_incident_upload.py
+++ atd-etl/prefect/afd_ems_incidents/afd_incident_upload.py
@@ -20,6 +20,7 @@ import datetime
 import json
 import boto3
 import pprint
+import os
 import time
 import email
 import tempfile
@@ -38,30 +39,43 @@ pp = pprint.PrettyPrinter(indent=4)
 kv_data = get_key_value(key="Vision Zero Development")
 environment_variables = json.loads(kv_data)
 
-# Retrieve the db configuration
-DB_USERNAME = environment_variables["AFD_DB_USERNAME"]
-DB_PASSWORD = environment_variables["AFD_DB_PASSWORD"]
-DB_HOSTNAME = environment_variables["AFD_DB_HOSTNAME"]
-DB_PORT = environment_variables["AFD_DB_PORT"]
-DB_DATABASE = environment_variables["AFD_DB_DATABASE"]
-AWS_ACCESS_KEY_ID = environment_variables["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = environment_variables["AWS_SECRET_ACCESS_KEY"]
-AFD_S3_SOURCE_BUCKET = environment_variables["AFD_S3_SOURCE_BUCKET"]
-AFD_S3_ARCHIVE_BUCKET = environment_variables["AFD_S3_ARCHIVE_BUCKET"]
-AFD_S3_SOURCE_PREFIX = environment_variables["AFD_S3_SOURCE_PREFIX"]
-AFD_S3_ARCHIVE_PREFIX = environment_variables["AFD_S3_ARCHIVE_PREFIX"]
+DB_USERNAME = None
+DB_PASSWORD = None
+DB_HOSTNAME = None
+DB_PORT = None
+DB_DATABASE = None
+AWS_ACCESS_KEY_ID = None
+AWS_SECRET_ACCESS_KEY = None
+AFD_S3_SOURCE_BUCKET = None
+AFD_S3_ARCHIVE_BUCKET = None
+AFD_S3_SOURCE_PREFIX = None
+AFD_S3_ARCHIVE_PREFIX = None
 
-# DB_USERNAME = os.getenv("AFD_DB_USERNAME")
-# DB_PASSWORD = os.getenv("AFD_DB_PASSWORD")
-# DB_HOSTNAME = os.getenv("AFD_DB_HOSTNAME")
-# DB_PORT = os.getenv("AFD_DB_PORT")
-# DB_DATABASE = os.getenv("AFD_DB_DATABASE")
-# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-# AFD_S3_SOURCE_BUCKET = os.getenv("AFD_S3_SOURCE_BUCKET")
-# AFD_S3_ARCHIVE_BUCKET = os.getenv("AFD_S3_ARCHIVE_BUCKET")
-# AFD_S3_SOURCE_PREFIX = os.getenv("AFD_S3_SOURCE_PREFIX")
-# AFD_S3_ARCHIVE_PREFIX = os.getenv("AFD_S3_ARCHIVE_PREFIX")
+# Retrieve the db configuration
+if False:
+    DB_USERNAME = environment_variables["AFD_DB_USERNAME"]
+    DB_PASSWORD = environment_variables["AFD_DB_PASSWORD"]
+    DB_HOSTNAME = environment_variables["AFD_DB_HOSTNAME"]
+    DB_PORT = environment_variables["AFD_DB_PORT"]
+    DB_DATABASE = environment_variables["AFD_DB_DATABASE"]
+    AWS_ACCESS_KEY_ID = environment_variables["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = environment_variables["AWS_SECRET_ACCESS_KEY"]
+    AFD_S3_SOURCE_BUCKET = environment_variables["AFD_S3_SOURCE_BUCKET"]
+    AFD_S3_ARCHIVE_BUCKET = environment_variables["AFD_S3_ARCHIVE_BUCKET"]
+    AFD_S3_SOURCE_PREFIX = environment_variables["AFD_S3_SOURCE_PREFIX"]
+    AFD_S3_ARCHIVE_PREFIX = environment_variables["AFD_S3_ARCHIVE_PREFIX"]
+else:
+    DB_USERNAME = os.getenv("AFD_DB_USERNAME")
+    DB_PASSWORD = os.getenv("AFD_DB_PASSWORD")
+    DB_HOSTNAME = os.getenv("AFD_DB_HOSTNAME")
+    DB_PORT = os.getenv("AFD_DB_PORT")
+    DB_DATABASE = os.getenv("AFD_DB_DATABASE")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+    AFD_S3_SOURCE_BUCKET = os.getenv("AFD_S3_SOURCE_BUCKET")
+    AFD_S3_ARCHIVE_BUCKET = os.getenv("AFD_S3_ARCHIVE_BUCKET")
+    AFD_S3_SOURCE_PREFIX = os.getenv("AFD_S3_SOURCE_PREFIX")
+    AFD_S3_ARCHIVE_PREFIX = os.getenv("AFD_S3_ARCHIVE_PREFIX")
 
 
 @task
@@ -270,5 +284,5 @@ with Flow("AFD Import ETL") as flow:
     clean_up_token = clean_up(attachment_location, upstream_tasks=[upload_token])
 
 # you can use record_age_maximum=False if you want a full import
-flow.run(parameters=dict(record_age_maximum=60))
+flow.run(parameters=dict(record_age_maximum=10))
 # f.register(project_name="vision-zero")
