2b52fd71746b86d0fa0da1f59ca462ee6764c608
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-09-27 18:02:49 -0500

    add preselected values and make updates to component to match mock up

--- atd-vze/src/views/Crashes/Recommendations/RecommendationMultipleSelectDropdown.js
+++ atd-vze/src/views/Crashes/Recommendations/RecommendationMultipleSelectDropdown.js
@@ -1,18 +1,42 @@
 import React, { useState } from "react";
 import Multiselect from "multiselect-react-dropdown";
+import "../crash.scss";
 
-const RecommendationMultipleSelectDropdown = ({ options, onOptionClick }) => {
+const RecommendationMultipleSelectDropdown = ({
+  options,
+  onOptionClick,
+  partners,
+  fieldConfig,
+}) => {
   // Add a null option to enable users to clear out the value
   const makeOptionsWithNullOption = options => [
     { id: null, description: "None" },
     ...options,
   ];
 
+  const getSelectedValues = ({ lookupOptions, key }) => {
+    return partners.map(partner => partner?.[lookupOptions] || "");
+  };
+
+  console.log(getSelectedValues(fieldConfig.fields.partner_id));
+
   return (
     <Multiselect
       options={makeOptionsWithNullOption(options)}
       displayValue={"description"}
       showCheckbox
+      selectedValues={getSelectedValues(fieldConfig.fields.partner_id)}
+      showArrow
+      hideSelectedList
+      placeholder={getSelectedValues(fieldConfig.fields.partner_id).map(
+        partner => partner.description
+      )}
+      id="css_custom"
+      style={{
+        searchBox: {
+          border: "none",
+        },
+      }}
     ></Multiselect>
   );
 };
