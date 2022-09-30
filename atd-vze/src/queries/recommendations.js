d0bbfea7938bc97efd2ebd965467bdbed5de1964
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-09-29 19:02:18 -0500

    fix query to many to many for recommendations_partners table

--- atd-vze/src/queries/recommendations.js
+++ atd-vze/src/queries/recommendations.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost";
 
 export const GET_RECOMMENDATIONS = gql`
-  query FindRecommendations($crashId: Int, $recommendationId: Int) {
+  query FindRecommendations($crashId: Int) {
     recommendations(where: { crash_id: { _eq: $crashId } }) {
       id
       created_at
@@ -9,11 +9,14 @@ export const GET_RECOMMENDATIONS = gql`
       created_by
       crash_id
       update
-      atd__coordination_partners_lkp {
-        description
-      }
       atd__recommendation_status_lkp {
         description
+      }
+      recommendations_partners {
+        atd__coordination_partners_lkp {
+          id
+          description
+        }
       }
     }
     atd__coordination_partners_lkp(order_by: { description: asc }) {
@@ -24,15 +27,6 @@ export const GET_RECOMMENDATIONS = gql`
       id
       description
     }
-    recommendations_partners(
-      where: { recommendation_id: { _eq: $recommendationId } }
-    ) {
-      recommendation_id
-      partner_id
-      atd__coordination_partners_lkp {
-        description
-      }
-    }
   }
 `;
 
@@ -42,8 +36,8 @@ export const INSERT_RECOMMENDATION = gql`
     $update: String
     $crashId: Int
     $userEmail: String
-    $coordination_partner_id: Int
     $recommendation_status_id: Int
+    $partner_id: Int
   ) {
     insert_recommendations(
       objects: {
@@ -51,8 +45,8 @@ export const INSERT_RECOMMENDATION = gql`
         update: $update
         crash_id: $crashId
         created_by: $userEmail
-        coordination_partner_id: $coordination_partner_id
         recommendation_status_id: $recommendation_status_id
+        recommendations_partners: { data: { partner_id: $partner_id } }
       }
     ) {
       returning {
