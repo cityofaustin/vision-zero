659cc83513d0abbaa72d0c527836a3f53feae005
dr_g0nz0 <67130963+roseeichelmann@users.noreply.github.com>
2022-09-29 19:06:23 -0500

    add handleOptionClick function, add onSelect component property, adjust comp properties for selectedValues and pplaceholder for if there are no partners yet

--- atd-vze/src/views/Crashes/Recommendations/RecommendationMultipleSelectDropdown.js
+++ atd-vze/src/views/Crashes/Recommendations/RecommendationMultipleSelectDropdown.js
@@ -7,7 +7,14 @@ const RecommendationMultipleSelectDropdown = ({
   onOptionClick,
   partners,
   fieldConfig,
+  field,
 }) => {
+  const handleOptionClick = selectedItem => {
+    console.log(selectedItem[0].id);
+    // Mutation expect lookup IDs as integers
+    const valuesObject = { [field]: parseInt(selectedItem[0].id) };
+    onOptionClick(valuesObject);
+  };
   // Add a null option to enable users to clear out the value
   const makeOptionsWithNullOption = options => [
     { id: null, description: "None" },
@@ -23,18 +30,27 @@ const RecommendationMultipleSelectDropdown = ({
       options={makeOptionsWithNullOption(options)}
       displayValue={"description"}
       showCheckbox
-      selectedValues={getSelectedValues(fieldConfig.fields.partner_id)}
+      selectedValues={
+        partners ? getSelectedValues(fieldConfig.fields.partner_id) : null
+      }
       showArrow
       hideSelectedList
-      placeholder={getSelectedValues(fieldConfig.fields.partner_id)
-        .map(partner => partner.description)
-        .join(",  ")}
+      placeholder={
+        partners
+          ? getSelectedValues(fieldConfig.fields.partner_id)
+              .map(partner => partner.description)
+              .join(",  ")
+          : null
+      }
       id="css_custom"
       style={{
         searchBox: {
           border: "none",
         },
       }}
+      onSelect={(selectedList, selectedItem) =>
+        handleOptionClick(selectedList, selectedItem)
+      }
     ></Multiselect>
   );
 };
