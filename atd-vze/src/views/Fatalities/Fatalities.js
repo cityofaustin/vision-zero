14ddef67cab0795dd6df2865efb5f76124d41faf
rose <rose.eichelmann@gmail.com>
2023-01-13 13:23:50 -0600

    create bare bones fatalities component

---
+++ atd-vze/src/views/Fatalities/Fatalities.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { withApollo } from "react-apollo";
+
+import GridTable from "../../Components/GridTable";
+import gqlAbstract from "../../queries/gqlAbstract";
+
+const Fatalities = () => {
+  return <div></div>;
+};
+
+export default withApollo(Fatalities);
