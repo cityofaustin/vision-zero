45ccbc332f1b010301ca8dba23d11ae102b37f80
rose <rose.eichelmann@gmail.com>
2023-01-23 12:01:13 -0600

    create Fatalities page component

--- atd-vze/src/views/Fatalities/Fatalities.js
+++ atd-vze/src/views/Fatalities/Fatalities.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { withApollo } from "react-apollo";
 
+import { subYears } from "date-fns";
 import GridTable from "../../Components/GridTable";
 import gqlAbstract from "../../queries/gqlAbstract";
+import { fatalityGridTableColumns } from "./fatalityGridTableParameters";
+
+// Our initial query configuration
+let queryConf = {
+  table: "fatalities",
+  single_item: "crashes",
+  showDateRange: false,
+  columns: fatalityGridTableColumns,
+  order_by: {},
+  where: {},
+  limit: 25,
+  offset: 0,
+};
+
+const fatalitiesQuery = new gqlAbstract(queryConf);
+
+const minDate = subYears(new Date(), 10);
 
 const Fatalities = () => {
-  return <div></div>;
+  return (
+    <GridTable query={fatalitiesQuery} title={"Fatalities"} minDate={minDate} />
+  );
 };
 
 export default withApollo(Fatalities);
