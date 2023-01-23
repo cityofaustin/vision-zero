b7a1c5a1cda8ed51372f7565918da8033f66cd19
rose <rose.eichelmann@gmail.com>
2023-01-23 11:58:57 -0600

    add fatalities page to nav bar, admin only access

--- atd-vze/src/_nav.js
+++ atd-vze/src/_nav.js
@@ -30,7 +30,6 @@ export const navigation = roles => {
         url: "/locations",
         icon: "icon-map",
       },
-      { name: "Fatalities", url: "/fatalities" },
       {
         divider: true,
       },
@@ -52,7 +51,11 @@ export const navigation = roles => {
       title: true,
       name: "Admin",
     },
-
+    {
+      name: "Fatalities",
+      url: "/fatalities",
+      icon: "fa-light fa-heart-pulse",
+    },
     {
       name: "Reports",
       url: "#",
