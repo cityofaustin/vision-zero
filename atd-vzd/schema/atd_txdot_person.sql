47eeba9108271ea5b34134e71d27802508678891
rose <rose.eichelmann@gmail.com>
2023-02-16 17:34:00 -0600

    update person schema with new trigger added

--- atd-vzd/schema/atd_txdot_person.sql
+++ atd-vzd/schema/atd_txdot_person.sql
@@ -2,10 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.6
--- Dumped by pg_dump version 10.10
-
--- Started on 2019-10-15 13:48:03 CDT
+-- Dumped from database version 14.6 (Debian 14.6-1.pgdg110+1)
+-- Dumped by pg_dump version 14.6 (Ubuntu 14.6-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,11 +18,10 @@ SET row_security = off;
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
--- TOC entry 248 (class 1259 OID 2345860)
--- Name: atd_txdot_person; Type: TABLE; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person; Type: TABLE; Schema: public; Owner: visionzero
 --
 
 CREATE TABLE public.atd_txdot_person (
@@ -40,7 +37,7 @@ CREATE TABLE public.atd_txdot_person (
     prsn_name_sfx character varying(32),
     prsn_injry_sev_id integer,
     prsn_age integer,
-    prsn_ethnicity_id integer,
+    prsn_ethnicity_id integer DEFAULT 0,
     prsn_gndr_id integer,
     prsn_ejct_id integer,
     prsn_rest_id integer,
@@ -67,15 +64,18 @@ CREATE TABLE public.atd_txdot_person (
     updated_by character varying(64),
     person_id integer NOT NULL,
     is_retired boolean DEFAULT false,
-    years_of_life_lost integer DEFAULT 0 NOT NULL
+    years_of_life_lost integer GENERATED ALWAYS AS (
+CASE
+    WHEN (prsn_injry_sev_id = 4) THEN GREATEST((75 - prsn_age), 0)
+    ELSE 0
+END) STORED
 );
 
 
-ALTER TABLE public.atd_txdot_person OWNER TO atd_vz_data;
+ALTER TABLE public.atd_txdot_person OWNER TO visionzero;
 
 --
--- TOC entry 357 (class 1259 OID 2346257)
--- Name: atd_txdot_person_person_id_seq; Type: SEQUENCE; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person_person_id_seq; Type: SEQUENCE; Schema: public; Owner: visionzero
 --
 
 CREATE SEQUENCE public.atd_txdot_person_person_id_seq
@@ -87,69 +87,147 @@ CREATE SEQUENCE public.atd_txdot_person_person_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.atd_txdot_person_person_id_seq OWNER TO atd_vz_data;
+ALTER TABLE public.atd_txdot_person_person_id_seq OWNER TO visionzero;
 
 --
--- TOC entry 5784 (class 0 OID 0)
--- Dependencies: 357
--- Name: atd_txdot_person_person_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person_person_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: visionzero
 --
 
 ALTER SEQUENCE public.atd_txdot_person_person_id_seq OWNED BY public.atd_txdot_person.person_id;
 
 
 --
--- TOC entry 5620 (class 2604 OID 2346322)
--- Name: atd_txdot_person person_id; Type: DEFAULT; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person person_id; Type: DEFAULT; Schema: public; Owner: visionzero
 --
 
 ALTER TABLE ONLY public.atd_txdot_person ALTER COLUMN person_id SET DEFAULT nextval('public.atd_txdot_person_person_id_seq'::regclass);
 
 
 --
--- TOC entry 5621 (class 1259 OID 2346775)
--- Name: atd_txdot_person_is_retired_index; Type: INDEX; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person atd_txdot_person_person_id_key; Type: CONSTRAINT; Schema: public; Owner: visionzero
+--
+
+ALTER TABLE ONLY public.atd_txdot_person
+    ADD CONSTRAINT atd_txdot_person_person_id_key UNIQUE (person_id);
+
+
+--
+-- Name: atd_txdot_person atd_txdot_person_pkey; Type: CONSTRAINT; Schema: public; Owner: visionzero
+--
+
+ALTER TABLE ONLY public.atd_txdot_person
+    ADD CONSTRAINT atd_txdot_person_pkey PRIMARY KEY (person_id);
+
+
+--
+-- Name: atd_txdot_person atd_txdot_person_unique; Type: CONSTRAINT; Schema: public; Owner: visionzero
+--
+
+ALTER TABLE ONLY public.atd_txdot_person
+    ADD CONSTRAINT atd_txdot_person_unique UNIQUE (crash_id, unit_nbr, prsn_nbr, prsn_type_id, prsn_occpnt_pos_id);
+
+
+--
+-- Name: atd_txdot_person_death_cnt_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_death_cnt_index ON public.atd_txdot_person USING btree (death_cnt);
+
+
+--
+-- Name: atd_txdot_person_is_retired_index; Type: INDEX; Schema: public; Owner: visionzero
 --
 
 CREATE INDEX atd_txdot_person_is_retired_index ON public.atd_txdot_person USING btree (is_retired);
 
 
 --
--- TOC entry 5622 (class 1259 OID 2346776)
--- Name: atd_txdot_person_person_id_index; Type: INDEX; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person_person_id_index; Type: INDEX; Schema: public; Owner: visionzero
 --
 
 CREATE INDEX atd_txdot_person_person_id_index ON public.atd_txdot_person USING btree (person_id);
 
 
 --
--- TOC entry 5623 (class 1259 OID 2346796)
--- Name: idx_atd_txdot_person_crash_id; Type: INDEX; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person_prsn_age_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_age_index ON public.atd_txdot_person USING btree (prsn_age);
+
+
+--
+-- Name: atd_txdot_person_prsn_death_date_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_death_date_index ON public.atd_txdot_person USING btree (prsn_death_date);
+
+
+--
+-- Name: atd_txdot_person_prsn_death_time_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_death_time_index ON public.atd_txdot_person USING btree (prsn_death_time);
+
+
+--
+-- Name: atd_txdot_person_prsn_ethnicity_id_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_ethnicity_id_index ON public.atd_txdot_person USING btree (prsn_ethnicity_id);
+
+
+--
+-- Name: atd_txdot_person_prsn_gndr_id_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_gndr_id_index ON public.atd_txdot_person USING btree (prsn_gndr_id);
+
+
+--
+-- Name: atd_txdot_person_prsn_injry_sev_id_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_prsn_injry_sev_id_index ON public.atd_txdot_person USING btree (prsn_injry_sev_id);
+
+
+--
+-- Name: atd_txdot_person_sus_serious_injry_cnt_index; Type: INDEX; Schema: public; Owner: visionzero
+--
+
+CREATE INDEX atd_txdot_person_sus_serious_injry_cnt_index ON public.atd_txdot_person USING btree (sus_serious_injry_cnt);
+
+
+--
+-- Name: idx_atd_txdot_person_crash_id; Type: INDEX; Schema: public; Owner: visionzero
 --
 
 CREATE INDEX idx_atd_txdot_person_crash_id ON public.atd_txdot_person USING btree (crash_id);
 
 
 --
--- TOC entry 5625 (class 2620 OID 2346816)
--- Name: atd_txdot_person atd_txdot_person_audit_log; Type: TRIGGER; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person atd_txdot_person_audit_log; Type: TRIGGER; Schema: public; Owner: visionzero
 --
 
-CREATE TRIGGER atd_txdot_person_audit_log BEFORE UPDATE ON public.atd_txdot_person FOR EACH ROW EXECUTE PROCEDURE public.atd_txdot_person_updates_audit_log();
+CREATE TRIGGER atd_txdot_person_audit_log BEFORE UPDATE ON public.atd_txdot_person FOR EACH ROW EXECUTE FUNCTION public.atd_txdot_person_updates_audit_log();
+
+ALTER TABLE public.atd_txdot_person DISABLE TRIGGER atd_txdot_person_audit_log;
 
 
 --
--- TOC entry 5624 (class 2606 OID 2346844)
--- Name: atd_txdot_person atd_txdot_person_prsn_injry_sev_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: atd_vz_data
+-- Name: atd_txdot_person atd_txdot_person_fatal_insert; Type: TRIGGER; Schema: public; Owner: visionzero
+--
+
+CREATE TRIGGER atd_txdot_person_fatal_insert AFTER INSERT ON public.atd_txdot_person FOR EACH ROW WHEN ((new.prsn_injry_sev_id = 4)) EXECUTE FUNCTION public.fatality_insert();
+
+
+--
+-- Name: atd_txdot_person atd_txdot_person_prsn_injry_sev_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: visionzero
 --
 
 ALTER TABLE ONLY public.atd_txdot_person
     ADD CONSTRAINT atd_txdot_person_prsn_injry_sev_id_fkey FOREIGN KEY (prsn_injry_sev_id) REFERENCES public.atd_txdot__injry_sev_lkp(injry_sev_id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 
--- Completed on 2019-10-15 13:48:07 CDT
-
 --
 -- PostgreSQL database dump complete
 --
-
