688032a775d7e1365128ff4ba635c18a222bebc7
rose <rose.eichelmann@gmail.com>
2023-03-02 10:38:28 -0600

    add austin full purpose flag conditional

--- atd-toolbox/fatalities_backfill/fatalities_backfill.sql
+++ atd-toolbox/fatalities_backfill/fatalities_backfill.sql
@@ -4,10 +4,10 @@ INSERT INTO fatalities (crash_id, primaryperson_id, location, year)
 SELECT atd_txdot_primaryperson.crash_id, primaryperson_id, CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), TO_CHAR(crash_date,'yyyy')
 FROM atd_txdot_primaryperson
 JOIN atd_txdot_crashes ON atd_txdot_primaryperson.crash_id = atd_txdot_crashes.crash_id
-WHERE prsn_injry_sev_id = 4;
+WHERE prsn_injry_sev_id = 4 AND austin_full_purpose = "Y";
 
 INSERT INTO fatalities (crash_id, person_id, location, year)
 SELECT atd_txdot_person.crash_id, person_id, CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), TO_CHAR(crash_date,'yyyy')
 FROM atd_txdot_person
 JOIN atd_txdot_crashes ON atd_txdot_person.crash_id = atd_txdot_crashes.crash_id
-WHERE prsn_injry_sev_id = 4;
+WHERE prsn_injry_sev_id = 4 AND austin_full_purpose = "Y";
