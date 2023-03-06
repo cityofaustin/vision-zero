f1b7ff592c7cb31d0764f4a4ee75fd06aad2dd79
rose <rose.eichelmann@gmail.com>
2023-03-06 13:11:32 -0600

    tidy up comments

--- atd-vzd/triggers/crashes_update.sql
+++ atd-vzd/triggers/crashes_update.sql
@@ -1,13 +1,3 @@
--- if crash location/coordinates is updated
--- go through all persons associated with that crash
--- and if that person was killed
--- and if the crash was previously outside of austin but now its in austin
--- add the person to the fatalities table
--- else if the crash was previously in austin but is now outside of austin
--- delete person from the fatalities table
--- else if the crash was in austin and is still in austin but the location changed
--- just update the location column for that person in the fatalities table
-
 CREATE OR REPLACE FUNCTION crash_location_update() 
     RETURNS trigger
     LANGUAGE plpgsql
@@ -18,11 +8,10 @@ AS $function$
     BEGIN
         -- check if the position was updated
         IF (OLD.position IS DISTINCT FROM NEW.position) THEN
-            -- loop thru fatalities in person table if there are any
+            -- loop through fatalities in person table if there are any
             FOR person IN (SELECT person_id FROM atd_txdot_person WHERE crash_id = NEW.crash_id AND prsn_injry_sev_id = 4) loop
                 -- if person is not in fatalities table but is in austin then add them
                 IF (NOT EXISTS (SELECT person_id FROM fatalities WHERE person_id = person.person_id) AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), NEW.position)) THEN
-                    -- insert person into fatalities table
                     INSERT INTO fatalities (crash_id, person_id, year, location)
                     VALUES (NEW.crash_id, NEW.person_id, TO_CHAR(NEW.crash_date,'yyyy'), CONCAT_WS(' ', NEW.rpt_block_num, NEW.rpt_street_pfx, NEW.rpt_street_name, '(', NEW.rpt_sec_block_num, NEW.rpt_sec_street_pfx, NEW.rpt_sec_street_name, ')'));
                 END IF;
@@ -31,11 +20,10 @@ AS $function$
                     DELETE FROM fatalities WHERE person_id = person.person_id;
                 END IF;
             end loop;
-            -- loop thru fatalities in primary person table if there are any
+            -- loop through fatalities in primary person table if there are any
             FOR primary_person IN (SELECT primaryperson_id FROM atd_txdot_primaryperson WHERE crash_id = NEW.crash_id AND prsn_injry_sev_id = 4) loop
                 -- if person is not in fatalities table but is in austin then add them
                 IF (NOT EXISTS (SELECT primaryperson_id FROM fatalities WHERE primaryperson_id = primary_person.primaryperson_id) AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), NEW.position)) THEN
-                    -- insert person into fatalities table
                     INSERT INTO fatalities (crash_id, primaryperson_id, year, location)
                     VALUES (NEW.crash_id, NEW.primaryperson_id, TO_CHAR(NEW.crash_date,'yyyy'), CONCAT_WS(' ', NEW.rpt_block_num, NEW.rpt_street_pfx, NEW.rpt_street_name, '(', NEW.rpt_sec_block_num, NEW.rpt_sec_street_pfx, NEW.rpt_sec_street_name, ')'));
                 END IF;
@@ -45,7 +33,7 @@ AS $function$
                 END IF;
             end loop;
         END IF;
-        -- if the crash date was updated then update the year column for that crash in the fatalities table
+        -- if the crash date was updated then update the year column in the fatalities table
         IF (OLD.crash_date IS DISTINCT FROM NEW.crash_date) THEN
             UPDATE fatalities
                 SET year = TO_CHAR(NEW.crash_date,'yyyy')
