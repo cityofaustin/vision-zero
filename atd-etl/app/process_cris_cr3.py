6ca5e1d225b8daab0accd632c760aa447071773e
Mike <mike.dilley@gmail.com>
2023-03-06 16:31:40 -0600

    Log only crash IDs that are going to be requested

--- atd-etl/app/process_cris_cr3.py
+++ atd-etl/app/process_cris_cr3.py
@@ -42,7 +42,7 @@ skipped_uploads_and_updates = []
 # CR3s for these crash IDs are not available in the CRIS database.
 # We can skip requesting them.
 # See https://github.com/cityofaustin/atd-data-tech/issues/9786
-known_skips = ["180290542"]
+known_skips = [180290542]
 
 crashes_list_without_skips = []
 
@@ -56,11 +56,11 @@ try:
     print("\nResponse from Hasura: %s" % json.dumps(response))
 
     crashes_list = response['data']['atd_txdot_crashes']
-    print("\nList of crashes: %s" % json.dumps(crashes_list))
 
     crashes_list_without_skips = [
         x for x in crashes_list if x["crash_id"] not in known_skips
     ]
+    print("\nList of crashes needing CR3 download: %s" % json.dumps(crashes_list_without_skips))
  
     print("\nStarting CR3 downloads:")
 except Exception as e:
