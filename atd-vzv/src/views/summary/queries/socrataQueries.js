f3fab009853b22b07d42b0b2794a0204093a81cc
Mike <mike.dilley@gmail.com>
2023-03-07 15:38:58 -0600

    Revert the updates to the Socrata 4x4s

--- atd-vzv/src/views/summary/queries/socrataQueries.js
+++ atd-vzv/src/views/summary/queries/socrataQueries.js
@@ -1,9 +1,7 @@
 import { isDevelopment, isPreview } from "../../../constants/nav";
 
-// const crashDatasetID = isDevelopment || isPreview ? "3aut-fhzp" : "y2wy-tgr5";
-const crashDatasetID = "y2wy-tgr5";
-// const personDatasetID = isDevelopment || isPreview ? "v3x4-fjgm" : "xecs-rpy9";
-const personDatasetID = "xecs-rpy9";
+const crashDatasetID = isDevelopment || isPreview ? "3aut-fhzp" : "y2wy-tgr5";
+const personDatasetID = isDevelopment || isPreview ? "v3x4-fjgm" : "xecs-rpy9";
 
 export const crashEndpointUrl = `https://data.austintexas.gov/resource/${crashDatasetID}.json`;
 export const crashGeoJSONEndpointUrl = `https://data.austintexas.gov/resource/${crashDatasetID}.geojson`;
