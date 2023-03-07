64af147a7a50cd7a625439d60225f21afc69d796
rose <rose.eichelmann@gmail.com>
2023-03-06 18:07:10 -0600

    change crash_position variable name, add manual AFP check to persons table insert conditional

--- atd-vzd/triggers/fatality_insert.sql
+++ atd-vzd/triggers/fatality_insert.sql
@@ -7,16 +7,16 @@ AS $function$
     DECLARE
     	year integer;
         location text;
-        position geometry;
+        crash_position geometry;
     BEGIN
-        SELECT TO_CHAR(crash_date,'yyyy'), CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), austin_full_purpose
-            INTO year, location, position
+        SELECT TO_CHAR(crash_date,'yyyy'), CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), position
+            INTO year, location, crash_position
             FROM atd_txdot_crashes
             WHERE crash_id = NEW.crash_id;
-    	IF (TG_TABLE_NAME = 'atd_txdot_primaryperson' AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), position)) THEN
+    	IF (TG_TABLE_NAME = 'atd_txdot_primaryperson' AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), crash_position)) THEN
     	    INSERT INTO fatalities (crash_id, primaryperson_id, year, location)
     		    VALUES (NEW.crash_id, NEW.primaryperson_id, year, location);
-    	ELSIF (TG_TABLE_NAME = 'atd_txdot_person' AND in_austin = 'Y') THEN
+    	ELSIF (TG_TABLE_NAME = 'atd_txdot_person' AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), crash_position)) THEN
     		INSERT INTO fatalities (crash_id, person_id, year, location)
     		    VALUES (NEW.crash_id, NEW.person_id, year, location);
     	END IF;
