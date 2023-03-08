ec3fbb5d2d47567d64e4c69998ac38664895c558
rose <rose.eichelmann@gmail.com>
2023-03-08 14:52:22 -0600

    remove jurisdiction check and year/location

--- atd-toolbox/fatalities_backfill/fatalities_backfill.sql
+++ atd-toolbox/fatalities_backfill/fatalities_backfill.sql
@@ -1,13 +1,11 @@
 -- This is a one time script to populate the fatalities table with historical data
 
-INSERT INTO fatalities (crash_id, primaryperson_id, location, year)
-SELECT atd_txdot_primaryperson.crash_id, primaryperson_id, CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), TO_CHAR(crash_date,'yyyy')
+INSERT INTO fatalities (crash_id, primaryperson_id)
+SELECT crash_id, primaryperson_id
 FROM atd_txdot_primaryperson
-JOIN atd_txdot_crashes ON atd_txdot_primaryperson.crash_id = atd_txdot_crashes.crash_id
-WHERE prsn_injry_sev_id = 4 AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), position);
+WHERE prsn_injry_sev_id = 4;
 
-INSERT INTO fatalities (crash_id, person_id, location, year)
-SELECT atd_txdot_person.crash_id, person_id, CONCAT_WS(' ', rpt_block_num, rpt_street_pfx, rpt_street_name, '(', rpt_sec_block_num, rpt_sec_street_pfx, rpt_sec_street_name, ')'), TO_CHAR(crash_date,'yyyy')
+INSERT INTO fatalities (crash_id, person_id)
+SELECT crash_id, person_id
 FROM atd_txdot_person
-JOIN atd_txdot_crashes ON atd_txdot_person.crash_id = atd_txdot_crashes.crash_id
-WHERE prsn_injry_sev_id = 4 AND ST_CONTAINS((SELECT geometry FROM atd_jurisdictions WHERE atd_jurisdictions.id = 5), position);
+WHERE prsn_injry_sev_id = 4;
