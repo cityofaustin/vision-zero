0cd0024a611a66a7246a03b0c7685cf90c88c6c1
Frank Hereford <frankhereford@users.noreply.github.com>
2023-03-19 14:55:13 -0500

    Add second, dual column index for locations listing page

--- atd-vzd/migrations/migration_index_crashes_2023_03_18.sql
+++ atd-vzd/migrations/migration_index_crashes_2023_03_18.sql
@@ -1,1 +1,2 @@
-create index crashes_location_id on atd_txdot_crashes (location_id);
+CREATE INDEX crashes_location_idx ON atd_txdot_crashes (location_id);
+CREATE INDEX crashes_crash_date_location_idx ON atd_txdot_crashes (crash_date, location_id);
