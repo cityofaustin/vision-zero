226f042291ef972c1d669cda51e60a3565743712
rose <rose.eichelmann@gmail.com>
2023-04-05 11:58:53 -0500

    filter out fields that shouldnt be rendered

--- atd-vze/src/Components/DataTable.js
+++ atd-vze/src/Components/DataTable.js
@@ -68,150 +68,156 @@ const DataTable = ({
               <CardBody>
                 <Table responsive striped hover>
                   <tbody>
-                    {Object.keys(section.fields).map((field, i) => {
-                      const isEditing = field === editField;
-                      const fieldConfigObject = section.fields[field];
+                    {Object.keys(section.fields)
+                      // Dont map through fields that are not supposed to render
+                      .filter(field => section.fields[field].render != false)
+                      .map((field, i) => {
+                        const isEditing = field === editField;
+                        const fieldConfigObject = section.fields[field];
 
-                      const fieldLabel = fieldConfigObject.label;
+                        const fieldLabel = fieldConfigObject.label;
 
-                      // Disable editing if user is only "readonly"
-                      if (fieldConfigObject.editable && isReadOnly(roles)) {
-                        fieldConfigObject.editable = false;
-                      }
-
-                      // Set data table (alternate if defined in data map)
-                      const fieldDataTable =
-                        fieldConfigObject.alternateTable || dataTable;
-
-                      // If data is nested in data object, define path in dataMap
-                      const nestedData =
-                        fieldConfigObject.dataPath &&
-                        get(
-                          data[fieldDataTable][0],
-                          fieldConfigObject.dataPath
-                        );
-
-                      const formattedDollarValue =
-                        fieldConfigObject.format === "dollars" &&
-                        formatCostToDollars(
-                          nestedData || data[fieldDataTable][0][field]
-                        );
-
-                      const formatDateTimeValue =
-                        fieldConfigObject.format === "datetime" &&
-                        formatDateTimeString(
-                          nestedData || data[fieldDataTable][0][field]
-                        );
-
-                      const fieldValue =
-                        formattedDollarValue ||
-                        formatDateTimeValue ||
-                        nestedData ||
-                        (formData && formData[field.data]) ||
-                        data[fieldDataTable][0][field];
-
-                      const fieldUiType = fieldConfigObject.uiType;
-
-                      const lookupPrefix = fieldConfigObject.lookupPrefix
-                        ? fieldConfigObject.lookupPrefix
-                        : field.split("_id")[0];
-
-                      // If there is no lookup options, we can assume the field value can be displayed as is.
-                      // If there is a lookup option, then the value is an ID to be referenced in a lookup table.
-                      const selectOptions =
-                        lookupSelectOptions[fieldConfigObject.lookupOptions];
-
-                      const renderLookupDescString = () => {
-                        // make sure the value isn't null blank
-                        if (
-                          fieldValue === null ||
-                          typeof fieldValue === "undefined"
-                        )
-                          return "";
-
-                        // make sure there is a lookup object in the config
-                        if (!selectOptions || !fieldConfigObject.lookupOptions)
-                          return fieldValue;
-
-                        // make sure the config lookup object matches with lookup queries
-                        const matchingLookupObject = selectOptions.find(
-                          item => item[`${lookupPrefix}_id`] === fieldValue
-                        );
-
-                        if (!matchingLookupObject) {
-                          console.warn(
-                            `${field} has a value of ${fieldValue} which has no match in the related lookup table. You should check to make sure the lookup table isn't missing any rows of data.`
-                          );
-                          return `ID: ${fieldValue}`;
-                        } else {
-                          return matchingLookupObject[`${lookupPrefix}_desc`];
+                        // Disable editing if user is only "readonly"
+                        if (fieldConfigObject.editable && isReadOnly(roles)) {
+                          fieldConfigObject.editable = false;
                         }
-                      };
 
-                      const fieldValueDisplay = renderLookupDescString();
+                        // Set data table (alternate if defined in data map)
+                        const fieldDataTable =
+                          fieldConfigObject.alternateTable || dataTable;
 
-                      return (
-                        <tr key={i}>
-                          <td>
-                            <strong>{fieldLabel}</strong>
-                          </td>
-                          <td>
-                            {isEditing ? (
-                              <form
-                                onSubmit={e =>
-                                  handleFieldUpdate(e, section.fields, field)
-                                }
-                              >
-                                {fieldUiType === "select" && (
-                                  <Input
-                                    name={field}
-                                    id={field}
-                                    onChange={e => handleInputChange(e)}
-                                    defaultValue={fieldValue}
-                                    type="select"
-                                  >
-                                    {selectOptions.map(option => (
-                                      <option
-                                        value={option[`${lookupPrefix}_id`]}
-                                      >
-                                        {option[`${lookupPrefix}_desc`]}
-                                      </option>
-                                    ))}
-                                  </Input>
-                                )}
-                                {fieldUiType === "text" && (
-                                  <input
-                                    type="text"
-                                    defaultValue={fieldValue}
-                                    onChange={e => handleInputChange(e)}
-                                  />
-                                )}
+                        // If data is nested in data object, define path in dataMap
+                        const nestedData =
+                          fieldConfigObject.dataPath &&
+                          get(
+                            data[fieldDataTable][0],
+                            fieldConfigObject.dataPath
+                          );
 
-                                <button type="submit">
-                                  <i className="fa fa-check edit-toggle" />
-                                </button>
-                                <button type="cancel">
-                                  <i
-                                    className="fa fa-times edit-toggle"
-                                    onClick={e => handleCancelClick(e)}
-                                  ></i>
-                                </button>
-                              </form>
-                            ) : (
-                              fieldValueDisplay
-                            )}
-                          </td>
-                          <td>
-                            {fieldConfigObject.editable && !isEditing && (
-                              <i
-                                className="fa fa-pencil edit-toggle"
-                                onClick={() => setEditField(field)}
-                              />
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })}
+                        const formattedDollarValue =
+                          fieldConfigObject.format === "dollars" &&
+                          formatCostToDollars(
+                            nestedData || data[fieldDataTable][0][field]
+                          );
+
+                        const formatDateTimeValue =
+                          fieldConfigObject.format === "datetime" &&
+                          formatDateTimeString(
+                            nestedData || data[fieldDataTable][0][field]
+                          );
+
+                        const fieldValue =
+                          formattedDollarValue ||
+                          formatDateTimeValue ||
+                          nestedData ||
+                          (formData && formData[field.data]) ||
+                          data[fieldDataTable][0][field];
+
+                        const fieldUiType = fieldConfigObject.uiType;
+
+                        const lookupPrefix = fieldConfigObject.lookupPrefix
+                          ? fieldConfigObject.lookupPrefix
+                          : field.split("_id")[0];
+
+                        // If there is no lookup options, we can assume the field value can be displayed as is.
+                        // If there is a lookup option, then the value is an ID to be referenced in a lookup table.
+                        const selectOptions =
+                          lookupSelectOptions[fieldConfigObject.lookupOptions];
+
+                        const renderLookupDescString = () => {
+                          // make sure the value isn't null blank
+                          if (
+                            fieldValue === null ||
+                            typeof fieldValue === "undefined"
+                          )
+                            return "";
+
+                          // make sure there is a lookup object in the config
+                          if (
+                            !selectOptions ||
+                            !fieldConfigObject.lookupOptions
+                          )
+                            return fieldValue;
+
+                          // make sure the config lookup object matches with lookup queries
+                          const matchingLookupObject = selectOptions.find(
+                            item => item[`${lookupPrefix}_id`] === fieldValue
+                          );
+
+                          if (!matchingLookupObject) {
+                            console.warn(
+                              `${field} has a value of ${fieldValue} which has no match in the related lookup table. You should check to make sure the lookup table isn't missing any rows of data.`
+                            );
+                            return `ID: ${fieldValue}`;
+                          } else {
+                            return matchingLookupObject[`${lookupPrefix}_desc`];
+                          }
+                        };
+
+                        const fieldValueDisplay = renderLookupDescString();
+
+                        return (
+                          <tr key={i}>
+                            <td>
+                              <strong>{fieldLabel}</strong>
+                            </td>
+                            <td>
+                              {isEditing ? (
+                                <form
+                                  onSubmit={e =>
+                                    handleFieldUpdate(e, section.fields, field)
+                                  }
+                                >
+                                  {fieldUiType === "select" && (
+                                    <Input
+                                      name={field}
+                                      id={field}
+                                      onChange={e => handleInputChange(e)}
+                                      defaultValue={fieldValue}
+                                      type="select"
+                                    >
+                                      {selectOptions.map(option => (
+                                        <option
+                                          value={option[`${lookupPrefix}_id`]}
+                                        >
+                                          {option[`${lookupPrefix}_desc`]}
+                                        </option>
+                                      ))}
+                                    </Input>
+                                  )}
+                                  {fieldUiType === "text" && (
+                                    <input
+                                      type="text"
+                                      defaultValue={fieldValue}
+                                      onChange={e => handleInputChange(e)}
+                                    />
+                                  )}
+
+                                  <button type="submit">
+                                    <i className="fa fa-check edit-toggle" />
+                                  </button>
+                                  <button type="cancel">
+                                    <i
+                                      className="fa fa-times edit-toggle"
+                                      onClick={e => handleCancelClick(e)}
+                                    ></i>
+                                  </button>
+                                </form>
+                              ) : (
+                                fieldValueDisplay
+                              )}
+                            </td>
+                            <td>
+                              {fieldConfigObject.editable && !isEditing && (
+                                <i
+                                  className="fa fa-pencil edit-toggle"
+                                  onClick={() => setEditField(field)}
+                                />
+                              )}
+                            </td>
+                          </tr>
+                        );
+                      })}
                   </tbody>
                 </Table>
                 {/* If button parameters are set and the defined condition is met, show button */}
