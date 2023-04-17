31a64fccbb1050c735636d3cb311dec4c409e790
Mike <mike.dilley@gmail.com>
2023-04-17 15:33:33 -0500

    Add geocoder package, add TODOs, start component

---
+++ atd-vzv/src/views/map/Geocoder/Geocoder.js
@@ -0,0 +1,11 @@
+// TODO: Geocoder in the top left
+// TODO: Type street into search bar
+// TODO: dropdown autocomplete of streets
+// TODO: zoom to location
+// TODO: clear search input
+
+const Geocoder = () => {
+  return <div>{"the geocoder"}</div>;
+};
+
+export default Geocoder;
