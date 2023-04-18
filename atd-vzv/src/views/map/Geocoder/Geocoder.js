1468d5c43b01ea40ec751dcf2dc996b7eba6825d
Mike <mike.dilley@gmail.com>
2023-04-18 18:32:01 -0500

    Add docstring with note about memoized function library requirement

--- atd-vzv/src/views/map/Geocoder/Geocoder.js
+++ atd-vzv/src/views/map/Geocoder/Geocoder.js
@@ -4,10 +4,17 @@ import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { MAPBOX_TOKEN } from "../Map";
 import { geocoderBbox } from "../mapData";
 
+/**
+ * Geocoder component for the map with forwarded map ref
+ * Important: all prop objects or function must be memoized for the geocoder
+ * dropdown to close after selecting a result.
+ * @see https://github.com/SamSamskies/react-map-gl-geocoder/issues/96
+ * @param {Function} handleViewportChange - Callback function to update the map viewport
+ */
 const MapGeocoder = React.forwardRef(({ handleViewportChange }, ref) => {
   const handleGeocoderViewportChange = useCallback(
     (viewport) => {
-      // Speed up the transition flyTo transition
+      // Speed up the flyTo transition
       const geocoderDefaultOverrides = {
         transitionDuration: 1500,
       };
@@ -23,7 +30,6 @@ const MapGeocoder = React.forwardRef(({ handleViewportChange }, ref) => {
   return (
     <Geocoder
       mapRef={ref}
-      // containerRef={geocoderContainerRef}
       onViewportChange={handleGeocoderViewportChange}
       mapboxApiAccessToken={MAPBOX_TOKEN}
       position="top-left"
