83ecb8ee2e0f4b3eaa76755dfd78ba8ad0564391
rose <rose.eichelmann@gmail.com>
2023-06-05 11:50:37 -0500

    add engineering areas table to db

---
+++ atd-vzd/schema/engineering_areas.sql
@@ -0,0 +1,22 @@
+CREATE TABLE public.engineering_areas(
+    id integer NOT NULL,
+    eng_area_label text,
+    eng_area_geometry geometry,
+);
+
+CREATE SEQUENCE public.engineering_areas_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+--
+-- Name: fatalities fatalities_pkey; Type: CONSTRAINT; Schema: public; Owner: visionzero
+--
+
+ALTER TABLE ONLY public.engineering_areas
+    ADD CONSTRAINT engineering_areas_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.engineering_areas ALTER COLUMN id SET DEFAULT nextval('public.engineering_areas_id_seq'::regclass);
