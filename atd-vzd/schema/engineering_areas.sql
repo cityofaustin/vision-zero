46926bab3d83f3c3cb27c3cc857e160488362eb9
rose <rose.eichelmann@gmail.com>
2023-06-07 16:54:34 -0500

    better way to define unique index

--- atd-vzd/schema/engineering_areas.sql
+++ atd-vzd/schema/engineering_areas.sql
@@ -1,14 +1,12 @@
 CREATE TABLE public.engineering_areas(
     area_id integer,
-    label text NOT NULL,
+    label text NOT NULL unique,
     geometry geometry(MultiPolygon, 4326) NOT NULL
 );
 
 ALTER TABLE ONLY public.engineering_areas
     ADD CONSTRAINT engineering_areas_pkey PRIMARY KEY (area_id);
 
-CREATE UNIQUE INDEX engineering_areas_label_key ON public.engineering_areas USING btree (label);
-
 CREATE INDEX engineering_areas_gix
-ON engineering_areas
+ON public.engineering_areas
 USING GIST (geometry);
