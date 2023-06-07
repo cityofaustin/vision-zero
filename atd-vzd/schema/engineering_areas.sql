44874cc4595101b0d764200546f611d0c00bdc04
rose <rose.eichelmann@gmail.com>
2023-06-07 12:42:46 -0500

    add area_id as primary key

--- atd-vzd/schema/engineering_areas.sql
+++ atd-vzd/schema/engineering_areas.sql
@@ -1,18 +1,12 @@
 CREATE TABLE public.engineering_areas(
-    id integer NOT NULL,
-    eng_area_label text,
-    eng_area_geometry geometry
+    area_id integer,
+    label text,
+    geometry geometry
 );
 
-CREATE SEQUENCE public.engineering_areas_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
 ALTER TABLE ONLY public.engineering_areas
-    ADD CONSTRAINT engineering_areas_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT engineering_areas_pkey PRIMARY KEY (area_id);
 
-ALTER TABLE ONLY public.engineering_areas ALTER COLUMN id SET DEFAULT nextval('public.engineering_areas_id_seq'::regclass);
+CREATE INDEX engineering_areas_gix
+ON engineering_areas
+USING GIST (geometry);
