ab044153b850b105ad91fb2571b54a238e42be5d
Frank Hereford <frankhereford@users.noreply.github.com>
2023-06-06 21:30:31 -0500

    unused import and comment

--- atd-etl/cris_import/lib/graphql.py
+++ atd-etl/cris_import/lib/graphql.py
@@ -1,10 +1,10 @@
-import os
 import requests
 
 
 # 🙏🏻 https://github.com/cityofaustin/atd-moped/blob/main/moped-toolbox/amd_milestones_backfill/utils.py#L18
 
 def make_hasura_request(*, query, variables={}, endpoint, admin_secret):
+    print("Endpoint: ", endpoint)
     headers = {"X-Hasura-Admin-Secret": admin_secret}
     payload = {"query": query, "variables": variables}
     res = requests.post(endpoint, json=payload, headers=headers)
