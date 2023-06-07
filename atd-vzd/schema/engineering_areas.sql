5f817ccaeddfe6a867c109e1fa969cfc20100d8a
rose <rose.eichelmann@gmail.com>
2023-06-07 16:46:44 -0500

    explicity make geometry a multipolygon type, add unique index on label

--- atd-vzd/schema/engineering_areas.sql
+++ atd-vzd/schema/engineering_areas.sql
@@ -1,11 +1,13 @@
 CREATE TABLE public.engineering_areas(
     area_id integer,
-    label text,
-    geometry geometry
+    label text NOT NULL,
+    geometry geometry(MultiPolygon, 4326) NOT NULL
 );
 
 ALTER TABLE ONLY public.engineering_areas
     ADD CONSTRAINT engineering_areas_pkey PRIMARY KEY (area_id);
+
+CREATE UNIQUE INDEX engineering_areas_label_key ON public.engineering_areas USING btree (label);
 
 CREATE INDEX engineering_areas_gix
 ON engineering_areas
