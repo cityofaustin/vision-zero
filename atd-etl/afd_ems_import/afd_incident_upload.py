dd265cdff2118474fd8ad90670ff5877006fb240
Frank Hereford <frankhereford@users.noreply.github.com>
2023-06-14 18:29:11 -0500

    strip out the prefect stuff

--- atd-etl/afd_ems_import/afd_incident_upload.py
+++ atd-etl/afd_ems_import/afd_incident_upload.py
@@ -7,8 +7,6 @@ Description: This flow uploads AFD Incident Response CSVs
     gets forwarded to a S3 bucket via AWS Simple Email Service.
 """
 
-import prefect
-from prefect import Flow, task, Parameter, case
 import datetime
 import json
 import boto3
@@ -43,45 +41,14 @@ AFD_S3_SOURCE_PREFIX = None
 AFD_S3_ARCHIVE_PREFIX = None
 DB_BASTION_HOST = None
 DB_RDS_HOST = None
-
-# Retrieve the db configuration
-if False:
-    DB_USERNAME = environment_variables_from_kv_store["AFD_DB_USERNAME"]
-    DB_PASSWORD = environment_variables_from_kv_store["AFD_DB_PASSWORD"]
-    DB_HOSTNAME = environment_variables_from_kv_store["AFD_DB_HOSTNAME"]
-    DB_PORT = environment_variables_from_kv_store["AFD_DB_PORT"]
-    DB_DATABASE = environment_variables_from_kv_store["AFD_DB_DATABASE"]
-    AWS_ACCESS_KEY_ID = environment_variables_from_kv_store["AWS_ACCESS_KEY_ID"]
-    AWS_SECRET_ACCESS_KEY = environment_variables_from_kv_store["AWS_SECRET_ACCESS_KEY"]
-    AFD_S3_SOURCE_BUCKET = environment_variables_from_kv_store["AFD_S3_SOURCE_BUCKET"]
-    AFD_S3_ARCHIVE_BUCKET = environment_variables_from_kv_store["AFD_S3_ARCHIVE_BUCKET"]
-    AFD_S3_SOURCE_PREFIX = environment_variables_from_kv_store["AFD_S3_SOURCE_PREFIX"]
-    AFD_S3_ARCHIVE_PREFIX = environment_variables_from_kv_store["AFD_S3_ARCHIVE_PREFIX"]
-    DB_BASTION_HOST = environment_variables_from_kv_store["DB_BASTION_HOST"]
-    DB_RDS_HOST = environment_variables_from_kv_store["DB_RDS_HOST"]
-else:
-    DB_USERNAME = os.getenv("AFD_DB_USERNAME")
-    DB_PASSWORD = os.getenv("AFD_DB_PASSWORD")
-    DB_HOSTNAME = os.getenv("AFD_DB_HOSTNAME")
-    DB_PORT = os.getenv("AFD_DB_PORT")
-    DB_DATABASE = os.getenv("AFD_DB_DATABASE")
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AFD_S3_SOURCE_BUCKET = os.getenv("AFD_S3_SOURCE_BUCKET")
-    AFD_S3_ARCHIVE_BUCKET = os.getenv("AFD_S3_ARCHIVE_BUCKET")
-    AFD_S3_SOURCE_PREFIX = os.getenv("AFD_S3_SOURCE_PREFIX")
-    AFD_S3_ARCHIVE_PREFIX = os.getenv("AFD_S3_ARCHIVE_PREFIX")
-    DB_BASTION_HOST = os.getenv("DB_BASTION_HOST")
-    DB_RDS_HOST = os.getenv("DB_RDS_HOST")
+RECORD_AGE_MAXIMUM = None
 
 
-@task
 def get_timestamp():
     current = datetime.now()
     return time.mktime(current.timetuple())
 
 
-@task
 def get_most_recent_email():
     """
     Find the most recently updated file in the bucket. This will be the newest email.
@@ -107,7 +74,6 @@ def get_most_recent_email():
     return email
 
 
-@task
 def extract_email_attachment(message):
     # Given the s3 object content is the SES email,
     # get the message content and attachment using email package
@@ -124,7 +90,6 @@ def extract_email_attachment(message):
     return tmpdir
 
 
-@task
 def upload_attachment_to_S3(location, timestamp):
     session = boto3.Session(
         aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -139,14 +104,12 @@ def upload_attachment_to_S3(location, timestamp):
     return True
 
 
-@task
 def create_and_parse_dataframe(location):
     # Parse XLSX into pandas dataframe
     data = pandas.read_excel(f"{location}/attachment.xlsx", header=0)
     return data
 
 
-@task
 def upload_data_to_postgres(data, age_cutoff):
 
     ssh_tunnel = SSHTunnelForwarder(
@@ -278,22 +241,17 @@ def upload_data_to_postgres(data, age_cutoff):
     return True
 
 
-@task
 def clean_up(path):
     # Clean up the temp location
     shutil.rmtree(path)
 
 
-with Flow("AFD Import ETL") as flow:
-    record_age_maximum = Parameter("record_age_maximum", default=0)
-    timestamp = get_timestamp()
-    newest_email = get_most_recent_email()
-    attachment_location = extract_email_attachment(newest_email)
-    uploaded_token = upload_attachment_to_S3(attachment_location, timestamp)
-    data = create_and_parse_dataframe(attachment_location)
-    upload_token = upload_data_to_postgres(data, record_age_maximum)
-    clean_up_token = clean_up(attachment_location, upstream_tasks=[upload_token])
+record_age_maximum = RECORD_AGE_MAXIMUM or 15
+timestamp = get_timestamp()
+newest_email = get_most_recent_email()
+attachment_location = extract_email_attachment(newest_email)
+uploaded_token = upload_attachment_to_S3(attachment_location, timestamp)
+data = create_and_parse_dataframe(attachment_location)
+upload_token = upload_data_to_postgres(data, record_age_maximum)
+clean_up_token = clean_up(attachment_location, upstream_tasks=[upload_token])
 
-# you can use record_age_maximum=False if you want a full import
-flow.run(parameters=dict(record_age_maximum=90))
-# f.register(project_name="vision-zero")
