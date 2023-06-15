80c8b7e8ce16a49d38bf4f45c3ed7837e784fd67
Tilly Whitson <35410637+tillyw@users.noreply.github.com>
2023-06-15 16:44:11 -0500

    update get_crash_modes trigger

--- atd-vzd/triggers/get_crash_modes.sql
+++ atd-vzd/triggers/get_crash_modes.sql
@@ -19,32 +19,32 @@ BEGIN
         FROM (
             SELECT unit_id, (
                        CASE
-                           -- PEDALCYCLIST / BYCICLE
+                           -- PEDALCYCLIST / BICYCLE (5)
                            WHEN vdesc.veh_unit_desc_id = 3 THEN 5
-                           -- MOTORIZED CONVEYANCE
+                           -- MOTORIZED CONVEYANCE (6)
                            WHEN vdesc.veh_unit_desc_id = 5 THEN 6
-                           -- PEDESTRIAN
+                           -- PEDESTRIAN (7)
                            WHEN vdesc.veh_unit_desc_id = 4 THEN 7
-                           -- TRAIN
+                           -- TRAIN (8)
                            WHEN vdesc.veh_unit_desc_id = 2 THEN 8
-                           -- MICROMOBILITY DEVICE
+                           -- MICROMOBILITY DEVICES
                            WHEN vdesc.veh_unit_desc_id = 177 THEN (
                                CASE
-                               -- E-SCOOTER
+                               -- E-SCOOTER (11)
                                WHEN vbody.veh_body_styl_id = 177 THEN 11
-
+                               -- MICROMOBILITY DEVICE (10)
                                ELSE 10 END
                            )
-                           -- MOTOR VEHICLE
+                           -- MOTOR VEHICLES
                            WHEN vdesc.veh_unit_desc_id = 1 THEN (
                                CASE
-                               -- PASSENGER CAR
+                               -- PASSENGER CAR (1)
                                WHEN vbody.veh_body_styl_id IN (100,104) THEN 1
-                               -- LARGE PASSENGER CAR
+                               -- LARGE PASSENGER CAR (2)
                                WHEN vbody.veh_body_styl_id IN (30, 69, 103, 106) THEN 2
-                               -- MOTORCYCLE
+                               -- MOTORCYCLE (3)
                                WHEN vbody.veh_body_styl_id IN (71, 90) THEN 3
-
+                               -- MOTOR VEHICLE - OTHER (4)
                                ELSE 4 END
                            ) ELSE 9
                        END
