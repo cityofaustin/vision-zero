d5cf7dca9fd5bb4335ac53fe5163747a8935b872
Frank Hereford <frankhereford@users.noreply.github.com>
2023-06-20 15:06:05 -0500

    let the entry file specify the program to use

--- atd-etl/afd_ems_import/entrypoint.sh
+++ atd-etl/afd_ems_import/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# spin up the SSH server so this image can be its own bastion
 /usr/sbin/sshd
-echo "did a thing"
+
+action=$1
+echo "action: '$action'"
+# Check for which program to run
+if [ "$action" == "ems" ]; then
+    python3 ./ems_incident_upload.py
+elif [ "$action" == "afd" ]; then
+    python3 ./afd_incident_upload.py
+else
+    echo "Invalid argument. Please specify either 'ems' or 'afd'."
+fi
