d6a91989137bedda9c270d7843e40625566c869b
Frank Hereford <frankhereford@users.noreply.github.com>
2023-06-20 15:21:22 -0500

    echo intent in entry script

--- atd-etl/afd_ems_import/entrypoint.sh
+++ atd-etl/afd_ems_import/entrypoint.sh
@@ -4,11 +4,12 @@
 /usr/sbin/sshd
 
 action=$1
-echo "action: '$action'"
 # Check for which program to run
 if [ "$action" == "ems" ]; then
+    echo "Starting up the EMS Incident Upload script."
     python3 ./ems_incident_upload.py
 elif [ "$action" == "afd" ]; then
+    echo "Starting up the AFD Incident Upload script."
     python3 ./afd_incident_upload.py
 else
     echo "Invalid argument. Please specify either 'ems' or 'afd'."
