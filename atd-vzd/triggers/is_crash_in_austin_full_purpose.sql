03abefeb2d3b41bece7d398bea8964a69cbe9650
rose <rose.eichelmann@gmail.com>
2023-06-27 17:05:54 -0500

    add newline

--- atd-vzd/triggers/is_crash_in_austin_full_purpose.sql
+++ atd-vzd/triggers/is_crash_in_austin_full_purpose.sql
@@ -1,6 +1,6 @@
 -- This function determines whether crash coordinates fall within the Austin Full Purpose jurisdiction polygon.
 -- If the crash has no coordinates, it returns whether the crash is in Austin (city_id = 5)
-CREATE OR REPLACE FUNCTION is_crash_in_austin_full_purpose(location geometry(MultiPolygon, 4326), city_id int) RETURNS boolean AS $$
+CREATE OR REPLACE FUNCTION is_crash_in_austin_full_purpose(location geometry(Geometry,4326), city_id int) RETURNS boolean AS $$
         BEGIN
             -- If we dont have coordinates for the crash and the city_id is Austin (22), return true
             IF (location IS NULL) THEN --TODO: Do we need to handle if city_id is null? If so should that make the afp flag default to false?
