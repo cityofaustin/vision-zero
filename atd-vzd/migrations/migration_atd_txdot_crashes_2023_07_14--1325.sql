a0169fa204bb87ae1b3ff8fe787973aca42f206d
rose <rose.eichelmann@gmail.com>
2023-07-17 11:04:18 -0500

    update wording

--- atd-vzd/migrations/migration_atd_txdot_crashes_2023_07_14--1325.sql
+++ atd-vzd/migrations/migration_atd_txdot_crashes_2023_07_14--1325.sql
@@ -1,5 +1,5 @@
 -- This trigger updates the city id of a crash to be in Austin if its currently
--- not in Austin and the position value is moved into valid Austin jurisdictions
+-- not in Austin and the position value is moved into any of the Austin jurisdictions
 CREATE OR REPLACE TRIGGER crashes_position_update_city_id
     BEFORE UPDATE ON atd_txdot_crashes
     FOR EACH ROW
