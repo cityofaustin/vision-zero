ef49e9d1e5b8bfe3bb3a4b07902f129a3c1aaf00
rose <rose.eichelmann@gmail.com>
2023-07-26 11:59:16 -0500

    default to port 8084 for local

--- atd-toolbox/load_council_districts/secrets_template.js
+++ atd-toolbox/load_council_districts/secrets_template.js
@@ -1,6 +1,6 @@
 const HASURA_AUTH = {
   hasura_graphql_endpoint: {
-    local: "http://localhost:8080/v1/graphql",
+    local: "http://localhost:8084/v1/graphql",
   },
   hasura_graphql_admin_secret: {
     local: "hasurapassword",
