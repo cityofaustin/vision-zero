fe70d84bebd4d1f2c68ab6a967a84d809c82b038
rose <rose.eichelmann@gmail.com>
2023-07-26 11:37:29 -0500

    add comment talking about generated column

--- atd-vzd/triggers/is_crash_in_austin_full_purpose.sql
+++ atd-vzd/triggers/is_crash_in_austin_full_purpose.sql
@@ -1,9 +1,9 @@
--- This function determines whether crash coordinates fall within the Austin Full Purpose jurisdiction polygon.
--- If the crash has no coordinates, it returns whether the crash is in Austin (city_id = 5)
+-- This function is invoked by a generated column in atd_txdot_crashes called in_austin_full_purpose and determines whether crash coordinates fall within 
+-- the Austin Full Purpose jurisdiction polygon. If the crash has no coordinates, it returns whether the crash is in Austin (city_id = 5)
 CREATE OR REPLACE FUNCTION is_crash_in_austin_full_purpose(location geometry(Geometry,4326), city_id int) RETURNS boolean AS $$
         BEGIN
             -- If we dont have coordinates for the crash and the city_id is Austin (22), return true
-            IF (location IS NULL) THEN --TODO: Do we need to handle if city_id is null? If so should that make the afp flag default to false?
+            IF (location IS NULL) THEN
                 RETURN (city_id = 22);
             -- If we do have coordinates return whether they are within the AFP jurisdiction (5) polygon
             ELSE
