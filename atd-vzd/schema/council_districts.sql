2071ae92ebff3baceb2d483d9a38d2e5c454330b
rose <rose.eichelmann@gmail.com>
2023-07-26 11:21:08 -0500

    add updated_at column

--- atd-vzd/schema/council_districts.sql
+++ atd-vzd/schema/council_districts.sql
@@ -1,7 +1,8 @@
 CREATE TABLE public.council_districts(
     id SERIAL PRIMARY KEY,
     council_district integer NOT NULL unique,
-    geometry geometry(MultiPolygon, 4326) NOT NULL
+    geometry geometry(MultiPolygon, 4326) NOT NULL,
+    updated_at timestamp without time zone DEFAULT now() NOT NULL
 );
 
 CREATE INDEX council_districts_gix
