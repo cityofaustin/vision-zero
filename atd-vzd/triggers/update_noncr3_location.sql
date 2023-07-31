48f01ec041ce39e11f8dec83a955e7d113510b28
Mike <mike.dilley@gmail.com>
2023-07-31 17:37:02 -0500

    Test out a generated column for location_id

--- atd-vzd/triggers/update_noncr3_location.sql
+++ atd-vzd/triggers/update_noncr3_location.sql
@@ -1,11 +1,3 @@
--- Summary for trigger
--- 1. Fire trigger on insert or update of atd_apd_blueform table
--- 2. Check if crash is main-lane using find_noncr3_mainlane_crash function in DB ✅
--- 3. If it is, then set the location_id to None ✅
--- 4. If it isn't, try to find the location_id with find_location_for_noncr3_collision function in DB
--- 5. If the new_location_id is the same as the old_location_id, then do nothing
--- 6. If the new_location_id is different from the old_location_id, then update the location_id with update_atd_apd_blueform function in DB
-
 -- Trigger function
 CREATE OR REPLACE FUNCTION update_noncr3_location()
   RETURNS TRIGGER 
@@ -38,13 +30,38 @@ $$
 
 -- Triggers
 CREATE TRIGGER update_noncr3_location
+AFTER INSERT ON atd_apd_blueform
+FOR EACH ROW
+WHEN (NEW.latitude IS NOT NULL AND NEW.longitude IS NOT NULL)
+EXECUTE FUNCTION update_noncr3_location();
+
+CREATE TRIGGER update_noncr3_location
 AFTER UPDATE ON atd_apd_blueform
 FOR EACH ROW
 WHEN (OLD.latitude IS DISTINCT FROM NEW.latitude
 OR OLD.longitude IS DISTINCT FROM NEW.longitude)
 EXECUTE FUNCTION update_noncr3_location();
 
--- TODO:
--- 1. Test mainlane crash update location to None ✅
--- 2. Test non-mainlane crash update location to a new location ✅
--- 3. Test non-mainlane crash update location to None ✅
+-- Function for generated column
+CREATE OR REPLACE FUNCTION update_noncr3_location(blueform_case_id integer)
+  RETURNS varchar 
+  LANGUAGE PLPGSQL
+  IMMUTABLE
+  AS
+$$
+BEGIN
+    -- Check if crash is main-lane
+	IF EXISTS (SELECT * FROM find_noncr3_mainlane_crash(blueform_case_id)) THEN
+        -- If it is, then set the location_id to None
+		RETURN NULL;
+    ELSE 
+        -- If it isn't main-lane, try to find a location_id for it
+        RETURN (SELECT location_id FROM find_location_for_noncr3_collision(blueform_case_id));
+    END IF;
+END;
+$$
+
+-- Test generated column
+ALTER TABLE atd_apd_blueform
+ADD COLUMN generated_location_id varchar 
+GENERATED ALWAYS AS (update_noncr3_location(case_id)) STORED;
