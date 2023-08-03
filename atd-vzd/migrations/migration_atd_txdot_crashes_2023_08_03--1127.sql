52d800eff7fafc2f4ea19f5b2e2e4f39b8dae601
rose <rose.eichelmann@gmail.com>
2023-08-03 16:47:40 -0500

    add generated column to crashes table for location_id

---
+++ atd-vzd/migrations/migration_atd_txdot_crashes_2023_08_03--1127.sql
@@ -0,0 +1,15 @@
+-- Create a stored generated column that finds the location id of a crash 
+-- using its geographic position and keeps it updated.
+ALTER TABLE atd_txdot_crashes
+ADD COLUMN generated_location_id text GENERATED ALWAYS AS (get_cr3_location_id(position, crash_id, rpt_road_part_id, rpt_hwy_num)) STORED;
+
+-- TODO drop DB views that use location_id (replace later with generated location_id)
+
+-- Drop current atd_txdot_crashes location_id column
+ALTER TABLE atd_txdot_crashes DROP COLUMN location_id;
+
+-- Rename generated_location_id to location_id to replace it
+ALTER TABLE atd_txdot_crashes 
+RENAME COLUMN generated_location_id TO location_id;
+
+-- TODO add DB views that use location_id
