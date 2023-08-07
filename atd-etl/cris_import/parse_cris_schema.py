6eb92c1854c24b44449ef5ad200c5ac79a3adce1
Frank Hereford <frankhereford@users.noreply.github.com>
2023-08-07 17:15:20 -0500

    wheh .. adding postgres connectivity

--- atd-etl/cris_import/parse_cris_schema.py
+++ atd-etl/cris_import/parse_cris_schema.py
@@ -1,6 +1,92 @@
 from openpyxl import load_workbook
 import re
 import json
+import os
+import psycopg2
+import psycopg2.extras
+import onepasswordconnectsdk
+from sshtunnel import SSHTunnelForwarder
+from onepasswordconnectsdk.client import Client, new_client
+import tempfile
+import shutil
+
+import lib.mappings as mappings
+import lib.sql as util
+
+DEPLOYMENT_ENVIRONMENT = os.environ.get(
+    "ENVIRONMENT", "development"
+)  # our current environment from ['production', 'development']
+ONEPASSWORD_CONNECT_TOKEN = os.getenv("OP_API_TOKEN")  # our secret to get secrets 🤐
+ONEPASSWORD_CONNECT_HOST = os.getenv("OP_CONNECT")  # where we get our secrets
+VAULT_ID = os.getenv("OP_VAULT_ID")
+
+def main():
+    global DB_HOST
+    global DB_USER
+    global DB_PASS
+    global DB_NAME
+    global DB_SSL_REQUIREMENT
+
+    global DB_BASTION_HOST_SSH_USERNAME
+    global DB_BASTION_HOST_SSH_PRIVATE_KEY
+    global DB_BASTION_HOST
+    global DB_RDS_HOST
+
+    secrets = get_secrets()
+
+    SFTP_ENDPOINT = secrets["SFTP_endpoint"]
+    ZIP_PASSWORD = secrets["archive_extract_password"]
+
+    AWS_ACCESS_KEY_ID = secrets["aws_access_key"]
+    AWS_SECRET_ACCESS_KEY = secrets["aws_secret_key"]
+    AWS_CSV_ARCHIVE_BUCKET_NAME = secrets["s3_archive_bucket_name"]
+    AWS_CSV_ARCHIVE_PATH = secrets["s3_archive_path"]
+
+    DB_HOST = secrets["database_host"]
+    DB_USER = secrets["database_username"]
+    DB_PASS = secrets["database_password"]
+    DB_NAME = secrets["database_name"]
+    DB_SSL_REQUIREMENT = secrets["database_ssl_policy"]
+
+    DB_BASTION_HOST_SSH_USERNAME = secrets["bastion_ssh_username"]
+    DB_BASTION_HOST_SSH_PRIVATE_KEY = secrets["bastion_ssh_private_key"]
+    DB_BASTION_HOST = secrets["bastion_host"]
+    DB_RDS_HOST = secrets["database_host"]
+
+    SFTP_ENDPOINT_SSH_PRIVATE_KEY = secrets["sftp_endpoint_private_key"]
+
+    # read_xlsx_to_get_FK_relationships("/data/cris_spec.xlsx")
+    create_cris_lookup_tables("/data/cris_spec.xlsx")
+
+
+def create_cris_lookup_tables(file_path):
+    with SshKeyTempDir() as key_directory:
+        write_key_to_file(key_directory + "/id_ed25519", DB_BASTION_HOST_SSH_PRIVATE_KEY + "\n") 
+        ssh_tunnel = SSHTunnelForwarder(
+            (DB_BASTION_HOST),
+            ssh_username=DB_BASTION_HOST_SSH_USERNAME,
+            ssh_private_key=f"{key_directory}/id_ed25519",
+            remote_bind_address=(DB_RDS_HOST, 5432),
+        )
+        ssh_tunnel.start()
+
+        pg = psycopg2.connect(
+            host="localhost",
+            port=ssh_tunnel.local_bind_port,
+            user=DB_USER,
+            password=DB_PASS,
+            dbname=DB_NAME,
+            sslmode=DB_SSL_REQUIREMENT,
+            sslrootcert="/root/rds-combined-ca-bundle.pem",
+        )
+
+        workbook = load_workbook(filename=file_path)
+        for worksheet in workbook.worksheets:
+            print("Title: ", worksheet.title.lower())
+            match = re.search(r"(\w+)_LKP", worksheet.title)
+            lookup_table = match.group(1).lower() if match else None
+            if lookup_table:
+                print("Lookup Table: ", lookup_table)
 
 
 def process_worksheet(worksheet, lookups):
@@ -46,8 +132,119 @@ def read_xlsx_to_get_FK_relationships(file_path):
     print(json.dumps(primaryperson_lookups, indent=4))
 
 
-def main():
-    read_xlsx_to_get_FK_relationships("/data/cris_spec.xlsx")
+def get_secrets():
+    REQUIRED_SECRETS = {
+        "SFTP_endpoint": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": f"Common.SFTP Endpoint",
+            "opvault": VAULT_ID,
+        },
+        "sftp_endpoint_private_key": {
+            "opitem": "SFTP Endpoint Key",
+            "opfield": ".private key",
+            "opvault": VAULT_ID,
+        },
+        "archive_extract_password": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": "Common.CRIS Archive Extract Password",
+            "opvault": VAULT_ID,
+        },
+        "bastion_host": {
+            "opitem": "RDS Bastion Host",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Host",
+            "opvault": VAULT_ID,
+        },
+        "bastion_ssh_username": {
+            "opitem": "RDS Bastion Host",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.ssh Username",
+            "opvault": VAULT_ID,
+        },
+        "database_host": {
+            "opitem": "Vision Zero Database",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Database Host",
+            "opvault": VAULT_ID,
+        },
+        "database_username": {
+            "opitem": "Vision Zero Database",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Database Username",
+            "opvault": VAULT_ID,
+        },
+        "database_password": {
+            "opitem": "Vision Zero Database",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Database Password",
+            "opvault": VAULT_ID,
+        },
+        "database_name": {
+            "opitem": "Vision Zero Database",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Database Name",
+            "opvault": VAULT_ID,
+        },
+        "database_ssl_policy": {
+            "opitem": "Vision Zero Database",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.Database SSL Policy",
+            "opvault": VAULT_ID,
+        },
+        "aws_access_key": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.AWS Access key",
+            "opvault": VAULT_ID,
+        },
+        "aws_secret_key": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.AWS Secret key",
+            "opvault": VAULT_ID,
+        },
+        "s3_archive_bucket_name": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.S3 Archive Bucket Name",
+            "opvault": VAULT_ID,
+        },
+        "s3_archive_path": {
+            "opitem": "Vision Zero CRIS Import",
+            "opfield": f"{DEPLOYMENT_ENVIRONMENT}.S3 Archive Path",
+            "opvault": VAULT_ID,
+        },
+        "sftp_endpoint_private_key": {
+            "opitem": "SFTP Endpoint Key",
+            "opfield": ".private key",
+            "opvault": VAULT_ID,
+            },
+        "bastion_ssh_private_key": {
+            "opitem": "RDS Bastion Key",
+            "opfield": ".private key",
+            "opvault": VAULT_ID,
+            },
+    }
+
+    # instantiate a 1Password client
+    client: Client = new_client(ONEPASSWORD_CONNECT_HOST, ONEPASSWORD_CONNECT_TOKEN)
+    # get the requested secrets from 1Password
+    return onepasswordconnectsdk.load_dict(client, REQUIRED_SECRETS)
+
+
+# these temp directories are used to store ssh keys, because they will
+# automatically clean themselves up when they go out of scope.
+class SshKeyTempDir:
+    def __init__(self):
+        self.path = None
+
+    def __enter__(self):
+        self.path = tempfile.mkdtemp(dir='/tmp')
+        return self.path
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.path)
+
+def write_key_to_file(path, content):
+    # Open the file with write permissions and create it if it doesn't exist
+    fd = os.open(path, os.O_WRONLY | os.O_CREAT, 0o600)
+
+    # Write the content to the file
+    os.write(fd, content.encode())
+
+    # Close the file
+    os.close(fd)
+
 
 
 if __name__ == "__main__":
