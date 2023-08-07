b3c6dd9925c7dd4c52413cefda79486f69879394
Frank Hereford <frankhereford@users.noreply.github.com>
2023-08-07 16:35:23 -0500

    works for units with no modification

--- atd-etl/cris_import/parse_cris_schema.py
+++ atd-etl/cris_import/parse_cris_schema.py
@@ -11,14 +11,9 @@ def read_xlsx(file_path):
 
             for row in worksheet.iter_rows(values_only=True, min_row=9):
                 if 'lookup' in str(row[10]).lower():
+                    pass
+
                     print("")
-                    #print("Row 7: ", row[7])
-                    #print("Row 8: ", row[8])
-                    #print("Row 9: ", row[9])
-                    #print("Row 10: ", row[10])
-                    #print("Row 11: ", row[11])
-
-
                     match = re.search(r"#'(\w+)_LKP'", row[9])
                     lookup_table = match.group(1).lower() if match else None
                     field = (str(row[7]).split('.')[1].lower())
@@ -26,6 +21,23 @@ def read_xlsx(file_path):
 
                     print(f"Lookup Table: '{lookup_table}'")
                     print(f"Field: '{field}'")
+
+    for worksheet in workbook.worksheets:
+        if worksheet.title.lower() == "unit file specification":
+            print("Title: ", worksheet.title.lower())
+
+            for row in worksheet.iter_rows(values_only=True, min_row=9):
+                if 'lookup' in str(row[10]).lower():
+                    print("")
+                    match = re.search(r"#'(\w+)_LKP'", row[9])
+                    lookup_table = match.group(1).lower() if match else None
+                    field = (str(row[7]).split('.')[1].lower())
+                    table = str(row[10]).lower()
+
+                    print(f"Lookup Table: '{lookup_table}'")
+                    print(f"Field: '{field}'")
+
+
 
 
 def main():
