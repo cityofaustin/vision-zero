72f6727742404e4bd2797fa701aec0af1a540ad1
Frank Hereford <frankhereford@users.noreply.github.com>
2023-08-07 16:47:45 -0500

    and clean it up like a human wrote it

--- atd-etl/cris_import/parse_cris_schema.py
+++ atd-etl/cris_import/parse_cris_schema.py
@@ -2,6 +2,23 @@ from openpyxl import load_workbook
 import re
 import json
 
+def process_worksheet(worksheet, lookups):
+    """
+    Process a worksheet based on title specification and update the lookups dictionary.
+    """
+    print("Title: ", worksheet.title.lower())
+
+    for row in worksheet.iter_rows(values_only=True, min_row=9):
+        if 'lookup' in str(row[10]).lower():
+            print("")
+            match = re.search(r"#'(\w+)_LKP'", row[9])
+            lookup_table = match.group(1).lower() if match else None
+            field = (str(row[7]).split('.')[1].lower())
+
+            #print(f"Lookup Table: '{lookup_table}'")
+            #print(f"Field: '{field}'")
+            lookups[field] = lookup_table
+
 def read_xlsx(file_path):
     # Load the workbook
     workbook = load_workbook(filename=file_path)
@@ -11,69 +28,21 @@ def read_xlsx(file_path):
     person_lookups = dict()
     primaryperson_lookups = dict()
 
+
     for worksheet in workbook.worksheets:
         if worksheet.title.lower() == "crash file specification":
-            print("Title: ", worksheet.title.lower())
-
-            for row in worksheet.iter_rows(values_only=True, min_row=9):
-                if 'lookup' in str(row[10]).lower():
-                    print("")
-                    match = re.search(r"#'(\w+)_LKP'", row[9])
-                    lookup_table = match.group(1).lower() if match else None
-                    field = (str(row[7]).split('.')[1].lower())
-
-                    print(f"Lookup Table: '{lookup_table}'")
-                    print(f"Field: '{field}'")
-                    crash_lookups[field] = lookup_table
-
-    for worksheet in workbook.worksheets:
+            process_worksheet(worksheet, crash_lookups)
         if worksheet.title.lower() == "unit file specification":
-            print("Title: ", worksheet.title.lower())
-
-            for row in worksheet.iter_rows(values_only=True, min_row=9):
-                if 'lookup' in str(row[10]).lower():
-                    print("")
-                    match = re.search(r"#'(\w+)_LKP'", row[9])
-                    lookup_table = match.group(1).lower() if match else None
-                    field = (str(row[7]).split('.')[1].lower())
-
-                    print(f"Lookup Table: '{lookup_table}'")
-                    print(f"Field: '{field}'")
-                    unit_lookups[field] = lookup_table
-
-
-    for worksheet in workbook.worksheets:
+            process_worksheet(worksheet, unit_lookups)
         if worksheet.title.lower() == "person file specification":
-            print("Title: ", worksheet.title.lower())
-
-            for row in worksheet.iter_rows(values_only=True, min_row=9):
-                if 'lookup' in str(row[10]).lower():
-                    print("")
-                    match = re.search(r"#'(\w+)_LKP'", row[9])
-                    lookup_table = match.group(1).lower() if match else None
-                    field = (str(row[7]).split('.')[1].lower())
-
-                    print(f"Lookup Table: '{lookup_table}'")
-                    print(f"Field: '{field}'")
-                    person_lookups[field] = lookup_table
-
-    for worksheet in workbook.worksheets:
+            process_worksheet(worksheet, person_lookups)
         if worksheet.title.lower() == "primaryperson file specification":
-            print("Title: ", worksheet.title.lower())
+            process_worksheet(worksheet, primaryperson_lookups)
 
-            for row in worksheet.iter_rows(values_only=True, min_row=9):
-                if 'lookup' in str(row[10]).lower():
-                    print("")
-                    match = re.search(r"#'(\w+)_LKP'", row[9])
-                    lookup_table = match.group(1).lower() if match else None
-                    field = (str(row[7]).split('.')[1].lower())
-
-                    print(f"Lookup Table: '{lookup_table}'")
-                    print(f"Field: '{field}'")
-                    primaryperson_lookups[field] = lookup_table
-
-    
-
+    print(json.dumps(crash_lookups, indent=4))
+    print(json.dumps(unit_lookups, indent=4))
+    print(json.dumps(person_lookups, indent=4))
+    print(json.dumps(primaryperson_lookups, indent=4))
 
 def main():
     read_xlsx("/data/cris_spec.xlsx")
