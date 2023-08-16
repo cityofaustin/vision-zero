d1cbab001224ac453dd3530cb589bca38a8827cb
Mike <mike.dilley@gmail.com>
2023-08-16 17:43:43 -0500

    Index for non_cr3_mainlanes geometry column

--- atd-vzd/migrations/migration_noncr3_location_2023_08_02--1648.sql
+++ atd-vzd/migrations/migration_noncr3_location_2023_08_02--1648.sql
@@ -12,12 +12,16 @@ WHEN (OLD.latitude IS DISTINCT FROM NEW.latitude
 OR OLD.longitude IS DISTINCT FROM NEW.longitude)
 EXECUTE PROCEDURE update_noncr3_location();
 
--- 2. Drop materialized views that use location_id and branch from all_atd_apd_blueform
+-- 3. Add index on non_cr3_mainlanes geometries
+CREATE INDEX non_cr3_mainlanes_geom_idx ON non_cr3_mainlanes
+USING GIST (geometry);
+
+-- 3. Drop materialized views that use location_id and branch from all_atd_apd_blueform
 DROP MATERIALIZED VIEW IF EXISTS all_crashes_off_mainlane;
 DROP MATERIALIZED VIEW IF EXISTS all_non_cr3_crashes_off_mainlane;
 DROP MATERIALIZED VIEW IF EXISTS all_atd_apd_blueform;
 
--- 3. Drop materialized views that use location_id and branch from five_year_atd_apd_blueform
+-- 4. Drop materialized views that use location_id and branch from five_year_atd_apd_blueform
 DROP MATERIALIZED VIEW IF EXISTS five_year_highway_polygons_with_crash_data;
 DROP MATERIALIZED VIEW IF EXISTS five_year_all_crashes_outside_any_polygons;
 
@@ -32,6 +36,6 @@ DROP MATERIALIZED VIEW IF EXISTS five_year_non_cr3_crashes_off_mainlane;
 
 DROP MATERIALIZED VIEW IF EXISTS five_year_atd_apd_blueform;
 
--- 4. Drop functions that were removed and rolled into update_noncr3_location function
+-- 5. Drop functions that were removed and rolled into update_noncr3_location function
 DROP FUNCTION find_noncr3_mainlane_crash;
 DROP FUNCTION find_location_for_noncr3_collision;
