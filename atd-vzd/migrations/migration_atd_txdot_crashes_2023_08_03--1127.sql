dd674a85aca828307bf751784575822d06dbb263
rose <rose.eichelmann@gmail.com>
2023-08-24 13:51:08 -0500

    add commands to drop unused functions

--- atd-vzd/migrations/migration_atd_txdot_crashes_2023_08_03--1127.sql
+++ atd-vzd/migrations/migration_atd_txdot_crashes_2023_08_03--1127.sql
@@ -1,3 +1,4 @@
+-- Create triggers to update atd_txdot_crashes location_id
 CREATE TRIGGER update_cr3_location_on_insert
 BEFORE INSERT ON atd_txdot_crashes
 FOR EACH ROW
@@ -11,3 +12,8 @@ WHEN (OLD.position IS DISTINCT FROM NEW.position)
 EXECUTE PROCEDURE update_cr3_location();
 
 COMMENT ON FUNCTION update_cr3_location IS 'This trigger function is used to update the location_id on insert and update of a record in atd_txdot_crashes.';
+
+-- Drop functions that are no longer needed
+DROP FUNCTION find_cr3_mainlane_crash;
+DROP FUNCTION find_location_for_cr3_collision;
+DROP FUNCTION find_location_id_for_cr3_collision;
