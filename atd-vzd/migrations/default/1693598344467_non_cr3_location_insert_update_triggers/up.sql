e4335cb032be583dd247c2c309ac2440efed9b10
Mike <mike.dilley@gmail.com>
2023-09-01 15:17:07 -0500

    Update migration so it can go down and up again

--- atd-vzd/migrations/default/1693598344467_non_cr3_location_insert_update_triggers/up.sql
+++ atd-vzd/migrations/default/1693598344467_non_cr3_location_insert_update_triggers/up.sql
@@ -1,11 +1,11 @@
 -- 1. Add triggers to update location_id on insert and update
-CREATE TRIGGER update_noncr3_location_on_insert
+CREATE OR REPLACE TRIGGER update_noncr3_location_on_insert
 BEFORE INSERT ON atd_apd_blueform
 FOR EACH ROW
 WHEN (NEW.latitude IS NOT NULL AND NEW.longitude IS NOT NULL)
 EXECUTE PROCEDURE update_noncr3_location();
 
-CREATE TRIGGER update_noncr3_location_on_update
+CREATE OR REPLACE TRIGGER update_noncr3_location_on_update
 BEFORE UPDATE ON atd_apd_blueform
 FOR EACH ROW
 WHEN (OLD.latitude IS DISTINCT FROM NEW.latitude
@@ -13,7 +13,7 @@ OR OLD.longitude IS DISTINCT FROM NEW.longitude)
 EXECUTE PROCEDURE update_noncr3_location();
 
 -- 3. Add index on non_cr3_mainlanes geometries
-CREATE INDEX non_cr3_mainlanes_geom_idx ON non_cr3_mainlanes
+CREATE INDEX IF NOT EXISTS non_cr3_mainlanes_geom_idx ON non_cr3_mainlanes
 USING GIST (geometry);
 
 -- 3. Drop materialized views that use location_id and branch from all_atd_apd_blueform
@@ -37,8 +37,8 @@ DROP MATERIALIZED VIEW IF EXISTS five_year_non_cr3_crashes_off_mainlane;
 DROP MATERIALIZED VIEW IF EXISTS five_year_atd_apd_blueform;
 
 -- 5. Drop functions that were removed and rolled into update_noncr3_location function
-DROP FUNCTION find_noncr3_mainlane_crash;
-DROP FUNCTION find_location_for_noncr3_collision;
+DROP FUNCTION IF EXISTS find_noncr3_mainlane_crash;
+DROP FUNCTION IF EXISTS find_location_for_noncr3_collision;
 
 -- 6. Add comment to update_noncr3_location function
 COMMENT ON FUNCTION update_noncr3_location IS 'This function is used to update the location_id on insert and update of a record in atd_apd_blueform.';
