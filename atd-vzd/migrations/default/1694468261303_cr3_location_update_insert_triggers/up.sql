928dfaee8a1173e8ac4ae925850bb8f7243882c4
rose <rose.eichelmann@gmail.com>
2023-09-12 13:37:01 -0500

    add if exists to function drops

--- atd-vzd/migrations/default/1694468261303_cr3_location_update_insert_triggers/up.sql
+++ atd-vzd/migrations/default/1694468261303_cr3_location_update_insert_triggers/up.sql
@@ -14,6 +14,6 @@ EXECUTE PROCEDURE update_cr3_location();
 COMMENT ON FUNCTION update_cr3_location IS 'This trigger function is used to update the location_id on insert and update of a record in atd_txdot_crashes.';
 
 -- Drop functions that are no longer needed
-DROP FUNCTION find_cr3_mainlane_crash;
-DROP FUNCTION find_location_for_cr3_collision;
-DROP FUNCTION find_location_id_for_cr3_collision;
+DROP IF EXISTS FUNCTION find_cr3_mainlane_crash;
+DROP IF EXISTS FUNCTION find_location_for_cr3_collision;
+DROP IF EXISTS FUNCTION find_location_id_for_cr3_collision;
