508b9b99b324df253ac705c11cfbf6a0cd636984
Frank Hereford <frankhereford@users.noreply.github.com>
2023-09-12 09:36:45 -0500

    missed a file in the last commit

--- atd-toolbox/materials_from_ldm_conversion/triggers_to_maintain_VZ_records_for_fact_tables.sql
+++ atd-toolbox/materials_from_ldm_conversion/triggers_to_maintain_VZ_records_for_fact_tables.sql
@@ -1,26 +1,26 @@
-drop trigger if exists trg_vz_create_atd_txdot_crashes ON cris_fact_tables.atd_txdot_crashes;
-drop function if exists cris_fact_tables.maintain_vz_schema_crash_records;
+drop trigger if exists trg_vz_create_atd_txdot_crashes ON cris_facts.atd_txdot_crashes;
+drop function if exists cris_facts.maintain_vz_schema_crash_records;
 
-drop trigger if exists trg_vz_create_atd_txdot_units ON cris_fact_tables.atd_txdot_units;
-drop function if exists cris_fact_tables.maintain_vz_schema_unit_records;
+drop trigger if exists trg_vz_create_atd_txdot_units ON cris_facts.atd_txdot_units;
+drop function if exists cris_facts.maintain_vz_schema_unit_records;
 
-drop trigger if exists trg_vz_create_atd_txdot_person on cris_fact_tables.atd_txdot_person;
-drop function if exists cris_fact_tables.maintain_vz_schema_person_records;
+drop trigger if exists trg_vz_create_atd_txdot_person on cris_facts.atd_txdot_person;
+drop function if exists cris_facts.maintain_vz_schema_person_records;
 
-drop trigger if exists trg_vz_create_atd_txdot_primaryperson on cris_fact_tables.atd_txdot_primaryperson;
-drop function if exists cris_fact_tables.maintain_vz_schema_primaryperson_records;
-
-
+drop trigger if exists trg_vz_create_atd_txdot_primaryperson on cris_facts.atd_txdot_primaryperson;
+drop function if exists cris_facts.maintain_vz_schema_primaryperson_records;
 
 
 
-CREATE OR REPLACE FUNCTION cris_fact_tables.maintain_vz_schema_crash_records()
+
+
+CREATE OR REPLACE FUNCTION cris_facts.maintain_vz_schema_crash_records()
 RETURNS TRIGGER AS $$
 BEGIN
   IF TG_OP = 'INSERT' THEN
-    INSERT INTO vz_fact_tables.atd_txdot_crashes (crash_id) VALUES (NEW.crash_id);
+    INSERT INTO vz_facts.atd_txdot_crashes (crash_id) VALUES (NEW.crash_id);
   ELSIF TG_OP = 'DELETE' THEN
-    DELETE FROM vz_fact_tables.atd_txdot_crashes WHERE crash_id = OLD.crash_id;
+    DELETE FROM vz_facts.atd_txdot_crashes WHERE crash_id = OLD.crash_id;
   END IF;
   RETURN NULL;
 END;
@@ -29,22 +29,22 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_vz_create_atd_txdot_crashes
 AFTER INSERT OR DELETE
-ON cris_fact_tables.atd_txdot_crashes
+ON cris_facts.atd_txdot_crashes
 FOR EACH ROW
-EXECUTE FUNCTION cris_fact_tables.maintain_vz_schema_crash_records();
+EXECUTE FUNCTION cris_facts.maintain_vz_schema_crash_records();
 
 
 
 
 
 
-CREATE OR REPLACE FUNCTION cris_fact_tables.maintain_vz_schema_unit_records()
+CREATE OR REPLACE FUNCTION cris_facts.maintain_vz_schema_unit_records()
 RETURNS TRIGGER AS $$
 BEGIN
   IF TG_OP = 'INSERT' THEN
-    INSERT INTO vz_fact_tables.atd_txdot_units (crash_id, unit_nbr) VALUES (NEW.crash_id, NEW.unit_nbr);
+    INSERT INTO vz_facts.atd_txdot_units (crash_id, unit_nbr) VALUES (NEW.crash_id, NEW.unit_nbr);
   ELSIF TG_OP = 'DELETE' THEN
-    DELETE FROM vz_fact_tables.atd_txdot_units WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr;
+    DELETE FROM vz_facts.atd_txdot_units WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr;
   END IF;
   RETURN NULL;
 END;
@@ -52,22 +52,22 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_vz_create_atd_txdot_units
 AFTER INSERT OR DELETE
-ON cris_fact_tables.atd_txdot_units
+ON cris_facts.atd_txdot_units
 FOR EACH ROW
-EXECUTE FUNCTION cris_fact_tables.maintain_vz_schema_unit_records();
+EXECUTE FUNCTION cris_facts.maintain_vz_schema_unit_records();
 
 
 
 
 
 
-CREATE OR REPLACE FUNCTION cris_fact_tables.maintain_vz_schema_person_records()
+CREATE OR REPLACE FUNCTION cris_facts.maintain_vz_schema_person_records()
 RETURNS TRIGGER AS $$
 BEGIN
   IF TG_OP = 'INSERT' THEN
-    INSERT INTO vz_fact_tables.atd_txdot_person (crash_id, unit_nbr, prsn_nbr) VALUES (NEW.crash_id, NEW.unit_nbr, NEW.prsn_nbr);
+    INSERT INTO vz_facts.atd_txdot_person (crash_id, unit_nbr, prsn_nbr) VALUES (NEW.crash_id, NEW.unit_nbr, NEW.prsn_nbr);
   ELSIF TG_OP = 'DELETE' THEN
-    DELETE FROM vz_fact_tables.atd_txdot_person WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr and prsn_nbr = OLD.prsn_nbr;
+    DELETE FROM vz_facts.atd_txdot_person WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr and prsn_nbr = OLD.prsn_nbr;
   END IF;
   RETURN NULL;
 END;
@@ -75,22 +75,22 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_vz_create_atd_txdot_person
 AFTER INSERT OR DELETE
-ON cris_fact_tables.atd_txdot_person
+ON cris_facts.atd_txdot_person
 FOR EACH ROW
-EXECUTE FUNCTION cris_fact_tables.maintain_vz_schema_person_records();
+EXECUTE FUNCTION cris_facts.maintain_vz_schema_person_records();
 
 
 
 
 
 
-CREATE OR REPLACE FUNCTION cris_fact_tables.maintain_vz_schema_primaryperson_records()
+CREATE OR REPLACE FUNCTION cris_facts.maintain_vz_schema_primaryperson_records()
 RETURNS TRIGGER AS $$
 BEGIN
   IF TG_OP = 'INSERT' THEN
-    INSERT INTO vz_fact_tables.atd_txdot_primaryperson (crash_id, unit_nbr, prsn_nbr) VALUES (NEW.crash_id, NEW.unit_nbr, NEW.prsn_nbr);
+    INSERT INTO vz_facts.atd_txdot_primaryperson (crash_id, unit_nbr, prsn_nbr) VALUES (NEW.crash_id, NEW.unit_nbr, NEW.prsn_nbr);
   ELSIF TG_OP = 'DELETE' THEN
-    DELETE FROM vz_fact_tables.atd_txdot_primaryperson WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr and prsn_nbr = OLD.prsn_nbr;
+    DELETE FROM vz_facts.atd_txdot_primaryperson WHERE crash_id = OLD.crash_id and unit_nbr = OLD.unit_nbr and prsn_nbr = OLD.prsn_nbr;
   END IF;
   RETURN NULL;
 END;
@@ -98,6 +98,6 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_vz_create_atd_txdot_primaryperson
 AFTER INSERT OR DELETE
-ON cris_fact_tables.atd_txdot_primaryperson
+ON cris_facts.atd_txdot_primaryperson
 FOR EACH ROW
-EXECUTE FUNCTION cris_fact_tables.maintain_vz_schema_primaryperson_records();
+EXECUTE FUNCTION cris_facts.maintain_vz_schema_primaryperson_records();
