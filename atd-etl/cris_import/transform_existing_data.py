a15485126801c9c635acbd924fdd2eab85f70e3c
Frank Hereford <frankhereford@users.noreply.github.com>
2023-09-14 14:27:03 -0500

    workaround for string as int problem

--- atd-etl/cris_import/transform_existing_data.py
+++ atd-etl/cris_import/transform_existing_data.py
@@ -95,6 +95,20 @@ def main():
         # build up the query and run it
         for table in tables:
             for field in table['lookup_map']:
+
+                # 👇 Workaround for `character` as `integer` datatype in VZDB crashes tables
+                if field["field_name"] == 'hwy_dsgn_lane_id':
+                    sql = """update production_fact_tables.atd_txdot_crashes set hwy_dsgn_lane_id = null 
+                            where
+                                    hwy_dsgn_lane_id = 'null'
+                                or  hwy_dsgn_lane_id = ''
+                            """
+                    cursor = pg.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+                    cursor.execute(sql)
+                    pg.commit()
+                    cursor.close()
+                
+
                 sql = f"update production_fact_tables.{table['imported_table']} set "
                 assignments = []
                 if field["lookup_table"] is not None:
