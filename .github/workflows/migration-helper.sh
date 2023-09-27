fafba17e260c9abdb28b7b0cf33b695a885dec21
Mike <mike.dilley@gmail.com>
2023-09-27 13:19:46 -0500

    Fix env var names, disable interactive migrate apply command

--- .github/workflows/migration-helper.sh
+++ .github/workflows/migration-helper.sh
@@ -12,13 +12,19 @@ esac
 echo "SOURCE -> BRANCH_NAME: ${BRANCH_NAME}"
 echo "SOURCE -> WORKING_STAGE: ${WORKING_STAGE}"
 
+#
+# Export environment variables that Hasura CLI will use
+#
 function export_hasura_env_vars() {
     # Use jq to get Hasura endpoint and admin secret from ZAPPA_SETTINGS
-    export HASURA_ENDPOINT=$(echo ${ZAPPA_SETTINGS} | jq -r ".\"${WORKING_STAGE}\".aws_environment_variables.HASURA_ENDPOINT")
-    export HASURA_GRAPHQL_ADMIN_SECRET=$(echo ${ZAPPA_SETTINGS} | jq -r ".\"${WORKING_STAGE}\".aws_environment_variables.HASURA_ADMIN_SECRET")
+    export HASURA_GRAPHQL_ENDPOINT=$(echo ${ZAPPA_SETTINGS} | \
+    jq -r ".\"${WORKING_STAGE}\".aws_environment_variables.HASURA_ENDPOINT")
+    
+    export HASURA_GRAPHQL_ADMIN_SECRET=$(echo ${ZAPPA_SETTINGS} | \
+    jq -r ".\"${WORKING_STAGE}\".aws_environment_variables.HASURA_ADMIN_SECRET")
     
     # Remove /v1/graphql from the end of the graphql endpoint for Hasura CLI commands
-    export HASURA_ENDPOINT=$(basename "${HASURA_ENDPOINT%/v1/graphql}")
+    export HASURA_GRAPHQL_ENDPOINT=$(basename "${HASURA_ENDPOINT%/v1/graphql}")
 }
 
 # TODO add skip check for default db
@@ -26,7 +32,7 @@ function run_migration() {
   echo "----- MIGRATIONS STARTED -----";
   hasura --skip-update-check version;
   echo "Applying migration";
-  hasura --skip-update-check migrate apply;
+  hasura --skip-update-check --disable-interactive --database-name default migrate apply;
   echo "Applying metadata";
   hasura --skip-update-check metadata apply;
   echo "----- MIGRATIONS FINISHED -----";
