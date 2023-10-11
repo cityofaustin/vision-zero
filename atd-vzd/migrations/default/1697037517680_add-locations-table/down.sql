4830f60f9f1dd4f4f3ecffd287b9c21e39f59aa4
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-11 10:23:42 -0500

    make migrations out of SQL commands

---
+++ atd-vzd/migrations/default/1697037517680_add-locations-table/down.sql
@@ -0,0 +1,3 @@
+drop table if exists public.atd_txdot_locations cascade;
+drop function if exists atd_txdot_locations_updates_audit_log;
+drop function if exists atd_txdot_locations_updates_crash_locations;
