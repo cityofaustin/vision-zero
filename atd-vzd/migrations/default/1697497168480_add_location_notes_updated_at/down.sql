df5054da83543ccadebb84c5964e7db21fdd9bf1
Mike <mike.dilley@gmail.com>
2023-10-16 18:00:44 -0500

    Add trigger functino

---
+++ atd-vzd/migrations/default/1697497168480_add_location_notes_updated_at/down.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION IF EXISTS public.set_current_timestamp_updated_at()
---
+++ atd-vzd/migrations/default/1697497168480_add_location_notes_updated_at/up.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION public.set_current_timestamp_updated_at()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$function$;
