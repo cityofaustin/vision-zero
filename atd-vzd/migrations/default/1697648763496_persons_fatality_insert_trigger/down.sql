be40d762139072863aa16b36d6ef6e39dbc914c8
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-18 12:11:18 -0500

    add trigger for atd_txdot_person_fatal_insert to vz_facts.persons table

---
+++ atd-vzd/migrations/default/1697648763496_persons_fatality_insert_trigger/down.sql
@@ -0,0 +1,1 @@
+drop trigger if exists atd_txdot_person_fatal_insert on vz_facts.atd_txdot_person;
---
+++ atd-vzd/migrations/default/1697648763496_persons_fatality_insert_trigger/up.sql
@@ -0,0 +1,9 @@
+create trigger atd_txdot_person_fatal_insert after
+insert
+    on
+    vz_facts.atd_txdot_person for each row
+    when (
+        (
+            new.prsn_injry_sev_id = 4
+        )
+    ) execute function fatality_insert()
