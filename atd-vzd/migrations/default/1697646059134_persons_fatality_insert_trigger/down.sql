6c227d0b8debc940d8ababd540f7e9fc868f6a98
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-18 14:02:41 -0500

    bind trigger on both tables

--- atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/down.sql
+++ atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/down.sql
@@ -1,1 +1,2 @@
 drop trigger if exists atd_txdot_person_fatal_insert on vz_facts.atd_txdot_person;
+drop trigger if exists atd_txdot_person_fatal_insert on cris_facts.atd_txdot_person;
--- atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/up.sql
+++ atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/up.sql
@@ -6,4 +6,14 @@ insert
         (
             new.prsn_injry_sev_id = 4
         )
-    ) execute function fatality_insert()
+    ) execute function fatality_insert();
+
+create trigger atd_txdot_person_fatal_insert after
+insert
+    on
+    cris_facts.atd_txdot_person for each row
+    when (
+        (
+            new.prsn_injry_sev_id = 4
+        )
+    ) execute function fatality_insert();
