8fe9efd58889bc18b8b785a257e20e4ffaa1fc7a
Mike <mike.dilley@gmail.com>
2023-10-24 17:06:11 -0500

    Fix change log trigger function to insert previous state from the unified view

--- atd-vzd/migrations/default/1697836965147_add_units_change_log_function/up.sql
+++ atd-vzd/migrations/default/1697836965147_add_units_change_log_function/up.sql
@@ -1,11 +1,11 @@
+-- Insert the previous state of the record from the unified public.atd_txdot_units view into the change log table
 CREATE OR REPLACE FUNCTION public.units_change_log_insert()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
 BEGIN
     INSERT INTO public.atd_txdot_change_log (record_id, record_crash_id, record_type, record_json)
-    VALUES (old.unit_id, old.crash_id, 'units', row_to_json(old));
-
+    VALUES (old.unit_id, old.crash_id, 'units', (SELECT row_to_json(atd_txdot_units) FROM public.atd_txdot_units WHERE unit_nbr = old.unit_nbr AND crash_id = old.crash_id));
    RETURN NEW;
 END;
 $function$;
