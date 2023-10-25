9670289b73786ed49e53eab86933dd1d16af2a41
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-25 16:36:54 -0500

    add example of how to look up a LDM id to use instead of what's in NEW

--- atd-vzd/migrations/default/1697646059133_fataility_insert_function/up.sql
+++ atd-vzd/migrations/default/1697646059133_fataility_insert_function/up.sql
@@ -2,14 +2,20 @@ CREATE OR REPLACE FUNCTION public.fatality_insert()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
+DECLARE
+ldm_person_id INTEGER;
     BEGIN
     	IF (TG_TABLE_NAME = 'atd_txdot_primaryperson') THEN
     	    INSERT INTO fatalities (crash_id, primaryperson_id)
     		    VALUES (NEW.crash_id, NEW.primaryperson_id);
     	ELSIF (TG_TABLE_NAME = 'atd_txdot_person') THEN
 			   
+        --SELECT some_column INTO new_value
+        --FROM some_table
+        --WHERE some_condition = NEW.some_attribute;
 
-
+        --INSERT INTO fatalities (crash_id, person_id)
+            --VALUES (NEW.crash_id, new_value);
 
     		INSERT INTO fatalities (crash_id, person_id)
     		    VALUES (NEW.crash_id, NEW.person_id);
