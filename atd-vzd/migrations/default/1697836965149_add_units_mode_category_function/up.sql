f5119924165aece73bb89045f4c9f0289652d7a4
Mike <mike.dilley@gmail.com>
2023-10-25 14:27:02 -0500

    Update atd mode category trigger to select from cris_facts on insert and new update value on update

--- atd-vzd/migrations/default/1697836965149_add_units_mode_category_function/up.sql
+++ atd-vzd/migrations/default/1697836965149_add_units_mode_category_function/up.sql
@@ -2,34 +2,49 @@ CREATE OR REPLACE FUNCTION public.units_mode_category()
  RETURNS trigger
  LANGUAGE plpgsql
 AS $function$
+DECLARE 
+    cris_unit_desc_id int;
+    cris_veh_body_styl_id int;
 BEGIN
+    -- Set the veh_body_styl_id and unit_desc_id variables from cris_facts schema on INSERT or
+    -- updated vz_facts table row on UPDATE
+    if  (TG_OP = 'INSERT') then
+        SELECT unit_desc_id, veh_body_styl_id INTO cris_unit_desc_id, cris_veh_body_styl_id 
+        FROM cris_facts.atd_txdot_units WHERE unit_nbr = NEW.unit_nbr AND crash_id = NEW.crash_id;
+    end if;
+
+    if  (TG_OP = 'UPDATE') then
+        cris_unit_desc_id := NEW.unit_desc_id;
+        cris_veh_body_styl_id := NEW.veh_body_styl_id;
+    end if;
+
     -- output value (in parentheses) refers to atd__mode_category_lkp table
     NEW.atd_mode_category = (CASE
     -- PEDALCYCLIST / BICYCLE (5)
-    WHEN NEW.unit_desc_id = 3 THEN 5
+    WHEN cris_unit_desc_id = 3 THEN 5
     -- MOTORIZED CONVEYANCE (6)
-    WHEN NEW.unit_desc_id = 5 THEN 6
+    WHEN cris_unit_desc_id = 5 THEN 6
     -- PEDESTRIAN (7)
-    WHEN NEW.unit_desc_id = 4 THEN 7
+    WHEN cris_unit_desc_id = 4 THEN 7
     -- TRAIN (8)
-    WHEN NEW.unit_desc_id = 2 THEN 8
+    WHEN cris_unit_desc_id = 2 THEN 8
     -- MICROMOBILITY DEVICES
-    WHEN NEW.unit_desc_id = 177 THEN (
+    WHEN cris_unit_desc_id = 177 THEN (
         CASE
         -- E-SCOOTER (11)
-        WHEN NEW.veh_body_styl_id = 177 THEN 11
+        WHEN cris_veh_body_styl_id = 177 THEN 11
         -- MICROMOBILITY DEVICE (10)
         ELSE 10 END
     )
     -- MOTOR VEHICLES
-    WHEN NEW.unit_desc_id = 1 THEN (
+    WHEN cris_unit_desc_id = 1 THEN (
         CASE
         -- PASSENGER CAR (1)
-        WHEN NEW.veh_body_styl_id IN (100,104) THEN 1
+        WHEN cris_veh_body_styl_id IN (100,104) THEN 1
         -- LARGE PASSENGER CAR (2)
-        WHEN NEW.veh_body_styl_id IN (30, 69, 103, 106) THEN 2
+        WHEN cris_veh_body_styl_id IN (30, 69, 103, 106) THEN 2
         -- MOTORCYCLE (3)
-        WHEN NEW.veh_body_styl_id IN (71, 90) THEN 3
+        WHEN cris_veh_body_styl_id IN (71, 90) THEN 3
         -- MOTOR VEHICLE - OTHER (4)
         ELSE 4 END
     ) ELSE 9
