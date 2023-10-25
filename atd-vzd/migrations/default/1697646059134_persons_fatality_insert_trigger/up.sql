3a8e0012eae7456085ff43fe6771e1b2baa01c72
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-25 16:38:01 -0500

    only want to track 'INSERT' events on the cris side, not the VZ side
    We'll always make a record in CRIS (normally by the ETL) and it won't make sense to ever be inserting ourselves (only via the trigger) on the vz side.

--- atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/up.sql
+++ atd-vzd/migrations/default/1697646059134_persons_fatality_insert_trigger/up.sql
@@ -1,12 +1,13 @@
-create trigger trg_20_person_fatal_insert after
-insert
-    on
-    vz_facts.atd_txdot_person for each row
-    when (
-        (
-            new.prsn_injry_sev_id = 4
-        )
-    ) execute function fatality_insert();
+-- not creating this - trigger only on the cris (leading) schema
+-- create trigger trg_20_person_fatal_insert after
+-- insert
+--    on
+--    vz_facts.atd_txdot_person for each row
+--    when (
+--        (
+--            new.prsn_injry_sev_id = 4
+--        )
+--    ) execute function fatality_insert('vz_facts.atd_txdot_person');
 
 create trigger trg_20_person_fatal_insert after
 insert
