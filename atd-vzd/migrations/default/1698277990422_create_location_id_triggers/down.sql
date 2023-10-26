af0bb08cd6904cf7243254418cd381bf54224bfb
rose <rose.eichelmann@gmail.com>
2023-10-25 19:24:22 -0500

    create location id triggers up

---
+++ atd-vzd/migrations/default/1698277990422_create_location_id_triggers/down.sql
@@ -0,0 +1,4 @@
+DROP TRIGGER IF EXISTS update_cr3_location_on_cris_update ON cris_facts.atd_txdot_crashes;
+DROP TRIGGER IF EXISTS update_cr3_location_on_vz_update ON vz_facts.atd_txdot_crashes;
+DROP TRIGGER IF EXISTS update_cr3_location_on_cris_insert ON cris_facts.atd_txdot_crashes;
+DROP TRIGGER IF EXISTS update_cr3_location_on_vz_insert ON vz_facts.atd_txdot_crashes;
