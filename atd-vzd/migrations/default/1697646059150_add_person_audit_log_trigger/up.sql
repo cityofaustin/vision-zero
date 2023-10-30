57b32951756862f862c4add417fb479af7bc9e38
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-30 09:54:54 -0500

    bind on edit to the VZ side

--- atd-vzd/migrations/default/1697646059150_add_person_audit_log_trigger/up.sql
+++ atd-vzd/migrations/default/1697646059150_add_person_audit_log_trigger/up.sql
@@ -1,3 +1,5 @@
 create trigger atd_txdot_person_audit_log before
 update on cris_facts.atd_txdot_person for each row execute function atd_txdot_person_updates_audit_log();
 
+create trigger atd_txdot_person_audit_log before
+update on vz_facts.atd_txdot_person for each row execute function atd_txdot_person_updates_audit_log();
