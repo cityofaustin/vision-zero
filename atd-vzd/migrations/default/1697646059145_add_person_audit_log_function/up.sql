1ad71af1fe6ee9e3f53e667c3506ff2ead390f62
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-30 09:53:29 -0500

    remove debug statements

--- atd-vzd/migrations/default/1697646059145_add_person_audit_log_function/up.sql
+++ atd-vzd/migrations/default/1697646059145_add_person_audit_log_function/up.sql
@@ -5,14 +5,13 @@ AS $function$
 DECLARE
     cris_id integer; 
 BEGIN
-    RAISE NOTICE 'Were here.';
     RAISE NOTICE 'TG_TABLE_SCHEMA: %', TG_TABLE_SCHEMA;
     IF (TG_TABLE_SCHEMA = 'cris_facts') THEN
-      RAISE NOTICE 'CRIS SCHEMA';
+      --RAISE NOTICE 'CRIS SCHEMA';
       INSERT INTO atd_txdot_change_log (record_id, record_crash_id, record_type, record_json)
       VALUES (old.person_id, old.crash_id, 'person', row_to_json(old));
     ELSE
-      RAISE NOTICE 'VZ SCHEMA';
+      --RAISE NOTICE 'VZ SCHEMA';
       SELECT cris_person_id
       INTO cris_id
       FROM public.atd_txdot_person 
