d50189e0c3085283b74c5702828ef967bd4d486e
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-30 15:43:18 -0500

    remove comment

--- atd-vzd/migrations/default/1697646059133_fataility_insert_function/up.sql
+++ atd-vzd/migrations/default/1697646059133_fataility_insert_function/up.sql
@@ -9,16 +9,6 @@ AS $function$
     	    INSERT INTO fatalities (crash_id, primaryperson_id)
     		    VALUES (NEW.crash_id, NEW.primaryperson_id);
     	ELSIF (TG_TABLE_NAME = 'atd_txdot_person') THEN
-			   
-				 -- since we have a 1:1 relationship between our VZ and CRIS fact tables,
-				 -- use the person_id from one of them as the unique identifier for the combined
-				 -- pair of records in other relations? otherwise, use this method to get whatever value
-				 -- is needed.
-
-				 --SELECT some_column INTO new_value
-        --FROM some_table
-        --WHERE some_condition = NEW.some_attribute;
-			   
     		INSERT INTO fatalities (crash_id, person_id)
     		    VALUES (NEW.crash_id, NEW.person_id);
     	END IF;
