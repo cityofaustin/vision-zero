b9d3e9e2bb220b30ca38e8eb14dbc44b4ee96bcf
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-30 09:05:27 -0500

    clean up some debug stuff from here

--- atd-vzd/migrations/default/1697646059138_update_fatality_soft_delete_function/up.sql
+++ atd-vzd/migrations/default/1697646059138_update_fatality_soft_delete_function/up.sql
@@ -35,19 +35,20 @@ DECLARE
                 WHERE (f.person_id = NEW.person_id);
             END IF;
         ELSIF (TG_TABLE_NAME = 'atd_txdot_person' and TG_TABLE_SCHEMA = 'vz_facts') THEN
-            RAISE NOTICE 'The value of NEW.person_id is %', NEW.person_id;
-            SELECT cris_person_id, prsn_injry_sev_id INTO cris_id, injury_severity_id FROM public.atd_txdot_person WHERE vz_person_id = NEW.person_id;
-            RAISE NOTICE 'The value of cris_id is %', cris_id;
-            RAISE NOTICE 'The value of prsn_injry_sev_id is %', injury_severity_id;
+            --RAISE NOTICE 'The value of NEW.person_id is %', NEW.person_id;
+            SELECT cris_person_id, prsn_injry_sev_id 
+            INTO cris_id, injury_severity_id 
+            FROM public.atd_txdot_person 
+            WHERE vz_person_id = NEW.person_id;
+            --RAISE NOTICE 'The value of cris_id is %', cris_id;
+            --RAISE NOTICE 'The value of prsn_injry_sev_id is %', injury_severity_id;
 
             IF (NEW.prsn_injry_sev_id = 4) THEN
-                RAISE NOTICE 'we are in the insert branch';
                 INSERT INTO fatalities (crash_id, person_id)
                 VALUES (NEW.crash_id, NEW.person_id)
                 ON CONFLICT (person_id)
                 DO UPDATE SET is_deleted = false;
             ELSE
-                RAISE NOTICE 'we are in the update branch';
                 UPDATE fatalities f
                 SET is_deleted = true
                 WHERE (f.person_id = NEW.person_id);
