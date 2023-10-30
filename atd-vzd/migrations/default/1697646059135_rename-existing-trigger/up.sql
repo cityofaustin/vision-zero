d00066d5d518ce2ce96a57cd1c30b9aaeb01ca41
Frank Hereford <frankhereford@users.noreply.github.com>
2023-10-30 13:47:52 -0500

    missed a file

--- atd-vzd/migrations/default/1697646059135_rename-existing-trigger/up.sql
+++ atd-vzd/migrations/default/1697646059135_rename-existing-trigger/up.sql
@@ -1,6 +1,4 @@
--- ALTER TRIGGER trg_vz_create_atd_txdot_person ON cris_facts.atd_txdot_person RENAME TO trg_10_create_atd_txdot_person;
 DROP TRIGGER trg_vz_create_atd_txdot_person ON cris_facts.atd_txdot_person; 
-
 
 create trigger trg_10_create_atd_txdot_person before 
 insert or delete on cris_facts.atd_txdot_person 
