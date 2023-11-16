ccd3e965b9dcd6187f4332231accf0f6453048fe
rose <rose.eichelmann@gmail.com>
2023-11-16 14:13:05 -0600

    revise column, its not a foreign key its an object relationship

--- atd-vzd/migrations/default/1700162755001_alter_table_public_atd_txdot_primaryperson_add_column_ems_id/up.sql
+++ atd-vzd/migrations/default/1700162755001_alter_table_public_atd_txdot_primaryperson_add_column_ems_id/up.sql
@@ -1,4 +1,4 @@
 alter table "public"."atd_txdot_primaryperson" add column "ems_id" integer
  null;
 
-COMMENT ON COLUMN public.atd_txdot_primaryperson.ems_id IS 'This field has a foreign key relationship with the "id" field of the ems__incidents table.';
+COMMENT ON COLUMN public.atd_txdot_primaryperson.ems_id IS 'This field has an object relationship with the "id" field of the ems__incidents table.';
