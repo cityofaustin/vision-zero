cde706a158559709a5011278ba8d850cdf38c7e0
rose <rose.eichelmann@gmail.com>
2023-12-21 14:36:02 -0600

    update comments

--- atd-vzd/migrations/default/1700162755001_alter_table_public_atd_txdot_primaryperson_add_column_ems_id/up.sql
+++ atd-vzd/migrations/default/1700162755001_alter_table_public_atd_txdot_primaryperson_add_column_ems_id/up.sql
@@ -1,4 +1,4 @@
 alter table "public"."atd_txdot_primaryperson" add column "ems_id" integer
  null;
 
-COMMENT ON COLUMN public.atd_txdot_primaryperson.ems_id IS 'This field has an object relationship with the "id" field of the ems__incidents table.';
+COMMENT ON COLUMN public.atd_txdot_primaryperson.ems_id IS 'This field holds the foreign key that references a single record in the ems__incidents table.';
