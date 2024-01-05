11a0087646d5bd071c41890189d71a5fa4e930d8
Mike <mike.dilley@gmail.com>
2024-01-05 15:10:17 -0600

    Update confirm button to fire handleDeleteClick

--- atd-vze/src/Components/Notes/Notes.js
+++ atd-vze/src/Components/Notes/Notes.js
@@ -45,7 +45,7 @@ const Notes = ({
   const [editNote] = useMutation(UPDATE_NOTE);
   const [deleteNote] = useMutation(DELETE_NOTE);
 
-  // Confirm delete note modal
+  // Confirm delete modal
   const [showModal, setShowModal] = useState(false);
 
   const toggleModal = () => {
@@ -105,217 +105,214 @@ const Notes = ({
 
   // function to handle delete note button click
   const handleDeleteClick = row => {
-    toggleModal();
-    // const id = row.id;
-    // deleteNote({
-    //   variables: {
-    //     id: id,
-    //   },
-    // })
-    //   .then(response => {
-    //     refetch();
-    //   })
-    //   .catch(error => console.error(error));
+    const id = row.id;
+    deleteNote({
+      variables: {
+        id: id,
+      },
+    })
+      .then(response => {
+        refetch();
+      })
+      .catch(error => console.error(error));
   };
 
   // render notes card and table
   return (
-    <>
-      <Card>
-        <CardHeader>{fieldConfig.title}</CardHeader>
-        <CardBody style={{ padding: "5px 20px 20px 20px" }}>
-          <Table style={{ width: "100%" }}>
-            <thead>
-              {/* display label for each field in table header*/}
-              <tr>
-                <th
-                  style={{
-                    width: "10%",
-                    borderTop: "0px",
-                    borderBottom: "1px",
-                  }}
-                >
-                  {fieldConfig.fields.date.label}
-                </th>
-                <th
-                  style={{
-                    width: "24%",
-                    borderTop: "0px",
-                    borderBottom: "1px",
-                  }}
-                >
-                  {fieldConfig.fields.user_email.label}
-                </th>
-                <th
-                  style={{
-                    width: "54%",
-                    borderTop: "0px",
-                    borderBottom: "1px",
-                  }}
-                >
-                  {fieldConfig.fields.text.label}
-                </th>
-                {/* only create extra columns if user has edit permissions */}
-                {!isReadOnly(roles) && (
-                  <th
-                    style={{
-                      width: "6%",
-                      borderTop: "0px",
-                      borderBottom: "1px",
-                    }}
-                  ></th>
-                )}
-                {/* only create extra columns if user has edit permissions */}
-                {!isReadOnly(roles) && (
-                  <th
-                    style={{
-                      width: "6%",
-                      borderTop: "0px",
-                      borderBottom: "1px",
-                    }}
-                  ></th>
-                )}
-              </tr>
-            </thead>
-            <tbody>
-              {/* display user input row for users with edit permissions*/}
+    <Card>
+      <CardHeader>{fieldConfig.title}</CardHeader>
+      <CardBody style={{ padding: "5px 20px 20px 20px" }}>
+        <Table style={{ width: "100%" }}>
+          <thead>
+            {/* display label for each field in table header*/}
+            <tr>
+              <th
+                style={{
+                  width: "10%",
+                  borderTop: "0px",
+                  borderBottom: "1px",
+                }}
+              >
+                {fieldConfig.fields.date.label}
+              </th>
+              <th
+                style={{
+                  width: "24%",
+                  borderTop: "0px",
+                  borderBottom: "1px",
+                }}
+              >
+                {fieldConfig.fields.user_email.label}
+              </th>
+              <th
+                style={{
+                  width: "54%",
+                  borderTop: "0px",
+                  borderBottom: "1px",
+                }}
+              >
+                {fieldConfig.fields.text.label}
+              </th>
+              {/* only create extra columns if user has edit permissions */}
               {!isReadOnly(roles) && (
-                <tr>
-                  <td></td>
-                  <td></td>
-                  <td>
-                    <Input
-                      type="textarea"
-                      placeholder="Enter new note here..."
-                      value={newNote}
-                      onChange={e => setNewNote(e.target.value)}
-                    />
-                  </td>
-                  <td style={{ padding: "12px 4px 12px 12px" }}>
-                    <Button
-                      type="submit"
-                      color="primary"
-                      onClick={handleAddNoteClick}
-                      className="btn-pill mt-2"
-                      size="sm"
-                      style={{ width: "50px" }}
-                    >
-                      Add
-                    </Button>
-                  </td>
-                  <td></td>
-                </tr>
+                <th
+                  style={{
+                    width: "6%",
+                    borderTop: "0px",
+                    borderBottom: "1px",
+                  }}
+                ></th>
               )}
-              {/* iterate through each row in notes table */}
-              {data[tableName].map(row => {
-                const isEditing = editRow === row;
-                const isUser = row.user_email === userEmail;
-                return (
-                  <tr key={`table-${tableName}-${row[keyField]}`}>
-                    {/* iterate through each field in the row and render its value */}
-                    {Object.keys(fieldConfig.fields).map((field, i) => {
-                      return (
-                        <td key={i}>
-                          {/* if user is editing display editing input text box */}
-                          {isEditing && field === "text" ? (
-                            <Input
-                              type="textarea"
-                              defaultValue={row.text}
-                              onChange={e => setEditedNote(e.target.value)}
-                            />
-                          ) : field === "date" ? (
-                            format(parseISO(row[field]), "MM/dd/yyyy")
-                          ) : (
-                            row[field]
-                          )}
-                        </td>
-                      );
-                    })}
-                    {/* display edit button if row was created by current user,
+              {/* only create extra columns if user has edit permissions */}
+              {!isReadOnly(roles) && (
+                <th
+                  style={{
+                    width: "6%",
+                    borderTop: "0px",
+                    borderBottom: "1px",
+                  }}
+                ></th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {/* display user input row for users with edit permissions*/}
+            {!isReadOnly(roles) && (
+              <tr>
+                <td></td>
+                <td></td>
+                <td>
+                  <Input
+                    type="textarea"
+                    placeholder="Enter new note here..."
+                    value={newNote}
+                    onChange={e => setNewNote(e.target.value)}
+                  />
+                </td>
+                <td style={{ padding: "12px 4px 12px 12px" }}>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    onClick={handleAddNoteClick}
+                    className="btn-pill mt-2"
+                    size="sm"
+                    style={{ width: "50px" }}
+                  >
+                    Add
+                  </Button>
+                </td>
+                <td></td>
+              </tr>
+            )}
+            {/* iterate through each row in notes table */}
+            {data[tableName].map(row => {
+              const isEditing = editRow === row;
+              const isUser = row.user_email === userEmail;
+              return (
+                <tr key={`table-${tableName}-${row[keyField]}`}>
+                  {/* iterate through each field in the row and render its value */}
+                  {Object.keys(fieldConfig.fields).map((field, i) => {
+                    return (
+                      <td key={i}>
+                        {/* if user is editing display editing input text box */}
+                        {isEditing && field === "text" ? (
+                          <Input
+                            type="textarea"
+                            defaultValue={row.text}
+                            onChange={e => setEditedNote(e.target.value)}
+                          />
+                        ) : field === "date" ? (
+                          format(parseISO(row[field]), "MM/dd/yyyy")
+                        ) : (
+                          row[field]
+                        )}
+                      </td>
+                    );
+                  })}
+                  {/* display edit button if row was created by current user,
                   user has edit permissions, and user is not currently editing */}
-                    {isUser && !isReadOnly(roles) && !isEditing ? (
-                      <td style={{ padding: "12px 4px 12px 12px" }}>
-                        <Button
-                          type="submit"
-                          color="secondary"
-                          size="sm"
-                          className="btn-pill mt-2"
-                          style={{ width: "50px" }}
-                          onClick={e => handleEditClick(row)}
-                        >
-                          <i className="fa fa-pencil edit-toggle" />
-                        </Button>
-                      </td>
-                    ) : (
-                      // else if user has edit permissions and is not editing render empty cell
-                      !isReadOnly(roles) && !isEditing && <td></td>
-                    )}
-                    {/* display delete button if row was created by current user,
+                  {isUser && !isReadOnly(roles) && !isEditing ? (
+                    <td style={{ padding: "12px 4px 12px 12px" }}>
+                      <Button
+                        type="submit"
+                        color="secondary"
+                        size="sm"
+                        className="btn-pill mt-2"
+                        style={{ width: "50px" }}
+                        onClick={e => handleEditClick(row)}
+                      >
+                        <i className="fa fa-pencil edit-toggle" />
+                      </Button>
+                    </td>
+                  ) : (
+                    // else if user has edit permissions and is not editing render empty cell
+                    !isReadOnly(roles) && !isEditing && <td></td>
+                  )}
+                  {/* display delete button if row was created by current user,
                   user has edit permissions, and user is not currently editing */}
-                    {isUser && !isReadOnly(roles) && !isEditing ? (
-                      <td style={{ padding: "12px 4px 12px 4px" }}>
-                        <Button
-                          type="submit"
-                          color="secondary"
-                          className="btn-pill mt-2"
-                          size="sm"
-                          style={{ width: "50px" }}
-                          onClick={e => handleDeleteClick(row)}
-                        >
-                          <i className="fa fa-trash" />
-                        </Button>
-                      </td>
-                    ) : (
-                      // else if user has edit permissions and is not editing render empty cell
-                      !isReadOnly(roles) && !isEditing && <td></td>
-                    )}
-                    {/* display save button if user is editing */}
-                    {!isReadOnly(roles) && isEditing && (
-                      <td style={{ padding: "12px 4px 12px 12px" }}>
-                        <Button
-                          color="primary"
-                          className="btn-pill mt-2"
-                          size="sm"
-                          style={{ width: "50px" }}
-                          onClick={e => handleSaveClick(row)}
-                        >
-                          <i className="fa fa-check edit-toggle" />
-                        </Button>
-                      </td>
-                    )}
-                    {/* display cancel button if user is editing */}
-                    {!isReadOnly(roles) && isEditing && (
-                      <td style={{ padding: "12px 4px 12px 4px" }}>
-                        <Button
-                          type="submit"
-                          color="danger"
-                          className="btn-pill mt-2"
-                          size="sm"
-                          style={{ width: "50px" }}
-                          onClick={e => handleCancelClick(e)}
-                        >
-                          <i className="fa fa-times edit-toggle" />
-                        </Button>
-                      </td>
-                    )}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </CardBody>
-        <CardFooter></CardFooter>
-      </Card>
-      <ConfirmModal
-        modalHeader={"Delete Note"}
-        modalBody={"Are you sure you want to delete this note?"}
-        confirmClick={() => {
-          console.log("delete note");
-        }}
-        toggleModal={toggleModal}
-        showModal={showModal}
-      />
-    </>
+                  {isUser && !isReadOnly(roles) && !isEditing ? (
+                    <td style={{ padding: "12px 4px 12px 4px" }}>
+                      <Button
+                        type="submit"
+                        color="secondary"
+                        className="btn-pill mt-2"
+                        size="sm"
+                        style={{ width: "50px" }}
+                        onClick={() => toggleModal()}
+                      >
+                        <i className="fa fa-trash" />
+                      </Button>
+                      <ConfirmModal
+                        modalHeader={"Delete Note"}
+                        modalBody={"Are you sure you want to delete this note?"}
+                        confirmClick={() => {
+                          handleDeleteClick(row);
+                        }}
+                        toggleModal={toggleModal}
+                        showModal={showModal}
+                      />
+                    </td>
+                  ) : (
+                    // else if user has edit permissions and is not editing render empty cell
+                    !isReadOnly(roles) && !isEditing && <td></td>
+                  )}
+                  {/* display save button if user is editing */}
+                  {!isReadOnly(roles) && isEditing && (
+                    <td style={{ padding: "12px 4px 12px 12px" }}>
+                      <Button
+                        color="primary"
+                        className="btn-pill mt-2"
+                        size="sm"
+                        style={{ width: "50px" }}
+                        onClick={e => handleSaveClick(row)}
+                      >
+                        <i className="fa fa-check edit-toggle" />
+                      </Button>
+                    </td>
+                  )}
+                  {/* display cancel button if user is editing */}
+                  {!isReadOnly(roles) && isEditing && (
+                    <td style={{ padding: "12px 4px 12px 4px" }}>
+                      <Button
+                        type="submit"
+                        color="danger"
+                        className="btn-pill mt-2"
+                        size="sm"
+                        style={{ width: "50px" }}
+                        onClick={e => handleCancelClick(e)}
+                      >
+                        <i className="fa fa-times edit-toggle" />
+                      </Button>
+                    </td>
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </CardBody>
+      <CardFooter></CardFooter>
+    </Card>
   );
 };
 
