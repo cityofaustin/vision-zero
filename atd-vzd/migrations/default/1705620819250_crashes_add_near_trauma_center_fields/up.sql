2215c82d01bbe23e63c87acc9276181c1e851565
rose <rose.eichelmann@gmail.com>
2024-01-18 23:00:21 -0600

    add comment

--- atd-vzd/migrations/default/1705620819250_crashes_add_near_trauma_center_fields/up.sql
+++ atd-vzd/migrations/default/1705620819250_crashes_add_near_trauma_center_fields/up.sql
@@ -1,3 +1,4 @@
+-- Add new trauma center fields from CRIS to the crashes table
 ALTER TABLE public.atd_txdot_crashes
 ADD COLUMN near_trauma_center_id integer NULL,
 ADD COLUMN near_trauma_center_distance double precision NULL;
