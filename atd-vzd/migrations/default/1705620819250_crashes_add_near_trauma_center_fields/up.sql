cb62ec9166db1d0ca55047c9d002e98bb7909550
rose <rose.eichelmann@gmail.com>
2024-01-23 16:35:09 -0600

    delete this other migration folder

--- atd-vzd/migrations/default/1705620819250_crashes_add_near_trauma_center_fields/up.sql
+++
@@ -1,4 +0,0 @@
--- Add new trauma center fields from CRIS to the crashes table
-ALTER TABLE public.atd_txdot_crashes
-ADD COLUMN near_trauma_center_id integer NULL,
-ADD COLUMN near_trauma_center_distance double precision NULL;
