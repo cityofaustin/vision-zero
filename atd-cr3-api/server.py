fefd5f5166699ed9309f6fe8d16a7658c542b80f
Frank Hereford <frankhereford@users.noreply.github.com>
2024-01-30 13:44:30 -0600

    Revert "use the 'g' object instead of the flask internal"
    This reverts commit 000419412f3087099d39ace3c1fedded43cb4729.

--- atd-cr3-api/server.py
+++ atd-cr3-api/server.py
@@ -14,7 +14,8 @@ from os import environ as env
 from functools import wraps
 from six.moves.urllib.request import urlopen
 
-from flask import Flask, request, redirect, jsonify, abort, g
+# from flask import Flask, request, redirect, jsonify, _request_ctx_stack, abort
+from flask import Flask, request, redirect, jsonify, abort
 
 from flask_cors import cross_origin
 from werkzeug.local import LocalProxy
@@ -215,7 +216,7 @@ def requires_auth(f):
                     },
                     401,
                 )
-            g.current_user = payload
+            _request_ctx_stack.top.current_user = payload
             return f(*args, **kwargs)
         raise AuthError(
             {"code": "invalid_header", "description": "Unable to find appropriate key"},
@@ -225,7 +226,7 @@ def requires_auth(f):
     return decorated
 
 
-current_user = LocalProxy(lambda: getattr(g, "current_user", None))
+current_user = LocalProxy(lambda: getattr(_request_ctx_stack.top, "current_user", None))
 
 # Controllers API
 
