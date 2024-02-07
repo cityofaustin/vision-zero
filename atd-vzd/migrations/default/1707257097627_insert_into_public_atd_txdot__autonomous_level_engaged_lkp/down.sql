06ac57d8be1b45bc2e6524b71f9834bad41e8226
rose <rose.eichelmann@gmail.com>
2024-02-07 15:51:41 -0600

    insert missing lookup values to autonomous level engaged table

--- atd-vzd/migrations/default/1707257097627_insert_into_public_atd_txdot__autonomous_level_engaged_lkp/down.sql
+++ atd-vzd/migrations/default/1707257097627_insert_into_public_atd_txdot__autonomous_level_engaged_lkp/down.sql
@@ -3,3 +3,5 @@ DELETE FROM "public"."atd_txdot__autonomous_level_engaged_lkp" WHERE "autonomous
 DELETE FROM "public"."atd_txdot__autonomous_level_engaged_lkp" WHERE "autonomous_level_engaged_id" = 4;
 
 DELETE FROM "public"."atd_txdot__autonomous_level_engaged_lkp" WHERE "autonomous_level_engaged_id" = 5;
+
+UPDATE "public"."atd_txdot__autonomous_level_engaged_lkp" SET autonomous_level_engaged_desc = 'AUTOMATION LEVEL ENGAGED UNKNOWN' WHERE atd_txdot__autonomous_level_engaged_lkp = 6;
--- atd-vzd/migrations/default/1707257097627_insert_into_public_atd_txdot__autonomous_level_engaged_lkp/up.sql
+++ atd-vzd/migrations/default/1707257097627_insert_into_public_atd_txdot__autonomous_level_engaged_lkp/up.sql
@@ -5,3 +5,5 @@ INSERT INTO "public"."atd_txdot__autonomous_level_engaged_lkp"("autonomous_level
 INSERT INTO "public"."atd_txdot__autonomous_level_engaged_lkp"("autonomous_level_engaged_id", "autonomous_level_engaged_desc") VALUES (4, E'HIGH AUTOMATION');
 
 INSERT INTO "public"."atd_txdot__autonomous_level_engaged_lkp"("autonomous_level_engaged_id", "autonomous_level_engaged_desc") VALUES (5, E'FULL AUTOMATION');
+
+UPDATE "public"."atd_txdot__autonomous_level_engaged_lkp" SET autonomous_level_engaged_desc = 'AUTOMATION LEVEL UNKNOWN' WHERE atd_txdot__autonomous_level_engaged_lkp = 6;
