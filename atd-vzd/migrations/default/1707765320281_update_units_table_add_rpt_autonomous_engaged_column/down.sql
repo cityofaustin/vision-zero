d56f0124d04ff6d6380597ffc8bb782db9935cc8
rose <rose.eichelmann@gmail.com>
2024-02-12 13:22:45 -0600

    redo this migration

---
+++ atd-vzd/migrations/default/1707765320281_update_units_table_add_rpt_autonomous_engaged_column/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."atd_txdot_units"
+DROP COLUMN "rpt_autonomous_level_engaged_id";
---
+++ atd-vzd/migrations/default/1707765320281_update_units_table_add_rpt_autonomous_engaged_column/up.sql
@@ -0,0 +1,2 @@
+alter table "public"."atd_txdot_units" add column "rpt_autonomous_level_engaged_id" integer
+ null;
