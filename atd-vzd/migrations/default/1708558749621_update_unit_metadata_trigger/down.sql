ba42948356eacdf20b5aa3e700eb597caf603e3c
Mike <mike.dilley@gmail.com>
2024-02-21 18:08:34 -0600

    Add comments

--- atd-vzd/migrations/default/1708558749621_update_unit_metadata_trigger/down.sql
+++ atd-vzd/migrations/default/1708558749621_update_unit_metadata_trigger/down.sql
@@ -5,3 +5,5 @@ CREATE TRIGGER atd_txdot_units_mode_category_metadata_update
 	FOR EACH ROW
 	WHEN (old.atd_mode_category IS DISTINCT FROM new.atd_mode_category)
 	EXECUTE FUNCTION public.atd_txdot_units_mode_category_metadata_update ();
+
+COMMENT ON TRIGGER atd_txdot_units_mode_category_metadata_update ON public.atd_txdot_units IS 'Updates the associated crash table atd_mode_category_metadata column when a unit atd_mode_category is updated.';
