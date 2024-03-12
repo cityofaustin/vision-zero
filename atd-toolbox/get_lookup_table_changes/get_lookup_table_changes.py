71481978a4f0d48cbe80aa42aa831709fc06148a
rose <rose.eichelmann@gmail.com>
2024-03-12 11:54:58 -0500

    add this back in

--- atd-toolbox/get_lookup_table_changes/get_lookup_table_changes.py
+++ atd-toolbox/get_lookup_table_changes/get_lookup_table_changes.py
@@ -16,6 +16,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_NAME = os.getenv("DB_NAME")
+DB_SSL_REQUIREMENT = os.getenv("DB_SSL_REQUIREMENT")
 
 
 def get_pg_connection():
@@ -27,6 +28,7 @@ def get_pg_connection():
         user=DB_USER,
         password=DB_PASS,
         dbname=DB_NAME,
+        sslmode=DB_SSL_REQUIREMENT,
         sslrootcert="/root/rds-combined-ca-bundle.pem",
     )
 
