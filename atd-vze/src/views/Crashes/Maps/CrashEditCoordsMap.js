369421c191dd8f7f9c49367cc79577a21e156ac8
Mike <mike.dilley@gmail.com>
2024-03-13 17:13:17 -0500

    Use rest of CrashEditCoordsMap props; update handle form submit

--- atd-vze/src/views/Crashes/Maps/CrashEditCoordsMap.js
+++ atd-vze/src/views/Crashes/Maps/CrashEditCoordsMap.js
@@ -153,7 +153,13 @@ import {
 
 // export default withApollo(CrashEditCoordsMap);
 
-const CrashEditCoordsMap = ({ data }) => {
+const CrashEditCoordsMap = ({
+  data,
+  mapGeocoderAddress,
+  crashId,
+  refetchCrashData,
+  setIsEditingCoords,
+}) => {
   const { latitude_primary = null, longitude_primary = null } = data;
 
   const mapRef = React.useRef();
@@ -163,6 +169,8 @@ const CrashEditCoordsMap = ({ data }) => {
     longitude: longitude_primary,
   });
 
+  const [updateCrashCoordinates] = useMutation(UPDATE_COORDS);
+
   const onDrag = e => {
     const latitude = e.viewState.latitude;
     const longitude = e.viewState.longitude;
@@ -170,30 +178,60 @@ const CrashEditCoordsMap = ({ data }) => {
     setMarkerCoordinates({ latitude, longitude });
   };
 
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    const variables = {
+      qaStatus: 3, // Lat/Long entered manually to Primary
+      geocodeProvider: 5, // Manual Q/A
+      crashId: crashId,
+      // TODO: Get email from context instead
+      updatedBy: localStorage.getItem("hasura_user_email"),
+      ...markerCoordinates,
+    };
+
+    updateCrashCoordinates({
+      variables: variables,
+    }).then(() => {
+      refetchCrashData();
+      setIsEditingCoords(false);
+    });
+  };
+
+  // TODO: handle initial geocoder value?
+
   return (
-    <MapGL
-      ref={mapRef}
-      initialViewState={{
-        latitude: latitude_primary || defaultInitialState.latitude,
-        longitude: longitude_primary || defaultInitialState.longitude,
-        zoom: defaultInitialState.zoom,
-      }}
-      style={{ width: "100%", height: "100%" }}
-      {...mapParameters}
-      cooperativeGestures={true}
-      draggable
-      onDragStart={() => setIsDragging(true)}
-      onDrag={onDrag}
-      onDragEnd={() => setIsDragging(false)}
-    >
-      <FullscreenControl position="top-left" />
-      <NavigationControl position="top-left" showCompass={false} />
-      <Marker {...markerCoordinates}>
-        <Pin size={40} color={"warning"} isDragging={isDragging} animated />
-      </Marker>
-      {/* add nearmap raster source and style */}
-      {!isDev && <LabeledAerialSourceAndLayer />}
-    </MapGL>
+    <div>
+      <MapGL
+        ref={mapRef}
+        initialViewState={{
+          latitude: latitude_primary || defaultInitialState.latitude,
+          longitude: longitude_primary || defaultInitialState.longitude,
+          zoom: defaultInitialState.zoom,
+        }}
+        style={{ width: "100%", height: "100%" }}
+        {...mapParameters}
+        cooperativeGestures={true}
+        draggable
+        onDragStart={() => setIsDragging(true)}
+        onDrag={onDrag}
+        onDragEnd={() => setIsDragging(false)}
+      >
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" showCompass={false} />
+        <Marker {...markerCoordinates}>
+          <Pin size={40} color={"warning"} isDragging={isDragging} animated />
+        </Marker>
+        {/* add nearmap raster source and style */}
+        {!isDev && <LabeledAerialSourceAndLayer />}
+      </MapGL>
+      <CrashEditLatLonForm
+        {...markerCoordinates}
+        handleFormSubmit={handleFormSubmit}
+        handleFormReset={handleMapFormReset}
+        handleFormCancel={handleMapFormCancel}
+      />
+    </div>
   );
 };
 
