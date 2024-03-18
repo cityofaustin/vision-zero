dd9c82673185efa0ad63b049c0d1b35a26931a9b
Mike <mike.dilley@gmail.com>
2024-03-18 15:54:56 -0500

    Prettier and fix component key warnings

--- atd-vze/src/views/Crashes/CrashChangeLog.js
+++ atd-vze/src/views/Crashes/CrashChangeLog.js
@@ -118,37 +118,43 @@ class CrashChangeLog extends Component {
     // Define a function to pass to JSON.stringify which will skip over
     // the __typename key in the JS object being stringified.
     const stringifyReplacer = (key, value) => {
-      if (key === '__typename') {
+      if (key === "__typename") {
         return undefined;
       }
       return value;
-    }
+    };
 
     // For each entry created in the diff array, generate an HTML table row.
-    let modalItems = diff.map(item => {
+    let modalItems = diff.map((item, i) => {
       // The following two conditions check if an the current or archived value to
       // to be shown in the crach's changelog are objects, such as found when a jsonb
-      // field is pulled from the database. In this case, they are stringified for 
+      // field is pulled from the database. In this case, they are stringified for
       // human-readble output.
-      if (typeof item.original_record_value === 'object') {
-        item.original_record_value = JSON.stringify(item.original_record_value, stringifyReplacer);
+      if (typeof item.original_record_value === "object") {
+        item.original_record_value = JSON.stringify(
+          item.original_record_value,
+          stringifyReplacer
+        );
       }
 
-      if (typeof item.archived_record_value === 'object') {
-        item.archived_record_value = JSON.stringify(item.archived_record_value, stringifyReplacer);
+      if (typeof item.archived_record_value === "object") {
+        item.archived_record_value = JSON.stringify(
+          item.archived_record_value,
+          stringifyReplacer
+        );
       }
 
       return (
-      <tr key={`recordHistory-${record.id}`}>
-        <td>{item.original_record_key}</td>
-        <td>
-          <Badge color="primary">{String(item.original_record_value)}</Badge>
-        </td>
-        <td>
-          <Badge color="danger">{String(item.archived_record_value)}</Badge>
-        </td>
-      </tr>
-      )
+        <tr key={`recordHistory-${i}`}>
+          <td>{item.original_record_key}</td>
+          <td>
+            <Badge color="primary">{String(item.original_record_value)}</Badge>
+          </td>
+          <td>
+            <Badge color="danger">{String(item.archived_record_value)}</Badge>
+          </td>
+        </tr>
+      );
     });
 
     // Generate the body of the modal box
@@ -211,7 +217,7 @@ class CrashChangeLog extends Component {
             </thead>
             <tbody>
               {this.props.data.atd_txdot_change_log.map(record => (
-                <tr key={`changelog-${record.id}`}>
+                <tr key={`changelog-${record.change_log_id}`}>
                   <td>
                     <Badge color="warning">
                       {this.timeConverter(record.update_timestamp)}
