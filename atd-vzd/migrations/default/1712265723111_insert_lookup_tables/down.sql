8eae55bdbdb722e3707fcdce13b670465d6e2ca3
rose <rose.eichelmann@gmail.com>
2024-04-04 16:31:15 -0500

    write up/down sql migras

---
+++ atd-vzd/migrations/default/1712265723111_insert_lookup_tables/down.sql
@@ -0,0 +1,104 @@
+-- Dropping table agency_lkp
+drop table if exists db.agency_lkp;
+
+-- Dropping table autonomous_level_engaged_lkp
+drop table if exists db.autonomous_level_engaged_lkp;
+
+-- Dropping table autonomous_unit_lkp
+drop table if exists db.autonomous_unit_lkp;
+
+-- Dropping table city_lkp
+drop table if exists db.city_lkp;
+
+-- Dropping table cnty_lkp
+drop table if exists db.cnty_lkp;
+
+-- Dropping table collsn_lkp
+drop table if exists db.collsn_lkp;
+
+-- Dropping table contrib_factr_lkp
+drop table if exists db.contrib_factr_lkp;
+
+-- Dropping table e_scooter_lkp
+drop table if exists db.e_scooter_lkp;
+
+-- Dropping table gndr_lkp
+drop table if exists db.gndr_lkp;
+
+-- Dropping table harm_evnt_lkp
+drop table if exists db.harm_evnt_lkp;
+
+-- Dropping table helmet_lkp
+drop table if exists db.helmet_lkp;
+
+-- Dropping table injry_sev_lkp
+drop table if exists db.injry_sev_lkp;
+
+-- Dropping table intrsct_relat_lkp
+drop table if exists db.intrsct_relat_lkp;
+
+-- Dropping table light_cond_lkp
+drop table if exists db.light_cond_lkp;
+
+-- Dropping table obj_struck_lkp
+drop table if exists db.obj_struck_lkp;
+
+-- Dropping table occpnt_pos_lkp
+drop table if exists db.occpnt_pos_lkp;
+
+-- Dropping table pbcat_pedalcyclist_lkp
+drop table if exists db.pbcat_pedalcyclist_lkp;
+
+-- Dropping table pbcat_pedestrian_lkp
+drop table if exists db.pbcat_pedestrian_lkp;
+
+-- Dropping table pedalcyclist_action_lkp
+drop table if exists db.pedalcyclist_action_lkp;
+
+-- Dropping table pedestrian_action_lkp
+drop table if exists db.pedestrian_action_lkp;
+
+-- Dropping table prsn_type_lkp
+drop table if exists db.prsn_type_lkp;
+
+-- Dropping table rest_lkp
+drop table if exists db.rest_lkp;
+
+-- Dropping table road_part_lkp
+drop table if exists db.road_part_lkp;
+
+-- Dropping table rwy_sys_lkp
+drop table if exists db.rwy_sys_lkp;
+
+-- Dropping table specimen_type_lkp
+drop table if exists db.specimen_type_lkp;
+
+-- Dropping table surf_cond_lkp
+drop table if exists db.surf_cond_lkp;
+
+-- Dropping table surf_type_lkp
+drop table if exists db.surf_type_lkp;
+
+-- Dropping table traffic_cntl_lkp
+drop table if exists db.traffic_cntl_lkp;
+
+-- Dropping table unit_desc_lkp
+drop table if exists db.unit_desc_lkp;
+
+-- Dropping table veh_body_styl_lkp
+drop table if exists db.veh_body_styl_lkp;
+
+-- Dropping table veh_damage_description_lkp
+drop table if exists db.veh_damage_description_lkp;
+
+-- Dropping table veh_damage_severity_lkp
+drop table if exists db.veh_damage_severity_lkp;
+
+-- Dropping table veh_make_lkp
+drop table if exists db.veh_make_lkp;
+
+-- Dropping table veh_mod_lkp
+drop table if exists db.veh_mod_lkp;
+
+-- Dropping table wthr_cond_lkp
+drop table if exists db.wthr_cond_lkp;
