89dddd19e7dbc0892c14114ed860c7e407ab7863
John Clary <john.clary@austintexas.gov>
2024-05-03 15:50:32 -0400

    cascade delete charges on crash delete

--- atd-vzd/migrations/default/1708558755621_charges/up.sql
+++ atd-vzd/migrations/default/1708558755621_charges/up.sql
@@ -3,7 +3,7 @@ create table db.charges_cris (
     charge text,
     charge_cat_id integer references lookups.charge_cat_lkp (id) on update cascade on delete cascade,
     citation_nbr text,
-    crash_id integer not null references db.crashes_cris (crash_id) ,
+    crash_id integer not null references db.crashes_cris (crash_id) on update cascade on delete cascade,
     person_id integer not null references db.people_cris (id) ,
     prsn_nbr integer not null,
     unit_nbr numeric not null
