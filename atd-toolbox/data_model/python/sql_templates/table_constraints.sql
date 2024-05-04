89e8459439cd90d76fe5aadb5a60f2e2c747634a
John Clary <john.clary@austintexas.gov>
2024-05-04 14:37:56 -0400

    remove comments from table_constraints.sql

--- atd-toolbox/data_model/python/sql_templates/table_constraints.sql
+++ atd-toolbox/data_model/python/sql_templates/table_constraints.sql
@@ -3,7 +3,3 @@ alter table db.units_unified add constraint unique_units unique (crash_id, unit_
 alter table db.people_cris add constraint unique_people_cris unique (unit_id, prsn_nbr);
 alter table db.people_edits add constraint unique_people_edits unique (unit_id, prsn_nbr);
 alter table db.people_unified add constraint unique_people_unified unique (unit_id, prsn_nbr);
--- create index on db.people_cris (unit_id);
--- create index on db.people_cris (crash_id);
--- create index on db.people_vz (unit_id);
--- create index on db.people (unit_id);
