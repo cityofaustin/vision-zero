150b9d9c547a57fd2af0928fd54fa5f2782f32d8
rose <rose.eichelmann@gmail.com>
2024-05-06 17:49:21 -0500

    delete some crufty functions

---
+++ atd-vzd/migrations/default/1715033725867_drop_crufty_function/up.sql
@@ -0,0 +1,11 @@
+DROP FUNCTION find_service_road_location_for_centerline_crash;
+
+DROP FUNCTION find_noncr3_collisions_for_location;
+
+DROP FUNCTION find_cr3_collisions_for_location;
+
+DROP FUNCTION search_atd_location_crashes;
+
+DROP FUNCTION search_atd_crash_locations;
+
+DROP FUNCTION search_atd_crash_location;
