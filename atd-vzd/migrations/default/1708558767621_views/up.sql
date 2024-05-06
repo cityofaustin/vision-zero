c0d104027b642c50767ec074ec8742010b74855a
John Clary <john.clary@austintexas.gov>
2024-05-06 13:57:17 -0400

    add more crashes_list columns, rename others, and add hasura permissions for all users

--- atd-vzd/migrations/default/1708558767621_views/up.sql
+++ atd-vzd/migrations/default/1708558767621_views/up.sql
@@ -4,29 +4,55 @@ create or replace view crashes_list as with injury_severities as (
         unit_id,
         est_comp_cost_crash_based,
         case
-            when (prsn_injry_sev_id = 2) then 1
+            when (prsn_injry_sev_id = 9) then 1
             else 0
-        end as non_capacitating_injury,
+        end as unkn_injry,
         case
             when (prsn_injry_sev_id = 1) then 1
             else 0
-        end as serious_injury,
+        end as sus_serious_injry,
+        case
+            when (prsn_injry_sev_id = 2) then 1
+            else 0
+        end as nonincap_injry,
+        case
+            when (prsn_injry_sev_id = 3) then 1
+            else 0
+        end as poss_injry,
         case
             when (prsn_injry_sev_id = 4) then 1
             else 0
-        end as fatal_injury
+        end as fatal_injury,
+        case
+            when (prsn_injry_sev_id = 5) then 1
+            else 0
+        end as non_injry
     from
         db.people_unified
+),
+
+cris_fatal_injury_counts as (
+    select
+        crashes.crash_id,
+        count(crashes.crash_id) as cris_fatality_cnt
+    from db.crashes_cris as crashes
+    left join db.units_cris as units on crashes.crash_id = units.crash_id
+    left join db.people_cris as people on units.id = people.unit_id
+    where people.prsn_injry_sev_id = 4
+    group by crashes.crash_id
 ),
 
 injury_counts as (
     select
         crashes.crash_id,
+        sum(injury_severities.unkn_injry) as unkn_injry_cnt,
         sum(
-            injury_severities.non_capacitating_injury
-        ) as non_capacitating_injury_count,
-        sum(injury_severities.serious_injury) as serious_injury_count,
-        sum(injury_severities.fatal_injury) as atd_fatality_count,
+            injury_severities.nonincap_injry
+        ) as nonincap_injry_cnt,
+        sum(injury_severities.poss_injry) as poss_injry_cnt,
+        sum(injury_severities.non_injry) as non_injry_cnt,
+        sum(injury_severities.sus_serious_injry) as sus_serious_injry_cnt,
+        sum(injury_severities.fatal_injury) as atd_fatality_cnt,
         sum(est_comp_cost_crash_based) as est_comp_cost_crash_based
     from
         db.crashes_unified as crashes
@@ -62,18 +88,52 @@ select
     db.crashes_unified.private_dr_fl,
     db.crashes_unified.in_austin_full_purpose,
     db.crashes_unified.location_id,
-    injury_counts.non_capacitating_injury_count,
-    injury_counts.serious_injury_count,
-    injury_counts.atd_fatality_count,
+    db.crashes_unified.rpt_block_num,
+    db.crashes_unified.rpt_street_pfx,
+    db.crashes_unified.rpt_street_name,
+    db.crashes_unified.rpt_sec_block_num,
+    db.crashes_unified.rpt_sec_street_pfx,
+    db.crashes_unified.rpt_sec_street_name,
+    db.crashes_unified.latitude,
+    db.crashes_unified.longitude,
+    db.crashes_unified.light_cond_id,
+    db.crashes_unified.wthr_cond_id,
+    db.crashes_unified.active_school_zone_fl,
+    db.crashes_unified.schl_bus_fl,
+    db.crashes_unified.at_intrsct_fl,
+    db.crashes_unified.onsys_fl,
+    db.crashes_unified.traffic_cntl_id,
+    db.crashes_unified.road_constr_zone_fl,
+    db.crashes_unified.rr_relat_fl,
+    db.crashes_unified.toll_road_fl,
+    db.crashes_unified.intrsct_relat_id,
+    db.crashes_unified.obj_struck_id,
+    db.crashes_unified.crash_speed_limit,
+    db.crashes_unified.council_district,
+    injury_counts.nonincap_injry_cnt,
+    injury_counts.poss_injry_cnt,
+    injury_counts.sus_serious_injry_cnt,
+    injury_counts.non_injry_cnt,
+    injury_counts.atd_fatality_cnt,
+    case
+        when (cris_fatal_injury_counts.cris_fatality_cnt is null) then 0
+    else cris_fatal_injury_counts.cris_fatality_cnt
+    end as cris_fatality_cnt,
     injury_counts.est_comp_cost_crash_based,
     lookups.collsn_lkp.label as collsn_desc,
     geocode_sources.geocode_source,
-    geocode_sources.has_no_cris_coordinates
+    geocode_sources.has_no_cris_coordinates,
+    upper(
+        to_char(db.crashes_unified.crash_date at time zone 'US/Central', 'dy')
+    ) as crash_day_of_week
 from
     db.crashes_unified
 left join
     injury_counts
     on db.crashes_unified.crash_id = injury_counts.crash_id
+left join
+    cris_fatal_injury_counts
+    on db.crashes_unified.crash_id = cris_fatal_injury_counts.crash_id
 left join
     geocode_sources
     on db.crashes_unified.crash_id = geocode_sources.crash_id
