c524a8b72ae92e23e22de581806c0073027ee9fc
John Clary <john.clary@austintexas.gov>
2024-05-09 12:37:03 -0400

    write down migra for crashes_list view

--- atd-toolbox/data_model/python/60_make_views.py
+++ atd-toolbox/data_model/python/60_make_views.py
@@ -1,4 +1,5 @@
 from utils import save_file, make_migration_dir, save_empty_down_migration
+from settings import SCHEMA_NAME
 
 
 def load_sql_template(name):
@@ -11,7 +12,7 @@ def main():
     crashes_list_view_sql = load_sql_template("sql_templates/crashes_list_view.sql")
     migration_path = make_migration_dir("views")
     save_file(f"{migration_path}/up.sql", crashes_list_view_sql)
-    save_empty_down_migration(migration_path)
+    save_file(f"{migration_path}/down.sql", "drop view public.crashes_list;\n\n")
 
 
 main()
