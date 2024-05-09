e2ca36ef9fd1b427e422502bfb29c3c720d81bc9
Frank Hereford <frank@frankhereford.com>
2024-05-09 11:35:35 -0500

    add some column comments to the migration

--- atd-vzd/migrations/default/1715094801442_add_import_log_table/up.sql
+++ atd-vzd/migrations/default/1715094801442_add_import_log_table/up.sql
@@ -7,3 +7,10 @@ CREATE TABLE cris_import_log (
     records_processed JSONB DEFAULT '{}',
     outcome_status TEXT NULL
 );
+
+COMMENT ON COLUMN cris_import_log.outcome_status IS 'This is a brief, textual indication of what the outcome was.';
+COMMENT ON COLUMN cris_import_log.records_processed IS 'A JSON blob that contains counts or a list of crashes imported per schema';
+COMMENT ON COLUMN cris_import_log.object_path IS 'The location within the bucket where the extract was found';
+COMMENT ON COLUMN cris_import_log.object_name IS 'The name of the object (file) within the bucket';
+COMMENT ON COLUMN cris_import_log.created_at IS 'Audit field for when the import started';
+COMMENT ON COLUMN cris_import_log.completed_at IS 'Audit field for when the import finished';
