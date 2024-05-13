42cd61f8f1d088697ad41deae93b8be4b4a20f13
John Clary <john.clary@austintexas.gov>
2024-05-13 16:10:11 -0400

    create unified change log view

--- atd-toolbox/data_model/python/60_make_views.py
+++ atd-toolbox/data_model/python/60_make_views.py
@@ -8,11 +8,16 @@ def load_sql_template(name):
 
 
 def main():
-    # crashes list
+    # crashes list and injury metrics
     crashes_list_view_sql = load_sql_template("sql_templates/crashes_list_view.sql")
-    migration_path = make_migration_dir("views")
+    migration_path = make_migration_dir("crashes_list_and_fatality_metrics_views")
     save_file(f"{migration_path}/up.sql", crashes_list_view_sql)
     save_file(f"{migration_path}/down.sql", "drop view public.crash_injury_metrics cascade;")
 
+    # crashes unified change log
+    crashes_change_log_sql = load_sql_template("sql_templates/crashes_change_log_view.sql")
+    migration_path = make_migration_dir("change_log_view")
+    save_file(f"{migration_path}/up.sql", crashes_change_log_sql)
+    save_file(f"{migration_path}/down.sql", "drop view public.crashes_change_log_view cascade;")
 
 main()
