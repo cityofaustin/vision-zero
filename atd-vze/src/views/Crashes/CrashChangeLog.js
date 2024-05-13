a30f8d4f4376f69a47ebc87e9347d9943d3635a4
John Clary <john.clary@austintexas.gov>
2024-05-13 16:00:40 -0400

    wip...

--- atd-vze/src/views/Crashes/CrashChangeLog.js
+++ atd-vze/src/views/Crashes/CrashChangeLog.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { useMemo } from "react";
 
 import {
   Badge,
@@ -16,12 +16,33 @@ import {
 } from "reactstrap";
 import { formatDateTimeString } from "../../helpers/format";
 
+const useChangeLogData = data =>
+  useMemo(() => {
+    if (!data || !data?.crashes_by_pk) {
+      return [];
+    }
+    const changes = [];
+    const crash = data?.crashes_by_pk;
+    const crashChanges = crash.change_log.map(change => {
+      change.record_type = "crash";
+      return change;
+    });
+    return data;
+  }, [data]);
+
 export default function CrashChangeLog({ data }) {
   console.log("HI", data);
   return (
-    <Card >
+    <Card>
       <CardHeader>Changes</CardHeader>
-      <CardBody></CardBody>
+      <CardBody>
+        <Table responsive striped hover>
+          <thead>
+            <th>id</th>
+          </thead>
+          <tbody></tbody>
+        </Table>
+      </CardBody>
     </Card>
   );
 }
