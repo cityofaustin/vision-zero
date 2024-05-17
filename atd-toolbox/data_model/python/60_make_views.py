eaacaec31bea116b7fb52682b47fc330609e020c
John Clary <john.clary@austintexas.gov>
2024-05-17 11:11:34 -0400

    fix view down migra

--- atd-toolbox/data_model/python/60_make_views.py
+++ atd-toolbox/data_model/python/60_make_views.py
@@ -12,7 +12,10 @@ def main():
     crashes_list_view_sql = load_sql_template("sql_templates/crashes_list_view.sql")
     migration_path = make_migration_dir("views")
     save_file(f"{migration_path}/up.sql", crashes_list_view_sql)
-    save_file(f"{migration_path}/down.sql", "drop view public.crash_injury_metrics cascade;")
+    save_file(
+        f"{migration_path}/down.sql",
+        "drop view public.person_injury_metrics_view cascade;",
+    )
 
 
 main()
