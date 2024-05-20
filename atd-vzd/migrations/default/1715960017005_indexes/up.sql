9fd360150b138e3172545932c568804a9f23fda1
John Clary <john.clary@austintexas.gov>
2024-05-20 12:44:33 -0400

    add council district migra

--- atd-vzd/migrations/default/1715960017005_indexes/up.sql
+++ atd-vzd/migrations/default/1715960017005_indexes/up.sql
@@ -8,6 +8,7 @@ create index on public.people (unit_id);
 create index on public.people (prsn_injry_sev_id);
 create index on public.crashes (location_id);
 create index on public.crashes (crash_date);
+create index on public.atd_txdot_locations (council_district);
 -- more todo
 -- address fields
 
