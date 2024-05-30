c5e5acf29a3d124f973311b411a221964d583dd6
Frank Hereford <frank@frankhereford.com>
2024-05-30 09:38:29 -0500

    expand constraint condition

--- atd-vzd/migrations/default/1717076272414_fix_data_issues/down.sql
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/down.sql
@@ -3,3 +3,12 @@ where surf_type_id = 99;
 
 delete from lookups.unit_desc_lkp
 where id = 94 and label = 'REPORTED INVALID' and source = 'vz';
+
+alter table lookups.unit_desc_lkp
+drop constraint unit_desc_owner_check,
+add constraint unit_desc_owner_check check (
+    (
+        (id < 177 and source = 'cris')
+        or (id >= 177 and source = 'vz')
+    )
+);
--- atd-vzd/migrations/default/1717076272414_fix_data_issues/up.sql
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/up.sql
@@ -1,6 +1,16 @@
 update public.atd_txdot_crashes set surf_type_id = 99
 where surf_type_id = 'null';
 
+alter table lookups.unit_desc_lkp
+drop constraint unit_desc_owner_check,
+add constraint unit_desc_owner_check check (
+    (
+        (id < 177 and source = 'cris')
+        or (id >= 177 and source = 'vz')
+        or (id = 94 and source = 'vz')
+    )
+);
+
 insert into lookups.unit_desc_lkp (id, label, source) values (
     94, 'REPORTED INVALID', 'vz'
 );
