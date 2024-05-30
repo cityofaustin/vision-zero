cde6c52fe800e9bb8b7c555079ce867225120ab7
Frank Hereford <frank@frankhereford.com>
2024-05-30 08:48:02 -0500

    add data issue migration

---
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/down.sql
@@ -0,0 +1,2 @@
+update public.atd_txdot_crashes set surf_type_id = 'null'
+where surf_type_id = 99;
---
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/up.sql
@@ -0,0 +1,2 @@
+update public.atd_txdot_crashes set surf_type_id = 99
+where surf_type_id = 'null';
