82d2b904bc3c1bbc37292717014e97c6336e3281
Frank Hereford <frank@frankhereford.com>
2024-05-30 09:11:10 -0500

    add missing lookup value

--- atd-vzd/migrations/default/1717076272414_fix_data_issues/down.sql
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/down.sql
@@ -1,2 +1,5 @@
 update public.atd_txdot_crashes set surf_type_id = 'null'
 where surf_type_id = 99;
+
+delete from lookup.unit_desc_lkp
+where id = 94 and label = 'REPORTED INVALID' and source = 'vz';
--- atd-vzd/migrations/default/1717076272414_fix_data_issues/up.sql
+++ atd-vzd/migrations/default/1717076272414_fix_data_issues/up.sql
@@ -1,2 +1,6 @@
 update public.atd_txdot_crashes set surf_type_id = 99
 where surf_type_id = 'null';
+
+insert into lookup.unit_desc_lkp (id, label, source) values (
+    94, 'REPORTED INVALID', 'vz'
+);
