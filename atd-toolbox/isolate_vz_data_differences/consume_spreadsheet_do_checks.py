23cfd4c8ec127ffe8dc8687ca7f3936b974954c7
Frank Hereford <frank@frankhereford.com>
2024-06-05 12:14:12 -0500

    and there's units in the same style

--- atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
+++ atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
@@ -8,6 +8,7 @@ import json
 from datetime import datetime
 import decimal
 import logging
+import random
 
 
 logging.basicConfig(
@@ -25,6 +26,83 @@ def main():
     job = read_json_file("spreadsheet_of_columns.json")
 
     crashes(db_connection_string, job)
+    units(db_connection_string, job)
+
+
+def units(db_connection_string, job):
+    with psycopg2.connect(db_connection_string) as conn:
+        # build up a mondo dictionary of the whole table keyed on a tuple of the primary key(s)
+        sql = "select * from units_cris"
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(sql)
+            rows = cur.fetchall()
+            units_cris = {
+                (row["cris_crash_id"], row["unit_nbr"]): dict(row)
+                for row in tqdm(rows, desc="Building dictionary of units_cris")
+            }
+
+            # random_key = random.choice(list(units_cris.keys()))
+            # print("Key:", random_key)
+            # print("Value:", units_cris[random_key])
+
+        # another big dictionary of the whole table
+        sql = "select * from atd_txdot_units"
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(sql)
+            rows = cur.fetchall()
+            units_classic_vz = {
+                (row["crash_id"], row["unit_nbr"]): dict(row)
+                for row in tqdm(rows, desc="Building dictionary of atd_txdot_units")
+            }
+
+        # cast the char Y/Ns to booleans
+        for _, unit_data in tqdm(
+            units_classic_vz.items(), desc="Casting Y/N into booleans"
+        ):
+            for key, value in list(
+                unit_data.items()
+            ):  # We use list to create a copy of items for iteration
+                if key.endswith("_fl"):
+                    if value == "Y":
+                        unit_data[key] = True
+                    elif value == "N":
+                        unit_data[key] = False
+
+        columns = filter_by_old_table_name(job, "atd_txdot_units")
+
+        # fmt: off
+        updates = []
+        for unit_key in tqdm(units_cris, desc="Comparing values"):
+            for column in columns:
+                if column["target column name"] != "-":
+                    if column["target column name"] in units_cris[unit_key]:
+                        if (units_classic_vz[unit_key][column["old column name"]] != units_cris[unit_key][column["target column name"]]):
+                            logging.debug(f"❌ {column['old column name']}: {units_classic_vz[unit_key][column['old column name']]} != {units_cris[unit_key][column['target column name']]}")
+                            sql = f"update units_edits set {column['target column name']} = %s where id = (select id from units_cris where cris_crash_id = %s and unit_nbr = %s)"
+                            parameters = (units_classic_vz[unit_key][column["old column name"]], unit_key[0], unit_key[1])
+                            updates.append((sql, parameters))
+                        else:
+                            logging.debug(f"✅ {column['old column name']}: {units_classic_vz[unit_key][column['old column name']]} == {units_cris[unit_key][column['target column name']]}")
+                    else:
+                        if units_classic_vz[unit_key][column['old column name']]:
+                            logging.debug(f"✨ VZ only column: No {column['target column name']} in units_cris, so {units_classic_vz[unit_key][column['old column name']]} going into units_edits")
+                            sql = f"update units_edits set {column['target column name']} = %s where id = (select id from units_cris where cris_crash_id = %s and unit_nbr = %s)"
+                            parameters = (units_classic_vz[unit_key][column["old column name"]], unit_key[0], unit_key[1])
+                            updates.append((sql, parameters))
+                        else:
+                            logging.debug(f"🤷 VZ only column, but no {column['old column name']} in classic VZ data, so no value in units_edits's {column['target column name']}")
+        # fmt: on
+
+        # Mogrify and print the queries
+        for update in tqdm(updates, desc="Mogrifying Queries"):
+            with conn.cursor() as cur:
+                mogrified_query = cur.mogrify(update[0], update[1]).decode()
+                logging.debug(mogrified_query)
+
+        # Execute the queries
+        with conn.cursor() as cur:
+            for query, params in tqdm(updates, desc="Executing Queries"):
+                cur.execute(query, params)
 
 
 def crashes(db_connection_string, job):
@@ -52,7 +130,7 @@ def crashes(db_connection_string, job):
             }
 
         # cast the char Y/Ns to booleans
-        for crash_id, crash_data in tqdm(
+        for _, crash_data in tqdm(
             crashes_classic_vz.items(), desc="Casting Y/N into booleans"
         ):
             for key, value in list(
@@ -106,18 +184,15 @@ def crashes(db_connection_string, job):
                             logging.debug(f"🤷 VZ only column, but no {column['old column name']} in classic VZ data, so no value in crashes_edits's {column['target column name']}")
         # fmt: on
 
-        mogrified_queries = []
-
         # Mogrify and print the queries
         for update in tqdm(updates, desc="Mogrifying Queries"):
             with conn.cursor() as cur:
                 mogrified_query = cur.mogrify(update[0], update[1]).decode()
                 logging.debug(mogrified_query)
-                mogrified_queries.append((update[0], update[1]))
 
         # Execute the queries
         with conn.cursor() as cur:
-            for query, params in tqdm(mogrified_queries, desc="Executing Queries"):
+            for query, params in tqdm(updates, desc="Executing Queries"):
                 cur.execute(query, params)
 
 
