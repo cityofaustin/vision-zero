f99a1ea4b42332ac764e91f484e33a1b4a7820b7
Frank Hereford <frank@frankhereford.com>
2024-06-05 13:11:16 -0500

    update spreadsheet to reflect id change in people

--- atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
+++ atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
@@ -25,8 +25,89 @@ def main():
 
     job = read_json_file("spreadsheet_of_columns.json")
 
-    crashes(db_connection_string, job)
-    units(db_connection_string, job)
+    # crashes(db_connection_string, job)
+    # units(db_connection_string, job)
+    persons(db_connection_string, job)
+
+
+def persons(db_connection_string, job):
+    with psycopg2.connect(db_connection_string) as conn:
+        # build up a mondo dictionary of the whole table keyed on a tuple of the primary key(s)
+        sql = "select * from people_cris where is_primary_person is False"
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(sql)
+            rows = cur.fetchall()
+            people_cris = {
+                (row["cris_crash_id"], row["unit_nbr"], row["prsn_nbr"]): dict(row)
+                for row in tqdm(rows, desc="Building dictionary of people_cris")
+            }
+
+        # random_key = random.choice(list(people_cris.keys()))
+        # print("Key:", random_key)
+        # print("Value:", people_cris[random_key])
+
+        # another big dictionary of the whole table
+        sql = "select * from atd_txdot_person"
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+            cur.execute(sql)
+            rows = cur.fetchall()
+            persons_classic_vz = {
+                (row["crash_id"], row["unit_nbr"], row["prsn_nbr"]): dict(row)
+                for row in tqdm(rows, desc="Building dictionary of atd_txdot_person")
+            }
+
+        # random_key = random.choice(list(persons_classic_vz.keys()))
+        # print("Key:", random_key)
+        # print("Value:", persons_classic_vz[random_key])
+
+        # cast the char Y/Ns to booleans
+        for _, person_data in tqdm(
+            persons_classic_vz.items(), desc="Casting Y/N into booleans"
+        ):
+            for key, value in list(
+                person_data.items()
+            ):  # We use list to create a copy of items for iteration
+                if key.endswith("_fl"):
+                    if value == "Y":
+                        person_data[key] = True
+                    elif value == "N":
+                        person_data[key] = False
+
+        columns = filter_by_old_table_name(job, "atd_txdot_person")
+
+        # fmt: off
+        updates = []
+        for person_key in tqdm(people_cris, desc="Comparing values"):
+            for column in columns:
+                if column["target column name"] != "-":
+                    if column["target column name"] in people_cris[person_key]:
+                        if (persons_classic_vz[person_key][column["old column name"]] != people_cris[person_key][column["target column name"]]):
+                            logging.debug(f"❌ {column['old column name']}: {persons_classic_vz[person_key][column['old column name']]} != {people_cris[person_key][column['target column name']]}")
+                            sql = f"update people_edits set {column['target column name']} = %s where id = (select id from people_cris where cris_crash_id = %s and unit_nbr = %s and prsn_nbr = %s)"
+                            parameters = (persons_classic_vz[person_key][column["old column name"]], person_key[0], person_key[1], person_key[2])
+                            updates.append((sql, parameters))
+                        else:
+                            logging.debug(f"✅ {column['old column name']}: {persons_classic_vz[person_key][column['old column name']]} == {people_cris[person_key][column['target column name']]}")
+                    else:
+                        if persons_classic_vz[person_key][column['old column name']]:
+                            logging.debug(f"✨ VZ only column: No {column['target column name']} in people_cris, so {persons_classic_vz[person_key][column['old column name']]} going into people_edits")
+                            sql = f"update people_edits set {column['target column name']} = %s where id = (select id from people_cris where cris_crash_id = %s and unit_nbr = %s and prsn_nbr = %s)"
+                            parameters = (persons_classic_vz[person_key][column["old column name"]], person_key[0], person_key[1], person_key[2])
+                            updates.append((sql, parameters))
+                        else:
+                            logging.debug(f"🤷 VZ only column, but no {column['old column name']} in classic VZ data, so no value in people_edits's {column['target column name']}")
+        # fmt: on
+
+        # Mogrify and print the queries (for debugging)
+        # for update in tqdm(updates, desc="Mogrifying Queries"):
+        #     with conn.cursor() as cur:
+        #         mogrified_query = cur.mogrify(update[0], update[1]).decode()
+        #         logging.debug(mogrified_query)
+
+        # Execute the queries
+        with conn.cursor() as cur:
+            for query, params in tqdm(updates, desc="Executing Queries"):
+                cur.execute(query, params)
 
 
 def units(db_connection_string, job):
