4a7b7da0f731a54feaa10cb466868f2d22938700
John Clary <john.clary@austintexas.gov>
2024-06-06 15:23:42 -0400

    linting

--- atd-toolbox/data_model/python/70_make_column_metadata_table.py
+++ atd-toolbox/data_model/python/70_make_column_metadata_table.py
@@ -7,20 +7,19 @@ def load_sql_template(name):
 
 
 def main():
-    # translate our data model migration column metadata into the structure
-    # we will use in the DB
+    # translate our data model migration column metadata into the  db table structure
     columns = load_column_metadata()
-
     column_inserts = []
-
     for col in columns:
-        is_imported_from_cris =  True if col["source"] == "cris" and col["is_cris_column"] else False
+        is_imported_from_cris = (
+            True if col["source"] == "cris" and col["is_cris_column"] else False
+        )
         col_insert_sql = f"""insert into _column_metadata (column_name, column_label, record_type, is_imported_from_cris)
         values ('{col['column_name']}', '{col['column_name']}', '{col['record_type']}', {str(is_imported_from_cris).lower()})"""
         column_inserts.append(col_insert_sql)
-    
+
     metadata_table_sql = load_sql_template("sql_templates/column_metadata_table.sql")
-    col_insert_sql = ';\n'.join(column_inserts)
+    col_insert_sql = ";\n".join(column_inserts)
     migration_sql = f"{metadata_table_sql}\n\n{col_insert_sql}"
     migration_path = make_migration_dir("column_metadata_table")
     save_file(f"{migration_path}/up.sql", migration_sql)
