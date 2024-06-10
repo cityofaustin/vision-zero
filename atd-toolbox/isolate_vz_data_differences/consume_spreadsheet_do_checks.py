bedec111dcbe2af86db75f39285115cc138c90cf
Frank Hereford <frank@frankhereford.com>
2024-06-10 12:18:17 -0500

    forgot to bring these back in

--- atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
+++ atd-toolbox/isolate_vz_data_differences/consume_spreadsheet_do_checks.py
@@ -35,10 +35,10 @@ def main():
 
     job = read_json_file("spreadsheet_of_columns.json")
 
-    # crashes(db_connection_string, job)
-    # units(db_connection_string, job)
+    crashes(db_connection_string, job)
+    units(db_connection_string, job)
     persons(db_connection_string, job)
-    # primary_persons(db_connection_string, job)
+    primary_persons(db_connection_string, job)
 
 
 def primary_persons(db_connection_string, job):
