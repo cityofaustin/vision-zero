e92b197e8f6949aeb0964389d72095901a15d958
John Clary <john.clary@austintexas.gov>
2024-06-27 16:11:39 -0400

    make the restore archive helper functional again

--- atd-etl/data_model/_restore_zips_from_archive.py
+++ atd-etl/data_model/_restore_zips_from_archive.py
@@ -7,8 +7,9 @@ from tempfile import TemporaryDirectory
 
 import boto3
 
+
 from utils.utils import (
-    get_extracts_todo_from_s3,
+    get_extract_zips_to_download_s3,
 )
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
@@ -18,15 +19,23 @@ def main():
     s3_client = boto3.client("s3")
     s3_resource = boto3.resource("s3")
 
-    extracts = get_extracts_todo_from_s3(s3_client, "archive")
+    extracts = get_extract_zips_to_download_s3(s3_client, subdir="archive")
 
     print(f"{len(extracts)} extracts to restore")
 
     if not extracts:
         return
 
+    ok_to_proceed = input(
+        f"About to copy {len(extracts)} extracts into the  './new' bucket subdirectory. Type 'y' to continue: "
+    )
+
+    if ok_to_proceed != "y":
+        print("aborted")
+        return
+
     for extract in extracts:
-        file_key = extract["file_key"]
+        file_key = extract["s3_file_key"]
         new_key = file_key.replace("archive", "new")
         print(f"Copying zip to {BUCKET_NAME}/{new_key}")
         s3_resource.meta.client.copy(
