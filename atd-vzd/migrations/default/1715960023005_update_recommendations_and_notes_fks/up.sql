614b344831467ce2ab374eb2a6a632bdd7d51cba
rose <rose.eichelmann@gmail.com>
2024-06-28 14:35:51 -0500

    drop/add new unique constraint

--- atd-vzd/migrations/default/1715960023005_update_recommendations_and_notes_fks/up.sql
+++ atd-vzd/migrations/default/1715960023005_update_recommendations_and_notes_fks/up.sql
@@ -4,7 +4,9 @@ alter table recommendations
     add column crash_id integer,
     add constraint recommendations_crashes_id_fkey foreign key (
         crash_id
-    ) references crashes (id);
+    ) references crashes (id),
+    drop constraint recommendations_crash_id_key,
+    add constraint recommendations_crash_id_key unique (crash_id);
 
 alter table crash_notes rename column crash_id to atd_txdot_crashes_crash_id;
 
