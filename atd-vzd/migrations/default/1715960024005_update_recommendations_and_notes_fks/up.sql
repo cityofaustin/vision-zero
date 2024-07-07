62091ecd53d07c7f8357705834be12fce2871263
John Clary <john.clary@austintexas.gov>
2024-07-07 00:00:54 -0400

    syn cup these migration changes that will conflict with the reccs and notes branch

--- atd-vzd/migrations/default/1715960024005_update_recommendations_and_notes_fks/up.sql
+++ atd-vzd/migrations/default/1715960024005_update_recommendations_and_notes_fks/up.sql
@@ -1,16 +1,20 @@
 alter table recommendations rename column crash_id to atd_txdot_crashes_crash_id;
 
 alter table recommendations
-    add column crash_id integer,
-    add constraint recommendations_crashes_id_fkey foreign key (
-        crash_id
-    ) references crashes (id);
+add column crash_id integer,
+add constraint recommendations_crashes_id_fkey foreign key (
+    crash_id
+) references crashes (id),
+drop constraint recommendations_crash_id_key,
+add constraint recommendations_crash_id_key unique (crash_id),
+alter column atd_txdot_crashes_crash_id drop not null;;
 
 alter table crash_notes rename column crash_id to atd_txdot_crashes_crash_id;
 
 alter table crash_notes
-    add column crash_id integer,
-    add constraint notes_crashes_id_fkey foreign key (
-        crash_id
-    ) references crashes (id);
+add column crash_id integer,
+add constraint notes_crashes_id_fkey foreign key (
+    crash_id
+) references crashes (id),
+alter column atd_txdot_crashes_crash_id drop not null;
 
