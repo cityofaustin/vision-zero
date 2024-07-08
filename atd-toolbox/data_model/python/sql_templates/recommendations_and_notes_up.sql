ab096813a85a31b3cdb9f760353c707217e4de3c
rose <rose.eichelmann@gmail.com>
2024-07-08 11:03:15 -0500

    update sql templates with migration changes

--- atd-toolbox/data_model/python/sql_templates/recommendations_and_notes_up.sql
+++ atd-toolbox/data_model/python/sql_templates/recommendations_and_notes_up.sql
@@ -4,7 +4,11 @@ alter table recommendations
     add column crash_id integer,
     add constraint recommendations_crashes_id_fkey foreign key (
         crash_id
-    ) references crashes (id);
+    ) references crashes (id),
+    drop constraint recommendations_crash_id_key,
+    add constraint recommendations_crash_id_key unique (crash_id),
+    alter column atd_txdot_crashes_crash_id drop not null;
+;
 
 alter table crash_notes rename column crash_id to atd_txdot_crashes_crash_id;
 
@@ -12,4 +16,6 @@ alter table crash_notes
     add column crash_id integer,
     add constraint notes_crashes_id_fkey foreign key (
         crash_id
-    ) references crashes (id);
+    ) references crashes (id),
+    alter column atd_txdot_crashes_crash_id drop not null;
+;
