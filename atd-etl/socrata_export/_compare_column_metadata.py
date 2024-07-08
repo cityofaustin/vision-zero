e9cc03dff318210939ff3a498bea556cd12f082e
John Clary <john.clary@austintexas.gov>
2024-07-08 14:01:01 -0400

    make the output fancy

--- atd-etl/socrata_export/_compare_column_metadata.py
+++ atd-etl/socrata_export/_compare_column_metadata.py
@@ -9,8 +9,8 @@ datasets = [
 ]
 
 
-def compare_col_arrays(list_a, list_b):
-    missing = []
+def compare_col_arrays(list_a, list_b, a_list_name, b_list_name, primary_list="a"):
+    todos = []
     for col in list_a:
         key = col["fieldName"]
         matching_col = None
@@ -21,25 +21,38 @@ def compare_col_arrays(list_a, list_b):
                 if col_to_match["fieldName"] == key
             )
         except StopIteration:
-            print(f"{key} is missing")
-            missing.append(col)
+            if (primary_list == "a"):
+                todo = f"create column {key} type {col['dataTypeName']} in the {b_list_name} dataset"
+                todos.append(todo)
+            else:
+                todo = f"delete column {key} from the {b_list_name} dataset"
+                todos.append(todo)
             continue
         # see if the column type is the same
         a_col_type = col["dataTypeName"]
         b_col_type = matching_col["dataTypeName"]
         if a_col_type != b_col_type:
-            print(f"{key} is type {a_col_type} vs {b_col_type}")
+            if primary_list == "a":
+                todo = f"change {key} from type {b_col_type} to {a_col_type} in the {b_list_name} dataset"
+                todos.append(todo)
+            else:
+                # we don't need to print these type mismatches
+                # todo = f"change {key} from type {a_col_type} to {b_col_type}"
+                # todos.append(todo)
+                continue
+    return todos
 
 
 def main():
     socrata_client = get_socrata_client()
     for dataset in datasets:
+        print(f"\n\n**** {dataset['name']} dataset changes ****")
         cols_staging = socrata_client.get_metadata(dataset["staging"])["columns"]
         cols_prod = socrata_client.get_metadata(dataset["prod"])["columns"]
-        print("\n\n**** Comparing staging columns against prod ****")
-        compare_col_arrays(cols_staging, cols_prod)
-        print("\n\n**** Comparing prod columns against staging ****")
-        compare_col_arrays(cols_prod, cols_staging)
+        todos = compare_col_arrays(cols_staging, cols_prod, "staging", "prod", primary_list="a")
+        todos += compare_col_arrays(cols_prod, cols_staging, "staging", "prod", primary_list="b")
+        todos.sort()
+        print("\n".join(todos))
 
 
 if __name__ == "__main__":
