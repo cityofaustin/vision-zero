296d5c046365c7c6af5f105d76a7cbe6b5903a76
John Clary <john.clary@austintexas.gov>
2024-07-12 17:23:50 -0400

    adding debugging notes

--- atd-toolbox/data_model/python/populate_edits_sql/debug.sql
+++ atd-toolbox/data_model/python/populate_edits_sql/debug.sql
@@ -1,3 +1,11 @@
+-- 18203059 - missing a person record - https://github.com/cityofaustin/atd-data-tech/issues/7720
+-- 180290542 this is a 'permanent' temp record? see https://github.com/cityofaustin/atd-data-tech/issues/15220
+-- 18790889 - appears to be a missing / mismatched person record
+-- 18848386 - appears to be a missing / mismatched person record
+-- 19609765 - appears to be a missing / mismatched person record
+
+
+-- all fatal crashes in the old schema that are missing in the new, based burely on the `atd_fatality_count` column
 with newdm as (
     select
         id,
@@ -6,19 +14,19 @@ with newdm as (
     from
         crashes_list_view
     where
-        crash_date_ct > '2022-12-13'
-        and crash_date_ct < '2024-01-01'
-        and vz_fatality_count > 0
+        vz_fatality_count > 0
         and private_dr_fl = FALSE
         and in_austin_full_purpose = TRUE
-)
-
+),
+joined_crashes as(
 select old.crash_id, newdm.crash_id as new_crash_id
 from atd_txdot_crashes old
 left join newdm on newdm.crash_id = old.crash_id
 where
-    old.crash_date > '2022-12-31' and
+    old.crash_date >= '2018-01-01' and 
     old.crash_date < '2024-01-01' and
     old.atd_fatality_count > 0 and
     old.private_dr_fl = 'N'
-    and old.in_austin_full_purpose = 'Y';
+    and old.in_austin_full_purpose = 'Y')
+
+select * from joined_crashes where new_crash_id is null;
