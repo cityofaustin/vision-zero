581cad5c8b5024237527625e177d1f9164a8e987
John Clary <john.clary@austintexas.gov>
2024-07-13 12:18:15 -0400

    update with notes about sus serious injuries

--- atd-toolbox/data_model/python/populate_edits_sql/debug.sql
+++ atd-toolbox/data_model/python/populate_edits_sql/debug.sql
@@ -1,3 +1,5 @@
+--     old.crash_date >= '2018-01-01' and 
+-- old.crash_date < '2024-01-01' and
 -- 18203059 - missing a person record - https://github.com/cityofaustin/atd-data-tech/issues/7720
 -- 180290542 this is a 'permanent' temp record? see https://github.com/cityofaustin/atd-data-tech/issues/15220
 -- 18790889 - appears to be a missing / mismatched person record
@@ -18,15 +20,55 @@ with newdm as (
         and private_dr_fl = FALSE
         and in_austin_full_purpose = TRUE
 ),
-joined_crashes as(
-select old.crash_id, newdm.crash_id as new_crash_id
-from atd_txdot_crashes old
-left join newdm on newdm.crash_id = old.crash_id
-where
-    old.crash_date >= '2018-01-01' and 
-    old.crash_date < '2024-01-01' and
-    old.atd_fatality_count > 0 and
-    old.private_dr_fl = 'N'
-    and old.in_austin_full_purpose = 'Y')
 
-select * from joined_crashes where new_crash_id is null;
+joined_crashes as (
+    select
+        old.crash_id,
+        newdm.crash_id as new_crash_id
+    from atd_txdot_crashes as old
+    left join newdm on old.crash_id = newdm.crash_id
+    where
+        old.crash_date >= '2018-01-01'
+        and old.crash_date < '2024-01-01'
+        and old.atd_fatality_count > 0
+        and old.private_dr_fl = 'N'
+        and old.in_austin_full_purpose = 'Y'
+)
+
+select * from joined_crashes where new_crash_id is NULL;
+
+
+-- sus serious injuries
+-- 18551519 - crash not in the new db
+-- 19200655 - the people record in the old db was not edited to reflect the injury. Xavier needs to review.
+-- 18788739 - there are dupe people records in the old db. the best course of action will probably be to delete them.
+
+with newdm as (
+    select
+        id,
+        crash_id,
+        crash_injry_sev_id
+    from
+        crashes_list_view
+    where
+        sus_serious_injry_count > 0
+        and private_dr_fl = FALSE
+        and in_austin_full_purpose = TRUE
+),
+
+joined_crashes as (
+    select
+        old.crash_id,
+        newdm.crash_id as new_crash_id,
+        newdm.crash_injry_sev_id as new_crash_injry_sev_id
+    from atd_txdot_crashes as old
+    left join newdm on old.crash_id = newdm.crash_id
+    where
+        old.crash_date >= '2018-01-01'
+        and old.crash_date < '2024-01-01'
+        and old.crash_sev_id = 1
+        and old.private_dr_fl = 'N'
+        and old.in_austin_full_purpose = 'Y'
+)
+
+select * from joined_crashes where new_crash_id is NULL;
