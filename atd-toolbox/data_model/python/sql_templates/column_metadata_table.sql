7d1bd89249aaf69c711ecfd93869553364d2c5ac
John Clary <john.clary@austintexas.gov>
2024-07-16 17:04:47 -0400

    metadata table: add comments, drop column_label... and deudpe columns

--- atd-toolbox/data_model/python/sql_templates/column_metadata_table.sql
+++ atd-toolbox/data_model/python/sql_templates/column_metadata_table.sql
@@ -1,7 +1,12 @@
 create table _column_metadata (
     id serial primary key,
     column_name text not null,
-    column_label text not null,
     record_type text not null,
-    is_imported_from_cris boolean not null default false
+    is_imported_from_cris boolean not null default false,
+    UNIQUE (column_name, record_type)
 );
+
+comment on table _column_metadata is 'Table which tracks column metadata for crashes, units, people, and charges records. Is the used by CRIS import to determine which fields will be processed/upserted.';
+comment on column _column_metadata.column_name is 'The name of the column in the db';
+comment on column _column_metadata.record_type is 'The type of record tables associated with this colum: crashes/units/people/charges';
+comment on column _column_metadata.is_imported_from_cris is 'If this column is populated via the CRIS import ETL';
