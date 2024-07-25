54919e3cf030977804fd9f3549d95cb235278fe6
rose <rose.eichelmann@gmail.com>
2024-07-25 18:29:08 -0400

    update this comment

--- atd-vze/src/queries/dashboard.js
+++ atd-vze/src/queries/dashboard.js
@@ -1,8 +1,7 @@
 import { gql } from "apollo-boost";
 
-// These queries are derived from the ETL that populates our Socrata data sets
+// This query is derived from the view that populates our Socrata dataset
 // so that the VZV and VZE widget totals match up
-// See https://github.com/cityofaustin/atd-vz-data/blob/master/atd-etl/app/process/socrata_queries.py
 export const GET_CRASHES_YTD = gql`
   query GetCrashesYTD($yearStart: String, $yearEnd: String) {
     socrata_export_crashes_view_aggregate(
