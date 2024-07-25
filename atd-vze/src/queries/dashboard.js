67bb56fa619bcd241858a5e0ef7c2c0df8871a2e
rose <rose.eichelmann@gmail.com>
2024-07-25 17:56:25 -0400

    query the socrata view instead for ytd aggregates

--- atd-vze/src/queries/dashboard.js
+++ atd-vze/src/queries/dashboard.js
@@ -4,89 +4,20 @@ import { gql } from "apollo-boost";
 // so that the VZV and VZE widget totals match up
 // See https://github.com/cityofaustin/atd-vz-data/blob/master/atd-etl/app/process/socrata_queries.py
 export const GET_CRASHES_YTD = gql`
-  query GetCrashesYTD($yearStart: date, $yearEnd: date) {
-    fatalities: atd_txdot_crashes_aggregate(
-      where: {
-        crash_date: { _lt: $yearEnd, _gte: $yearStart }
-        private_dr_fl: { _eq: "N" }
-        _and: [
-          { crash_date: { _is_null: false } }
-          { crash_time: { _is_null: false } }
-        ]
-        _or: [
-          { in_austin_full_purpose: { _eq: true } }
-          { _and: [{ city_id: { _eq: 22 } }, { position: { _is_null: true } }] }
-        ]
-      }
+  query GetCrashesYTD($yearStart: String, $yearEnd: String) {
+    socrata_export_crashes_view_aggregate(
+      where: { crash_timestamp: { _lt: $yearEnd, _gte: $yearStart } }
     ) {
       aggregate {
         sum {
-          atd_fatality_count
+          death_cnt
         }
       }
-    }
-    seriousInjuriesAndTotal: atd_txdot_crashes_aggregate(
-      where: {
-        crash_date: { _lt: $yearEnd, _gte: $yearStart }
-        private_dr_fl: { _eq: "N" }
-        _and: [
-          { crash_date: { _is_null: false } }
-          { crash_time: { _is_null: false } }
-        ]
-        _or: [
-          { in_austin_full_purpose: { _eq: true } }
-          { _and: [{ city_id: { _eq: 22 } }, { position: { _is_null: true } }] }
-        ]
-      }
-    ) {
       aggregate {
         sum {
           sus_serious_injry_cnt
         }
       }
-    }
-    primaryPersonFatalities: atd_txdot_primaryperson_aggregate(
-      where: {
-        crash: { private_dr_fl: { _eq: "N" } }
-        _and: {
-          prsn_injry_sev_id: { _eq: 4 }
-          crash: { crash_date: { _lt: $yearEnd, _gte: $yearStart } }
-          _or: [
-            { crash: { in_austin_full_purpose: { _eq: true } } }
-            {
-              _and: [
-                { crash: { city_id: { _eq: 22 } } }
-                { crash: { position: { _is_null: true } } }
-              ]
-            }
-          ]
-        }
-      }
-    ) {
-      aggregate {
-        sum {
-          years_of_life_lost
-        }
-      }
-    }
-    personFatalities: atd_txdot_person_aggregate(
-      where: {
-        crash: { private_dr_fl: { _eq: "N" } }
-        _and: {
-          prsn_injry_sev_id: { _eq: 4 }
-          crash: { crash_date: { _lt: $yearEnd, _gte: $yearStart } }
-          _or: [
-            { crash: { in_austin_full_purpose: { _eq: true } } }
-            {
-              _and: [
-                { crash: { city_id: { _eq: 22 } } }
-                { crash: { position: { _is_null: true } } }
-              ]
-            }
-          ]
-        }
-      }
-    ) {
       aggregate {
         sum {
           years_of_life_lost
