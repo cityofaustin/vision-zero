6455002cc08d8f226a0e07d087233c54cca667ae
rose <rose.eichelmann@gmail.com>
2024-08-07 15:23:50 -0500

    add sql commands to backfill notes and recs

---
+++ atd-toolbox/data_model/python/post_migration_scripts/notes_recs_crash_pk_backfill.sql
@@ -0,0 +1,20 @@
+-- Backfill crash notes with the crash_pk
+UPDATE
+  crash_notes
+SET
+  crash_pk=crashes.id
+FROM
+  crashes
+WHERE
+  crash_notes.atd_txdot_crashes_crash_id=crashes.cris_crash_id;
+
+-- Backfill crash recommendations with the crash_pk
+UPDATE
+  recommendations
+SET
+  crash_pk=crashes.id
+FROM
+  crashes
+WHERE
+  recommendations.atd_txdot_crashes_crash_id=crashes.cris_crash_id;
+  
