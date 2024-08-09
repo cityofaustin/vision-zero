b2ebb1972c1621873b3f4e2c0e55c6242baab887
rose <rose.eichelmann@gmail.com>
2024-08-09 12:13:00 -0500

    switch order of these commands

--- atd-toolbox/data_model/python/post_migration_scripts/notes_recs_crash_pk_backfill.sql
+++ atd-toolbox/data_model/python/post_migration_scripts/notes_recs_crash_pk_backfill.sql
@@ -23,12 +23,12 @@ DELETE FROM recommendations WHERE atd_txdot_crashes_crash_id = 19724784;
 
 DELETE FROM crash_notes WHERE atd_txdot_crashes_crash_id = 19724784;
 
--- Drop old columns
-ALTER TABLE recommendations DROP COLUMN atd_txdot_crashes_crash_id;
-
-ALTER TABLE crash_notes DROP COLUMN atd_txdot_crashes_crash_id;
-
 -- Each record must always have an associated crash_pk
 ALTER TABLE recommendations ALTER COLUMN crash_pk SET NOT NULL;
 
 ALTER TABLE crash_notes ALTER COLUMN crash_pk SET NOT NULL;
+
+-- Drop old columns
+ALTER TABLE recommendations DROP COLUMN atd_txdot_crashes_crash_id;
+
+ALTER TABLE crash_notes DROP COLUMN atd_txdot_crashes_crash_id;
