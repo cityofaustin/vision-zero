adb1a4bb96af528f8e46b3f141390cab4368d0b7
John Clary <john.clary@austintexas.gov>
2024-08-27 08:31:38 -0400

    handle smaller cr3 pdf page size

--- atd-etl/cris_import/utils/settings.py
+++ atd-etl/cris_import/utils/settings.py
@@ -2,8 +2,9 @@ LOCAL_EXTRACTS_DIR = "extracts"
 
 """This is the bbox used to crop the crash diagram"""
 DIAGRAM_BBOX_PIXELS = {
-    "old": (2589, 3531, 5001, 6048),
-    "new": (2496, 3036, 4836, 5464),
+    "v1": (2589, 3531, 5001, 6048),
+    "v2_small": (658, 791, 1270, 1430),
+    "v2_large": (2496, 3036, 4836, 5464),
 }
 
 """If all four of these pixels are black, it is a 'new' CR3 pdf"""
