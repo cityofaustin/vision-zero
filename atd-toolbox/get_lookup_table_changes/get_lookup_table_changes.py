e4665f2274bd3ab887714875db9005703a899fea
John Clary <john.clary@austintexas.gov>
2024-09-05 10:55:19 -0400

    collect errors and print them at the end

--- atd-toolbox/get_lookup_table_changes/get_lookup_table_changes.py
+++ atd-toolbox/get_lookup_table_changes/get_lookup_table_changes.py
@@ -51,25 +51,39 @@ def get_cris_lookups_from_file():
 def main():
     cris_lookups = get_cris_lookups_from_file()
     vz_lookup_table_names = get_vz_lookup_table_names()
+    errors = []
     for table_name in vz_lookup_table_names:
         cris_lookup = cris_lookups.get(table_name)
         if not cris_lookup:
-            print(f"skip {table_name}")
+            errors.append(f"Lookup table {table_name} does not exist in CRIS")
             continue
-        print(f"getting values for {table_name}")
+
+        print(f"Gettling lookup values for {table_name}")
         vz_values = get_vz_lookup_table_values(table_name)
         cris_values = cris_lookup["values"]
         for vz_value in vz_values:
             try:
                 cris_value = next(x for x in cris_values if x["id"] == vz_value["id"])
             except StopIteration:
-                print(f"no cris ID match for table {table_name}: {vz_value}!")
+                errors.append(
+                    f"{table_name}: VZ value {vz_value['label']} ({vz_value['id']}) does not exist in CRIS"
+                )
                 continue
 
             if cris_value["label"] != vz_value["label"]:
-                print("value mismatch")
+                errors.append(
+                    f"{table_name}: VZ value {vz_value['label']} ({vz_value['id']}) has a different label in CRIS: {cris_value['label']}"
+                )
 
-    breakpoint()
+        for cris_value in cris_values:
+            try:
+                vz_value = next(v for v in vz_values if v["id"] == cris_value["id"])
+            except StopIteration:
+                errors.append(
+                    f"{table_name}: CRIS value {cris_value['label']} ({cris_value['id']}) does not exist in VZ"
+                )
+                continue
+    print("\n".join(errors))
     return
 
 
