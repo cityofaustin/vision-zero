443ea68e8625347bb1ae46b59aa6877aded5d74b
John Clary <john.clary@austintexas.gov>
2024-09-06 12:50:46 -0400

    finalize actual lookup migrations

--- atd-vzd/migrations/default/1725561923206_cris_v28_lookup_changes/down.sql
+++ atd-vzd/migrations/default/1725561923206_cris_v28_lookup_changes/down.sql
@@ -1,95 +1,105 @@
--- select '(' || id || ', ''' ||  REPLACE(label, '''', '''''') || ''',' || '''cris''' || ')' as thing from lookups.agency where id in (42,71,151,167,212,220,234,262,275,321,795,1167,1201,1226,1278,1326,1441,1520,1666,1684,1723,1765,1802,1822,1832,1894,1930,1956,1968,1983,2070,2092,2202,2272,2296,2314,2320,2446,2461,2586,2590,2602,2779,2831,2839,2871,2872,2887,3078,3123,3168,3171,3173,3174,3181,3183,3186,3250,3254,3257,3258,3262,3264,3270,3273,3275,3281,3283,3284,3287,3290,3291,3295,3299,3310,3313,3314,3323,3335,23673,23682,23773,23794,23816,23845,23846,23854,23897,24043);
-insert into lookups.city (id, label, source) values
-(42, 'ANGELINA COLLEGE POLICE DEPARTMENT', 'cris'),
-(71, 'AUSTIN COLLEGE POLICE DEPARTMENT', 'cris'),
-(151, 'BRAZOS COUNTY SHERIFF''S OFFICE', 'cris'),
-(167, 'BROWN COUNTY SHERIFF''S OFFICE', 'cris'),
-(212, 'CARRIZO SPRINGS CISD POLICE DEPARTMENT', 'cris'),
-(220, 'CASTRO COUNTY SHERIFF''S OFFICE', 'cris'),
-(234, 'CHAMBERS COUNTY SHERIFF''S OFFICE', 'cris'),
-(262, 'LEFORS CITY MARSHAL''S OFFICE', 'cris'),
-(275, 'STAR HARBOR POLICE DEPARTMENT', 'cris'),
-(321, 'CONCORDIA UNIVERSITY POLICE DEPARTMENT', 'cris'),
-(795, 'FALLS COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(1167, 'DALLAS CITY MARSHAL''S OFFICE', 'cris'),
-(1201, 'DIBOLL ISD POLICE DEPARTMENT', 'cris'),
-(1226, 'EAGLE PASS ISD POLICE DEPARTMENT', 'cris'),
-(1278, 'ESTELLINE POLICE DEPARTMENT', 'cris'),
-(1326, 'FREESTONE COUNTY SHERIFF''S OFFICE', 'cris'),
-(1441, 'HENDERSON COUNTY SHERIFF''S OFFICE', 'cris'),
-(1520, 'ITASCA POLICE DEPARTMENT', 'cris'),
-(1666, 'LIBERTY EYLAU ISD POLICE DEPARTMENT', 'cris'),
-(1684, 'LOCKNEY POLICE DEPARTMENT', 'cris'),
-(1723, 'MALONE POLICE DEPARTMENT', 'cris'),
-(1765, 'MEDINA COUNTY SHERIFF''S OFFICE', 'cris'),
-(1802, 'MOORE COUNTY SHERIFF''S OFFICE', 'cris'),
-(1822, 'NORTH FOREST ISD POLICE DEPARTMENT', 'cris'),
-(1832, 'NAVARRO COUNTY SHERIFF''S OFFICE', 'cris'),
-(1894, 'PALACIOS ISD POLICE DEPARTMENT', 'cris'),
-(1930, 'PINE FOREST POLICE DEPARTMENT', 'cris'),
-(1956, 'PORT OF HOUSTON AUTHORITY POLICE DEPARTMENT', 'cris'),
-(1968, 'PRESIDIO ISD POLICE DEPARTMENT', 'cris'),
-(1983, 'RANDALL COUNTY SHERIFF''S OFFICE', 'cris'),
-(2070, 'SAN ANTONIO PARK POLICE DEPARTMENT', 'cris'),
-(2092, 'SANTA ROSA ISD POLICE DEPARTMENT', 'cris'),
-(2202, 'SWEENY ISD POLICE DEPARTMENT', 'cris'),
-(2272, 'THORNTON POLICE DEPARTMENT', 'cris'),
-(2296, 'TRINITY COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(2314, 'TYLER ISD POLICE DEPARTMENT', 'cris'),
-(2320, 'UNIVERSITY OF MARY HARDIN BAYLOR POLICE DEPARTMENT', 'cris'),
-(2446, 'WINONA POLICE DEPARTMENT', 'cris'),
-(2461, 'YOAKUM COUNTY SHERIFF''S OFFICE', 'cris'),
-(2586, 'CHEROKEE COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(2590, 'COCHRAN COUNTY CONSTABLE PRECINCT 1', 'cris'),
-(2602, 'COMAL COUNTY CONSTABLE PRECINCT 2', 'cris'),
-(2779, 'HOUSTON COUNTY CONSTABLE PRECINCT 2', 'cris'),
-(2831, 'KENDALL COUNTY CONSTABLE PRECINCT 4', 'cris'),
-(2839, 'KLEBERG COUNTY CONSTABLE PRECINCT 1', 'cris'),
-(2871, 'LIBERTY COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(2872, 'LIBERTY COUNTY CONSTABLE PRECINCT 4', 'cris'),
-(2887, 'LUBBOCK COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(3078, 'TRINITY COUNTY CONSTABLE PRECINCT 1', 'cris'),
-(3123, 'WEBB COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(3168, 'LAREDO AIRPORT POLICE DEPARTMENT', 'cris'),
-(3171, 'PANOLA COLLEGE POLICE DEPARTMENT', 'cris'),
-(3173, 'VINTON POLICE DEPARTMENT', 'cris'),
-(3174, 'WHITE RIVER WATER DISTRICT POLICE DEPARTMENT', 'cris'),
-(3181, 'HUGHES SPRINGS ISD POLICE DEPARTMENT', 'cris'),
-(3183, 'WAYLAND BAPTIST UNIVERSITY POLICE DEPARTMENT', 'cris'),
-(3186, 'SAN ANGELO REGIONAL AIRPORT POLICE DEPARTMENT', 'cris'),
-(3250, 'UNIVERSITY OF NORTH TEXAS AT DALLAS POLICE DEPARTMENT', 'cris'),
-(3254, 'HOWE ISD POLICE DEPARTMENT', 'cris'),
-(3257, 'ELLIS COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(3258, 'DALLAS COUNTY CONSTABLE PRECINCT 2', 'cris'),
-(3262, 'KAUFMAN COUNTY CONSTABLE PRECINCT 2', 'cris'),
-(3264, 'OLDHAM COUNTY SHERIFF''S OFFICE', 'cris'),
-(3270, 'DODD CITY ISD POLICE DEPARTMENT', 'cris'),
-(3273, 'LONDON ISD POLICE DEPARTMENT', 'cris'),
-(3275, 'SEYMOUR ISD POLICE DEPARTMENT', 'cris'),
-(3281, 'PECOS BARSTOW TOYAH ISD POLICE DEPARTMENT', 'cris'),
-(3283, 'FREER INDEPENDENT SCHOOL DISTRICT POLICE DEPARTMENT', 'cris'),
-(3284, 'SANDY OAKS MARSHAL''S OFFICE', 'cris'),
-(3287, 'WALKER COUNTY CONSTABLE PRECINCT 3', 'cris'),
-(3290, 'MCMULLEN COUNTY SHERIFF''S OFFICE', 'cris'),
-(3291, 'CAMERON COUNTY SHERIFF''S OFFICE', 'cris'),
-(3295, 'WASHINGTON COUNTY CONSTABLE PCT. 1', 'cris'),
-(3299, 'JEFFERSON COUNTY SHERIFF''S OFFICE', 'cris'),
-(3310, 'WASHINGTON COUNTY CONSTABLE OFFICE PRECINCT 2', 'cris'),
-(3313, 'CENTER POINT ISD POLICE DEPARTMENT', 'cris'),
-(3314, 'SULPHUR SPRINGS ISD POLICE DEPARTMENT', 'cris'),
-(3323, 'POLK COUNTY CONSTABLE PRECINCT 1', 'cris'),
-(3335, 'IDALOU ISD POLICE DEPARTMENT', 'cris'),
-(23794, 'ANGELINA COLLEGE POLICE DEPARTMENT', 'cris'),
-(
-    23673,
-    'BURLINGTON NORTHERN SANTA FE RAILROAD COMPANY POLICE DEPARTMENT',
-    'cris'
-),
-(23816, 'COLEMAN COUNTY SHERIFF’S OFFICE', 'cris'),
-(23682, 'COMAL COUNTY CONSTABLE’S OFFICE PCT 4', 'cris'),
-(23845, 'COPPER CANYON POLICE DEPARTMENT', 'cris'),
-(23854, 'JEFFERSON COUNTY SHERIFF’S OFFICE', 'cris'),
-(23897, 'SWISHER COUNTY SHERIFF’S OFFICE', 'cris'),
-(23773, 'TRAVIS COUNTY - AUSTIN EMS', 'cris'),
-(23846, 'WILSON COUNTY CONSTABLE OFFICE PRECINCT 3', 'cris'),
-(24043, 'AUSTIN COMMUNITY COLLEGE DISTRICT POLICE DEPARTMENT', 'cris');
+delete from lookups.agency where id = 10013;
+delete from lookups.agency where id = 10228;
+delete from lookups.agency where id = 10229;
+delete from lookups.agency where id = 10231;
+delete from lookups.agency where id = 10232;
+delete from lookups.agency where id = 3355;
+delete from lookups.agency where id = 3356;
+delete from lookups.agency where id = 3357;
+delete from lookups.agency where id = 3359;
+delete from lookups.agency where id = 3360;
+delete from lookups.agency where id = 3361;
+delete from lookups.agency where id = 3362;
+delete from lookups.agency where id = 3363;
+delete from lookups.agency where id = 3364;
+insert into lookups.agency (id, label, source) values (1167, 'DALLAS CITY MARSHAL''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1201, 'DIBOLL ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1226, 'EAGLE PASS ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1278, 'ESTELLINE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1326, 'FREESTONE COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1441, 'HENDERSON COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (151, 'BRAZOS COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1520, 'ITASCA POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1666, 'LIBERTY EYLAU ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (167, 'BROWN COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1684, 'LOCKNEY POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1723, 'MALONE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1765, 'MEDINA COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1802, 'MOORE COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1822, 'NORTH FOREST ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1832, 'NAVARRO COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (1894, 'PALACIOS ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1930, 'PINE FOREST POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1956, 'PORT OF HOUSTON AUTHORITY POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1968, 'PRESIDIO ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (1983, 'RANDALL COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (2070, 'SAN ANTONIO PARK POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2092, 'SANTA ROSA ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (212, 'CARRIZO SPRINGS CISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (220, 'CASTRO COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (2202, 'SWEENY ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2272, 'THORNTON POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2296, 'TRINITY COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (2314, 'TYLER ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2320, 'UNIVERSITY OF MARY HARDIN BAYLOR POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (234, 'CHAMBERS COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (23673, 'BURLINGTON NORTHERN SANTA FE RAILROAD COMPANY POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (23682, 'COMAL COUNTY CONSTABLE’S OFFICE PCT 4', 'cris');
+insert into lookups.agency (id, label, source) values (23773, 'TRAVIS COUNTY - AUSTIN EMS', 'cris');
+insert into lookups.agency (id, label, source) values (23794, 'ANGELINA COLLEGE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (23816, 'COLEMAN COUNTY SHERIFF’S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (23845, 'COPPER CANYON POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (23846, 'WILSON COUNTY CONSTABLE OFFICE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (23854, 'JEFFERSON COUNTY SHERIFF’S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (23897, 'SWISHER COUNTY SHERIFF’S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (24043, 'AUSTIN COMMUNITY COLLEGE DISTRICT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2446, 'WINONA POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2461, 'YOAKUM COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (2586, 'CHEROKEE COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (2590, 'COCHRAN COUNTY CONSTABLE PRECINCT 1', 'cris');
+insert into lookups.agency (id, label, source) values (2602, 'COMAL COUNTY CONSTABLE PRECINCT 2', 'cris');
+insert into lookups.agency (id, label, source) values (262, 'LEFORS CITY MARSHAL''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (275, 'STAR HARBOR POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (2779, 'HOUSTON COUNTY CONSTABLE PRECINCT 2', 'cris');
+insert into lookups.agency (id, label, source) values (2831, 'KENDALL COUNTY CONSTABLE PRECINCT 4', 'cris');
+insert into lookups.agency (id, label, source) values (2839, 'KLEBERG COUNTY CONSTABLE PRECINCT 1', 'cris');
+insert into lookups.agency (id, label, source) values (2871, 'LIBERTY COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (2872, 'LIBERTY COUNTY CONSTABLE PRECINCT 4', 'cris');
+insert into lookups.agency (id, label, source) values (2887, 'LUBBOCK COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (3078, 'TRINITY COUNTY CONSTABLE PRECINCT 1', 'cris');
+insert into lookups.agency (id, label, source) values (3123, 'WEBB COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (3168, 'LAREDO AIRPORT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3171, 'PANOLA COLLEGE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3173, 'VINTON POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3174, 'WHITE RIVER WATER DISTRICT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3181, 'HUGHES SPRINGS ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3183, 'WAYLAND BAPTIST UNIVERSITY POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3186, 'SAN ANGELO REGIONAL AIRPORT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (321, 'CONCORDIA UNIVERSITY POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3250, 'UNIVERSITY OF NORTH TEXAS AT DALLAS POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3254, 'HOWE ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3257, 'ELLIS COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (3258, 'DALLAS COUNTY CONSTABLE PRECINCT 2', 'cris');
+insert into lookups.agency (id, label, source) values (3262, 'KAUFMAN COUNTY CONSTABLE PRECINCT 2', 'cris');
+insert into lookups.agency (id, label, source) values (3264, 'OLDHAM COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3270, 'DODD CITY ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3273, 'LONDON ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3275, 'SEYMOUR ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3281, 'PECOS BARSTOW TOYAH ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3283, 'FREER INDEPENDENT SCHOOL DISTRICT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3284, 'SANDY OAKS MARSHAL''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3287, 'WALKER COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (3290, 'MCMULLEN COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3291, 'CAMERON COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3295, 'WASHINGTON COUNTY CONSTABLE PCT. 1', 'cris');
+insert into lookups.agency (id, label, source) values (3299, 'JEFFERSON COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3310, 'WASHINGTON COUNTY CONSTABLE OFFICE PRECINCT 2', 'cris');
+insert into lookups.agency (id, label, source) values (3313, 'CENTER POINT ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3314, 'SULPHUR SPRINGS ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3323, 'POLK COUNTY CONSTABLE PRECINCT 1', 'cris');
+insert into lookups.agency (id, label, source) values (3335, 'IDALOU ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (42, 'ANGELINA COLLEGE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (71, 'AUSTIN COLLEGE POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (795, 'FALLS COUNTY CONSTABLE PRECINCT 3', 'cris');
+update lookups.agency set label = 'TEXAS WOMAN''S UNIVERSITY DEPARTMENT OF PUBLIC SAFETY' where id = 2264;
+update lookups.agency set label = 'TRAVIS COUNTY-TNR-TE' where id = 10170;
