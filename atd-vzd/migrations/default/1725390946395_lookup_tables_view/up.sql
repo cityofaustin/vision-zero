bb1f0f67e69b758b962c01de59124f7eadc1ed3a
John Clary <john.clary@austintexas.gov>
2024-09-06 13:11:46 -0400

    add comment to view

--- atd-vzd/migrations/default/1725390946395_lookup_tables_view/up.sql
+++ atd-vzd/migrations/default/1725390946395_lookup_tables_view/up.sql
@@ -4,3 +4,5 @@ create or replace view _lookup_tables_view as (
     where table_schema = 'lookups'
     order by table_name asc
 );
+
+comment on view _lookup_tables_view is 'View which provides a list of tables in the "lookups" schema and is used by helper script to identify CRIS lookup table changes';
