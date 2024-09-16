ae7055f164c645935466a1634b3be3d77e525f82
rose <rose.eichelmann@gmail.com>
2024-09-16 16:26:55 -0500

    rigth justified button, handle confirm click to do swap address mutation

--- atd-vze/src/views/Crashes/SwapAddressButton.js
+++ atd-vze/src/views/Crashes/SwapAddressButton.js
@@ -1,16 +1,48 @@
 import React, { useState } from "react";
 import { Button } from "reactstrap";
 import ConfirmModal from "../../Components/ConfirmModal";
+import { UPDATE_CRASH } from "../../queries/crashes";
 
-const SwapAddressButton = () => {
+const SwapAddressButton = ({ data, crashRefetch, ...props }) => {
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+  const crash = data?.crash;
 
   const toggleModal = () => {
     setIsConfirmModalOpen(!isConfirmModalOpen);
   };
 
+  const onConfirm = () => {
+    props.client
+      .mutate({
+        mutation: UPDATE_CRASH,
+        variables: {
+          id: crash.id,
+          changes: {
+            rpt_block_num: crash.rpt_sec_block_num,
+            rpt_street_name: crash.rpt_sec_street_name,
+            rpt_street_desc: crash.rpt_sec_street_desc,
+            rpt_road_part_id: crash.rpt_sec_road_part_id,
+            rpt_rdwy_sys_id: crash.rpt_sec_rdwy_sys_id,
+            rpt_hwy_num: crash.rpt_sec_hwy_num,
+            rpt_street_pfx: crash.rpt_sec_street_pfx,
+            rpt_street_sfx: crash.rpt_sec_street_sfx,
+            rpt_sec_block_num: crash.rpt_block_num,
+            rpt_sec_street_name: crash.rpt_street_name,
+            rpt_sec_street_desc: crash.rpt_street_desc,
+            rpt_sec_road_part_id: crash.rpt_road_part_id,
+            rpt_sec_rdwy_sys_id: crash.rpt_rdwy_sys_id,
+            rpt_sec_hwy_num: crash.rpt_hwy_num,
+            rpt_sec_street_pfx: crash.rpt_street_pfx,
+            rpt_sec_street_sfx: crash.rpt_street_sfx,
+          },
+        },
+      })
+      .then(res => crashRefetch());
+  };
+
   return (
-    <td>
+    <td className="float-right">
       <Button color="primary" onClick={toggleModal}>
         Swap Address
       </Button>
@@ -21,6 +53,7 @@ const SwapAddressButton = () => {
         modalBody={
           "Are you sure you want to swap the primary and secondary address?"
         }
+        confirmClick={onConfirm}
       ></ConfirmModal>
     </td>
   );
