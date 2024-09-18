fdfe01f6195fdb144f48135dddc812d108b2466f
rose <rose.eichelmann@gmail.com>
2024-09-18 16:54:04 -0500

    put this gnarly object into a variable

--- atd-vze/src/views/Crashes/SwapAddressButton.js
+++ atd-vze/src/views/Crashes/SwapAddressButton.js
@@ -12,31 +12,33 @@ const SwapAddressButton = ({ data, crashRefetch, ...props }) => {
     setIsConfirmModalOpen(!isConfirmModalOpen);
   };
 
+  const mutationVariables = {
+    id: crash.id,
+    changes: {
+      rpt_block_num: crash.rpt_sec_block_num,
+      rpt_street_name: crash.rpt_sec_street_name,
+      rpt_street_desc: crash.rpt_sec_street_desc,
+      rpt_road_part_id: crash.rpt_sec_road_part_id,
+      rpt_rdwy_sys_id: crash.rpt_sec_rdwy_sys_id,
+      rpt_hwy_num: crash.rpt_sec_hwy_num,
+      rpt_street_pfx: crash.rpt_sec_street_pfx,
+      rpt_street_sfx: crash.rpt_sec_street_sfx,
+      rpt_sec_block_num: crash.rpt_block_num,
+      rpt_sec_street_name: crash.rpt_street_name,
+      rpt_sec_street_desc: crash.rpt_street_desc,
+      rpt_sec_road_part_id: crash.rpt_road_part_id,
+      rpt_sec_rdwy_sys_id: crash.rpt_rdwy_sys_id,
+      rpt_sec_hwy_num: crash.rpt_hwy_num,
+      rpt_sec_street_pfx: crash.rpt_street_pfx,
+      rpt_sec_street_sfx: crash.rpt_street_sfx,
+    },
+  };
+
   const onConfirm = () => {
     props.client
       .mutate({
         mutation: UPDATE_CRASH,
-        variables: {
-          id: crash.id,
-          changes: {
-            rpt_block_num: crash.rpt_sec_block_num,
-            rpt_street_name: crash.rpt_sec_street_name,
-            rpt_street_desc: crash.rpt_sec_street_desc,
-            rpt_road_part_id: crash.rpt_sec_road_part_id,
-            rpt_rdwy_sys_id: crash.rpt_sec_rdwy_sys_id,
-            rpt_hwy_num: crash.rpt_sec_hwy_num,
-            rpt_street_pfx: crash.rpt_sec_street_pfx,
-            rpt_street_sfx: crash.rpt_sec_street_sfx,
-            rpt_sec_block_num: crash.rpt_block_num,
-            rpt_sec_street_name: crash.rpt_street_name,
-            rpt_sec_street_desc: crash.rpt_street_desc,
-            rpt_sec_road_part_id: crash.rpt_road_part_id,
-            rpt_sec_rdwy_sys_id: crash.rpt_rdwy_sys_id,
-            rpt_sec_hwy_num: crash.rpt_hwy_num,
-            rpt_sec_street_pfx: crash.rpt_street_pfx,
-            rpt_sec_street_sfx: crash.rpt_street_sfx,
-          },
-        },
+        variables: mutationVariables,
       })
       .then(res => crashRefetch());
   };
