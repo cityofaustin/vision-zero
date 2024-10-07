72221a6dfa5fe8eb07facd79fd5bb377c217c08e
John Clary <john.clary@austintexas.gov>
2024-10-07 14:50:33 -0400

    add gist index

--- database/migrations/default/1728059618657_signal_eng_areas/up.sql
+++ database/migrations/default/1728059618657_signal_eng_areas/up.sql
@@ -14,6 +14,8 @@ create table public.signal_engineer_areas (
     updated_at timestamptz default now() not null
 );
 
+create index on signal_engineer_areas using gist (geometry);
+
 comment on table public.signal_engineer_areas is 'Polygon zones assigned to traffic signal engineers. These zones cover the Full and Limited Purpose juristdiction areas of the City of Austin.';
 
 create or replace trigger set_updated_at_signal_engineer_areas
