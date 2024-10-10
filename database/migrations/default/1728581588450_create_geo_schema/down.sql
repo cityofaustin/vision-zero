a2d0e4e1451370da46ef46647d58d162f4ec3537
John Clary <john.clary@austintexas.gov>
2024-10-10 13:58:40 -0400

    update various trigger functions

--- database/migrations/default/1728581588450_create_geo_schema/down.sql
+++ database/migrations/default/1728581588450_create_geo_schema/down.sql
@@ -89,4 +89,64 @@ begin
 end;
 $function$
 
+CREATE OR REPLACE FUNCTION public.afd_incidents_trigger()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  update afd__incidents set
+    austin_full_purpose = (
+      select ST_Contains(jurisdiction.geometry, incidents.geometry)
+      from afd__incidents incidents
+      left join atd_jurisdictions jurisdiction on (jurisdiction.jurisdiction_label = 'AUSTIN FULL PURPOSE')
+      where incidents.id = new.id),
+    location_id = (
+      select locations.location_id
+      from afd__incidents incidents
+      join atd_txdot_locations locations on (
+        true
+        and locations.location_group = 1 -- this was added to rule out old level 5s
+        and incidents.geometry && locations.geometry
+        and ST_Contains(locations.geometry, incidents.geometry)
+      )
+      where incidents.id = new.id),
+    latitude = ST_Y(afd__incidents.geometry),
+    longitude = ST_X(afd__incidents.geometry),
+    ems_incident_number_1 = afd__incidents.ems_incident_numbers[1],
+    ems_incident_number_2 = afd__incidents.ems_incident_numbers[2],
+    call_date = date(afd__incidents.call_datetime),
+    call_time = afd__incidents.call_datetime::time
+    where afd__incidents.id = new.id;
+RETURN NEW;
+END;
+$function$
+
+CREATE OR REPLACE FUNCTION public.ems_incidents_trigger()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  update ems__incidents set
+    austin_full_purpose = (
+      select ST_Contains(jurisdiction.geometry, incidents.geometry)
+      from ems__incidents incidents
+      left join atd_jurisdictions jurisdiction on (jurisdiction.jurisdiction_label = 'AUSTIN FULL PURPOSE')
+      where incidents.id = new.id),
+    location_id = (
+      select locations.location_id
+      from ems__incidents incidents
+      join atd_txdot_locations locations on (locations.location_group = 1 and incidents.geometry && locations.geometry and ST_Contains(locations.geometry, incidents.geometry))
+      where incidents.id = new.id),
+    latitude = ST_Y(ems__incidents.geometry),
+    longitude = ST_X(ems__incidents.geometry),
+    apd_incident_number_1 = ems__incidents.apd_incident_numbers[1],
+    apd_incident_number_2 = ems__incidents.apd_incident_numbers[2],
+    mvc_form_date = date(ems__incidents.mvc_form_extrication_datetime),
+    mvc_form_time = ems__incidents.mvc_form_extrication_datetime::time
+    where ems__incidents.id = new.id;
+RETURN NEW;
+END;
+$function$
+
+
 drop schema if exists geo;
