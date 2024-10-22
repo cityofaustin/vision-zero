e741eb15cc02f557183a23b634c9900bd7c735cb
John Clary <john.clary@austintexas.gov>
2024-10-22 14:13:02 -0400

    add migrations for new spatial cols and updated trigger

---
+++ database/migrations/default/1729608549704_new_crash_spatial_cols/up.sql
@@ -0,0 +1,17 @@
+alter table crashes
+    add column signal_engineer_area_id integer,
+    add constraint crashes_signal_engineer_area_id_fkey
+        foreign key (signal_engineer_area_id)  
+        references geo.signal_engineer_areas (signal_engineer_area_id)
+        on update cascade on delete set null,
+    add column zipcode text,
+    add constraint crashes_zipcode_fkey
+        foreign key (zipcode)  
+        references geo.zip_codes (zipcode)
+        on update cascade on delete set null,
+    add column apd_sector_id integer,
+    add constraint crashes_apd_sector_id_fkey
+        foreign key (apd_sector_id)  
+        references geo.apd_sectors (primary_key)
+        on update cascade on delete set null,
+    add column is_coa_roadway boolean default false;
