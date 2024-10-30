cfdd391e11eb7175688b94bb56088df14ea5d4c5
John Clary <john.clary@austintexas.gov>
2024-10-30 12:22:42 -0400

    undo rename is_non_coa_roadway to is_coa_roadway

--- database/migrations/default/1729608549704_new_crash_spatial_cols/down.sql
+++ database/migrations/default/1729608549704_new_crash_spatial_cols/down.sql
@@ -2,4 +2,4 @@ alter table crashes
     drop column signal_engineer_area_id,
     drop column zipcode,
     drop column apd_sector_id,
-    drop column is_coa_roadway;
+    drop column is_non_coa_roadway;
