018b5fa81005aaab5f7a6c2b701053d0d38ca83d
John Clary <john.clary@austintexas.gov>
2024-11-01 11:51:00 -0400

    make header slightly nicer

--- app/components/CrashHeader.tsx
+++ app/components/CrashHeader.tsx
@@ -1,3 +1,4 @@
+import { formatDateTime } from "@/utils/formatters";
 import { Crash } from "@/types/types";
 
 interface CrashHeaderProps {
@@ -6,14 +7,15 @@ interface CrashHeaderProps {
 
 export default function CrashHeader({ crash }: CrashHeaderProps) {
   return (
-    <div>
-      <h6 className="display-6">{crash.address_primary}</h6>
-      <span>Updated by </span>
-      <span className="me-1">{crash.updated_by}</span>
-
-      <span>
-        {crash.updated_at && new Date(crash.updated_at).toISOString()}
-      </span>
+    <div className="d-flex justify-content-between mb-3">
+      <span className="display-6">{crash.address_primary}</span>
+      <div>
+        <span>Updated by </span>
+        <span className="me-1">
+          {crash.updated_by?.replace("@austintexas.gov", " on ")}
+        </span>
+        <span>{crash.updated_at && formatDateTime(crash.updated_at)}</span>
+      </div>
     </div>
   );
 }
