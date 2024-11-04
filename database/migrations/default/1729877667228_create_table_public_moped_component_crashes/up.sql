3ea09af7bd3ecab348f60c62fe7df94c04c88272
rose <rose.eichelmann@gmail.com>
2024-11-04 12:48:00 -0600

    prettify

--- database/migrations/default/1729877667228_create_table_public_moped_component_crashes/up.sql
+++ database/migrations/default/1729877667228_create_table_public_moped_component_crashes/up.sql
@@ -1,3 +1,8 @@
-CREATE TABLE "public"."moped_component_crashes" ("crash_pk" integer NOT NULL, "mopd_proj_component_id" integer NOT NULL, PRIMARY KEY ("crash_pk","mopd_proj_component_id") , FOREIGN KEY ("crash_pk") REFERENCES "public"."crashes"("id") ON UPDATE cascade ON DELETE cascade);
+CREATE TABLE "public"."moped_component_crashes" (
+    "crash_pk" INTEGER NOT NULL,
+    "mopd_proj_component_id" INTEGER NOT NULL,
+    PRIMARY KEY ("crash_pk", "mopd_proj_component_id"),
+    FOREIGN KEY ("crash_pk") REFERENCES "public"."crashes"("id") ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-comment on table "public"."moped_component_crashes" is E'This table enables crashes to be linked with their associated moped project components.';
+COMMENT ON TABLE "public"."moped_component_crashes" IS E'This table enables crashes to be linked with their associated moped project components.';
