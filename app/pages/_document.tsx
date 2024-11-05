f1ed9c16cae239d4b07fec57a9bac79f323b2764
John Clary <john.clary@austintexas.gov>
2024-11-05 13:11:43 -0500

    update commented out dark mode

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    // <Html lang="en" data-bs-theme="dark">
+    // <Html lang="en" style={{fontSize: "14px"}} data-bs-theme="dark">
     <Html lang="en" style={{fontSize: "14px"}}>
       <Head />
       <body>
