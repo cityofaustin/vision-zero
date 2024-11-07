43391ab60951a080824e71117f492e1c566b1919
Frank Hereford <frank@frankhereford.com>
2024-11-07 09:37:11 -0600

    add cors headers for local

--- api/server.py
+++ api/server.py
@@ -103,7 +103,10 @@ def add_custom_headers(response):
     response.headers["Expect-CT"] = "max-age=86400, enforce"
 
     # Access-Control-Allow-Origin for CORS
-    response.headers["Access-Control-Allow-Origin"] = "https://trusted-origin.com"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
 
     return response
 
