af18eda5e424351ea8416cfe0fd8dee30c93d0d1
John Clary <john.clary@austintexas.gov>
2024-11-19 17:44:38 -0500

    add injury metrics to query, types

--- app/types/types.ts
+++ app/types/types.ts
@@ -92,6 +92,7 @@ export interface Crash extends Record<string, unknown> {
   address_secondary: string | null;
   at_intrsct_fl: boolean | null;
   case_id: string | null;
+  crash_injury_metrics_view: CrashInjuryMetrics;
   cr3_stored_fl: boolean | null;
   crash_speed_limit: number | null;
   crash_timestamp: string | null;
@@ -140,6 +141,21 @@ export interface Crash extends Record<string, unknown> {
   wthr_cond_id: number | null;
   change_logs: ChangeLogEntry[];
   units: Unit[];
+}
+
+export interface CrashInjuryMetrics {
+  vz_fatality_count: number | null;
+  years_of_life_lost: number | null;
+  est_comp_cost_crash_based: number | null;
+  est_total_person_comp_cost: number | null;
+  crash_injry_sev_id: number | null;
+  nonincap_injry_count: number | null;
+  sus_serious_injry_count: number | null;
+  poss_injry_count: number | null;
+  unkn_injry_count: number | null;
+  tot_injry_count: number | null;
+  cris_fatality_count: number | null;
+  law_enf_fatality_count: number | null;
 }
 
 /**
