a1f2ec52b3867dc46547a9f34caf0aef48000451
John Clary <john.clary@austintexas.gov>
2024-11-22 16:11:44 -0500

    crashes list: add search fields to init query config

--- app/components/TableSearchFieldSelector.tsx
+++ app/components/TableSearchFieldSelector.tsx
@@ -1,22 +1,16 @@
 import Form from "react-bootstrap/Form";
 import { TableSearchProps } from "./TableSearch";
 
-// todo: move to prop
-const fields = [
-  { label: "Crash ID", value: "record_locator" },
-  { label: "Case ID", value: "case_id" },
-  { label: "Address", value: "address_primary" },
-];
-
 export default function TableSearchFieldSelector({
   searchSettings,
   setSearchSettings,
+  queryConfig,
 }: TableSearchProps) {
   // todo: bug here where changing the search field clears the search input if it hasn't been searched
   return (
     <>
       <Form.Label className="fw-bold me-2 mb-0">Search by </Form.Label>
-      {fields.map((field) => {
+      {queryConfig.searchFields.map((field) => {
         return (
           <Form.Check
             key={field.value}
