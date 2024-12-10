08212bfb054ee2a9ee8594dfed59f264b35be613
John Clary <john.clary@austintexas.gov>
2024-12-10 13:09:33 -0500

    add user form

--- app/app/users/page.tsx
+++ app/app/users/page.tsx
@@ -1,28 +1,40 @@
 "use client";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Spinner from "react-bootstrap/Spinner";
 import Table from "react-bootstrap/Table";
+import { FaUserPlus, FaCopy } from "react-icons/fa6";
 import AlignedLabel from "@/components/AlignedLabel";
 import AppBreadCrumb from "@/components/AppBreadCrumb";
-import NewUserModal from "@/components/NewUserModal";
+import UserModal from "@/components/UserModal";
+import UserForm from "@/components/UserForm";
 import { useUsersInfinite } from "@/utils/users";
 import { useToken } from "@/utils/auth";
-import { FaUserPlus, FaCopy } from "react-icons/fa6";
+import { User } from "@/types/users";
 
 export default function Users() {
   const token = useToken();
   const router = useRouter();
-  const { users, isLoading, error } = useUsersInfinite(token);
+  const { users, isLoading, error, mutate } = useUsersInfinite(token);
   const [showNewUserModal, setShowNewUserModal] = useState(false);
   const onCloseModal = () => setShowNewUserModal(false);
 
   if (error) {
     console.error(error);
   }
+
+  const onSaveUserCallback = useCallback(
+    (user: User) => {
+      // refetch the entire user list in the background
+      mutate();
+      // navigate to the user details pagee
+      router.push(`/users/${user.user_id}`);
+    },
+    [router, mutate]
+  );
 
   return (
     <>
@@ -90,7 +102,7 @@ export default function Users() {
                         ? new Date(user.last_login).toLocaleDateString()
                         : ""}
                     </td>
-                    <td>{user.logins_count}</td>
+                    <td>{user.logins_count || "0"}</td>
                     <td>{user.app_metadata.roles[0]}</td>
                   </tr>
                 );
@@ -99,7 +111,9 @@ export default function Users() {
           </Table>
         </Card.Body>
       </Card>
-      {showNewUserModal && <NewUserModal onClose={onCloseModal} />}
+      <UserModal onClose={onCloseModal} show={showNewUserModal} mode="create">
+        <UserForm onSubmitCallback={onSaveUserCallback} />
+      </UserModal>
     </>
   );
 }
