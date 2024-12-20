a8c5404d1f4c0dcf4df1343a5f553acdc3c00e5b
Frank Hereford <frank@frankhereford.com>
2024-12-19 18:39:48 -0600

    layout for image

--- app/components/CrashDiagramCard.tsx
+++ app/components/CrashDiagramCard.tsx
@@ -102,48 +102,52 @@ export default function CrashDiagramCard({ crash }: { crash: Crash }) {
         <TransformWrapper>
           <ZoomResetControls setRotation={setRotation} />
           <TransformComponent>
-            {!diagramError && (
-              <Image
-                fluid
-                style={{
-                  height: "100%",
-                  maxWidth: "100%",
-                  transform: `rotate(${rotation}deg)`,
-                }}
-                src={`${CR3_DIAGRAM_BASE_URL}/${crash.record_locator}.jpeg`}
-                alt="crash diagram"
-                onError={() => {
-                  console.error("Error loading CR3 diagram image");
-                  setDiagramError(true);
-                }}
-              />
-            )}
-            {diagramError && crash.is_temp_record && (
-              <Alert variant="info">
-                <i className="fa fa-info-circle" /> Crash diagrams are not
-                available for user-created crash records
-              </Alert>
-            )}
-            {diagramError && !crash.is_temp_record && (
-              <Alert variant="danger">
-                <p>
-                  The crash diagram is not available. Typically, this indicates
-                  there was an error when processing this crash&aposs CR3 PDF.
-                </p>
-                <p>
-                  For additional assistance, you can&nbsp;
-                  <a
-                    href="https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Vision%20Zero%20Editor%22%7D"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    report a bug
-                    <i className="fa fa-external-link"></i>
-                  </a>
-                  .
-                </p>
-              </Alert>
-            )}
+            <div style={{ height: "100%", overflow: "hidden" }}>
+              {!diagramError && (
+                <Image
+                  fluid
+                  style={{
+                    height: "100%",
+                    width: "100%",
+                    transform: `rotate(${rotation}deg)`,
+                    objectFit: "contain",
+                  }}
+                  src={`${CR3_DIAGRAM_BASE_URL}/${crash.record_locator}.jpeg`}
+                  alt="crash diagram"
+                  onError={() => {
+                    console.error("Error loading CR3 diagram image");
+                    setDiagramError(true);
+                  }}
+                />
+              )}
+              {diagramError && crash.is_temp_record && (
+                <Alert variant="info">
+                  <i className="fa fa-info-circle" /> Crash diagrams are not
+                  available for user-created crash records
+                </Alert>
+              )}
+              {diagramError && !crash.is_temp_record && (
+                <Alert variant="danger">
+                  <p>
+                    The crash diagram is not available. Typically, this
+                    indicates there was an error when processing this
+                    crash&aposs CR3 PDF.
+                  </p>
+                  <p>
+                    For additional assistance, you can&nbsp;
+                    <a
+                      href="https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Vision%20Zero%20Editor%22%7D"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      report a bug
+                      <i className="fa fa-external-link"></i>
+                    </a>
+                    .
+                  </p>
+                </Alert>
+              )}
+            </div>
           </TransformComponent>
         </TransformWrapper>
       </Card.Body>
