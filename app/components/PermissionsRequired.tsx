e2186d13c0923271abeb4667f41ec76c66528bf0
John Clary <john.clary@austintexas.gov>
2025-01-14 16:11:53 -0500

    create PermissionsRequired component

---
+++ app/components/PermissionsRequired.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { hasRole } from "@/utils/auth";
+
+interface PermissionsRequiredProps {
+  children: ReactNode;
+  allowedRoles?: string[];
+}
+
+/**
+ * Component wrapper that renders `null` if the user does not have one of the
+ * allwed roles. If the allowedRoles array is undefined then the permission
+ * check is skipped
+ */
+export default function PermissionsRequired({
+  children,
+  allowedRoles,
+}: PermissionsRequiredProps) {
+  const { user } = useAuth0();
+  if (!allowedRoles || (user && hasRole(allowedRoles, user))) {
+    return children;
+  }
+  return null;
+}
