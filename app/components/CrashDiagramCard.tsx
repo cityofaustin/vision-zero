396bea49b3ab2489510a030f0ee07dfabbd580bc
Frank Hereford <frank@frankhereford.com>
2025-01-14 09:37:13 -0600

    Incorporate John's JSX

--- app/components/CrashDiagramCard.tsx
+++ app/components/CrashDiagramCard.tsx
@@ -98,57 +98,53 @@ export default function CrashDiagramCard({ crash }: { crash: Crash }) {
   return (
     <Card>
       <Card.Header>Diagram</Card.Header>
-      <Card.Body className="crash-header-card-body text-center">
+      <Card.Body className="crash-header-card-body text-center d-flex flex-column">
         <TransformWrapper initialScale={1}>
           {!diagramError && <ZoomResetControls setRotation={setRotation} />}
           <TransformComponent>
-            <div style={{ height: "330px", width: "100%", overflow: "hidden" }}>
-              {!diagramError && (
-                <Image
-                  fluid
-                  style={{
-                    height: "100%",
-                    width: "440px",
-                    transform: `rotate(${rotation}deg)`,
-                    objectFit: "contain",
-                  }}
-                  src={`${CR3_DIAGRAM_BASE_URL}/${crash.record_locator}.jpeg`}
-                  alt="crash diagram"
-                  onError={() => {
-                    console.error("Error loading CR3 diagram image");
-                    setDiagramError(true);
-                  }}
-                />
-              )}
-              {diagramError && crash.is_temp_record && (
-                <div
-                  className="d-flex align-items-start justify-content-center h-100 mt-3"
-                  style={{ width: "490px" }}
-                >
-                  <Alert variant="info" className="mx-auto">
-                    <i className="fa fa-info-circle" />
-                    Crash diagrams are not available for temporary crash records
-                  </Alert>
-                </div>
-              )}
-              {diagramError && !crash.is_temp_record && (
-                <Alert variant="danger" style={{ marginTop: "20px" }}>
-                  <p>The crash diagram is not available.</p>
-                  <p>
-                    For additional assistance, you can&nbsp;
-                    <a
-                      href="https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Vision%20Zero%20Editor%22%7D"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      report a bug
-                      <i className="fa fa-external-link"></i>
-                    </a>
-                    .
-                  </p>
+            {!diagramError && (
+              <Image
+                fluid
+                style={{
+                  transform: `rotate(${rotation}deg)`,
+                }}
+                src={`${CR3_DIAGRAM_BASE_URL}/${crash.record_locator}.jpeg`}
+                alt="crash diagram"
+                onError={() => {
+                  console.error("Error loading CR3 diagram image");
+                  setDiagramError(true);
+                }}
+              />
+            )}
+
+            {diagramError && crash.is_temp_record && (
+              <div
+                className="d-flex align-items-start justify-content-center h-100 mt-3"
+                style={{ width: "490px" }}
+              >
+                <Alert variant="info" className="mx-auto">
+                  <i className="fa fa-info-circle" />
+                  Crash diagrams are not available for temporary crash records
                 </Alert>
-              )}
-            </div>
+              </div>
+            )}
+            {diagramError && !crash.is_temp_record && (
+              <Alert variant="danger" style={{ marginTop: "20px" }}>
+                <p>The crash diagram is not available.</p>
+                <p>
+                  For additional assistance, you can&nbsp;
+                  <a
+                    href="https://atd.knack.com/dts#new-service-request/?view_249_vars=%7B%22field_398%22%3A%22Bug%20Report%20%E2%80%94%20Something%20is%20not%20working%22%2C%22field_399%22%3A%22Vision%20Zero%20Editor%22%7D"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    report a bug
+                    <i className="fa fa-external-link"></i>
+                  </a>
+                  .
+                </p>
+              </Alert>
+            )}
           </TransformComponent>
         </TransformWrapper>
       </Card.Body>
