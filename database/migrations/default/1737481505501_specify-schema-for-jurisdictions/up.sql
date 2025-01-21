112a6a2569a08f531eebe2abca0390d35bc25dee
Frank Hereford <frank@frankhereford.com>
2025-01-21 12:14:09 -0600

    reflect wacky CTM geometry handling

--- database/migrations/default/1737481505501_specify-schema-for-jurisdictions/up.sql
+++ database/migrations/default/1737481505501_specify-schema-for-jurisdictions/up.sql
@@ -10,30 +10,38 @@ returns trigger
 language plpgsql
 as $function$
 BEGIN
-  update afd__incidents set
+  WITH jurisdiction_union AS (
+    SELECT ST_Union(geometry) AS geometry
+    FROM geo.jurisdictions
+    WHERE jurisdiction_label = 'AUSTIN FULL PURPOSE'
+  )
+  UPDATE afd__incidents
+  SET
     austin_full_purpose = (
-      select ST_Contains(jurisdiction.geometry, incidents.geometry)
-      from afd__incidents incidents
-      left join geo.jurisdictions jurisdiction on (jurisdiction.jurisdiction_label = 'AUSTIN FULL PURPOSE')
-      where incidents.id = new.id),
+      SELECT ST_Contains(jurisdiction_union.geometry, incidents.geometry)
+      FROM afd__incidents incidents, jurisdiction_union
+      WHERE incidents.id = NEW.id
+    ),
     location_id = (
-      select locations.location_id
-      from afd__incidents incidents
-      join atd_txdot_locations locations on (
-        true
-        and locations.location_group = 1 -- this was added to rule out old level 5s
-        and incidents.geometry && locations.geometry
-        and ST_Contains(locations.geometry, incidents.geometry)
+      SELECT locations.location_id
+      FROM afd__incidents incidents
+      JOIN atd_txdot_locations locations ON (
+        TRUE
+        AND locations.location_group = 1 -- This was added to rule out old level 5s
+        AND incidents.geometry && locations.geometry
+        AND ST_Contains(locations.geometry, incidents.geometry)
       )
-      where incidents.id = new.id),
+      WHERE incidents.id = NEW.id
+    ),
     latitude = ST_Y(afd__incidents.geometry),
     longitude = ST_X(afd__incidents.geometry),
     ems_incident_number_1 = afd__incidents.ems_incident_numbers[1],
     ems_incident_number_2 = afd__incidents.ems_incident_numbers[2],
-    call_date = date(afd__incidents.call_datetime),
-    call_time = afd__incidents.call_datetime::time
-    where afd__incidents.id = new.id;
-RETURN NEW;
+    call_date = DATE(afd__incidents.call_datetime),
+    call_time = afd__incidents.call_datetime::TIME
+  WHERE afd__incidents.id = NEW.id;
+
+  RETURN NEW;
 END;
 $function$;
 
