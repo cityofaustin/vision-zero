58de9b33045200de46c46f032a770ff005d635dd
Frank Hereford <frank@frankhereford.com>
2025-01-21 11:49:04 -0600

    specify the schema of the geo data

---
+++ database/migrations/default/1737481505501_specify-schema-for-jurisdictions/up.sql
@@ -0,0 +1,33 @@
+DROP FUNCTION public.afd_incidents_trigger ();
+
+CREATE OR REPLACE FUNCTION public.afd_incidents_trigger()
+RETURNS trigger
+LANGUAGE plpgsql
+AS $function$
+BEGIN
+  update afd__incidents set
+    austin_full_purpose = (
+      select ST_Contains(jurisdiction.geometry, incidents.geometry)
+      from afd__incidents incidents
+      left join geo.jurisdictions jurisdiction on (jurisdiction.jurisdiction_label = 'AUSTIN FULL PURPOSE')
+      where incidents.id = new.id),
+    location_id = (
+      select locations.location_id
+      from afd__incidents incidents
+      join atd_txdot_locations locations on (
+        true
+        and locations.location_group = 1 -- this was added to rule out old level 5s
+        and incidents.geometry && locations.geometry
+        and ST_Contains(locations.geometry, incidents.geometry)
+      )
+      where incidents.id = new.id),
+    latitude = ST_Y(afd__incidents.geometry),
+    longitude = ST_X(afd__incidents.geometry),
+    ems_incident_number_1 = afd__incidents.ems_incident_numbers[1],
+    ems_incident_number_2 = afd__incidents.ems_incident_numbers[2],
+    call_date = date(afd__incidents.call_datetime),
+    call_time = afd__incidents.call_datetime::time
+    where afd__incidents.id = new.id;
+RETURN NEW;
+END;
+$function$;
