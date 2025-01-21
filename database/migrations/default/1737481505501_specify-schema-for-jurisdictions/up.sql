8f43b91f439353208dceff7183d429a56039cf36
Frank Hereford <frank@frankhereford.com>
2025-01-21 13:58:42 -0600

    bring same fix into EMS function

--- database/migrations/default/1737481505501_specify-schema-for-jurisdictions/up.sql
+++ database/migrations/default/1737481505501_specify-schema-for-jurisdictions/up.sql
@@ -37,3 +37,37 @@ BEGIN
   RETURN NEW;
 END;
 $function$;
+
+
+
+create or replace function public.ems_incidents_trigger()
+returns trigger
+language plpgsql
+as $function$
+BEGIN
+WITH jurisdiction_union AS (
+    SELECT ST_Union(geometry) AS geometry
+    FROM geo.jurisdictions
+    WHERE jurisdiction_label = 'AUSTIN FULL PURPOSE'
+  )
+  update ems__incidents set
+    austin_full_purpose = (
+      select ST_Contains(jurisdiction.geometry, incidents.geometry)
+      from ems__incidents incidents
+      left join jurisdiction_union jurisdiction on (jurisdiction.jurisdiction_label = 'AUSTIN FULL PURPOSE')
+      where incidents.id = new.id),
+    location_id = (
+      select locations.location_id
+      from ems__incidents incidents
+      join atd_txdot_locations locations on (locations.location_group = 1 and incidents.geometry && locations.geometry and ST_Contains(locations.geometry, incidents.geometry))
+      where incidents.id = new.id),
+    latitude = ST_Y(ems__incidents.geometry),
+    longitude = ST_X(ems__incidents.geometry),
+    apd_incident_number_1 = ems__incidents.apd_incident_numbers[1],
+    apd_incident_number_2 = ems__incidents.apd_incident_numbers[2],
+    mvc_form_date = date(ems__incidents.mvc_form_extrication_datetime),
+    mvc_form_time = ems__incidents.mvc_form_extrication_datetime::time
+    where ems__incidents.id = new.id;
+RETURN NEW;
+END;
+$function$;
