0d3529c1477a20d267a0f93dd6be3bb31f3926fb
John Clary <john.clary@austintexas.gov>
2025-01-27 11:22:38 -0500

    fix typo

--- app/components/LocationMapCard.tsx
+++ app/components/LocationMapCard.tsx
@@ -12,7 +12,7 @@ export default function LocationMapCard({ location }: { location: Location }) {
   const mapRef = useRef<MapRef | null>(null);
   /**
    * Trigger resize() when the map container size changes - this ensures that
-   * the map repaints when the sidebar is collased/expanded.
+   * the map repaints when the sidebar is collapsed/expanded.
    */
   const mapContainerRef = useResizeObserver<HTMLDivElement>(() => {
     mapRef.current?.resize();
