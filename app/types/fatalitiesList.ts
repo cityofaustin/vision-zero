ed3afcc88f162b6c1197557961efe4bd6ca53627
Frank Hereford <frank@frankhereford.com>
2025-01-27 11:03:31 -0600

    all nulls for 4 values in a fatalitiesListRow

--- app/types/fatalitiesList.ts
+++ app/types/fatalitiesList.ts
@@ -1,11 +1,11 @@
 export type fatalitiesListRow = {
   year: number;
   record_locator: string;
-  case_id: string;
-  law_enforcement_ytd_fatality_num: number;
+  case_id: string | null;
+  law_enforcement_ytd_fatality_num: number | null;
   ytd_fatal_crash: number;
   ytd_fatality: number;
   crash_date_ct: string;
-  location: string;
-  victim_name: string;
+  location: string | null;
+  victim_name: string | null;
 };
