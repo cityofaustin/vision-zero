3515a18b530ace8da24c02dfd569bdae732fdb98
John Clary <john.clary@austintexas.gov>
2025-01-31 12:53:31 -0500

    use flexbox layout

--- app/components/SidebarLayout.tsx
+++ app/components/SidebarLayout.tsx
@@ -3,9 +3,6 @@ import { ReactNode, useState, useCallback, useEffect } from "react";
 import { usePathname, useSelectedLayoutSegments } from "next/navigation";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 import ListGroup from "react-bootstrap/ListGroup";
 import { FaAngleRight } from "react-icons/fa6";
 import AppNavBar from "@/components/AppNavBar";
@@ -67,59 +64,58 @@ export default function SidebarLayout({ children }: { children: ReactNode }) {
   }
 
   return (
-    <Container fluid style={{ height: "100vh", overflow: "hidden" }}>
-      <Row className="h-100">
-        {/* Sidebar */}
-        <div
-          className={`bg-dark text-white p-0 app-sidebar app-sidebar-${
-            isCollapsed ? "collapsed" : "expanded"
-          }`}
-          style={{ overflowX: "hidden" }}
-        >
-          <div className="d-flex flex-column h-100">
-            <div className="p-2">
-              <Button
-                onClick={toggleSidebar}
-                variant="dark"
-                className="w-100 text-secondary"
-              >
-                <FaAngleRight
-                  className={`sidebar-toggle-${
-                    isCollapsed ? "closed" : "open"
-                  }`}
-                />
-              </Button>
-            </div>
-            <ListGroup variant="flush">
-              {routes.map((route) => (
-                <PermissionsRequired
-                  allowedRoles={route.allowedRoles}
-                  key={route.path}
-                >
-                  <SideBarListItem
-                    isCollapsed={isCollapsed}
-                    isCurrentPage={segments.includes(route.path)}
-                    Icon={route.icon}
-                    label={route.label}
-                    href={`/${route.path}`}
-                  />
-                </PermissionsRequired>
-              ))}
-            </ListGroup>
-          </div>
+    <div style={{ height: "100vh", overflow: "hidden" }} className="d-flex">
+      {/* Sidebar */}
+      <div
+        className={`bg-dark text-white app-sidebar app-sidebar-${
+          isCollapsed ? "collapsed" : "expanded"
+        }`}
+      >
+        <div className="p-2">
+          <Button
+            onClick={toggleSidebar}
+            variant="dark"
+            className="w-100 text-secondary"
+          >
+            <FaAngleRight
+              className={`sidebar-toggle-${isCollapsed ? "closed" : "open"}`}
+            />
+          </Button>
         </div>
-        {/* Main content */}
-        <Col
-          style={{
-            height: "100vh",
-            overflowY: "auto",
-          }}
-          className="p-0"
-        >
-          <AppNavBar user={user} logout={logout} />
-          <main className="px-3">{children}</main>
-        </Col>
-      </Row>
-    </Container>
+        <ListGroup variant="flush" className="px-2">
+          {routes.map((route) => (
+            <PermissionsRequired
+              allowedRoles={route.allowedRoles}
+              key={route.path}
+            >
+              <SideBarListItem
+                isCollapsed={isCollapsed}
+                isCurrentPage={segments.includes(route.path)}
+                Icon={route.icon}
+                label={route.label}
+                href={`/${route.path}`}
+              />
+            </PermissionsRequired>
+          ))}
+        </ListGroup>
+      </div>
+      {/* Main content */}
+      <div
+        className="flex-grow-1 d-flex flex-column w-100"
+        style={{ minWidth: 0 }}
+      >
+        <AppNavBar user={user} logout={logout} />
+        <main className="px-3 flex-grow-1" style={{ overflowY: "auto" }}>
+          {children}
+        </main>
+        <div className="bg-light text-secondary text-center py-2 px-3 border-top d-flex justify-content-between">
+          <span className="">Vision Zero Editor v2.5</span>
+          <span>
+            Built by{" "}
+            <a href="https://austinmobility.io">Data & Technology Services</a>
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
