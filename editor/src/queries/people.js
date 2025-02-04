5c7ac636eac10151689fee17381dffb539f243fc
John Clary <john.clary@austintexas.gov>
2025-02-04 14:33:47 -0500

    move nextjs into /editor dir

--- editor/src/queries/people.js
+++
@@ -1,9 +0,0 @@
-import { gql } from "apollo-boost";
-
-export const UPDATE_PERSON = gql`
-  mutation UpdatePerson($personId: Int!, $changes: people_set_input) {
-    update_people_by_pk(pk_columns: { id: $personId }, _set: $changes) {
-      id
-    }
-  }
-`;
--- editor/src/views/index.js
+++
@@ -1,5 +0,0 @@
-import Dashboard from "./Dashboard";
-
-import { Login, Page500, Register } from "./Pages";
-
-export { Page500, Register, Login, Dashboard };
