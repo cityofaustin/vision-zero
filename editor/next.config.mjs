b479b2328e9cc9c53b87685d143c686c46fd1b8c
John Clary <john.clary@austintexas.gov>
2025-02-05 08:06:28 -0500

    add baseBath to next config

--- editor/next.config.mjs
+++ editor/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "", // Set via env in production
   async redirects() {
     return [
       {
