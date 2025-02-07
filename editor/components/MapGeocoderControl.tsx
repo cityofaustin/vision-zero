6ff31aaee9b06b675120c03d7ce42ec6b6884a53
John Clary <john.clary@austintexas.gov>
2025-02-07 14:36:20 -0500

    adds geocoder control to crash map

---
+++ editor/components/MapGeocoderControl.tsx
@@ -0,0 +1,124 @@
+import { useControl, ControlPosition } from "react-map-gl";
+import MapboxGeocoder, { GeocoderOptions } from "@mapbox/mapbox-gl-geocoder";
+import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import { LatLon } from "@/components/CrashMap";
+
+const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
+type GeocoderControlProps = Omit<
+  GeocoderOptions,
+  "accessToken" | "mapboxgl" | "marker"
+> & {
+  position: ControlPosition;
+  onResult?: (latLon: LatLon) => void;
+};
+
+const ATX_BOUNDING_BOX: [number, number, number, number] = [
+  -98.182, 29.987, -97.304, 30.663,
+];
+
+/**
+ * Geocode control
+ * See: https://github.com/visgl/react-map-gl/tree/7.0-release/examples/geocoder
+ */
+export default function MapGeocoderControl(props: GeocoderControlProps) {
+  const geocoder = useControl<any>( // eslint-disable-line @typescript-eslint/no-explicit-any
+    () => {
+      const ctrl = new MapboxGeocoder({
+        ...props,
+        marker: false,
+        accessToken: MAPBOX_TOKEN || "",
+        bbox: ATX_BOUNDING_BOX,
+        flyTo: { speed: 2.5 },
+      });
+      ctrl.on("result", (evt) => {
+        const { result } = evt;
+        const location =
+          result &&
+          (result.center ||
+            (result.geometry?.type === "Point" && result.geometry.coordinates));
+        if (location && props.onResult) {
+          props.onResult({
+            longitude: Number(location[0]),
+            latitude: Number(location[1]),
+          });
+        }
+      });
+      return ctrl;
+    },
+    {
+      position: props.position,
+    }
+  );
+
+  if (geocoder._map) {
+    if (
+      geocoder.getProximity() !== props.proximity &&
+      props.proximity !== undefined
+    ) {
+      geocoder.setProximity(props.proximity);
+    }
+    if (
+      geocoder.getRenderFunction() !== props.render &&
+      props.render !== undefined
+    ) {
+      geocoder.setRenderFunction(props.render);
+    }
+    if (
+      geocoder.getLanguage() !== props.language &&
+      props.language !== undefined
+    ) {
+      geocoder.setLanguage(props.language);
+    }
+    if (geocoder.getZoom() !== props.zoom && props.zoom !== undefined) {
+      geocoder.setZoom(props.zoom);
+    }
+    if (geocoder.getFlyTo() !== props.flyTo && props.flyTo !== undefined) {
+      geocoder.setFlyTo(props.flyTo);
+    }
+    if (
+      geocoder.getPlaceholder() !== props.placeholder &&
+      props.placeholder !== undefined
+    ) {
+      geocoder.setPlaceholder(props.placeholder);
+    }
+    if (
+      geocoder.getCountries() !== props.countries &&
+      props.countries !== undefined
+    ) {
+      geocoder.setCountries(props.countries);
+    }
+    if (geocoder.getTypes() !== props.types && props.types !== undefined) {
+      geocoder.setTypes(props.types);
+    }
+    if (
+      geocoder.getMinLength() !== props.minLength &&
+      props.minLength !== undefined
+    ) {
+      geocoder.setMinLength(props.minLength);
+    }
+    if (geocoder.getLimit() !== props.limit && props.limit !== undefined) {
+      geocoder.setLimit(props.limit);
+    }
+    if (geocoder.getFilter() !== props.filter && props.filter !== undefined) {
+      geocoder.setFilter(props.filter);
+    }
+    if (geocoder.getOrigin() !== props.origin && props.origin !== undefined) {
+      geocoder.setOrigin(props.origin);
+    }
+    // Types missing from @types/mapbox__mapbox-gl-geocoder
+    // if (geocoder.getAutocomplete() !== props.autocomplete && props.autocomplete !== undefined) {
+    //   geocoder.setAutocomplete(props.autocomplete);
+    // }
+    // if (geocoder.getFuzzyMatch() !== props.fuzzyMatch && props.fuzzyMatch !== undefined) {
+    //   geocoder.setFuzzyMatch(props.fuzzyMatch);
+    // }
+    // if (geocoder.getRouting() !== props.routing && props.routing !== undefined) {
+    //   geocoder.setRouting(props.routing);
+    // }
+    // if (geocoder.getWorldview() !== props.worldview && props.worldview !== undefined) {
+    //   geocoder.setWorldview(props.worldview);
+    // }
+  }
+  return null;
+}
