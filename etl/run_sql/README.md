1e964265fc2484162f242285ba1076fe06d0a3cb
John Clary <john.clary@austintexas.gov>
2025-02-09 12:12:17 -0500

    create etl that does the same thing as the hasura cron trigger

---
+++ etl/run_sql/README.md
@@ -0,0 +1,21 @@
+# Run SQL
+
+This tool enables us to run arbitrary SQL using the Hasura [schema API](https://hasura.io/docs/2.0/api-reference/schema-api/run-sql/). It is intended to be used to schedule 
+
+## Configuration
+
+1. Start your local Vision Zero cluster database + Hasura graphql API
+
+2. Save a copy of the `env_template` file as `.env`, and fill in the graphql endpoint and admin secret. Note that the endpoint should end with `/v2/query` (see the [Hasura docs](https://hasura.io/docs/2.0/api-reference/schema-api/run-sql/) for more info).
+
+3. Configure a python environment with `requests` installed. 
+
+```
+# see available commands
+python run_sql.py -h
+```
+
+```
+# refresh location crashes
+python run_sql.py -c refresh_location_crashes
+```
---
+++ etl/run_sql/run_sql.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import json
+import logging
+import os
+import sys
+
+import requests
+
+from utils.cli import get_cli_args
+from utils.commands import COMMANDS
+
+ENDPOINT = os.getenv("HASURA_GRAPHQL_ENDPOINT")
+ADMIN_SECRET = os.getenv("HASURA_GRAPHQL_ADMIN_SECRET")
+
+ENDPOINT="http://localhost:8084/v2/query"
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def run_sql(sql):
+    """Run arbitrary SQL through the the Hasura schema API.
+
+    The ENDPOINT must provide the full path to the schema API, which
+    is currently `/v2/query`.
+
+    Args:
+        sql (Str): The sql to be executed, including a final semicolon.
+    """
+    if not ENDPOINT:
+        raise OSError("HASURA_GRAPHQL_ENDPOINT env var is required")
+
+    payload = {
+        "type": "run_sql",
+        "args": {"source": "default", "sql": sql},
+    }
+
+    logging.info(f"Executing: {sql}")
+    res = requests.post(
+        ENDPOINT, json=payload, headers={"x-hasura-admin-secret": ADMIN_SECRET}
+    )
+
+    try:
+        logging.info(res.json())
+    except json.JSONDecodeError:
+        logging.info(res.text)
+
+    res.raise_for_status()
+
+
+
+if __name__ == "__main__":
+    cli_args = get_cli_args()
+    command = next(c for c in COMMANDS if c["name"] == cli_args.command)
+    run_sql(command["sql"])
