6aa35d4e3d0a350a2f39c6f6d33cc78dd59b169a
rose <rose.eichelmann@gmail.com>
2025-02-19 13:51:49 -0600

    drop  not null

--- database/migrations/default/1739913612321_alter_notes_audit_fields/down.sql
+++ database/migrations/default/1739913612321_alter_notes_audit_fields/down.sql
@@ -1,11 +1,11 @@
 alter table crash_notes
     add column date timestamp with time zone DEFAULT now(),
     add column user_email text,
-    alter column created_by set null,
-    alter column updated_by set null;
+    alter column created_by drop not null,
+    alter column updated_by drop not null;
 
 alter table location_notes
     add column date timestamp with time zone DEFAULT now(),
     add column user_email text,
-    alter column created_by set null,
-    alter column updated_by set null;
+    alter column created_by drop not null,
+    alter column updated_by drop not null;
