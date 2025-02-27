8a6333840d2195d68ab1a23653d956d84eaa10c0
rose <rose.eichelmann@gmail.com>
2025-02-27 17:03:31 -0600

    dont fire callback when focused element is input or textarea, add comments, uppercase everything

--- editor/utils/shortcuts.ts
+++ editor/utils/shortcuts.ts
@@ -1,11 +1,15 @@
 import { useCallback, useLayoutEffect, useRef, useEffect } from "react";
 
 /**
- * Custom hook for adding keyboard shortcuts
+ * Custom hook for adding keyboard shortcuts.
+ * 
+ * The shift key must be used in conjunction with the array of keys passed
+ * and the focused element can't be an input or text area
+ * in order to fire the handleKeyPress callback.
  */
 export const useKeyboardShortcut = (
-  // Array of keys that should trigger the callback
-  keys: string[],
+  // Array of keys that should trigger the callback when used alongside the shift key
+  shortcutKeys: string[],
   // Callback that handles what happens when the keys are pressed
   callback: (event: KeyboardEvent) => void
 ) => {
@@ -15,11 +19,19 @@ export const useKeyboardShortcut = (
   });
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
-      if (keys.some((key) => event.shiftKey === true && event.key === key)) {
-        callbackRef.current(event);
+      // Type guard to access tagName property, see https://iifx.dev/en/articles/156175355
+      if (event.target instanceof Element) {
+        // Don't trigger callback if focused element is an input or textarea
+        if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
+          return
+        }
+        // Check if key in shortcutKeys array and used in conjunction with shift key
+        if (shortcutKeys.some((key) => event.shiftKey === true && event.key.toUpperCase() === key.toUpperCase())) {
+          callbackRef.current(event);
+        }
       }
     },
-    [keys]
+    [shortcutKeys]
   );
   useEffect(() => {
     if (document) {
