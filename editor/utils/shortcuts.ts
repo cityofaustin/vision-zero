58fa9e3ca1bf3c41f7bd636b832ce8e525d8a88c
rose <rose.eichelmann@gmail.com>
2025-02-28 13:55:44 -0600

    add select to disabled target shortcuts

--- editor/utils/shortcuts.ts
+++ editor/utils/shortcuts.ts
@@ -2,7 +2,7 @@ import { useCallback, useLayoutEffect, useRef, useEffect } from "react";
 
 /**
  * Custom hook for adding keyboard shortcuts.
- * 
+ *
  * The shift key must be used in conjunction with the array of keys passed
  * and the focused element can't be an input or text area
  * in order to fire the handleKeyPress callback.
@@ -19,14 +19,21 @@ export const useKeyboardShortcut = (
   });
   const handleKeyPress = useCallback(
     (event: KeyboardEvent) => {
+      const targetsToDisable = ["INPUT", "TEXTAREA", "SELECT"];
       // Type guard to access tagName property, see https://iifx.dev/en/articles/156175355
       if (event.target instanceof Element) {
         // Don't trigger callback if focused element is an input or textarea
-        if (event.target.tagName === "INPUT" || event.target.tagName === "TEXTAREA") {
-          return
+        if (targetsToDisable.includes(event.target.tagName)) {
+          return;
         }
         // Check if key in shortcutKeys array and used in conjunction with shift key
-        if (shortcutKeys.some((key) => event.shiftKey === true && event.key.toUpperCase() === key.toUpperCase())) {
+        if (
+          shortcutKeys.some(
+            (key) =>
+              event.shiftKey === true &&
+              event.key.toUpperCase() === key.toUpperCase()
+          )
+        ) {
           callbackRef.current(event);
         }
       }
