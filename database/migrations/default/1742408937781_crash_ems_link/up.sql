4252e8a3f817a8d51ecefb3b23322bb4f2537e17
John Clary <john.clary@austintexas.gov>
2025-03-27 14:28:05 -0400

    update trigger - add travel_mode column - add 1-1 person_id constraint - nix apd incident number columns - use manual qa match status to prevent overrides

--- database/migrations/default/1742408937781_crash_ems_link/up.sql
+++ database/migrations/default/1742408937781_crash_ems_link/up.sql
@@ -1,35 +1,44 @@
--- todo: modify AFD/EMS ETL to do nothing on conflict?
--- todo: enforce ems/person 1-1 constraint?
-
+--
 -- rename fk column to crash_pk and add fk constraint
+--
 alter table ems__incidents rename column crash_id to crash_pk;
 alter table ems__incidents
 add constraint ems__incidents_crashes_crash_pk_fkey foreign key (crash_pk)
-references public.crashes (id) on update cascade on delete set null;
+    references public.crashes (id) on update cascade on delete set null;
 
 comment on column ems__incidents.crash_pk is 'Crash ID matched to this record';
 
-alter table ems__incidents add column person_id integer,
-add constraint ems__incidents_person_id foreign key (
-    person_id
-) references people (id);
+alter table ems__incidents
+    add column person_id integer unique deferrable initially deferred,
+    add constraint ems__incidents_person_id foreign key (
+        person_id
+    ) references people (id);
 
 comment on column ems__incidents.person_id is 'Person ID matched to this record';
 
 --
--- i don't think we need this column or index anymore
+-- add column to track match metadata and multiple crash_pks
 --
--- -- add generated array of stringified case IDs bc crash case IDs are strings :/
--- alter table ems__incidents add column apd_case_ids text[] generated always as (
---     apd_incident_numbers::text[]
--- ) stored;
--- create index ems__incidents_apd_case_ids_index on ems__incidents using gin (apd_case_ids);
+alter table ems__incidents
+add column crash_match_status text default 'unmatched'
+check (
+    crash_match_status in (
+        'unmatched',
+        'matched_by_automation',
+        'multiple_matches_by_automation',
+        'matched_by_manual_qa'
+    )
+),
+add column matched_crash_pks integer [],
+add constraint matched_crash_pks_non_null check (
+    (crash_match_status = 'multiple_matches_by_automation')
+    or (matched_crash_pks is NULL)
+);
+comment on column ems__incidents.crash_match_status is 'The status of the crash record match';
 
--- add column to track match metadata
-alter table ems__incidents add column match_status text default 'unmatched';
-comment on column ems__incidents.match_status is 'The status of the crash record match - updated by update_crash_ems_match trigger';
-
+--
 -- add column to assign injury severity
+--
 alter table ems__incidents
 add column patient_injry_sev integer generated always as (
     case
@@ -96,27 +105,41 @@ references lookups.injry_sev on update restrict on delete restrict;
 
 comment on column ems__incidents.patient_injry_sev is 'The patient injury severity as mapped to the CRIS injury severity lookup';
 
--- -- add mode column
--- ALTER TABLE ems__incidents
--- ADD COLUMN mode text GENERATED ALWAYS AS (
--- CASE
---     WHEN LOWER(mvc_form_vehicle_type) = 'motorcycle' THEN 'Motorcycle'
---     WHEN LOWER(mvc_form_vehicle_type) = 'moped' THEN 'Motorcycle'
---     WHEN LOWER(pcr_cause_of_injury) LIKE '%motorcy%' THEN 'Motorcycle'
---     WHEN LOWER(pcr_cause_of_injury) LIKE '%scoot%' OR LOWER(pcr_patient_complaints) LIKE '%scoot%' THEN 'E-Scooter'
---     WHEN LOWER(pcr_cause_of_injury) LIKE '%bike%' OR LOWER(pcr_cause_of_injury) LIKE '%bicy%' OR 
---          LOWER(pcr_patient_complaints) LIKE '%bike%' OR LOWER(pcr_patient_complaints) LIKE '%bicy%' THEN 'Bicycle'
---     WHEN LOWER(pcr_cause_of_injury) LIKE '%pede%' OR LOWER(pcr_patient_complaints) LIKE '%pede%' OR
---          (LOWER(pcr_cause_of_injury) LIKE '%auto%' AND LOWER(pcr_cause_of_injury) LIKE '%ped%') OR
---          (LOWER(pcr_patient_complaints) LIKE '%auto%' AND LOWER(pcr_patient_complaints) LIKE '%ped%') THEN 'Pedestrian'
---     WHEN LOWER(pcr_cause_of_injury) LIKE '%motorcy%' OR LOWER(pcr_patient_complaints) LIKE '%mc %' THEN 'Motorcycle'
---     WHEN LOWER(mvc_form_vehicle_type) = 'motorized scooter' THEN 'E-Scooter'
---     ELSE 'Motor Vehicle'
---     END
--- ) STORED;
+--
+-- add travel_mode column
+--
+alter table ems__incidents
+add column travel_mode text generated always as (
+    case
+        when lower(mvc_form_vehicle_type) = 'motorcycle' then 'Motorcycle'
+        when lower(mvc_form_vehicle_type) = 'moped' then 'Motorcycle'
+        when lower(pcr_cause_of_injury) like '%motorcy%' then 'Motorcycle'
+        when
+            lower(pcr_cause_of_injury) like '%scoot%'
+            or lower(pcr_patient_complaints) like '%scoot%'
+            then 'E-Scooter'
+        when
+            lower(pcr_cause_of_injury) like '%bike%'
+            or lower(pcr_cause_of_injury) like '%bicy%'
+            or lower(pcr_patient_complaints) like '%bike%' or lower(pcr_patient_complaints) like '%bicy%' then 'Bicycle'
+        when
+            lower(pcr_cause_of_injury) like '%pede%'
+            or lower(pcr_patient_complaints) like '%pede%'
+            or (lower(pcr_cause_of_injury) like '%auto%' and lower(pcr_cause_of_injury) like '%ped%')
+            or (lower(pcr_patient_complaints) like '%auto%' and lower(pcr_patient_complaints) like '%ped%') then 'Pedestrian'
+        when
+            lower(pcr_cause_of_injury) like '%motorcy%'
+            or lower(pcr_patient_complaints) like '%mc %'
+            then 'Motorcycle'
+        when lower(mvc_form_vehicle_type) = 'motorized scooter' then 'E-Scooter'
+        else 'Motor Vehicle'
+    end
+) stored;
 
 
+--
 -- add some indexes
+--
 create index ems__incidents_crashes_crash_pk_index on public.ems__incidents (
     crash_pk
 );
@@ -131,7 +154,13 @@ create index ems__incidents_geometry_index on ems__incidents using gist (
 create index ems__incidents_patient_injry_sev_index on ems__incidents (
     patient_injry_sev
 );
+create index ems__incidents_crash_match_status_index on ems__incidents (
+    crash_match_status
+);
 create index crashes_case_id_index on crashes (case_id);
+create index crashes_is_temp_record_index on crashes (
+    is_temp_record
+);
 -- these indexes optimize our ST_Distance operations, which require geography types and are the fastes way to check point-to-point proximity
 create index idx_ems_geometry_geography on ems__incidents using gist (
     (geometry::geography)
@@ -140,19 +169,18 @@ create index idx_crashes_position_geography on crashes using gist (
     (position::geography)
 );
 
-
--- todo: do we want to ignore already-matched records?
--- todo: does performance improve after more ems records have been matched?
--- todo: don't fire trigger for temp records
-
+--
 -- Create or replace the trigger function for the crashes table
+--
 create or replace function update_crash_ems_match()
 returns trigger as $$
 DECLARE
     matching_ems RECORD;
     ems_record RECORD;
     match_count INTEGER;
+    matched_crash_ids INTEGER[];
     meters_threshold INTEGER := 1000;
+    time_threshold INTERVAL := '60 minutes';
 BEGIN
     -- Find all EMS records near the crash location + time
     FOR matching_ems IN (
@@ -163,71 +191,87 @@ BEGIN
         FROM 
             ems__incidents e
         WHERE
-            -- todo: match already ignored records: is this ok?
-            e.crash_pk IS NULL
-            AND e.incident_received_datetime  >= (NEW.crash_timestamp - INTERVAL '60 minutes')
-            AND e.incident_received_datetime  <= (NEW.crash_timestamp + INTERVAL '60 minutes')
+            -- ignore manual qa matches: these should not be modified
+            e.crash_match_status != 'matched_by_manual_qa'
+            AND e.incident_received_datetime  >= (NEW.crash_timestamp - time_threshold)
+            AND e.incident_received_datetime  <= (NEW.crash_timestamp + time_threshold)
             AND e.geometry IS NOT NULL
             AND NEW.position IS NOT NULL
-
             -- todo: try use_spheroid = false
             AND ST_DWithin(e.geometry::geography, NEW.position::geography, meters_threshold)
     ) LOOP
         -- Find all crashes which match this EMS record location + time
-        WITH matching_crashes AS (
-            SELECT c.id
-            FROM crashes c
-            WHERE 
-                c.is_deleted = false
-                and not c.is_temp_record
-                AND matching_ems.incident_received_datetime  >= (c.crash_timestamp - INTERVAL '60 minutes')
-                AND matching_ems.incident_received_datetime  <= (c.crash_timestamp + INTERVAL '60 minutes')
-                AND c.position IS NOT NULL
-                AND ST_DWithin(matching_ems.geometry::geography, c.position::geography, meters_threshold)
-        )
-        SELECT COUNT(*) INTO match_count FROM matching_crashes;
+        SELECT 
+            ARRAY(
+                SELECT c.id
+                FROM crashes c
+                WHERE 
+                    c.is_deleted = false
+                    and not c.is_temp_record
+                    AND matching_ems.incident_received_datetime  >= (c.crash_timestamp - time_threshold)
+                    AND matching_ems.incident_received_datetime  <= (c.crash_timestamp + time_threshold)
+                    AND c.position IS NOT NULL
+                    AND ST_DWithin(matching_ems.geometry::geography, c.position::geography, meters_threshold)
+            ) INTO matched_crash_ids;
+            
+        -- Get the count from the array length (handling when array_length is null as 0)
+        SELECT COALESCE(array_length(matched_crash_ids, 1), 0) INTO match_count;
         
-        IF match_count = 1 THEN
+        IF match_count = 0 THEN
+            UPDATE ems__incidents 
+            SET crash_pk = NULL,
+                crash_match_status = 'unmatched',
+                matched_crash_pks = NULL
+            WHERE id = matching_ems.id;
+        ELSIF match_count = 1 THEN
             -- this EMS record is only matched to one crash - we can assign the crash_pk
             UPDATE ems__incidents 
             SET crash_pk = NEW.id,
-                match_status = 'matched'
+                crash_match_status = 'matched_by_automation',
+                matched_crash_pks = NULL
             WHERE id = matching_ems.id;
         ELSE
             -- multiple matching crashes found - cannot assign crash_pk
             UPDATE ems__incidents 
-            SET match_status = 'multiple',
-                crash_pk = NULL
+            SET crash_match_status = 'multiple_matches_by_automation',
+                crash_pk = NULL,
+                matched_crash_pks = matched_crash_ids
             WHERE id = matching_ems.id;
         END IF;
     END LOOP;
 
-    -- handle previously matched EMS records which may need to be unlinked
-    -- todo: are we sure we want to remove matches?
     IF TG_OP = 'UPDATE' THEN
-        UPDATE ems__incidents 
+        UPDATE ems__incidents
         SET crash_pk = NULL,
-            match_status = 'unmatched'
+            crash_match_status = 'unmatched',
+            matched_crash_pks = NULL
         WHERE crash_pk = NEW.id
           AND (
-              incident_received_datetime  < (NEW.crash_timestamp - INTERVAL '60 minutes')
-              OR incident_received_datetime  > (NEW.crash_timestamp + INTERVAL '60 minutes')
+              incident_received_datetime  < (NEW.crash_timestamp - time_threshold)
+              OR incident_received_datetime  > (NEW.crash_timestamp + time_threshold)
               OR geometry IS NULL
               OR NEW.position IS NULL
               OR (NOT ST_DWithin(geometry::geography, NEW.position::geography, meters_threshold))
-          );
+          )
+          AND 
+            -- ignore manual qa matches: these should not be modified
+            crash_match_status != 'matched_by_manual_qa';
     END IF;
     RETURN NEW;
 END;
 $$ language plpgsql;
 
 
--- insert trigger
+--
+-- create insert trigger
+--
 create or replace trigger crash_insert_ems_match_trigger
 after insert on crashes
 for each row execute function update_crash_ems_match();
 
--- update trigger
+--
+-- create update trigger
+--
 create or replace trigger crash_update_ems_match_trigger
 after update on crashes
 for each row when (
@@ -236,12 +280,52 @@ for each row when (
     or (new.crash_timestamp is distinct from old.crash_timestamp)
 ) execute function update_crash_ems_match();
 
--- testing with all crashes since 2024-01-01
--- 300 meters and interval 60 minutes: 4230 matches with 88 multiple
--- 300 meters and interval 90 minutes: 4300 with 88 multiple
--- 1000 meters and interval 60 minutes: 5056 matches with 125 multiple
--- 1000 meters interval 90 minutes: 5156 and 141 multiple
 
+--
+-- remove apd_incident_number parsing from this trigger
+--
+CREATE OR REPLACE FUNCTION public.ems_incidents_trigger()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  NEW.geometry = ST_SetSRID(ST_MakePoint(NEW.longitude, NEW.latitude), 4326);
+
+  SELECT 
+    COALESCE(EXISTS (
+      SELECT 1 
+      FROM geo.jurisdictions 
+      WHERE jurisdiction_label = 'AUSTIN FULL PURPOSE'
+      AND ST_Contains(geometry, NEW.geometry)
+    ), FALSE),
+    locations.location_id
+  INTO 
+    NEW.austin_full_purpose,
+    NEW.location_id
+  FROM (SELECT 1) AS dummy
+  LEFT JOIN atd_txdot_locations locations ON (
+    locations.location_group = 1 
+    AND NEW.geometry && locations.geometry 
+    AND ST_Contains(locations.geometry, NEW.geometry)
+  );
+  RETURN NEW;
+END;
+$function$;
+
+-- 
+-- remove incident numbers from trigger condition
+--
+drop trigger if exists ems_incidents_trigger_update on ems__incidents;
+
+create trigger ems_incidents_trigger_update before update on ems__incidents for each row
+    when (old.latitude is distinct from new.latitude or old.longitude is distinct from new.longitude)
+    execute function ems_incidents_trigger();
+
+--
+-- drop these individual incident number columns. a record may contain dozens of apd_incident_numbers
+-- 
+alter table ems__incidents drop column apd_incident_number_1;
+alter table ems__incidents drop column apd_incident_number_2;
 
 --
 -- drop redundant ems_id column and rebuild people_list_view
