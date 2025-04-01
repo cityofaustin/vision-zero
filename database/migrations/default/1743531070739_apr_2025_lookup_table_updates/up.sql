fa61275b998fb0ade5c740a1a8cdf75ff0755ec6
John Clary <john.clary@austintexas.gov>
2025-04-01 14:13:30 -0400

    add new agencies to lookup - generated by helper script

---
+++ database/migrations/default/1743531070739_apr_2025_lookup_table_updates/up.sql
@@ -0,0 +1,7 @@
+insert into lookups.agency (id, label, source) values (3372, 'WISE COUNTY SHERIFF''S OFFICE', 'cris');
+insert into lookups.agency (id, label, source) values (3373, 'MONAHANS WICKETT PYOTE ISD POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3374, 'EANES INDEPENDENT SCHOOL DISTRICT POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3375, 'FANNIN COUNTY CONSTABLE PRECINCT 3', 'cris');
+insert into lookups.agency (id, label, source) values (3376, 'CAPMETRO POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3377, 'UHLAND POLICE DEPARTMENT', 'cris');
+insert into lookups.agency (id, label, source) values (3378, 'NEWMAN INTERNATIONAL ACADEMY DEPARTMENT OF PUBLIC SAFETY', 'cris');
