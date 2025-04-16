dea6d4d41a6a11142c72e2410533d6d0aa9de23b
John Clary <john.clary@austintexas.gov>
2025-04-16 13:53:59 -0400

    Incorporate agency ID and add audit timestamps - Using investigat_agency_id in case_id soft     delete - Update crashes case_id index to use     investigat_agency_id - Add created_at and updated_at fields

--- database/migrations/default/1744126149550_noncr3_soft_del_dedupe/down.sql
+++ database/migrations/default/1744126149550_noncr3_soft_del_dedupe/down.sql
@@ -2,5 +2,7 @@ drop trigger if exists remove_dupe_non_cr3s_insert_update_trigger on atd_apd_blu
 drop function if exists remove_dupe_non_cr3s;
 
 alter table ems__incidents drop column atd_apd_blueform_case_id;
-alter table atd_apd_blueform drop column is_deleted;
-drop index crashes_case_id_idx;
+alter table atd_apd_blueform drop column is_deleted, drop column created_at, drop column updated_at;
+create trigger set_public_atd_apd_blueform_updated_at before update on public.atd_apd_blueform for each row execute function public.set_current_timestamp_updated_at();
+
+drop index crashes_case_id_investigat_agency_id_idx;
--- database/migrations/default/1744126149550_noncr3_soft_del_dedupe/up.sql
+++ database/migrations/default/1744126149550_noncr3_soft_del_dedupe/up.sql
@@ -13,6 +13,14 @@ references public.atd_apd_blueform (
 comment on column ems__incidents.atd_apd_blueform_case_id is 'The non-CR3 case ID matched to this record';
 
 --
+-- add timestamp audit fields 
+--
+alter table atd_apd_blueform add column created_at timestamptz not null default now();
+alter table atd_apd_blueform add column updated_at timestamptz not null default now();
+create trigger set_public_atd_apd_blueform_updated_at before update on public.atd_apd_blueform for each row execute function public.set_current_timestamp_updated_at();
+
+
+--
 -- add soft delete column
 --
 alter table atd_apd_blueform add is_deleted boolean not null default false;
@@ -30,13 +38,13 @@ create index atd_apd_blueform_is_deleted_index on public.atd_apd_blueform (
     is_deleted
 );
 
-create index crashes_case_id_idx on public.crashes (
-    case_id
+create index crashes_case_id_investigat_agency_id_idx on public.crashes (
+    case_id, investigat_agency_id
 );
 
 --
 -- Trigger which soft-deletes atd_apd_blueform records if there is a cr3 crash
--- with the same case_id
+-- with the same case_id and investigat_agency_id is 74 (Austin PD)
 --
 create or replace function remove_dupe_non_cr3s()
 returns trigger as $$
@@ -44,7 +52,7 @@ DECLARE
     text_case_id text;
 BEGIN
     text_case_id := NEW.case_id::text;
-    if exists (select 1 from crashes where case_id = text_case_id) then
+    if exists (select 1 from crashes where case_id = text_case_id and investigat_agency_id = 74) then
         new.is_deleted = true;
     end if;
     return new;
