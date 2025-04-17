1870c44eea3b4d25747ca0e966798771b9add561
John Clary <john.clary@austintexas.gov>
2025-04-17 15:23:01 -0400

    wip trigger to set person id

--- database/migrations/default/1744826895218_ems_remove_match_status_constraint/up.sql
+++ database/migrations/default/1744826895218_ems_remove_match_status_constraint/up.sql
@@ -1,7 +1,51 @@
-
 alter table ems__incidents
 drop constraint matched_crash_pks_non_null,
 add column created_by text not null default 'system',
 add column updated_by text not null default 'system';
 
 comment on column ems__incidents.matched_crash_pks is 'The IDs of multiple crashes that were found to match this record. Set via trigger.''';
+
+--
+-- Trigger function that sets the ems__incident `crash_pk` based on 
+-- on the 
+--
+create or replace function update_person_crash_ems_match()
+returns trigger as $$
+DECLARE
+    matching_person_record RECORD;
+BEGIN
+    IF NEW.person_id is null
+        THEN return NEW;
+    END IF;
+    --
+    -- Use SELECT INTO to assign query results to a variable
+    --
+    SELECT id, crash_pk INTO matching_person_record 
+    FROM people_list_view 
+    WHERE id = NEW.person_id;
+    
+    NEW.crash_pk = matching_person_record.crash_pk;
+    
+    --
+    -- Update related EMS records which have not already been matched to a crash
+    --
+    UPDATE ems__incidents 
+    SET 
+        crash_pk = NEW.crash_pk,
+        updated_by = NEW.updated_by,
+        crash_match_status = 'matched_by_manual_qa'
+    WHERE
+        incident_number = NEW.incident_number
+        AND crash_pk != NEW.crash_pk
+        AND person_id IS NULL;
+
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+
+create or replace trigger ems_update_person_match_trigger
+after update on ems__incidents
+for each row 
+when (old.person_id is distinct from new.person_id)
+execute function update_person_crash_ems_match();
