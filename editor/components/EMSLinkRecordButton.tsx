baaefadf2f2331a97247a7d840f162ddf009475c
rose <rose.eichelmann@gmail.com>
2025-05-05 17:09:07 -0500

    use mutation to update crash match status

--- editor/components/EMSLinkRecordButton.tsx
+++ editor/components/EMSLinkRecordButton.tsx
@@ -5,6 +5,7 @@ import { RowActionComponentProps } from "@/components/RelatedRecordTable";
 import { FaLink } from "react-icons/fa6";
 import PermissionsRequired from "@/components/PermissionsRequired";
 import AlignedLabel from "@/components/AlignedLabel";
+import { useMutation } from "@/utils/graphql";
 
 const allowedLinkRecordRoles = ["vz-admin", "editor"];
 
@@ -15,13 +16,15 @@ export interface EMSLinkRecordButtonProps extends Record<string, unknown> {
 
 const EMSLinkRecordButton: React.FC<
   RowActionComponentProps<EMSPatientCareRecord, EMSLinkRecordButtonProps>
-> = ({ record, additionalProps }) => {
+> = ({ record, additionalProps, mutation, onSaveCallback }) => {
   const isLinkingInProgress = !!additionalProps?.selectedEmsPcr;
   const isLinkingThisRecord =
     additionalProps?.selectedEmsPcr &&
     record.id === additionalProps?.selectedEmsPcr.id;
 
   const isRecordAlreadyLinked = !!record.person_id;
+
+  const { mutate: setRecordManualUnmatched } = useMutation(mutation);
 
   if (isRecordAlreadyLinked) {
     return null;
@@ -51,7 +54,19 @@ const EMSLinkRecordButton: React.FC<
             <FaEllipsisVertical />
           </Dropdown.Toggle>
           <Dropdown.Menu popperConfig={{ strategy: "fixed" }}>
-            <Dropdown.Item>Set as unmatched</Dropdown.Item>
+            <Dropdown.Item
+              onClick={async () => {
+                await setRecordManualUnmatched({
+                  id: record.id,
+                  updates: {
+                    crash_match_status: "unmatched_by_manual_qa",
+                  },
+                });
+                await onSaveCallback();
+              }}
+            >
+              Set as unmatched
+            </Dropdown.Item>
             <Dropdown.Item>Reset</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
