0a6ed628f0456afd6b49b3d8adafc94211b1282d
rose <rose.eichelmann@gmail.com>
2025-06-25 18:14:37 -0500

    add noncr3 columns to config

---
+++ editor/configs/nonCR3Columns.ts
@@ -0,0 +1,23 @@
+export const nonCR3Columns = {
+  address: {
+    path: "address",
+    label: "Address",
+    sortable: true,
+  },
+  case_timestamp: {
+    path: "case_timestamp",
+    label: "Crash date",
+    sortable: true,
+  },
+  case_id: {
+    path: "case_id",
+    label: "Case ID",
+    sortable: true,
+  },
+};
+
+export const emsNonCR3Columns = [
+  nonCR3Columns.address,
+  nonCR3Columns.case_timestamp,
+  nonCR3Columns.case_id,
+];
