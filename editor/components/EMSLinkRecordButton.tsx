0cda03d05ac9b03ddcdfeadaca5b308e4b1cb833
John Clary <john.clary@austintexas.gov>
2025-07-01 09:48:46 -0400

    wip match-event-based approach

--- editor/components/EMSLinkRecordButton.tsx
+++ editor/components/EMSLinkRecordButton.tsx
@@ -6,7 +6,6 @@ import { FaLink } from "react-icons/fa6";
 import PermissionsRequired from "@/components/PermissionsRequired";
 import AlignedLabel from "@/components/AlignedLabel";
 import { useMutation } from "@/utils/graphql";
-import { useMemo } from "react";
 
 const allowedLinkRecordRoles = ["vz-admin", "editor"];
 
@@ -28,34 +27,6 @@ const EMSLinkRecordButton: React.FC<
   const isLinkingThisRecord =
     additionalProps?.selectedEmsPcr &&
     record.id === additionalProps?.selectedEmsPcr.id;
-
-  /**
-   * Hook that gets the variables to be used in the reset button mutation
-   */
-  const resetButtonUpdates = useMemo(() => {
-    let updates = {};
-    if (record.matched_crash_pks === null) {
-      updates = {
-        crash_pk: null,
-        person_id: null,
-        crash_match_status: "unmatched",
-        person_match_status: "unmatched"
-      };
-    } else if (record.matched_crash_pks.length === 1) {
-      updates = {
-        crash_pk: record.matched_crash_pks[0],
-        person_id: null,
-        crash_match_status: "matched_by_automation",
-      };
-    } else if (record.matched_crash_pks.length > 1) {
-      updates = {
-        crash_pk: null,
-        person_id: null,
-        crash_match_status: "multiple_matches_by_automation",
-      };
-    }
-    return updates;
-  }, [record]);
 
   const { mutate: updateEMSRecord } = useMutation(mutation);
 
@@ -95,7 +66,8 @@ const EMSLinkRecordButton: React.FC<
                   await updateEMSRecord({
                     id: record.id,
                     updates: {
-                      crash_match_status: "unmatched_by_manual_qa",
+                      // TODO: implement this
+                      _match_event_name: "unmatched_by_manual_qa",
                       crash_pk: null,
                       person_id: null,
                     },
@@ -109,7 +81,9 @@ const EMSLinkRecordButton: React.FC<
                 onClick={async () => {
                   await updateEMSRecord({
                     id: record.id,
-                    updates: resetButtonUpdates,
+                    updates: {
+                      _match_event_name: "reset_crash_match",
+                    },
                   });
                   await onSaveCallback();
                 }}
