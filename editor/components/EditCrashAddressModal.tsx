f95f9a629551a35a7f07e3e53ae4c27d5ddfd49e
rose <rose.eichelmann@gmail.com>
2025-08-07 17:04:04 -0500

    add form Column props so that typescript doesnt get mad, add swap address button w functionality

--- editor/components/EditCrashAddressModal.tsx
+++ editor/components/EditCrashAddressModal.tsx
@@ -1,17 +1,26 @@
-import Modal from "react-bootstrap/Modal";
-import { Form, Col, Row } from "react-bootstrap";
+import { Dispatch, SetStateAction, useMemo } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { LookupTableOption } from "@/types/relationships";
-import { UPDATE_CRASH } from "@/queries/crash";
-import { useMutation } from "@/utils/graphql";
-import { crashDataCards } from "@/configs/crashDataCard";
-import { useLookupQuery, useQuery } from "@/utils/graphql";
-import { Crash } from "@/types/crashes";
-import { useMemo, Dispatch, SetStateAction } from "react";
+import Modal from "react-bootstrap/Modal";
+import { SubmitHandler, useForm } from "react-hook-form";
 import CrashSwapAddressButton from "@/components/CrashSwapAddressButton";
+import { crashesColumns } from "@/configs/crashesColumns";
+import { UPDATE_CRASH } from "@/queries/crash";
+import { Crash } from "@/types/crashes";
+import { LookupTableOption, Relationship } from "@/types/relationships";
+import { useLookupQuery, useMutation, useQuery } from "@/utils/graphql";
 
-// import Button from "react-bootstrap/Button";
+interface FormColumnProps {
+  /**
+   * The props from col are pulled from the crashesColumns config
+   */
+  col: {
+    path: keyof AddressFormInputs; // Ensures the path matches one of the AddressInputs keys
+    label: string;
+    inputType: "text" | "number" | "select";
+    relationship?: Relationship<Record<string, unknown>>; // Optional since it's conditionally used
+  };
+}
 
 interface EditCrashAddressModalProps {
   /**
@@ -25,13 +34,13 @@ interface EditCrashAddressModalProps {
    */
   onSaveCallback: () => void;
   /**
-  //  * If the modal should be visible or hidden
-  //  */
+   * If the modal should be visible or hidden
+   */
   show: boolean;
   crash: Crash;
 }
 
-type AddressInputs = {
+export type AddressFormInputs = {
   rpt_block_num: string | null;
   rpt_street_pfx: string | null;
   rpt_street_name: string | null;
@@ -48,11 +57,10 @@ type AddressInputs = {
   rpt_sec_road_part_id: number | null;
   rpt_sec_rdwy_sys_id: number | null;
   rpt_sec_hwy_num: string | null;
-  updated_by: string;
 };
 
 /**
- * Modal form component used for creating or editing a user
+ * Modal form component used for editing the crash address fields
  */
 export default function EditCrashAddressModal({
   setShowEditAddressModal,
@@ -84,16 +92,38 @@ export default function EditCrashAddressModal({
     );
   }, [crash]);
 
-  const { register, handleSubmit, reset } = useForm<AddressInputs>({
-    defaultValues: defaultValues,
-  });
+  const { register, handleSubmit, reset, getValues, setValue } =
+    useForm<AddressFormInputs>({
+      defaultValues: defaultValues,
+    });
 
   const { mutate } = useMutation(UPDATE_CRASH);
 
-  const columns = crashDataCards.address;
-  const secondaryColumns = crashDataCards.address_secondary;
+  const primaryColumns = [
+    crashesColumns.rpt_block_num,
+    crashesColumns.rpt_street_pfx,
+    crashesColumns.rpt_street_name,
+    crashesColumns.rpt_street_sfx,
+    crashesColumns.rpt_street_desc,
+    crashesColumns.rpt_road_part_id,
+    crashesColumns.rpt_rdwy_sys_id,
+    crashesColumns.rpt_hwy_num,
+  ];
+  const secondaryColumns = [
+    crashesColumns.rpt_sec_block_num,
+    crashesColumns.rpt_sec_street_pfx,
+    crashesColumns.rpt_sec_street_name,
+    crashesColumns.rpt_sec_street_sfx,
+    crashesColumns.rpt_sec_street_desc,
+    crashesColumns.rpt_sec_road_part_id,
+    crashesColumns.rpt_sec_rdwy_sys_id,
+    crashesColumns.rpt_sec_hwy_num,
+  ];
 
-  const onSubmit: SubmitHandler<AddressInputs> = async (data) => {
+  /**
+   * Submits mutation to database on save button click
+   */
+  const onSave: SubmitHandler<AddressFormInputs> = async (data) => {
     await mutate({
       id: crashId,
       updates: data,
@@ -102,7 +132,11 @@ export default function EditCrashAddressModal({
     onSaveCallback();
   };
 
-  const FormField = ({ col }) => {
+  /**
+   * Component that renders the form field based on the input type
+   * of the column, such as text or select
+   */
+  const FormField = ({ col }: FormColumnProps) => {
     const [query, typename] = useLookupQuery(
       col?.relationship ? col.relationship : undefined
     );
@@ -154,14 +188,14 @@ export default function EditCrashAddressModal({
     >
       <Modal.Header>
         <Modal.Title className="me-2">Edit crash address</Modal.Title>
-        <CrashSwapAddressButton />
+        <CrashSwapAddressButton getValues={getValues} setValue={setValue} />
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={handleSubmit(onSubmit)} id="addressForm">
+        <Form onSubmit={handleSubmit(onSave)} id="addressForm">
           <Row>
             <Col className="">
               <div className="fs-5 mb-2">Primary address</div>
-              {columns.map((col) => {
+              {primaryColumns.map((col) => {
                 return (
                   <Form.Group className="mb-3" key={String(col.path)}>
                     <Form.Label>{col.label}</Form.Label>
