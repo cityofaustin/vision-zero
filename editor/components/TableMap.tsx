c3f4e238a76b5f59a5b6bd809206730dd45d5c18
John Clary <john.clary@austintexas.gov>
2025-08-11 07:56:22 -0400

    add bbox control and effects

--- editor/components/TableMap.tsx
+++ editor/components/TableMap.tsx
@@ -1,4 +1,4 @@
-import { useCallback, MutableRefObject } from "react";
+import { useCallback, MutableRefObject, useMemo, useEffect } from "react";
 import MapGL, {
   FullscreenControl,
   NavigationControl,
@@ -6,12 +6,19 @@ import MapGL, {
   MapRef,
   Source,
   Layer,
+  LayerProps,
 } from "react-map-gl";
-import MapGeocoderControl from "@/components/MapGeocoderControl";
+import Button from "react-bootstrap/Button";
+import { FaHome } from "react-icons/fa";
+import AlignedLabel from "@/components/AlignedLabel";
+import { bbox } from "@turf/bbox";
 import { DEFAULT_MAP_PAN_ZOOM, DEFAULT_MAP_PARAMS } from "@/configs/map";
-import "mapbox-gl/dist/mapbox-gl.css";
 import { FeatureCollection } from "geojson";
 import { TableMapConfig } from "@/types/tableMapConfig";
+
+import "mapbox-gl/dist/mapbox-gl.css";
+import { LngLatBoundsLike } from "mapbox-gl";
+import { FaExpand } from "react-icons/fa6";
 
 export interface LatLon {
   latitude: number;
@@ -36,27 +43,93 @@ interface TableMapProps {
 }
 
 /**
+ * Custom control that fits map to current bounds
+ */
+function HomeControl({
+  mapRef,
+  bounds,
+}: {
+  mapRef: MutableRefObject<MapRef | null>;
+  bounds: LngLatBoundsLike | undefined;
+}) {
+  return (
+    <Button
+      size="lg"
+      className="m-2 px-2 rounded"
+      variant="primary"
+      style={{
+        position: "absolute",
+        cursor: "pointer",
+      }}
+      onClick={() => {
+        if (bounds) {
+          mapRef?.current?.fitBounds(bounds, { padding: 10 });
+        }
+      }}
+    >
+      <AlignedLabel>
+        <FaHome className="fs-4" />
+      </AlignedLabel>
+    </Button>
+  );
+}
+
+/**
+ * Hook which computes the bounding box of the provided geojson
+ *
+ * Returns undefined if the geojson has no features
+ */
+const useCurrentBounds = (
+  geojson: FeatureCollection
+): LngLatBoundsLike | undefined =>
+  useMemo(() => {
+    if (!geojson.features.length) {
+      return undefined;
+    }
+    const bounds = bbox(geojson);
+
+    return [
+      [bounds[0], bounds[1]],
+      [bounds[2], bounds[3]],
+    ];
+  }, [geojson]);
+
+/**
  * Map component which renders an editable point marker
  */
 export const TableMap = ({ mapRef, geojson, mapConfig }: TableMapProps) => {
-  const onDragEnd = useCallback((e: ViewStateChangeEvent) => {
-    // todo: something?
-    console.log("drag end", e);
+  const geojsonBounds = useCurrentBounds(geojson);
+
+  /**
+   * Initialize map based on initial geojson bounds
+   */
+  const initialViewState = useMemo(() => {
+    if (geojsonBounds) {
+      return { bounds: geojsonBounds };
+    }
+    // Fallback when bounds are not available
+    return {
+      latitude: DEFAULT_MAP_PAN_ZOOM.latitude,
+      longitude: DEFAULT_MAP_PAN_ZOOM.longitude,
+      zoom: 9,
+    };
   }, []);
+
+  useEffect(() => {
+    if (geojsonBounds) {
+      // update map bounds fit geojson bounds
+      mapRef?.current?.fitBounds(geojsonBounds, { padding: 10 });
+    }
+  }, [geojsonBounds]);
 
   return (
     <MapGL
       ref={mapRef}
-      initialViewState={{
-        latitude: DEFAULT_MAP_PAN_ZOOM.latitude,
-        longitude: DEFAULT_MAP_PAN_ZOOM.longitude,
-        zoom: DEFAULT_MAP_PAN_ZOOM.zoom - 5,
-      }}
+      initialViewState={initialViewState}
       {...DEFAULT_MAP_PARAMS}
       cooperativeGestures={true}
       // Resize the map canvas when parent row expands to fit crash
       onLoad={(e) => e.target.resize()}
-      onDragEnd={onDragEnd}
       maxZoom={21}
     >
       <FullscreenControl position="bottom-right" />
@@ -67,8 +140,7 @@ export const TableMap = ({ mapRef, geojson, mapConfig }: TableMapProps) => {
       <Source id="custom-source" type="geojson" data={geojson}>
         <Layer id="custom-layer" type="circle" {...mapConfig?.layerProps} />
       </Source>
-
-      <MapGeocoderControl position="top-left" />
+      <HomeControl mapRef={mapRef} bounds={geojsonBounds} />
     </MapGL>
   );
 };
