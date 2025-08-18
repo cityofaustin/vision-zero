22127685622d607d0bc1f4d782f15b9d3cd64d5e
John Clary <john.clary@austintexas.gov>
2025-08-18 13:17:40 -0400

    undo change to localstoragekey name

--- editor/app/ems/[incident_number]/page.tsx
+++ editor/app/ems/[incident_number]/page.tsx
@@ -265,7 +265,7 @@ export default function EMSDetailsPage({
             rowActionComponentAdditionalProps={linkRecordButtonProps}
             rowActionMutation={UPDATE_EMS_PCR}
             shouldShowColumnVisibilityPicker={true}
-            localStorageKey="emsIncidentDetailsEmsPatients"
+            localStorageKey="emsIncidentDetailsPatients"
           />
         </Col>
       </Row>
