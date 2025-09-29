529a6731f473af40516c742c5350caaf7506fe2a
chiaberry <analuciaby@gmail.com>
2025-09-29 13:24:45 -0500

    straight up copy the other mapconfig

--- editor/configs/locationCrashesTable.ts
+++ editor/configs/locationCrashesTable.ts
@@ -73,4 +73,38 @@ export const locationCrashesQueryConfig: QueryConfig = {
     }),
   },
   filterCards: locationCrashesFiltercards,
+  mapConfig: {
+    isActive: false,
+    layerProps: {
+      id: "points-layer",
+      type: "circle",
+      paint: {
+        "circle-radius": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          // zoom is 5 (or less)
+          5,
+          2,
+          // zoom is 20 (or greater)
+          20,
+          10,
+        ],
+        "circle-color": "#1276d1",
+        "circle-stroke-width": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          // zoom is 5 (or less)
+          5,
+          1,
+          // zoom is 20 (or greater)
+          20,
+          3,
+        ],
+        "circle-stroke-color": "#fff",
+      },
+    },
+    geojsonTransformerName: "latLon",
+  },
 };
