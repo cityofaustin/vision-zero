a75555f26a3aea74f33f4c349fd155dbc1ee1cd4
rose <rose.eichelmann@gmail.com>
2025-10-09 13:38:03 -0500

    add datacards column config

---
+++ editor/configs/fatalCrashDataCard.ts
@@ -0,0 +1,7 @@
+import { fatalCrashColumns } from "@/configs/fatalCrashColumns";
+
+export const fatalCrashDataCard = [
+  fatalCrashColumns.crash_timestamp,
+  fatalCrashColumns.collsn,
+  fatalCrashColumns.is_coa_roadway,
+];
