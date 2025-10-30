b1dc7bef2183b2d985c6e985711359e623f68a16
rose <rose.eichelmann@gmail.com>
2025-10-30 14:20:18 -0500

    delete these files

--- editor/configs/fatalCrashDataCard.ts
+++
@@ -1,7 +0,0 @@
-import { fatalCrashColumns } from "@/configs/fatalCrashColumns";
-
-export const fatalCrashDataCard = [
-  fatalCrashColumns.crash_timestamp,
-  fatalCrashColumns.collsn,
-  fatalCrashColumns.is_coa_roadway,
-];
