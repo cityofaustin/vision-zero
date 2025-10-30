8847eabf78739c80781e6cd4c13915ce8e649ed3
rose <rose.eichelmann@gmail.com>
2025-10-30 14:18:45 -0500

    completely custom component w fields and map

--- editor/app/fatalities/[record_locator]/page.tsx
+++ editor/app/fatalities/[record_locator]/page.tsx
@@ -8,13 +8,12 @@ import { useDocumentTitle } from "@/utils/documentTitle";
 import { formatAddresses } from "@/utils/formatters";
 import { formatYear } from "@/utils/formatters";
 import { Row, Col } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
 import { notFound } from "next/navigation";
 import { Card } from "react-bootstrap";
-import DataCard from "@/components/DataCard";
-import { fatalCrashDataCard } from "@/configs/fatalCrashDataCard";
 import { MapRef } from "react-map-gl";
-import { useResizeObserver } from "@/utils/map";
-import FatalCrashDetailsMap from "@/components/FatalCrashDetailsMap";
+import { formatIsoDateTimeWithDay } from "@/utils/formatters";
+import { PointMap } from "@/components/PointMap";
 
 export default function FatalCrashDetailsPage({
   params,
@@ -25,14 +24,6 @@ export default function FatalCrashDetailsPage({
   const recordLocator = params.record_locator;
 
   const typename = "crashes";
-
-  /**
-   * Trigger resize() when the map container size changes - this ensures that
-   * the map repaints when the sidebar is collapsed/expanded.
-   */
-  const mapContainerRef = useResizeObserver<HTMLDivElement>(() => {
-    mapRef.current?.resize();
-  });
 
   const { data, error } = useQuery<Crash>({
     query: recordLocator ? GET_CRASH : null,
@@ -81,12 +72,48 @@ export default function FatalCrashDetailsPage({
       </Row>
       <Row>
         <Col>
-          <Card ref={mapContainerRef}>
-            <DataCard
-              record={data[0]}
-              columns={fatalCrashDataCard}
-              footerComponent={FatalCrashDetailsMap}
-            ></DataCard>
+          <Card>
+            <Card.Body>
+              <Table>
+                <tbody>
+                  <tr>
+                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                      Date
+                    </td>
+                    <td>{formatIsoDateTimeWithDay(crash.crash_timestamp)}</td>
+                  </tr>
+                  <tr>
+                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                      Units involved
+                    </td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                      Collision type
+                    </td>
+                    <td>{crash.collsn?.label}</td>
+                  </tr>
+                  <tr>
+                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                      Roadway owner
+                    </td>
+                    <td>
+                      {crash.is_coa_roadway === true
+                        ? "City of Austin"
+                        : "TxDOT"}
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <Row className="p-1 crash-header-card-body">
+                <PointMap
+                  savedLatitude={crash.latitude}
+                  savedLongitude={crash.longitude}
+                  mapRef={mapRef}
+                />
+              </Row>
+            </Card.Body>
           </Card>
         </Col>
         <Col></Col>
