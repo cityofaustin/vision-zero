844ca8cccc74316a2a8919fc7ad7b72b51b30da7
John Clary <john.clary@austintexas.gov>
2025-10-30 11:38:36 -0400

    add a column comment

--- database/migrations/default/1761674927826_person_match_not_found_by_auto/up.sql
+++ database/migrations/default/1761674927826_person_match_not_found_by_auto/up.sql
@@ -14,3 +14,5 @@ ADD CONSTRAINT ems__incidents_person_match_status_check CHECK (
 );
 
 ALTER TABLE ems__incidents ADD COLUMN person_match_attributes text[];
+
+COMMENT ON COLUMN public.ems__incidents.person_match_attributes IS 'The matching attributes between this record and the linked person record, as assigned by the person matching ETL.';
