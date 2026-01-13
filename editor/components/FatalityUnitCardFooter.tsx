821d4a6e920d9dba550018bc6ae61fca0ba3d379
rose <rose.eichelmann@gmail.com>
2026-01-13 16:25:00 -0600

    add a sep card footer component to reduce code in main component

---
+++ editor/components/FatalityUnitCardFooter.tsx
@@ -0,0 +1,58 @@
+import { Card } from "react-bootstrap";
+import { LookupTableOption } from "@/types/relationships";
+import { Charge } from "@/types/charge";
+
+interface FatalityUnitCardFooterProps {
+  primaryContribFactors: (LookupTableOption | null)[];
+  possibleContribFactors: (LookupTableOption | null)[];
+  unitCharges: Charge[] | undefined;
+  hasCharges: boolean | undefined;
+  hasVictim: boolean | undefined;
+  hasContribFactors: boolean;
+}
+
+export default function FatalityUnitCardFooter({
+  hasCharges,
+  hasVictim,
+  hasContribFactors,
+  unitCharges,
+  primaryContribFactors,
+  possibleContribFactors,
+}: FatalityUnitCardFooterProps) {
+  return (
+    <Card.Footer
+      // If theres no card body remove extra padding
+      className={
+        "fatality-units-card-header-footer " + (!hasVictim ? "pt-0" : "")
+      }
+    >
+      {hasCharges && (
+        <div className="pb-1">
+          <div className="fw-bold">Charges</div>
+          {unitCharges?.map((charge) => (
+            <div className="ms-2" key={charge.id}>
+              {charge.charge}
+            </div>
+          ))}
+        </div>
+      )}
+      {hasContribFactors && (
+        <div>
+          <div className="fw-bold">Contributing factors</div>
+          {primaryContribFactors.map((factor) => (
+            <div className="ms-2" key={factor?.id}>
+              <span className="fw-bold">Primary: </span>
+              <span>{factor?.label}</span>
+            </div>
+          ))}
+          {possibleContribFactors.map((factor) => (
+            <div className="ms-2" key={factor?.id}>
+              <span className="fw-bold">Possible: </span>
+              <span>{factor?.label}</span>
+            </div>
+          ))}
+        </div>
+      )}
+    </Card.Footer>
+  );
+}
