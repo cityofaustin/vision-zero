54937f62aa4b386dbab7b52b4a0c18e092e2fead
John Clary <john.clary@austintexas.gov>
2026-02-06 15:40:21 -0500

    update a comment

--- database/migrations/default/1770404957914_crash_diagram_metadata/up.sql
+++ database/migrations/default/1770404957914_crash_diagram_metadata/up.sql
@@ -3,5 +3,5 @@ alter table crashes add column diagram_s3_object_key text;
 -- todo: this needs to be a backfill op
 -- update crashes set
 --     diagram_s3_object_key
---     = 'prod/cr3s/crash_diagrams/' || crashes.id::text || '.jpg'
+--     = 'prod/cr3s/crash_diagrams/' || crashes.record_locator::text || '.jpeg'
 -- where cr3_stored_fl is true;
