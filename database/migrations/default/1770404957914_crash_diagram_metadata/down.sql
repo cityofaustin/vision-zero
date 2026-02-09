512edd7956875a5228463aec4f925c6be37723a5
John Clary <john.clary@austintexas.gov>
2026-02-09 13:07:16 -0500

    dont use the _cris schema

--- database/migrations/default/1770404957914_crash_diagram_metadata/down.sql
+++ database/migrations/default/1770404957914_crash_diagram_metadata/down.sql
@@ -1,2 +1,1 @@
-alter table crashes_cris drop column diagram_s3_object_key;
 alter table crashes drop column diagram_s3_object_key;
--- database/migrations/default/1770404957914_crash_diagram_metadata/up.sql
+++ database/migrations/default/1770404957914_crash_diagram_metadata/up.sql
@@ -1,8 +1,1 @@
-alter table crashes_cris add column diagram_s3_object_key text;
 alter table crashes add column diagram_s3_object_key text;
-
--- todo: this needs to be a backfill op
--- update crashes set
---     diagram_s3_object_key
---     = 'prod/cr3s/crash_diagrams/' || crashes.record_locator::text || '.jpeg'
--- where cr3_stored_fl is true;
