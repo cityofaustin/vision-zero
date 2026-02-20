305365a098bbb106844b63c71aaca8ba058026fe
John Clary <john.clary@austintexas.gov>
2026-02-20 12:44:56 -0500

    wip

--- editor/configs/locationsListViewColumns.tsx
+++ editor/configs/locationsListViewColumns.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { ColDataCardDef } from "@/types/types";
 import { LocationsListRow } from "@/types/locationsList";
+import { formatArrayToString } from "@/utils/formatters";
 
 export const locationsListViewColumns: ColDataCardDef<LocationsListRow>[] = [
   {
@@ -39,21 +40,25 @@ export const locationsListViewColumns: ColDataCardDef<LocationsListRow>[] = [
     label: "APD sector(s)",
     sortable: false,
     defaultHidden: true,
+    valueFormatter: formatArrayToString,
   },
   {
     path: "area_eng_areas",
     label: "Area engineer",
     sortable: false,
+    valueFormatter: formatArrayToString,
   },
   {
     path: "signal_eng_areas",
     label: "Signal engineer",
     sortable: false,
+    valueFormatter: formatArrayToString,
   },
   {
     path: "council_districts",
     label: "Council district(s)",
     sortable: false,
+    valueFormatter: formatArrayToString,
   },
   {
     path: "is_hin",
@@ -78,5 +83,7 @@ export const locationsListViewColumns: ColDataCardDef<LocationsListRow>[] = [
     label: "Street levels",
     sortable: false,
     defaultHidden: true,
+    valueFormatter: formatArrayToString,
   },
+  // todo: move these columns to locationColumns and import them here? and show them on the location details page
 ];
