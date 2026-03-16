36cf1194293d49eca508fee7b00bd75e5d2dc294
John Clary <john.clary@austintexas.gov>
2026-03-16 17:30:08 -0400

    add end padding when readonly

--- editor/components/CrashHeader.tsx
+++ editor/components/CrashHeader.tsx
@@ -32,7 +32,7 @@ export default function CrashHeader({ crash, refetch }: CrashHeaderProps) {
     <Row className="d-flex justify-content-between align-items-center mb-3">
       <Col className="d-flex align-items-center">
         {isReadOnlyUser ? (
-          <span className="fs-3 fw-bold text-uppercase">
+          <span className="fs-3 fw-bold text-uppercase me-2">
             {crash.address_display}
           </span>
         ) : (
