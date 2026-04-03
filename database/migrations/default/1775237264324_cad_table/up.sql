d3be8c1bb913a70a2ddf0155243d9488df1275ce
John Clary <john.clary@austintexas.gov>
2026-04-03 16:12:26 -0400

    add location_id and in_austin_full_purpose

--- database/migrations/default/1775237264324_cad_table/up.sql
+++ database/migrations/default/1775237264324_cad_table/up.sql
@@ -1,5 +1,9 @@
 CREATE TABLE cad_incidents (
     id serial PRIMARY KEY,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL,
+    created_by text DEFAULT 'system'::text NOT NULL,
+    updated_by text DEFAULT 'system'::text NOT NULL,
     address text,
     agency_type text,
     call_disposition text,
@@ -16,23 +20,64 @@ CREATE TABLE cad_incidents (
     latitude numeric,
     longitude numeric,
     geom geometry(Point, 4326),
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    updated_at timestamp with time zone DEFAULT now() NOT NULL,
-    created_by text DEFAULT 'system'::text NOT NULL,
-    updated_by text DEFAULT 'system'::text NOT NULL
+    location_id text,
+    in_austin_full_purpose boolean
 );
+
+ALTER TABLE cad_incidents
+    ADD CONSTRAINT cad_incidents_location_id_fkey FOREIGN KEY (location_id) REFERENCES public.locations(location_id) ON UPDATE CASCADE ON DELETE SET NULL;
+
 
 comment on table cad_incidents is 'This dataset contains information on both 911 calls (usually referred to as Calls for Service or Dispatched Incidents) and officer-initiated incidents related to traffic crashes as recorded in the Austin public safety Computer Aided Dispatch (CAD) system. Data is provided by the public safety enterprise data team after approval by AFD, ATCEMS, and APD';
 
 comment on column cad_incidents.address is 'The incident address';
 
 
-CREATE OR REPLACE FUNCTION cad_incidents_set_spatial_attributes()
-RETURNS trigger AS $$
+CREATE
+OR REPLACE FUNCTION cad_incidents_set_spatial_attributes () RETURNS trigger AS $$
+DECLARE
+    v_location_id text;
 BEGIN
+    --
+    -- Create geometry
+    --
     NEW.geom := ST_SetSRID(
         ST_MakePoint(NEW.longitude, NEW.latitude),
         4326
+    );
+    --
+    -- Set location_id
+    --
+    -- Find location_id with group priority (1 first, then 2)
+    SELECT location_id INTO v_location_id
+    FROM locations
+    WHERE location_group = 1 
+        AND NEW.geom && geometry 
+        AND ST_Contains(geometry, NEW.geom)
+        AND is_deleted = FALSE
+    LIMIT 1;
+    -- If no group 1 location found, try group 2
+    IF v_location_id IS NULL THEN
+        SELECT location_id INTO v_location_id
+        FROM locations
+        WHERE location_group = 2 
+            AND NEW.geom && geometry 
+            AND ST_Contains(geometry, NEW.geom)
+            AND is_deleted = FALSE
+        LIMIT 1;
+    END IF;
+    NEW.location_id = v_location_id;
+    --
+    -- Set in_austin_full_purpose
+    --    
+    NEW.in_austin_full_purpose = EXISTS (
+        SELECT
+            1
+        FROM
+            geo.jurisdictions
+        WHERE
+            JURISDICTION_LABEL = 'AUSTIN FULL PURPOSE'
+            AND ST_Intersects (geometry, NEW.geom)
     );
     RETURN NEW;
 END;
