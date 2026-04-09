84791e85bcd8937150e65c4a3afc86c684b07f01
chiaberry <analuciaby@gmail.com>
2026-04-09 17:08:48 -0500

    there are buttons to go left and right

--- editor/components/PopupWrapper.tsx
+++ editor/components/PopupWrapper.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { Popup } from "react-map-gl";
+import { Button } from "react-bootstrap";
 import { TableMapPopupContentProps } from "./TableMapPopupContent";
 import { GeoJsonProperties } from "geojson";
+import { LuSquareArrowLeft, LuSquareArrowRight } from "react-icons/lu";
 
 interface PopupWrapperProps {
   latitude: number;
@@ -19,27 +21,25 @@ export default function PopupWrapper({
   PopupContent,
   onClose,
 }: PopupWrapperProps) {
-  const selectedFeaturesLength = selectedFeatures.length
-  console.log(selectedFeaturesLength)
+  const selectedFeaturesLength = selectedFeatures.length;
+  console.log(selectedFeaturesLength);
   const [activeFeature, setActiveFeature] = useState(0);
 
   const handleNext = () => {
-    if (activeFeature === selectedFeaturesLength -1) {
-      setActiveFeature(0)
+    if (activeFeature === selectedFeaturesLength - 1) {
+      setActiveFeature(0);
+    } else {
+      setActiveFeature(activeFeature + 1);
     }
-    else {
-      setActiveFeature(activeFeature+1)
-    }
-  }
+  };
 
   const handlePrevious = () => {
     if (activeFeature === 0) {
-      setActiveFeature(selectedFeaturesLength-1)
+      setActiveFeature(selectedFeaturesLength - 1);
+    } else {
+      setActiveFeature(activeFeature - 1);
     }
-    else { 
-      setActiveFeature(activeFeature -1)
-    }
-  }
+  };
 
   if (selectedFeaturesLength > 1) {
     return (
@@ -50,9 +50,18 @@ export default function PopupWrapper({
         closeOnClick={false}
         onClose={onClose}
       >
-        <PopupContent properties={selectedFeatures[activeFeature].properties} />
-        <hr></hr>
-        {activeFeature + 1}/{selectedFeaturesLength}
+        <PopupContent properties={selectedFeatures[activeFeature]?.properties} />
+        <div className="d-flex align-items-center">
+          <Button className="border-0" variant="link" onClick={handlePrevious}>
+            <LuSquareArrowLeft className="text-muted" />
+          </Button>{" "}
+          <span>
+            {activeFeature + 1}/{selectedFeaturesLength}
+          </span>
+          <Button className="border-0" variant="link" onClick={handleNext}>
+            <LuSquareArrowRight className="text-muted" />
+          </Button>{" "}
+        </div>
       </Popup>
     );
   }
