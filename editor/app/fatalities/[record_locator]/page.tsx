2c9507b235aa75b2726506da278c0eabcad18e49
chiaberry <analuciaby@gmail.com>
2026-04-14 10:46:42 -0500

    add fatality detail tracking view event

--- editor/app/fatalities/[record_locator]/page.tsx
+++ editor/app/fatalities/[record_locator]/page.tsx
@@ -19,6 +19,7 @@ import DataCard from "@/components/DataCard";
 import { crashesColumns } from "@/configs/crashesColumns";
 import CrashRecommendationCard from "@/components/CrashRecommendationCard";
 import NotesCard from "@/components/NotesCard";
+import UserEventsLogger from "@/components/UserEventsLogger";
 import { INSERT_CRASH_NOTE, UPDATE_CRASH_NOTE } from "@/queries/crashNotes";
 
 const otherCardColumns = [
@@ -74,126 +75,128 @@ export default function FatalCrashDetailsPage({
   const crash = data[0];
 
   return (
-    <>
-      <Row>
-        <div className="d-flex justify-content-between align-items-center mb-3">
-          <span className="fs-3 fw-bold text-uppercase">
-            {data[0].address_display}
-          </span>
-          <span className="text-nowrap bg-light-use-theme py-2 rounded-3 px-3 border">
-            <span className="fs-5 fw-bold me-1">Year</span>
-            <span className="fs-5 me-3">
-              {formatYear(data[0].crash_timestamp)}
+    <UserEventsLogger eventName="fatality_details_view">
+      <>
+        <Row>
+          <div className="d-flex justify-content-between align-items-center mb-3">
+            <span className="fs-3 fw-bold text-uppercase">
+              {data[0].address_display}
             </span>
-            <span className="fs-5 fw-bold me-1">Fatal Crash</span>
-            <span className="fs-5">
-              #{data[0].law_enforcement_ytd_fatality_num}
+            <span className="text-nowrap bg-light-use-theme py-2 rounded-3 px-3 border">
+              <span className="fs-5 fw-bold me-1">Year</span>
+              <span className="fs-5 me-3">
+                {formatYear(data[0].crash_timestamp)}
+              </span>
+              <span className="fs-5 fw-bold me-1">Fatal Crash</span>
+              <span className="fs-5">
+                #{data[0].law_enforcement_ytd_fatality_num}
+              </span>
             </span>
-          </span>
-        </div>
-      </Row>
-      <Row>
-        <Col className="mb-3" sm={12} md={6}>
-          <Card className="h-100">
-            <Card.Body>
-              <Table>
-                <tbody>
-                  <tr>
-                    <td className="fw-bold">Crash ID</td>
-                    <td>
-                      {crashesColumns.record_locator_hyperlinked.valueRenderer(
-                        crash
-                      )}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
-                      Date
-                    </td>
-                    <td>{formatIsoDateTimeWithDay(crash.crash_timestamp)}</td>
-                  </tr>
-                  <tr>
-                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
-                      Units involved
-                    </td>
-                    <td>
-                      {crash.unit_types_involved?.unit_types_involved || ""}
-                    </td>
-                  </tr>
-                  <tr>
-                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
-                      Collision type
-                    </td>
-                    <td>{crash.collsn?.label}</td>
-                  </tr>
-                  <tr>
-                    <td style={{ textWrap: "nowrap" }} className="fw-bold">
-                      Roadway owner
-                    </td>
-                    <td>
-                      {crash.is_coa_roadway === true && "City of Austin"}
-                      {crash.is_coa_roadway === false && "TxDOT"}
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
-              <Row className="p-1 crash-header-card-body">
-                <PointMap
-                  savedLatitude={crash.latitude}
-                  savedLongitude={crash.longitude}
-                  mapRef={mapRef}
-                  initialBasemapType="streets"
-                />
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-3" sm={12} md={6}>
-          <FatalityUnitsCards crash={crash} />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="mb-3" sm={12} md={6} lg={4}>
-          <CrashDiagramCard crash={crash} crashRefetch={refetch} />
-        </Col>
-        <Col className="mb-3" sm={12} md={6} lg={4}>
-          <CrashNarrativeEditableCard
-            crash={crash}
-            onSaveCallback={onSaveCallback}
-          />
-        </Col>
-        <Col className="mb-3">
-          <DataCard<Crash>
-            record={crash}
-            isValidating={false}
-            title="Details"
-            columns={otherCardColumns}
-            mutation={UPDATE_CRASH}
-            onSaveCallback={onSaveCallback}
-            shouldShowColumnVisibilityPicker={true}
-            localStorageKey="crashPageOther"
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col className="mb-3" sm={12} md={12} lg={6}>
-          <CrashRecommendationCard
-            crash_pk={crash.id}
-            onSaveCallback={onSaveCallback}
-            recommendation={crash.recommendation}
-          />
-        </Col>
-        <Col className="mb-3">
-          <NotesCard
-            notes={crash.crash_notes || []}
-            updateMutation={UPDATE_CRASH_NOTE}
-            insertMutation={INSERT_CRASH_NOTE}
-            onSaveCallback={onSaveCallback}
-            recordId={crash.id}
-            recordKey="crash_pk"
-          />
-        </Col>
-      </Row>
-    </>
+          </div>
+        </Row>
+        <Row>
+          <Col className="mb-3" sm={12} md={6}>
+            <Card className="h-100">
+              <Card.Body>
+                <Table>
+                  <tbody>
+                    <tr>
+                      <td className="fw-bold">Crash ID</td>
+                      <td>
+                        {crashesColumns.record_locator_hyperlinked.valueRenderer(
+                          crash
+                        )}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                        Date
+                      </td>
+                      <td>{formatIsoDateTimeWithDay(crash.crash_timestamp)}</td>
+                    </tr>
+                    <tr>
+                      <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                        Units involved
+                      </td>
+                      <td>
+                        {crash.unit_types_involved?.unit_types_involved || ""}
+                      </td>
+                    </tr>
+                    <tr>
+                      <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                        Collision type
+                      </td>
+                      <td>{crash.collsn?.label}</td>
+                    </tr>
+                    <tr>
+                      <td style={{ textWrap: "nowrap" }} className="fw-bold">
+                        Roadway owner
+                      </td>
+                      <td>
+                        {crash.is_coa_roadway === true && "City of Austin"}
+                        {crash.is_coa_roadway === false && "TxDOT"}
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <Row className="p-1 crash-header-card-body">
+                  <PointMap
+                    savedLatitude={crash.latitude}
+                    savedLongitude={crash.longitude}
+                    mapRef={mapRef}
+                    initialBasemapType="streets"
+                  />
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className="mb-3" sm={12} md={6}>
+            <FatalityUnitsCards crash={crash} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mb-3" sm={12} md={6} lg={4}>
+            <CrashDiagramCard crash={crash} crashRefetch={refetch} />
+          </Col>
+          <Col className="mb-3" sm={12} md={6} lg={4}>
+            <CrashNarrativeEditableCard
+              crash={crash}
+              onSaveCallback={onSaveCallback}
+            />
+          </Col>
+          <Col className="mb-3">
+            <DataCard<Crash>
+              record={crash}
+              isValidating={false}
+              title="Details"
+              columns={otherCardColumns}
+              mutation={UPDATE_CRASH}
+              onSaveCallback={onSaveCallback}
+              shouldShowColumnVisibilityPicker={true}
+              localStorageKey="crashPageOther"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mb-3" sm={12} md={12} lg={6}>
+            <CrashRecommendationCard
+              crash_pk={crash.id}
+              onSaveCallback={onSaveCallback}
+              recommendation={crash.recommendation}
+            />
+          </Col>
+          <Col className="mb-3">
+            <NotesCard
+              notes={crash.crash_notes || []}
+              updateMutation={UPDATE_CRASH_NOTE}
+              insertMutation={INSERT_CRASH_NOTE}
+              onSaveCallback={onSaveCallback}
+              recordId={crash.id}
+              recordKey="crash_pk"
+            />
+          </Col>
+        </Row>
+      </>
+    </UserEventsLogger>
   );
 }
