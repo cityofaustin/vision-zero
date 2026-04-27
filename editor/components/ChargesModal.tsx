b3ab6a7f9d930112cb47e68d5f2a1714ecc11cca
rose <rose.eichelmann@gmail.com>
2026-04-27 13:32:43 -0500

    adds add charge functionality

--- editor/components/ChargesModal.tsx
+++ editor/components/ChargesModal.tsx
@@ -2,8 +2,13 @@ import { Modal, Button, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { Unit } from "@/types/unit";
 import { Charge } from "@/types/charge";
+import { INSERT_CHARGE } from "@/queries/charges";
+import { useMutation } from "@/utils/graphql";
+import { SubmitHandler } from "react-hook-form";
+import { useAuth0 } from "@auth0/auth0-react";
 
 interface ChargesModalProps {
+  crashPk: number;
   show: boolean;
   unitCharges: Charge[] | undefined;
   setShowChargesModal: React.Dispatch<React.SetStateAction<boolean>>;
@@ -12,78 +17,96 @@ interface ChargesModalProps {
 }
 
 type ChargesFormInputs = {
-  id: number;
-  created_by: string;
-  updated_by: string;
   charge: string;
-  crash_pk: number;
-  unit_nbr: number;
-  prsn_nbr: number;
 };
 
 export default function ChargesModal({
+  crashPk,
   unit,
   unitCharges,
   show,
   setShowChargesModal,
   onSaveCallback,
 }: ChargesModalProps) {
-  console.log(unitCharges);
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  //   reset,
-  // } = useForm<ChargesFormInputs>({
-  //   defaultValues: { charge: unitCharges[0].charge || "" },
-  // });
+  const { user } = useAuth0();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<ChargesFormInputs>({
+    // defaultValues: { charge: unitCharges[0].charge || "" },
+  });
+
+  const { mutate, loading: isSubmitting } = useMutation(INSERT_CHARGE);
+
+  const onSubmit: SubmitHandler<ChargesFormInputs> = async (data) => {
+    const updates = {
+      charge: data.charge,
+      unit_nbr: unit.unit_nbr,
+      prsn_nbr: 1,
+      crash_pk: crashPk,
+      is_temp: true,
+      cris_schema_version: "2026",
+      created_by: user?.email,
+    };
+
+    const responseData = await mutate({ updates });
+    if (!!responseData) {
+      onSaveCallback();
+    }
+    reset();
+    onClose();
+  };
 
   const onClose = () => setShowChargesModal(false);
+
   return (
     <Modal
       show={show}
       onHide={onClose}
       onExited={() => {
-        // reset();
+        reset();
       }}
     >
-      <Modal.Header>
-        <Modal.Title>Edit charges</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form.Group>
-          <Form.Control
-            as="textarea"
-            aria-label="Charges text"
-            rows={6}
-            placeholder="Enter charges..."
-            // isInvalid={Boolean(errors.text)}
-            // ref={(e) => {
-            //   registerRef(e);
-            //   textareaRef.current = e;
-            // }}
-            // autoFocus
-          />
-        </Form.Group>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button
-          // disabled={!isDirty || isSubmitting}
-          form="contribFactorsForm"
-          type="submit"
-        >
-          Save
-        </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            onClose();
-          }}
-          // disabled={isSubmitting}
-        >
-          Cancel
-        </Button>
-      </Modal.Footer>
+      <Form onSubmit={handleSubmit(onSubmit)} id="chargesForm">
+        <Modal.Header>
+          <Modal.Title>Edit charges</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Control
+              as="textarea"
+              aria-label="Charges text"
+              rows={6}
+              placeholder="Enter charges..."
+              {...register("charge", {
+                // coerce empty fields to null
+                setValueAs: (v) => v?.trim() || null,
+              })}
+            />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            // disabled={!isDirty || isSubmitting}
+            form="chargesForm"
+            type="submit"
+            disabled={isSubmitting}
+          >
+            Save
+          </Button>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              onClose();
+            }}
+            disabled={isSubmitting}
+          >
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Form>
     </Modal>
   );
 }
