b92a79bd612117292767299f8dce40a4cb95311f
John Clary <john.clary@austintexas.gov>
2026-05-13 14:11:13 -0400

    add incident links table and metadata

---
+++ database/migrations/default/1778695662683_cad_incident_links/up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cad_incident_links (
+    id                  serial PRIMARY KEY,
+    incident_id_a       integer NOT NULL REFERENCES cad_incidents (master_incident_id),
+    incident_id_b       integer NOT NULL REFERENCES cad_incidents (master_incident_id),
+    distance_m          float NOT NULL,
+    delta_minutes       float NOT NULL,
+    created_at          timestamp with time zone DEFAULT now() NOT NULL,
+    CONSTRAINT no_duplicate_pairs CHECK (incident_id_a < incident_id_b),
+    CONSTRAINT unique_pair UNIQUE (incident_id_a, incident_id_b)
+);
+
+-- CREATE INDEX idx_cad_incident_links_super_incident_id
+--     ON cad_incident_links (super_incident_id);
+CREATE INDEX idx_cad_incident_links_a ON cad_incident_links (incident_id_a);
+CREATE INDEX idx_cad_incident_links_b ON cad_incident_links (incident_id_b);
