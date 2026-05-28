8a48bed7b0e3a63516d142dbcb0878b3c1c7a038
rose <rose.eichelmann@gmail.com>
2026-05-28 15:55:46 -0500

    add more options to config file to allow SVG and event emitter

--- viewer/vite.config.js
+++ viewer/vite.config.js
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      // svgr options: https://react-svgr.com/docs/options/
+      svgr({ svgrOptions: { icon: true } }),
+    ],
+    define: {
+      // This ensures the global variable is defined for libraries that expect it
+      global: "globalThis",
+    },
+    resolve: {
+      alias: {
+        // This provides the EventEmitter polyfill for mapbox-gl-geocoder and similar libraries
+        events: "events",
+      },
+    },
   };
 });
