0f9e3b7f0bf5f4d599f1cff1ad6de07632f12e58
John Clary <john.clary@austintexas.gov>
2026-05-28 14:24:13 -0400

    add agency to popup

--- editor/components/CadMapPopupContent.tsx
+++ editor/components/CadMapPopupContent.tsx
@@ -16,15 +16,17 @@ export default function CadMapPopupContent({
       </div>
       <div className="d-flex justify-content-between">
         <span className="fw-bold">VZ Incident ID</span>
-        <span className="text-muted">
-          {properties?.vz_incident_id}
-        </span>
+        <span className="text-muted">{properties?.vz_incident_id}</span>
       </div>
       <div className="d-flex justify-content-between">
         <span className="fw-bold">Date</span>
         <span className="text-muted">
           {formatIsoDateTime(properties?.response_date)}
         </span>
+      </div>
+      <div className="d-flex justify-content-between">
+        <span className="fw-bold">Agency</span>
+        <span className="text-muted">{properties?.agency_type}</span>
       </div>
     </div>
   );
