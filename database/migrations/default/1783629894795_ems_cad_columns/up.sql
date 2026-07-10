5eeb3a4a8244bd45dea46a86be05bba5f8774c01
John Clary <john.clary@austintexas.gov>
2026-07-10 11:36:14 -0400

    add column comments

--- database/migrations/default/1783629894795_ems_cad_columns/up.sql
+++ database/migrations/default/1783629894795_ems_cad_columns/up.sql
@@ -2,3 +2,13 @@ ALTER TABLE ems__incidents
     ADD COLUMN incident_id bigint,
     ADD COLUMN incident_problem_initial text,
     ADD COLUMN incident_dispositions text;
+
+COMMENT ON COLUMN public.ems__incidents.incident_id is 
+    'The CAD incident ID. Equivalent to cad_incidents.master_incident_id.';
+
+COMMENT ON COLUMN public.ems__incidents.incident_problem_initial is
+    'The initial problem of the corresponding CAD incident. ';
+
+COMMENT ON COLUMN public.ems__incidents.incident_dispositions is
+    'The slash (/)-separated dispositions listed in the corresponding'
+    'CAD incident. Equivalent to cad_incidents.call_disposition';
