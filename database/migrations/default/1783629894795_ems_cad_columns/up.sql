8b616c5380479e7a100f6e34be3ce01e8fa526ca
John Clary <john.clary@austintexas.gov>
2026-07-10 11:36:54 -0400

    lint

--- database/migrations/default/1783629894795_ems_cad_columns/up.sql
+++ database/migrations/default/1783629894795_ems_cad_columns/up.sql
@@ -7,7 +7,7 @@ COMMENT ON COLUMN public.ems__incidents.incident_id is
     'The CAD incident ID. Equivalent to cad_incidents.master_incident_id.';
 
 COMMENT ON COLUMN public.ems__incidents.incident_problem_initial is
-    'The initial problem of the corresponding CAD incident. ';
+    'The initial problem of the corresponding CAD incident.';
 
 COMMENT ON COLUMN public.ems__incidents.incident_dispositions is
     'The slash (/)-separated dispositions listed in the corresponding'
