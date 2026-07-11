c358d8de788eb3c4e333a53fed6ef81c928726fa
John Clary <john.clary@austintexas.gov>
2026-07-11 13:12:52 -0400

    update map popup

--- editor/components/VzMapPopupContent.tsx
+++ editor/components/VzMapPopupContent.tsx
@@ -1,9 +1,5 @@
 import Link from "next/link";
-import {
-  formatArrayToString,
-  formatIsoDate,
-  formatIsoDateTime,
-} from "@/utils/formatters";
+import { formatArrayToString, formatIsoDateTime } from "@/utils/formatters";
 import { GeoJsonProperties } from "geojson";
 
 export interface VzMapPopupContentProps {
@@ -16,22 +12,26 @@ export default function VzMapPopupContent({
   return (
     <div className="h-100 m-1 px-1" style={{ minWidth: "135px" }}>
       <div className="fw-bold fs-6 pb-2 border-bottom">
-        {properties?.address_earliest}
+        {properties?.address}
       </div>
       <div className="d-flex justify-content-between">
-        <span className="fw-bold">Incident ID</span>
-        <span className="text-muted">{properties?.id}</span>
-      </div>
-      <div className="d-flex justify-content-between">
-        <span className="fw-bold">Date</span>
+        <span className="fw-bold me-2">Incident ID</span>
         <span className="text-muted">
-          {formatIsoDateTime(properties?.response_date_earliest)}
+          <Link href={`/incidents/${properties?.id}`} prefetch={false}>
+            {properties?.id}
+          </Link>
         </span>
       </div>
       <div className="d-flex justify-content-between">
-        <span className="fw-bold">Agencies</span>
+        <span className="fw-bold me-2">Date</span>
+        <span className="text-muted">
+          {formatIsoDateTime(properties?.record_timestamp)}
+        </span>
+      </div>
+      <div className="d-flex justify-content-between">
+        <span className="fw-bold me-2">Agencies</span>
         <span className="text-muted text-uppercase">
-          {formatArrayToString(JSON.parse(properties?.agencies))}
+          {formatArrayToString(JSON.parse(properties?.responding_agencies))}
         </span>
       </div>
     </div>
