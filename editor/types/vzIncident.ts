ac2dfd88832515c40804dbee32d4745d824912d9
John Clary <john.clary@austintexas.gov>
2026-07-11 13:41:13 -0400

    add latitude and longitude to type

--- editor/types/vzIncident.ts
+++ editor/types/vzIncident.ts
@@ -5,12 +5,14 @@ export type VzIncident = {
   id?: number | null;
   in_austin_full_purpose?: boolean | null;
   incident_numbers?: string[] | null;
+  latitude?: number | null;
   location_ids?: string[] | null;
+  longitude?: number | null;
   point_feature?: Point | null;
   record_count?: number | null;
-  record_tables?: string[];
   record_tables_str?: string;
+  record_tables?: string[];
   record_timestamp?: string;
-  responding_agencies?: string[];  // todo: ensure no nulls in array
   responding_agencies_str?: string | null; // todo: ensure no nulls in string?
+  responding_agencies?: string[];  // todo: ensure no nulls in array
 };
