a4af5521f8a0ef960e6cae04d84f985f88540fc3
John Clary <john.clary@austintexas.gov>
2026-07-14 13:43:05 -0400

    add beta flag to incident list page

--- editor/app/incidents/page.tsx
+++ editor/app/incidents/page.tsx
@@ -4,7 +4,9 @@ import { vzListViewColumns } from "@/configs/vzListViewColumns";
 import { vzListViewQueryConfig } from "@/configs/vzListViewTable";
 import TableWrapper from "@/components/TableWrapper";
 import { useDocumentTitle } from "@/utils/documentTitle";
-
+import AlignedLabel from "@/components/AlignedLabel";
+import { LuInfo } from "react-icons/lu";
+import { Badge } from "react-bootstrap";
 
 const localStorageKey = "vzListViewQueryConfig";
 
@@ -14,8 +16,20 @@ export default function Incidents() {
   return (
     <UserEventsLogger eventName="incidents_list_view">
       <div className="h-100 d-flex flex-column">
-        <div className="d-flex justify-content-between">
-          <span className="fs-3 fw-bold">Incidents</span>
+        <div className="d-flex">
+          <div className="fs-3 fw-bold me-2">Incidents</div>
+          <div className="align-self-center">
+            <Badge bg="info">Beta</Badge>
+          </div>
+        </div>
+        <div className="fw-light text-secondary mb-2">
+          <AlignedLabel>
+            <LuInfo className="me-2" />
+            <span>
+              Incidents are currently in beta. Data may be inaccurate or change
+              significantly as we continue to refine the system.
+            </span>
+          </AlignedLabel>
         </div>
         <TableWrapper
           columns={vzListViewColumns}
