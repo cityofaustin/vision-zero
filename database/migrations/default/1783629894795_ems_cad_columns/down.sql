374cd00dcebd0ec3ca9ed16aeceeb54fdc92260c
John Clary <john.clary@austintexas.gov>
2026-07-15 10:22:42 -0400

    update injury trigger for updates

--- database/migrations/default/1783629894795_ems_cad_columns/down.sql
+++ database/migrations/default/1783629894795_ems_cad_columns/down.sql
@@ -2,3 +2,10 @@ ALTER TABLE ems__incidents
     DROP COLUMN incident_id,
     DROP COLUMN incident_problem_initial,
     DROP COLUMN incident_dispositions;
+
+ems_incidents_trigger_insert_update_set_patient_injry_sev
+DROP TRIGGER IF EXISTS ems_incidents_trigger_insert_update_set_patient_injry_sev on ems__incidents;
+
+CREATE TRIGGER ems_incidents_trigger_insert_set_patient_injry_sev
+    BEFORE INSERT ON ems__incidents
+    FOR EACH ROW EXECUTE FUNCTION update_ems_patient_injry_sev();
--- database/migrations/default/1783629894795_ems_cad_columns/up.sql
+++ database/migrations/default/1783629894795_ems_cad_columns/up.sql
@@ -12,3 +12,9 @@ COMMENT ON COLUMN public.ems__incidents.incident_problem_initial is
 COMMENT ON COLUMN public.ems__incidents.incident_dispositions is
     'The slash (/)-separated dispositions listed in the corresponding'
     'CAD incident. Equivalent to cad_incidents.call_disposition';
+
+DROP TRIGGER IF EXISTS ems_incidents_trigger_insert_set_patient_injry_sev on ems__incidents;
+
+CREATE TRIGGER ems_incidents_trigger_insert_update_set_patient_injry_sev
+    BEFORE INSERT OR UPDATE ON ems__incidents
+    FOR EACH ROW EXECUTE FUNCTION update_ems_patient_injry_sev();
