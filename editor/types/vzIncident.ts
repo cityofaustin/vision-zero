a09f0259b4b13dd4b720e2d66d43552f4c07404a
John Clary <john.clary@austintexas.gov>
2026-07-21 14:39:36 -0400

    remove todos

--- editor/types/vzIncident.ts
+++ editor/types/vzIncident.ts
@@ -13,6 +13,6 @@ export type VzIncident = {
   record_tables_str?: string;
   record_tables?: string[];
   record_timestamp?: string;
-  responding_agencies_str?: string | null; // todo: ensure no nulls in string?
-  responding_agencies?: string[];  // todo: ensure no nulls in array
+  responding_agencies_str?: string | null;
+  responding_agencies?: string[] | null;
 };
