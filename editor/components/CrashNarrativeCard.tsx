06385fc0015cc1a1f9abd89ed0279c41af1bb1f7
John Clary <john.clary@austintexas.gov>
2026-08-19 12:43:19 -0400

    adds permission check to cr download

--- editor/components/CrashNarrativeCard.tsx
+++ editor/components/CrashNarrativeCard.tsx
@@ -3,7 +3,11 @@ import Button from "react-bootstrap/Button";
 import { FaFilePdf } from "react-icons/fa6";
 import AlignedLabel from "@/components/AlignedLabel";
 import { Crash } from "@/types/crashes";
-import { useGetToken } from "@/utils/auth";
+import { hasRole, useGetToken } from "@/utils/auth";
+import { useAuth0 } from "@auth0/auth0-react";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+const allowedCrashReportDownloadRoles = ["editor", "admin"];
 
 // Downloads pdf from the CR3 API and opens it in a new tab
 export const onDownloadCR3 = async ({
@@ -44,22 +48,39 @@ export const onDownloadCR3 = async ({
 export default function CrashNarrativeCard({ crash }: { crash: Crash }) {
   const getToken = useGetToken();
 
+  const { user } = useAuth0();
+  const canDownloadCr3 = user
+    ? hasRole(allowedCrashReportDownloadRoles, user)
+    : false;
+
   const isCr3Stored = crash.cr3_stored_fl;
 
   return (
     <Card className="h-100">
       <Card.Header className="d-flex justify-content-between">
         <Card.Title>Narrative</Card.Title>
-        <Button
-          size="sm"
-          onClick={() => onDownloadCR3({ crash, getToken })}
-          disabled={!isCr3Stored}
+        <OverlayTrigger
+          placement="top"
+          overlay={
+            <Tooltip id="copy-address-tooltip" hidden={canDownloadCr3}>
+              Vision Zero team members can access crash reports - contact them for assistance
+            </Tooltip>
+          }
         >
-          <AlignedLabel>
-            <FaFilePdf className="me-2" />
-            <span>Open crash report</span>
-          </AlignedLabel>
-        </Button>
+            {/* load-bearing span that enables tooltips to render over the `disabled` button */}
+          <span>
+            <Button
+              size="sm"
+              onClick={() => onDownloadCR3({ crash, getToken })}
+              disabled={!isCr3Stored || !canDownloadCr3}
+            >
+              <AlignedLabel>
+                <FaFilePdf className="me-2" />
+                <span>Open crash report</span>
+              </AlignedLabel>
+            </Button>
+          </span>
+        </OverlayTrigger>
       </Card.Header>
       <Card.Body>
         <Card.Text>{crash.investigator_narrative || ""}</Card.Text>
