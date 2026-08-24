e2aedf662751df7f59e5ba43010cb3c94c0715b6
chiaberry <analuciaby@gmail.com>
2026-08-24 11:17:04 -0500

    explicity declare map style, they removed the default and replaced with an empty string

--- viewer/src/views/map/MapComponent.jsx
+++ viewer/src/views/map/MapComponent.jsx
@@ -487,6 +487,7 @@ const MapComponent = () => {
       {...viewState}
       onMove={onMove}
       mapboxAccessToken={import.meta.env.VITE_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/mapbox/light-v9"
       cursor="default"
       interactiveLayerIds={interactiveLayerIds}
       onClick={onClick}
