427f4045a5012f51a9d69384a177c7d1d4a7e010
rose <rose.eichelmann@gmail.com>
2026-08-24 11:43:32 -0500

    database migrations

---
+++ database/migrations/default/1787588257933_create_table_lookups_geolocation_provider/down.sql
@@ -0,0 +1,3 @@
+DROP TABLE "lookups"."geolocation_provider";
+
+alter table "public"."crashes" drop column "geolocation_provider_id";
---
+++ database/migrations/default/1787588257933_create_table_lookups_geolocation_provider/up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "lookups"."geolocation_provider" ("id" serial NOT NULL, "label" text NOT NULL, "source" text NOT NULL DEFAULT 'vz', PRIMARY KEY ("id") , UNIQUE ("id"));
+
+INSERT INTO "lookups"."geolocation_provider"("id", "label", "source") VALUES (1, E'cris', E'vz');
+
+INSERT INTO "lookups"."geolocation_provider"("id", "label", "source") VALUES (2, E'apd_cad', E'vz');
+
+INSERT INTO "lookups"."geolocation_provider"("id", "label", "source") VALUES (3, E'manual_qa', E'vz');
+
+alter table "public"."crashes" add column "geolocation_provider_id" integer
+ not null default '1';
