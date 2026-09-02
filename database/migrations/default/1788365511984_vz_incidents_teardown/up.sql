148099babbe72be3857801583440691114d1f56b
John Clary <john.clary@austintexas.gov>
2026-09-02 16:32:19 -0400

    add other cols to migra

--- database/migrations/default/1788365511984_vz_incidents_teardown/up.sql
+++ database/migrations/default/1788365511984_vz_incidents_teardown/up.sql
@@ -1,6 +1,23 @@
 drop view if exists vz_incident_records_view;
-alter table crashes drop column vz_incident_id;
-alter table cad_incidents drop column vz_incident_id;
-alter table ems__incidents drop column vz_incident_id;
-alter table afd__incidents drop column vz_incident_id;
+
+alter table crashes
+drop columnifvz_incident_id,
+drop column vz_incident_match_status,
+drop column vz_incident_matched_ids;
+
+alter table cad_incidents
+drop column vz_incident_id,
+drop column vz_incident_match_status,
+drop column vz_incident_matched_ids;
+
+alter table ems__incidents
+drop column vz_incident_id,
+drop column vz_incident_match_status,
+drop column vz_incident_matched_ids;
+
+alter table afd__incidents
+drop column vz_incident_id,
+drop column vz_incident_match_status,
+drop column vz_incident_matched_ids;
+
 drop table if exists vz_incidents;
