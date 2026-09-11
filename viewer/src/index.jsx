6c8c0625438f3cc0ea8185bfa1aff936f9c989f5
John Clary <john.clary@austintexas.gov>
2026-09-11 13:23:30 -0400

    cleanup dupe css declartions and make import our custom styles after bootsrap

--- viewer/src/index.jsx
+++ viewer/src/index.jsx
@@ -5,11 +5,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { basepath } from "./routes/routes";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import StoreProvider from "./utils/store";
 import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 // IE11 SVG Polyfill
 SVGElement.prototype.contains = function contains(node) {
