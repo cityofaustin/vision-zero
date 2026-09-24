9682571687e1d2ce6f4c8a48195006d246b8e214
John Clary <john.clary@austintexas.gov>
2026-09-24 11:05:46 -0400

    add a class

--- viewer/src/views/nav/SideDrawerContent.jsx
+++ viewer/src/views/nav/SideDrawerContent.jsx
@@ -46,7 +46,7 @@ const SideDrawerContent = ({ type }) => {
           key="vz-logo-static"
         />
       </StyledDrawerHeader>
-      <Container className="pt-3 pb-3 shadow">
+      <Container className="pt-3 pb-3 drawer-content shadow">
         <SideDrawerMobileNav />
         {currentPath === "/map" && <SideMapControl type={type} />}
       </Container>
